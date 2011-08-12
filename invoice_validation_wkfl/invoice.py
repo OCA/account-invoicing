7eb03105b8356c245959ceba48ee25b2de3f536f
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2011-08-12 14:35:23 +0200

    [ADD] First commit of the first financial modules to move in our new public branch (lp:c2c-addons/6.1  rev 1)

---
+++ invoice_validation_wkfl/invoice.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010 Camtocamp SA
+# @author Joël Grand-Guillaume
+# $Id: $
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import fields
+from osv import osv
+from tools.translate import _
+
+class account_invoice(osv.osv):
+    _inherit = "account.invoice"
+        
+    _columns = {
+        'state': fields.selection([
+                ('draft','Draft'),
+                ('to_send','To Send'),
+                ('to_valid','To Validate'),
+                ('proforma2','Pro-forma'),
+                ('open','Open'),
+                ('paid','Paid'),
+                ('cancel','Canceled')],'State', select=True, readonly=True),
+    }
+
+account_invoice()
