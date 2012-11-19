8a2a81e15ef6c3fa986e79c24d1827237e7d207c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2012-11-19 18:08:41 +0100

    [NEW] account_invoice_total: bring back account verification total on supplier invoices for OpenERP 6.1

---
+++ account_invoice_total/README.md
@@ -0,0 +1,21 @@
+# Account_invoice_total for OpenERP 6.1
+
+Until OpenERP 6.0.3 when creating a supplier invoice one first had to enter the grand total, this had to match with the computed total before the invoice could be verified.
+
+In OpenERP 6.1 this feature has silently been removed. This module brings back the field and the extra verification check.
+
+## Links
+
+Discussion : https://bugs.launchpad.net/openobject-addons/+bug/998008
+Code Revision : http://bazaar.launchpad.net/~openerp/openobject-addons/trunk/revision/6254
+
+## License
+
+Copyright (c) 2012 Ren Dao Solutions bvba
+Copyright (c) 2012 ACSONE SA/NV
+
+This module is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+For a full text of the license see <http://www.gnu.org/licenses/agpl-3.0.html>
---
+++ account_invoice_total/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    account_invoice_total module for OpenERP
+#    Copyright (C) 2012 Ren Dao Solutions (<http://rendaosolutions.com>).
+#    Copyright (C) 2012 Acsone (<http://acsone.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{   'name': 'Total Field on Supplier Invoice',
+    'version': '0.1',
+    'category': 'Accounting',
+    'description': """
+Since OpenERP 6.1 the check_total field in the supplier invoice form, and the corresponding
+check, has been removed. This module brings back the functionality from 6.0 and before.
+
+See discussion : https://bugs.launchpad.net/openobject-addons/+bug/998008
+See revision : http://bazaar.launchpad.net/~openerp/openobject-addons/trunk/revision/6254
+    """,
+    'author': 'Ren Dao Solutions bvba',
+    'depends': [
+        'account',
+    ],
+    'init_xml': [
+        'account_invoice_total_view.xml'
+    ],
+    'update_xml': [],
+    'demo_xml': [],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
