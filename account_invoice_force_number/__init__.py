cc6bcd5c2c3edf80a209f97d291a44176772c7d7
Alex Comba <alex.comba@agilebg.com>
2013-08-27 16:31:14 +0200

    [IMP] style and get installable

--- account_invoice_force_number/__init__.py
+++ account_invoice_force_number/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #    
-#    Copyright (C) 2011 Agile Business Group sagl (<http://www.agilebg.com>)
 #    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    All Rights Reserved
+#    Copyright (C) 2011-2013 Agile Business Group sagl (<http://www.agilebg.com>)
+#    @author Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+#    Ported to OpenERP 7.0 by Alex Comba <alex.comba@agilebg.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
