a37a737ba48a5f59fd4c2a2a7edcb90334c6cbf0
Andrea Cometa <info@andreacometa.it>
2013-09-11 16:16:15 +0200

    [fix] pep8 style

--- account_invoice_shipping_address/invoice.py
+++ account_invoice_shipping_address/invoice.py
@@ -27,9 +27,13 @@ class account_invoice(orm.Model):
     _inherit = "account.invoice"
 
     _columns = {
-        'address_shipping_id':
-            fields.many2one('res.partner', 'Shipping Address', readonly=True,
-                            states={'draft': [('readonly', False)],
-                                    'sent': [('readonly', False)]},
-                            help="Delivery address for current invoice."),
+        'address_shipping_id': fields.many2one(
+            'res.partner',
+            'Shipping Address',
+            readonly=True,
+            states={
+                'draft': [('readonly', False)],
+                'sent': [('readonly', False)]
+            },
+            help="Delivery address for current invoice."),
     }
