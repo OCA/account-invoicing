2fc8367469e45ce9cfe1ad0476ec804ae523440c
Jacques-Etienne Baudoux <je@bcim.be>
2013-09-17 16:16:45 +0200

    [MIG] ported module account_invoice_line_default_account to 7.0

--- account_invoice_line_default_account/__init__.py
+++ account_invoice_line_default_account/__init__.py
@@ -6,5 +6,7 @@ Created on 30 jan. 2013
 
 rportier@therp.nl
 http://www.therp.nl
+
+Ported to 7.0 on Sept. 17, 2013 by Jacques-Etienne Baudoux, BCIM (www.bcim.be)
 '''
-import model
+from . import model
