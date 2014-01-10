0b318f9ae057a4049ce1b4941f4ffd00c1166755
Nicola Malcontenti <nicola.malcontenti@agilebg.com>
2014-01-10 12:42:38 +0100

    [FIX] Added some Flake8 fix

--- product_customer_code_invoice/__init__.py
+++ product_customer_code_invoice/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import account_invoice
+from . import account_invoice
