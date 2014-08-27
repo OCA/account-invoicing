e45ab27892af1f7558c37ac773ce3cc71b1a67e9
Marc Cassuto <marc.cassuto@savoirfairelinux.com>
2014-08-27 19:48:54 -0400

    Minor changes to trigger a build

--- account_invoice_sale_origin/__init__.py
+++ account_invoice_sale_origin/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+###############################################################################
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2010-2013 Savoir-faire Linux. All Rights Reserved.
@@ -17,6 +17,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+###############################################################################
 
-from .import sale
+from . import sale
