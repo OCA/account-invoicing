b89c58554eea76623da2889cd950fa5ac1857828
Cédric Pigeon <cedric.pigeon@acsone.eu>
2015-01-26 10:12:47 +0100

    Port to Odoo 8.0 and new api

--- account_invoice_merge/invoice.py
+++ account_invoice_merge/invoice.py
@@ -18,16 +18,16 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.osv import orm
+from openerp import models, api
 from openerp import netsvc
 from openerp.osv.orm import browse_record, browse_null
 
 
-class account_invoice(orm.Model):
+class account_invoice(models.Model):
     _inherit = "account.invoice"
 
-    def _get_first_invoice_fields(self, cr, uid, invoice):
+    @api.model
+    def _get_first_invoice_fields(self, invoice):
         return {
             'origin': '%s' % (invoice.origin or '',),
             'partner_id': invoice.partner_id.id,
@@ -46,18 +46,21 @@ class account_invoice(orm.Model):
             'invoice_line': {},
         }
 
-    def _get_invoice_key_cols(self, cr, uid, invoice):
+    @api.model
+    def _get_invoice_key_cols(self, invoice):
         return ('partner_id', 'user_id', 'type',
                 'account_id', 'currency_id',
                 'journal_id', 'company_id')
 
-    def _get_invoice_line_key_cols(self, cr, uid, invoice_line):
+    @api.model
+    def _get_invoice_line_key_cols(self, invoice_line):
         return ('name', 'origin', 'discount',
                 'invoice_line_tax_id', 'price_unit',
                 'product_id', 'account_id',
                 'account_analytic_id')
 
-    def do_merge(self, cr, uid, ids, context=None):
+    @api.multi
+    def do_merge(self):
         """
         To merge similar type of account invoices.
         Invoices will only be merged if:
@@ -86,11 +89,13 @@ class account_invoice(orm.Model):
                 if field in ('product_id', 'account_id'):
                     if not field_val:
                         field_val = False
-                if isinstance(field_val, browse_record):
+                if (isinstance(field_val, browse_record) and
+                      field != 'invoice_line_tax_id'):
                     field_val = field_val.id
                 elif isinstance(field_val, browse_null):
                     field_val = False
-                elif isinstance(field_val, list):
+                elif (isinstance(field_val, list) or
+                      field == 'invoice_line_tax_id'):
                     field_val = ((6, 0, tuple([v.id for v in field_val])),)
                 list_key.append((field, field_val))
             list_key.sort()
@@ -100,16 +105,14 @@ class account_invoice(orm.Model):
 
         new_invoices = {}
         draft_invoices = [invoice
-                          for invoice in self.browse(cr, uid, ids,
-                                                     context=context)
+                          for invoice in self
                           if invoice.state == 'draft']
         seen_origins = {}
         seen_client_refs = {}
 
         for account_invoice in draft_invoices:
             invoice_key = make_key(
-                account_invoice, self._get_invoice_key_cols(cr, uid,
-                                                            account_invoice))
+                account_invoice, self._get_invoice_key_cols(account_invoice))
             new_invoice = new_invoices.setdefault(invoice_key, ({}, []))
             origins = seen_origins.setdefault(invoice_key, set())
             client_refs = seen_client_refs.setdefault(invoice_key, set())
@@ -117,7 +120,7 @@ class account_invoice(orm.Model):
             invoice_infos = new_invoice[0]
             if not invoice_infos:
                 invoice_infos.update(
-                    self._get_first_invoice_fields(cr, uid, account_invoice))
+                    self._get_first_invoice_fields(account_invoice))
                 origins.add(account_invoice.origin)
                 client_refs.add(account_invoice.reference)
             else:
@@ -140,7 +143,7 @@ class account_invoice(orm.Model):
             for invoice_line in account_invoice.invoice_line:
                 line_key = make_key(
                     invoice_line, self._get_invoice_line_key_cols(
-                        cr, uid, invoice_line))
+                        invoice_line))
                 o_line = invoice_infos['invoice_line'].setdefault(line_key, {})
                 if o_line:
                     # merge the line with an existing line
@@ -170,61 +173,51 @@ class account_invoice(orm.Model):
                 (0, 0, value) for value in
                 invoice_data['invoice_line'].itervalues()]
             # create the new invoice
-            newinvoice_id = self.create(cr, uid, invoice_data)
-            invoices_info.update({newinvoice_id: old_ids})
-            allinvoices.append(newinvoice_id)
+            newinvoice = self.create(invoice_data)
+            invoices_info.update({newinvoice.id: old_ids})
+            allinvoices.append(newinvoice.id)
             # make triggers pointing to the old invoices point to the new
             # invoice
             for old_id in old_ids:
                 wf_service.trg_redirect(
-                    uid, 'account.invoice', old_id, newinvoice_id, cr)
+                    self.env.uid, 'account.invoice', old_id, newinvoice.id,
+                    self.env.cr)
                 wf_service.trg_validate(
-                    uid, 'account.invoice', old_id, 'invoice_cancel', cr)
+                    self.env.uid, 'account.invoice', old_id, 'invoice_cancel',
+                    self.env.cr)
         # make link between original sale order or purchase order
-        so_obj = self.pool.get('sale.order')  # None if sale is not installed
-        order_line_obj = self.pool.get('sale.order.line')
-        invoice_line_obj = self.pool.get('account.invoice.line')
+        # None if sale is not installed
+        so_obj = self.env['sale.order']\
+            if 'sale.order' in self.env.registry else False
+        invoice_line_obj = self.env['account.invoice.line']
         # None if purchase is not installed
-        po_obj = self.pool.get('purchase.order')
+        po_obj = self.env['purchase.order']\
+            if 'purchase.order' in self.env.registry else False
         for new_invoice_id in invoices_info:
-            if so_obj is not None:
+            if so_obj:
                 todo_ids = so_obj.search(
-                    cr, uid,
-                    [('invoice_ids', 'in', invoices_info[new_invoice_id])],
-                    context=context)
-                for org_so in so_obj.browse(cr, uid, todo_ids, context=context):
-                    so_obj.write(
-                        cr, uid, [org_so.id],
-                        {'invoice_ids': [(4, new_invoice_id)]}, context)
+                    [('invoice_ids', 'in', invoices_info[new_invoice_id])])
+                for org_so in so_obj.browse(todo_ids):
+                    org_so.write({'invoice_ids': [(4, new_invoice_id)]})
                     for so_line in org_so.order_line:
                         invoice_line_ids = invoice_line_obj.search(
-                            cr, uid,
                             [('product_id', '=', so_line.product_id.id),
                              ('invoice_id', '=', new_invoice_id)])
                         if invoice_line_ids:
-                            order_line_obj.write(
-                                cr, uid, [so_line.id],
-                                {'invoice_lines': [(6, 0, invoice_line_ids)]},
-                                context=context)
-            if po_obj is not None:
+                            so_line.write(
+                                {'invoice_lines': [(6, 0, invoice_line_ids)]})
+            if po_obj:
                 todo_ids = po_obj.search(
-                    cr, uid,
-                    [('invoice_ids', 'in', invoices_info[new_invoice_id])],
-                    context=context)
-                for org_po in po_obj.browse(cr, uid, todo_ids, context=context):
-                    po_obj.write(
-                        cr, uid, [org_po.id],
-                        {'invoice_ids': [(4, new_invoice_id)]}, context)
+                    [('invoice_ids', 'in', invoices_info[new_invoice_id])])
+                for org_po in po_obj.browse(todo_ids):
+                    org_po.write({'invoice_ids': [(4, new_invoice_id)]})
         # recreate link (if any) between original analytic account line
         # (invoice time sheet for example) and this new invoice
-        anal_line_obj = self.pool['account.analytic.line']
+        anal_line_obj = self.env['account.analytic.line']
         if 'invoice_id' in anal_line_obj._columns:
             for new_invoice_id in invoices_info:
                 todo_ids = anal_line_obj.search(
-                    cr, uid,
-                    [('invoice_id', 'in', invoices_info[new_invoice_id])],
-                    context=context)
-                anal_line_obj.write(
-                    cr, uid, todo_ids, {'invoice_id': new_invoice_id},
-                    context=context)
+                    [('invoice_id', 'in', invoices_info[new_invoice_id])])
+                todos = anal_line_obj.browse(todo_ids)
+                todos.write({'invoice_id': new_invoice_id})
         return invoices_info
