ff451b5486b3c85bd2b2007e5b5d5976f4b2ba06
Cédric Pigeon <cedric.pigeon@acsone.eu>
2015-01-27 17:01:08 +0100

    Fix pylint issues

--- account_invoice_merge/__init__.py
+++ account_invoice_merge/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import invoice
-import wizard
+from . import invoice
+from . import wizard
