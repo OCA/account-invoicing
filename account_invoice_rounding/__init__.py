c2e6813f63deeb412cd82be96d87429e2baab0b2
hurrinico <nicola.malcontenti@agilebg.com>
2015-02-02 18:19:29 +0100

    [FIX] First develop

--- account_invoice_rounding/__init__.py
+++ account_invoice_rounding/__init__.py
@@ -18,6 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import company
-from . import res_config
-from . import account
+import company
+import res_config
+import account
