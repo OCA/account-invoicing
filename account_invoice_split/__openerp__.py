d4b5a230a3d2bcfa60097766093251be822d5a42
ecino <ecino@compassion.ch>
2015-04-20 10:11:28 +0200

    Update to OCA template module.

---
+++ account_invoice_split/__openerp__.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#                            in Jesus' name
+#
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Account Invoice Split wizard',
+    'summary': 'Split an invoice into two separate invoices',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'author': 'Compassion CH',
+    'website': 'http://www.compassion.ch',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'external_dependencies': {},
+    'data': [
+        'view/account_invoice_split_wizard_view.xml',
+    ],
+    'demo': [],
+    'description': """
+Account Invoice Split wizard
+============================
+
+This module adds the ability to split an existing invoice into
+two different invoices by adding a new wizard on invoices allowing
+the user to select which lines are to be moved in a new invoice.
+
+Works only on unpaid invoices.
+""",
+    'active': False,
+    'installable': True,
+}
