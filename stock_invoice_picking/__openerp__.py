4728c05779d8fd054fdc369d3eb5a52bac3502bd
Alex Comba <alex.comba@agilebg.com>
2015-04-30 10:08:38 +0200

    Manifest file: remove description since already present in README, remove deprecated key as active, remove other keys with empty value and change version

--- stock_invoice_picking/__openerp__.py
+++ stock_invoice_picking/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2015 Agile Business Group sagl (<http://www.agilebg.com>)
+#    Copyright (C) 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
@@ -19,13 +19,9 @@
 ##############################################################################
 
 {
-    'name': "Invoice picking",
-    'version': '0.1',
+    'name': "Stock invoice picking",
+    'version': '1.0',
     'category': 'Warehouse Management',
-    'description': """
-This module allows to create invoices directly from picking, without having to
-use sale or purchase orders.
-""",
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
@@ -33,9 +29,5 @@ use sale or purchase orders.
     "data": [
         "stock_view.xml",
     ],
-    "demo": [],
-    'test': [
-    ],
-    "active": False,
     "installable": True
 }
