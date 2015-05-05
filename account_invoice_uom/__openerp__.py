b75bbfc9fca2b25e0bbbbaccf5762442560cb0b6
Alex Comba <alex.comba@agilebg.com>
2015-05-05 12:41:52 +0200

    Add OCA as author

--- account_invoice_uom/__openerp__.py
+++ account_invoice_uom/__openerp__.py
@@ -21,7 +21,7 @@
 {
     "name": "Unit of measure for invoices",
     "version": "1.0",
-    "author": "Agile Business Group",
+    'author': "Agile Business Group, Odoo Community Association (OCA)",
     "website": "http://www.agilebg.com",
     'license': 'AGPL-3',
     "category": "Account",
