0670c16e3e3397124b46eb0eea539fd15de7412b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:30 +0200

    [UPD] prefix versions with 8.0

--- stock_picking_invoicing/__openerp__.py
+++ stock_picking_invoicing/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name': "Stock Picking Invoicing",
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Warehouse Management',
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'http://www.agilebg.com',
