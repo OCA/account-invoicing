70b94a4c323c9298eca8dcb0116cc9ce2a779181
Alex Comba <alex.comba@agilebg.com>
2015-11-27 11:09:14 +0100

    Extract the description in README.rst

--- account_invoice_sale_origin/__openerp__.py
+++ account_invoice_sale_origin/__openerp__.py
@@ -1,48 +1,21 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010-2013 Savoir-faire Linux. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# (c) 2010-2013 Savoir-faire Linux
+# (c) 2015 Alex Comba - Agile Business Group
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Account Invoice Sale Origin',
-    'version': '1.1',
-    'category': 'Finance',
-    'description': """
-
-    This module displays the origin field of account invoice lines and populate
-    it when a customer invoice is create from a sale order.
-
-Contributors
-------------
-* David Cormier (david.cormier@savoirfairelinux.com)
-* Marc Cassuto (marc.cassuto@savoirfairelinux.com)
-    """,
-    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
+    'version': '8.0.1.1.0',
+    'category': 'Accounting & Finance',
+    'author': "Savoir-faire Linux, Odoo Community Association (OCA)",
     'website': 'http://www.savoirfairelinux.com/',
-    'depends': ['account', 'sale'],
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'sale'
+    ],
     'data': [
         'account_invoice_view.xml',
     ],
-    'test': [
-    ],
-    'demo': [],
     'installable': True,
-    'active': False,
-    'certificate': False,
 }
