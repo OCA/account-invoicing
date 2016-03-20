2ce47d4ee387039d66e076ebcde71bfeb968f243
florent.thomas <florent.thomas@mind-and-go.com>
2016-03-20 14:30:31 +0100

    Implements the mechanics of views : * Loading * Onchanges Etc

--- stock_picking_invoicing/wizard/__init__.py
+++ stock_picking_invoicing/wizard/__init__.py
@@ -18,21 +18,4 @@
 #
 ##############################################################################
 
-{
-    'name': "Stock Picking Invoicing",
-    'version': '9.0.1.0.0',
-    'category': 'Warehouse Management',
-    'author': "Agile Business Group,Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
-    'license': 'AGPL-3',
-    "depends": [
-        "stock"
-        
-        ],
-    "data": [
-        "views/stock_view.xml",
-        "wizard/stock_invoice_onshipping_view.xml",
-    ],
-    
-    'installable': True
-}
+from . import stock_invoice_onshipping
