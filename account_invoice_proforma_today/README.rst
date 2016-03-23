20c7a2727181782de26555653efc63409ba7f287
ralwafaie <rami.alwafaie@initos.de>
2016-03-23 14:14:19 +0100

    #123 update to the new OCA guidlines

---
+++ account_invoice_proforma_today/README.rst
@@ -0,0 +1,35 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================================
+Account Invoice - Proforma Today
+================================
+
+* The module extends the account.invoice model to set the date of the invoice as the today's
+ date when changing state from 'proforma' to 'open'.
+
+Installation
+============
+
+* No thing is required to install this module
+
+Contributors
+------------
+
+* Rami Alwafaie <rami.alwafaie@initos.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- account_invoice_proforma_today/__openerp__.py
+++ account_invoice_proforma_today/__openerp__.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2015 OpenERP s.a. (<http://openerp.com>).
-#    Copyright (C) 2015 initOS GmbH (<http://www.initos.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © initOS GmbH 2016
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Account Invoice Proforma Today',
-    'version': '0.1',
-    'author': 'initOS GmbH',
+    'version': '7.0.1.0.0',
+    'author': 'initOS GmbH, Odoo Community Association (OCA)',
     'category': '',
     'description':
     """
