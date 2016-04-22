5e316d10c8ea225f4462b3f9a2eae8e3d4077dcf
Francesco Apruzzese <cescoap@gmail.com>
2016-04-22 18:15:45 +0200

    [FIX] Travis error with vim comment and import of deprecated libs

--- account_invoice_sequential_dates/__openerp__.py
+++ account_invoice_sequential_dates/__openerp__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#    
-#    Copyright (C) 2010-2012 Associazione OpenERP Italia
-#    (<http://www.openerp-italia.org>). 
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Apulia Software srl <info@apuliasoftware.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Check invoice date consistency',
     'version': '0.1',
@@ -38,5 +22,4 @@ Functionalities:
     "active": False,
     'installable': True
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
