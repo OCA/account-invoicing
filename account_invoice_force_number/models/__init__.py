e987d6bf9d98f9b22a53c6833e5d2740f70a7e77
Davide Corio <davide.corio@abstract.it>
2016-04-29 12:20:17 +0200

    [IMP] module structure

---
+++ account_invoice_force_number/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2011 Domsense srl (<http://www.domsense.com>)
+# © 2011-2016 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import invoice
