adb83eaf5ea873efda6f098cc149905c75deeba8
Thomas Binsfeld <thomas.binsfeld@gmail.com>
2016-07-12 11:12:14 +0200

    account_invoice_check_total

---
+++ account_invoice_check_total/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Acsone SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Account Invoice Check Total',
+    'summary': """
+        Check if the verification total is equal to the bill's total""",
+    'version': '9.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Acsone SA/NV,Odoo Community Association (OCA)',
+    'website': 'www.acsone.eu',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+    'demo': [
+    ],
+}
