666d63f2927a7130cccbbb1d38133c05cabd314a
susport <richard.pompa84@gmail.com>
2016-07-13 12:34:44 -0300

    Revert "account invoice move currency"
    This reverts commit ecf092d10d8a141699c67cadb03444a36a09d2d0.

--- account_invoice_move_currency/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import models
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
