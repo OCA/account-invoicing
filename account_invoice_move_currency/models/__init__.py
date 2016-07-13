32db4357c1849fcc188350e5c90fb686f3aba639
susport <richard.pompa84@gmail.com>
2016-07-13 10:48:42 -0300

    account invoice move currency
    It allows force the seat on an invoice generated save the data in
    another currency

---
+++ account_invoice_move_currency/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from . import account_invoice
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
