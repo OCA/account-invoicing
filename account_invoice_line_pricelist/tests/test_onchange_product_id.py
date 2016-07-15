1dee02787eb687943995a2b40096dd326d24b52b
Antonio Esposito <a.esposito@onestein.nl>
2016-07-15 16:26:39 +0200

    [IMP] Added a new test case to test new functionality (pricelist retrieved from invoice)

--- account_invoice_line_pricelist/tests/test_onchange_product_id.py
+++ account_invoice_line_pricelist/tests/test_onchange_product_id.py
@@ -19,22 +19,35 @@ class TestOnchangeProductId(TransactionCase):
             'name': 'Test Product',
             'lst_price': 10.00,
         })
-        version_dict = {
-            'name': 'Test version',
+        version_dict1 = {
+            'name': 'Test version 1',
             'items_id': [
                 (0, 0, {
                     'name': 'Test item default',
                     'base': 1,
                     'price_discount': 0.1, })]
         }
-        self.pricelist = self.env['product.pricelist'].create(
-            {'name': 'Test pricelist',
+        version_dict2 = {
+            'name': 'Test version 2',
+            'items_id': [
+                (0, 0, {
+                    'name': 'Test item default',
+                    'base': 1,
+                    'price_discount': 0.2, })]
+        }
+        self.plist1 = self.env['product.pricelist'].create(
+            {'name': 'Test pricelist 1',
              'type': 'sale',
              'currency_id': self.usd,
-             'version_id': [(0, 0, version_dict)]})
+             'version_id': [(0, 0, version_dict1)]})
+        self.plist2 = self.env['product.pricelist'].create(
+            {'name': 'Test pricelist 2',
+             'type': 'sale',
+             'currency_id': self.usd,
+             'version_id': [(0, 0, version_dict2)]})
         self.partner = self.env['res.partner'].create(
             {'name': 'Test partner',
-             'property_product_pricelist': self.pricelist.id})
+             'property_product_pricelist': self.plist1.id})
         self.invoice_line = self.env['account.invoice.line'].create(
             {'name': 'Test line',
              'quantity': 1.0,
@@ -50,6 +63,25 @@ class TestOnchangeProductId(TransactionCase):
             self.product.id, self.product.uom_id.id, qty=1.0,
             partner_id=self.partner.id, currency_id=self.eur,
             price_unit=price_unit, company_id=self.env.user.company_id.id)
+
+        self.assertLessEqual(
+            abs(exp_value - res['value']['price_unit']), 0.0001,
+            "ERROR in getting price from pricelist")
+
+    def test_onchange_product_id_invoice_pricelist(self):
+
+        currency = self.env['res.currency'].browse(self.usd)
+        rate = currency.rate_silent
+        price_unit = currency.round(10.00 * rate)
+        exp_value = currency.round((12.00 * rate))
+        res = (
+            self.invoice_line
+            .with_context({'pricelist_id': self.plist2.id})
+            .product_id_change(
+                self.product.id, self.product.uom_id.id, qty=1.0,
+                partner_id=self.partner.id, currency_id=self.usd,
+                price_unit=price_unit, company_id=self.env.user.company_id.id)
+        )
 
         self.assertLessEqual(
             abs(exp_value - res['value']['price_unit']), 0.0001,
