7df07eda4ca454d11ec6710907a9374ddfbadc3b
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-09-22 15:37:55 +0200

    [CHG] Add auto_install module in install_requires of principal module's setup

--- setup/account_invoice_merge/setup.py
+++ setup/account_invoice_merge/setup.py
@@ -2,5 +2,9 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
+    install_requires=[
+        'odoo8-addon-account_invoice_merge_purchase',
+        'odoo8-addon-account_invoice_merge_payment',
+    ],
     odoo_addon=True,
 )
