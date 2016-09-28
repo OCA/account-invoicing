0f74e404835ff1587d013f9c4ad02acdd10f4081
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2016-09-28 13:30:17 +0200

    [FIX] blocked fields stored for search domain in view

--- account_invoice_blocking_payment/models/account_invoice.py
+++ account_invoice_blocking_payment/models/account_invoice.py
@@ -2,13 +2,17 @@
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models, _
+from openerp import api, fields, models, _
 from openerp.exceptions import UserError
 
 
 class AccountInvoice(models.Model):
 
     _inherit = 'account.invoice'
+
+    blocked = fields.Boolean(store=True)
+
+    draft_blocked = fields.Boolean(store=True)
 
     @api.multi
     def create_account_payment_line(self):
