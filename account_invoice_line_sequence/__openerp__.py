3ebb145e5579d68b9d3dfb39dee3c1734c52b9eb
SerpentCS <support@serpentcs.com>
2016-09-28 18:06:08 +0530

    [ADD] added new module Account line Sequence

---
+++ account_invoice_line_sequence/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#    Author: Alexandre Fayolle
+#    Copyright 2013 Camptocamp SA
+#    Author: Damien Crier
+#    Copyright 2015 Camptocamp SA
+# © 2015 Eficent Business and IT Consulting Services S.L. -
+# Jordi Ballester Alomar
+# © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Account Invoice line with sequence number',
+    'version': '9.0.1.0.0',
+    'category': 'Accounting',
+    'author': "Camptocamp, "
+              "Eficent, "
+              "Serpent CS, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.camptocamp.com',
+    'depends': ['account'],
+    'installable': True,
+    'auto_install': False,
+    'license': "AGPL-3",
+}
