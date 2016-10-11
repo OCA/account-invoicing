5dccc4a4bae0851f4f4276ea706283fba0935b87
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-11 10:37:01 +0200

    Migration of account_invoice_supplier_ref_unique to 10.0

--- account_invoice_supplier_ref_unique/models/account_invoice.py
+++ account_invoice_supplier_ref_unique/models/account_invoice.py
@@ -2,8 +2,8 @@
 # Copyright 2016 Acsone
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class AccountInvoice(models.Model):
