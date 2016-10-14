39c1bfabe6fc5eb11cae256421d374963e7583ed
Angel Moya <amoyapardo@gmail.com>
2016-10-14 16:32:50 +0200

    MIG 9.0 invoice_fiscal_position_update

--- invoice_fiscal_position_update/__openerp__.py
+++ invoice_fiscal_position_update/__openerp__.py
@@ -1,45 +1,21 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Invoice Fiscal Position Update module for OpenERP
-#    Copyright (C) 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
-#    Copyright (C) 2014 Akretion (http://www.akretion.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
+# Copyright 2014 Akretion (http://www.akretion.com)
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Invoice Fiscal Position Update',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Changing the fiscal position of an invoice will auto-update '
                'invoice lines',
-    'description': """
-Invoice Fiscal Position Update
-==============================
-
-With this module, when a user changes the fiscal position of an invoice, the
-taxes and the accounts on all the invoice lines which have a product are
-automatically updated. The invoice lines without a product are not updated and
-a warning is displayed to the user in this case.
-""",
     'author': "Julius Network Solutions,"
               "Akretion,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     'depends': ['account'],
     'data': [],
-    'installable': False,
+    'installable': True,
 }
---
+++ invoice_fiscal_position_update/models/account_invoice.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
+# Copyright 2014 Akretion (http://www.akretion.com)
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.onchange('fiscal_position_id')
+    def onchange_fiscal_position_id(self):
+        """Updates taxes and accounts on all invoice lines"""
+        self.ensure_one()
+        res = {}
+        lines_without_product = []
+        fp = self.fiscal_position_id
+        inv_type = self.type
+        company_id = self.company_id or self.env.user.company_id
+        for line in self.invoice_line_ids:
+            if line.product_id:
+                product = line.product_id
+                if inv_type in ('out_invoice', 'out_refund'):
+                    account = (
+                        product.property_account_income_id or
+                        product.categ_id.property_account_income_categ_id)
+                    taxes = product.taxes_id.filtered(
+                        lambda r: r.company_id == company_id)
+
+                else:
+                    account = (
+                        product.property_account_expense_id or
+                        product.categ_id.property_account_expense_categ_id)
+                    taxes = product.supplier_taxes_id.filtered(
+                        lambda r: r.company_id == company_id)
+                taxes = taxes or account.tax_ids
+                if fp:
+                    account = fp.map_account(account)
+                    taxes = fp.map_tax(taxes).filtered(
+                        lambda r: r.company_id == company_id)
+
+                line.invoice_line_tax_ids = [(6, 0, taxes.ids)]
+                line.account_id = account.id
+            else:
+                lines_without_product.append(line.name)
+
+        if lines_without_product:
+            res['warning'] = {'title': _('Warning')}
+            if len(lines_without_product) == len(self.invoice_line):
+                res['warning']['message'] = _(
+                    "The invoice lines were not updated to the new "
+                    "Fiscal Position because they don't have products.\n"
+                    "You should update the Account and the Taxes of each "
+                    "invoice line manually.")
+            else:
+                res['warning']['message'] = _(
+                    "The following invoice lines were not updated "
+                    "to the new Fiscal Position because they don't have a "
+                    "Product:\n- %s\nYou should update the Account and the "
+                    "Taxes of these invoice lines manually."
+                ) % ('\n- '.join(lines_without_product))
+        return res
