2a3d2f1ccc4f8039333e6c0957a0ed78ee36f59a
Jairo Llopis <yajo.sk8@gmail.com>
2016-10-26 15:55:01 +0200

    [9.0][ADD][account_invoice_refund_option] Create refund invoices directly.
    This addon lets you create a refund out of nothing.

---
+++ account_invoice_refund_option/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Create Refund Invoice",
+    "summary": "Allows you to create a refund linked to nothing",
+    "version": "9.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.tecnativa.com/",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+}
