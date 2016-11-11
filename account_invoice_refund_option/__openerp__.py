4e351fc30fec57aec433982006f4c07a77eb474a
Jairo Llopis <Yajo@users.noreply.github.com>
2016-11-11 19:42:48 +0100

    [9.0][ADD][account_invoice_refund_option] Create refund invoices directly (#205)

---
+++ account_invoice_refund_option/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Create Refund Invoice",
+    "summary": "Allows you to create directly a refund without starting from "
+               "an invoice",
+    "version": "9.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.tecnativa.com/",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+}
