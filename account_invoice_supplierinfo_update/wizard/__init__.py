e5a7021dd49df6e7b23ed2bafb17b11cc7247571
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-26 14:25:28 +0100

    full refactoring: - OCA convention - module renamed - modularity in function. (prepare function added) [ADD] description [FIX] remove unused dependencies for the time being

---
+++ account_invoice_supplierinfo_update/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Chafique DELLI @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import wizard_update_invoice_supplierinfo
+from . import wizard_update_invoice_supplierinfo_line
