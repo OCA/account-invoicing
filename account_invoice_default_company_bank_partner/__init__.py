58a601757a5f315e8e1b20aad226f1042b6bda70
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-07 11:27:51 +0530

    [REF]

--- account_invoice_default_company_bank_partner/__init__.py
+++ account_invoice_default_company_bank_partner/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd
 # License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
 
 from . import models
