0395d9a1dc59c7349c8ca55a8cc2c1d161d51d65
sergio.teruel <sergio.teruel@tecnativa.com>
2016-12-09 13:40:26 +0100

    account_invoice_pricelist: Migration to v9

--- account_invoice_pricelist/model/account_invoice.py
+++ account_invoice_pricelist/model/account_invoice.py
@@ -1,24 +1,5 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Account - Pricelist on Invoices for Odoo
-#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
 
@@ -26,40 +7,22 @@ from openerp import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    # Column Section
     pricelist_id = fields.Many2one(
         comodel_name='product.pricelist', string='Pricelist',
-        help="The pricelist of the partner, when the invoice is created"
-        " or the partner has changed. This is a technical field used"
-        " for reporting.",
-        readonly=True, states={'draft': [('readonly', False)]})
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
 
-    @api.multi
-    def onchange_partner_id(
-            self, type, partner_id, date_invoice=False, payment_term=False,
-            partner_bank_id=False, company_id=False):
-        partner_obj = self.env['res.partner']
-        res = super(AccountInvoice, self).onchange_partner_id(
-            type, partner_id, date_invoice=date_invoice,
-            payment_term=payment_term, partner_bank_id=partner_bank_id,
-            company_id=company_id)
-        pricelist_id = False
-        if partner_id:
-            partner = partner_obj.browse(partner_id)
-            if type in ('out_invoice', 'out_refund'):
-                # Customer Invoices
-                pricelist_id = partner.property_product_pricelist.id
-            elif type in ('in_invoice', 'in_refund'):
-                # Supplier Invoices
-                if partner._model._columns.get(
-                        'property_product_pricelist_purchase', False):
-                    pricelist_id =\
-                        partner.property_product_pricelist_purchase.id
-        res['value']['pricelist_id'] = pricelist_id
-        return res
+    @api.onchange('partner_id')
+    def _onchange_partner_id_account_invoice_pricelist(self):
+        if self.type not in ('out_invoice', 'out_refund'):
+            return
+        if self.partner_id.property_product_pricelist:
+            self.pricelist_id = \
+                self.partner_id.property_product_pricelist.id
 
     @api.multi
     def button_update_prices_from_pricelist(self):
-        for this in self:
-            this.invoice_line.filtered('product_id').update_from_pricelist()
-            this.button_reset_taxes()
+        for invoice in self:
+            invoice.invoice_line_ids.filtered(
+                'product_id')._onchange_product_id_account_invoice_pricelist()
