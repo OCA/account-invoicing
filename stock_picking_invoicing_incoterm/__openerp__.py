1ea12bda1e4f061c58b24c250ac02fcb7850b046
Ronald Portier <ronald@therp.nl>
2017-01-02 21:40:45 +0100

    [ENH] Make sure pricelist on invoice is used.
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex

--- stock_picking_invoicing_incoterm/__openerp__.py
+++ stock_picking_invoicing_incoterm/__openerp__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014-2015 Agile Business Group sagl
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 {
     'name': "Stock Picking Invoicing Incoterm",
     'version': '8.0.1.0.0',
