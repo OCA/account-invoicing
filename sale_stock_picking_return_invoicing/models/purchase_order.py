97b4f0032eda3580d1d48096b69314a6b5b4d6dd
Valentin Thirion <vt@abakusitsolutions.eu>
2017-01-10 14:41:05 +0100

    [FIX] sale_stock_picking_return_invoicing Made the module work

--- sale_stock_picking_return_invoicing/models/purchase_order.py
+++ sale_stock_picking_return_invoicing/models/purchase_order.py
@@ -2,21 +2,38 @@
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from openerp import api, models, fields
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
     @api.depends('order_id.state', 'move_ids.state')
     def _compute_qty_received(self):
-        """Deduct moves marked to refund."""
-        qty = super(PurchaseOrderLine, self)._compute_qty_received()
-        for move in self.procurement_ids.mapped('move_ids').filtered(
-                lambda r: (r.state == 'done' and
-                           not r.scrapped and
-                           r.location_id.usage != "supplier" and
-                           r.to_refund_so)):
-            qty -= move.product_uom._compute_qty_obj(
-                move.product_uom, move.product_uom_qty, self.product_uom)
-        return qty
+        productuom = self.env['product.uom']
+        for line in self:
+            if line.order_id.state not in ['purchase', 'done']:
+                line.qty_received = 0.0
+                continue
+            if line.product_id.type not in ['consu', 'product']:
+                line.qty_received = line.product_qty
+                continue
+            bom_delivered = self.sudo()._get_bom_delivered(line.sudo())
+            if bom_delivered and any(bom_delivered.values()):
+                total = line.product_qty
+            elif bom_delivered:
+                total = 0.0
+            else:
+                total = 0.0
+                for move in line.move_ids:
+                    if move.state == 'done':
+                        if move.product_uom != line.product_uom:
+                            total += productuom._compute_qty_obj(move.product_uom, move.product_uom_qty, line.product_uom)
+                        else:
+                            if move.location_id.usage != 'supplier' and move.to_refund_so:
+                                total -= move.product_uom_qty
+                            else:
+                                total += move.product_uom_qty
+            line.qty_received = total
--- sale_stock_picking_return_invoicing/wizards/stock_return_picking.py
+++ sale_stock_picking_return_invoicing/wizards/stock_return_picking.py
@@ -20,7 +20,7 @@ class StockReturnPicking(models.TransientModel):
                 move = self.env["stock.move"].browse(line[2]["move_id"])
                 line[2]["sale_order_id"] = (move.procurement_id.sale_line_id
                                             .order_id.id)
-                line[2]["purchase_line_id"] = (move.purchase_line_id
+                line[2]["purchase_order_id"] = (move.purchase_line_id
                                             .order_id.id)
         except KeyError:
             pass
@@ -32,11 +32,15 @@ class StockReturnPicking(models.TransientModel):
         new_picking_id, pick_type_id = super(
             StockReturnPicking, self)._create_returns()
         new_picking = self.env['stock.picking'].browse(new_picking_id)
+        
         for move in new_picking.move_lines:
             return_picking_line = self.product_return_moves.filtered(
                 lambda r: r.move_id == move.origin_returned_move_id)
             if return_picking_line and return_picking_line.to_refund_so:
                 move.to_refund_so = True
+            # Add this new move to the purchase line
+            if return_picking_line.purchase_order_id:
+                move.purchase_line_id = return_picking_line.move_id.purchase_line_id
         return new_picking_id, pick_type_id
 
 
@@ -58,7 +62,7 @@ class StockReturnPickingLine(models.TransientModel):
         ondelete="cascade",
         readonly=True,
     )
-    purchase_line_id = fields.Many2one(
+    purchase_order_id = fields.Many2one(
         comodel_name="purchase.order",
         string="Purchase order",
         ondelete="cascade",
