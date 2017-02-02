d04a40768de6c4e2e435c8a21de8b6409af91dee
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-02-02 14:52:55 +0100

    [9.0][WIP] account_invoice_group_picking: picking_info

--- account_invoice_group_picking/models/account_invoice.py
+++ account_invoice_group_picking/models/account_invoice.py
@@ -2,11 +2,17 @@
 # Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import _, api, models, fields
+from openerp import _, api, fields, models, tools
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+
+    @api.model
+    @tools.ormcache("picking")
+    def _prepare_picking_info(self, picking):
+        return (picking and
+                '%s -- %s' % (picking.write_date[:10], picking.name) or '')
 
     @api.multi
     def lines_grouped_by_picking(self):
@@ -15,9 +21,9 @@ class AccountInvoice(models.Model):
         for line in self.invoice_line_ids:
             pick = (line.sale_move_link_ids[:1].move_id.picking_id or
                     line.sale_line_ids.procurement_ids.move_ids[:1].picking_id)
-
-            res.append((line, pick))
-        return sorted(res, key=lambda x: x[1].write_date)
+            res.append({'line': line, 'picking': pick,
+                        'picking_info': self._prepare_picking_info(pick)})
+        return sorted(res, key=lambda x: x['picking'].write_date)
 
 
 class AccountInvoiceLine(models.Model):
