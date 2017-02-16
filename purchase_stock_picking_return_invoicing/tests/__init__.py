585dce7dd176fb22700423b2b4cb8f035cb72456
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-16 16:38:02 +0100

    [ADD] purchase_stock_picking_return_invoicing

---
+++ purchase_stock_picking_return_invoicing/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services
+#           <contact@eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_refund
