481e9f5f73e9d71b28cf3dc928cbc38dd4a55f49
Jordi Ballester <jordi.ballester@eficent.com>
2017-02-16 16:38:02 +0100

    purchase_stock_picking_return_invoicing

---
+++ purchase_stock_picking_return_invoicing/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services
+#           <contact@eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import purchase_order
+from . import account_invoice
