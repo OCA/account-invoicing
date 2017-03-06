c0ef2240b5b7d385899b61dfd608c10200bd86ae
cubells <vicent@vcubells.net>
2017-03-06 20:15:22 +0100

    [MIG][9.0] account_invoice_merge_purchase module

--- account_invoice_merge_purchase/__openerp__.py
+++ account_invoice_merge_purchase/__openerp__.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2009-2016 Noviat
+# Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Account Invoice Merge Purchase",
-
     'summary': """Compatibility between purchase and account invoice merge""",
-    'author': "ACSONE SA/NV,Noviat,Odoo Community Association (OCA)",
+    'author': "ACSONE SA/NV, "
+              "Tecnativa, "
+              "Noviat,Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'Finance',
-    'version': '8.0.2.0.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'account_invoice_merge',
         'purchase',
     ],
     'auto_install': True,
-    'installable': False,
+    'installable': True,
 }
