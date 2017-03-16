660452a62ecd886c6ce8c9e00fc451dd0edd0667
lreficent <lois.rilo@eficent.com>
2017-03-16 16:18:49 +0100

    [MIG] account_invoice_merge to v10

--- account_invoice_merge/__manifest__.py
+++ account_invoice_merge/__manifest__.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2010-2011 Elico Corp. All Rights Reserved.
-# Author: Ian Li <ian.li@elico-corp.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2004-2010 Tiny SPRL (http://tiny.be).
+# Copyright 2010-2011 Elico Corp.
+# Copyright 2016 Acsone (https://www.acsone.eu/)
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Invoice Merge Wizard',
-    'version': '9.0.1.0.0',
+    'name': 'Account Invoice Merge',
+    'version': '10.0.1.0.0',
     'category': 'Finance',
+    'summary': "Merge invoices in draft",
     'author': "Elico Corp,Odoo Community Association (OCA)",
     'website': 'http://www.openerp.net.cn',
     'license': 'AGPL-3',
