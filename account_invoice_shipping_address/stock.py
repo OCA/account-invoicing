f431659acec60572466a0d9e5c71222adbc02bcc
Andrea Cometa <a.cometa@apuliasoftware.it>
2017-03-18 18:05:36 +0100

    del stock_transport_multi_address dependency

--- account_invoice_shipping_address/stock.py
+++ account_invoice_shipping_address/stock.py
@@ -30,8 +30,12 @@ class StockPicking(models.Model):
         invoice_vals = super(StockPicking, self)._get_invoice_vals(
             cr, uid, key, inv_type, journal_id, move, context=context)
         if move and move.partner_id:
-            invoice_vals['address_shipping_id'] = (
-                move.picking_id.delivery_address_id.id and
-                move.picking_id.delivery_address_id.id or
-                move.partner_id.id)
+            if 'delivery_address_id' in self.env['stock.picking']._fields:
+                invoice_vals['address_shipping_id'] = (
+                    move.picking_id.delivery_address_id and
+                    move.picking_id.delivery_address_id.id or
+                    move.picking_id.partner_id.id)
+            else:
+                invoice_vals[
+                    'address_shipping_id'] = move.picking_id.partner_id.id
         return invoice_vals
