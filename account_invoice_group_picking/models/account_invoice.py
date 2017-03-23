34795d0654ba956377bf3ed429caa37e8458ced0
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-03-23 13:40:47 +0100

    [9.0][IMP] account_invoice_group_picking: Improve service lines with picking lines combination

--- account_invoice_group_picking/models/account_invoice.py
+++ account_invoice_group_picking/models/account_invoice.py
@@ -9,10 +9,18 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.model
-    @tools.ormcache("picking")
-    def _prepare_picking_info(self, picking):
-        return (picking and
-                '%s -- %s' % (picking.write_date[:10], picking.name) or '')
+    @tools.ormcache('picking', 'order')
+    def _prepare_picking_info(self, picking, order, inv_line):
+        res = order and _('Order: %s') % order.name or ''
+        if picking:
+            res = _('%s Picking: %s %s') % (
+                res, picking.date_done[:10], picking.name)
+        return res
+
+    @api.model
+    def _sort_grouped_lines(self, lines_dic):
+        return sorted(lines_dic, key=lambda x: (
+            x['order'].date_order, x['picking'].date_done))
 
     @api.multi
     def lines_grouped_by_picking(self):
@@ -22,9 +30,13 @@ class AccountInvoice(models.Model):
             pick = (line.sale_move_link_ids.mapped('move_id.picking_id') or
                     line.sale_line_ids.mapped(
                         'procurement_ids.move_ids.picking_id'))[:1]
+            order = line.sale_line_ids.order_id
             res.append({'line': line, 'picking': pick,
-                        'picking_info': self._prepare_picking_info(pick)})
-        return sorted(res, key=lambda x: x['picking'].write_date)
+                        'order': order,
+                        'picking_info':
+                            self._prepare_picking_info(pick, order, line),
+                        })
+        return self._sort_grouped_lines(res)
 
 
 class AccountInvoiceLine(models.Model):
