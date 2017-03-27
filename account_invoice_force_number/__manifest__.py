0e99ef9f4a6f41fe98d634b4ef7cb70c1146b59d
Alex Comba <alex.comba@agilebg.com>
2017-03-27 14:36:13 +0200

    [MIGR] account_invoice_force_number: preparatory tasks for migration and  apply OCA guidelines

--- account_invoice_force_number/__manifest__.py
+++ account_invoice_force_number/__manifest__.py
@@ -1,47 +1,21 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
-#    Copyright (C) 2011-2013 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011-2016 Agile Business Group
+# Copyright 2017 Alex Comba - Agile Business Group
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Force Invoice Number",
-    'version': '8.0.0.1.0',
+    'name': 'Force Invoice Number',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
-    'summary': "Allows to force invoice numbering on specific invoices",
-    'description': """
-This module allows to force the invoice numbering.
-It displays the internal_number field. If user fills that field, the typed
-value will be used as invoice (and move) number.
-Otherwise, the next sequence number will be retrieved and saved.
-So, the new field has to be used when user doesn't want to use the default
-invoice numbering for a specific invoice.
-    """,
-    'author': "Agile Business Group,Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
+    'summary': 'Allows to force invoice numbering on specific invoices',
+    'author': 'Agile Business Group, Odoo Community Association (OCA)',
+    'website': 'https://www.agilebg.com',
     'license': 'AGPL-3',
-    "depends": [
+    'depends': [
         'account'
     ],
-    "data": [
+    'data': [
         'security/security.xml',
-        'invoice_view.xml'
+        'views/account_invoice_view.xml'
     ],
-    "active": False,
-    'installable': False,
+    'installable': True,
 }
