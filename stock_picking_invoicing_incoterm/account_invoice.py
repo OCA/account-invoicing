07ff7bc6fc086e10d7c501586e6056005cbccf30
Esther Martín <esthermartin001@gmail.com>
2017-03-30 10:04:58 +0200

    [MOD] stock_picking_invoicing_incoterm: remove dependency, update code

--- stock_picking_invoicing_incoterm/account_invoice.py
+++ stock_picking_invoicing_incoterm/account_invoice.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014-15 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014-2015 Agile Business Group sagl
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.osv import fields, orm
+from openerp import fields, models
 
 
-class AccountInvoice(orm.Model):
+class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    _columns = {
-        'incoterm': fields.many2one(
-            'stock.incoterms',
-            'Incoterm',
-            help="International Commercial Terms are a series of predefined "
-            "commercial terms used in international transactions."
-        ),
-    }
+    incoterm = fields.Many2one(
+        comodel_name='stock.incoterms',
+        string='Incoterm',
+        help="International Commercial Terms are a series of predefined "
+        "commercial terms used in international transactions.",
+    )
--- stock_picking_invoicing_incoterm/stock.py
+++ stock_picking_invoicing_incoterm/stock.py
@@ -1,44 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2014-15 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014-2015 Agile Business Group sagl
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.osv import fields, orm
+from openerp import api, fields, models
 
 
-class StockPicking(orm.Model):
+class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    _columns = {
-        'incoterm': fields.many2one(
-            'stock.incoterms',
-            'Incoterm',
-            help="International Commercial Terms are a series of predefined "
-            "commercial terms used in international transactions."
-        ),
-    }
+    incoterm = fields.Many2one(
+        comodel_name='stock.incoterms',
+        string='Incoterm',
+        help="International Commercial Terms are a series of predefined "
+        "commercial terms used in international transactions.",
+    )
 
-    def _get_invoice_vals(
-            self, cr, uid, key,
-            inv_type, journal_id, move, context=None):
+    @api.model
+    def _get_invoice_vals(self, key, inv_type, journal_id, move):
         invoice_vals = super(StockPicking, self)._get_invoice_vals(
-            cr, uid, key, inv_type, journal_id, move, context=context)
+            key, inv_type, journal_id, move)
         if move.picking_id.incoterm:
             invoice_vals['incoterm'] = move.picking_id.incoterm.id
         return invoice_vals
