34257b1fabaa53a845ce874a2c4521d64d7b36e1
Jordi Ballester <jordi.ballester@eficent.com>
2017-03-30 11:34:07 +0200

    account_invoice_view_payment

---
+++ account_invoice_view_payment/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#        (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Account Invoice View Payment",
+    'version': '9.0.1.0.0',
+    'category': 'Accounting',
+    'author': 'Eficent,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-payment',
+    'license': 'AGPL-3',
+    "depends": [
+        'account',
+    ],
+    "data": [
+        'views/account_payment_view.xml',
+        'views/account_invoice_view.xml',
+    ],
+    "installable": True
+}
