f02627729a55b1f46d6e64c4c12d36fa66709850
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-31 12:35:59 +0200

    [MIG] account_invoice_supplierinfo_update: Migrated to 10.0

--- account_invoice_supplierinfo_update/models/account_invoice_line.py
+++ account_invoice_supplierinfo_update/models/account_invoice_line.py
@@ -2,7 +2,7 @@
 # © 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import api, models
 
 
 class AccountInvoiceLine(models.Model):
@@ -18,56 +18,40 @@ class AccountInvoiceLine(models.Model):
         return supplierinfos and supplierinfos[0] or False
 
     @api.multi
-    def _get_partnerinfo(self, supplierinfo):
-        """Given an invoice line and a supplierinfo, return the partnerinfo
-        that matches with line info, if exist"""
-        self.ensure_one()
-        if not len(supplierinfo.pricelist_ids):
-            return False
-        # Select partnerinfo, depending of the quantity
-        for partnerinfo in supplierinfo.pricelist_ids.sorted(
-                key=lambda r: r.min_quantity):
-            if partnerinfo.min_quantity <= self.quantity:
-                return partnerinfo
-        return False
-
-    @api.multi
-    def _is_correct_partner_info(self, partnerinfo):
+    def _is_correct_price(self, supplierinfo):
         """Return True if the partner information matche with line info
             Overload this function in custom module if extra fields
-            are added in partnerinfo. (discount for exemple)
+            are added in supplierinfo. (discount for exemple)
         """
         self.ensure_one()
-        return self.price_unit == partnerinfo.price
+        return self.price_unit == supplierinfo.price
 
     @api.multi
-    def _prepare_supplier_wizard_line(self, supplierinfo, partnerinfo):
+    def _prepare_supplier_wizard_line(self, supplierinfo):
         """Prepare the value that will proposed to user in the wizard
         to update supplierinfo"""
         self.ensure_one()
         price_variation = False
+
         if not supplierinfo:
             state = 'new_supplierinfo'
-        elif not partnerinfo:
-            state = 'new_partnerinfo'
         else:
-            state = 'update_partnerinfo'
+            state = 'update_supplierinfo'
             # Compute price variation
-            if partnerinfo.price:
+            if supplierinfo.price:
                 price_variation = 100 *\
-                    (self.price_unit - partnerinfo.price) / partnerinfo.price
+                    (self.price_unit - supplierinfo.price) / supplierinfo.price
             else:
                 price_variation = False
         return {
             'product_id': self.product_id.id,
             'supplierinfo_id': supplierinfo and supplierinfo.id or False,
-            'partnerinfo_id': partnerinfo and partnerinfo.id or False,
-            'current_price': partnerinfo and partnerinfo.price or False,
+            'current_price': supplierinfo and supplierinfo.price or False,
             'new_price': self.price_unit,
             'current_min_quantity':
-                partnerinfo and partnerinfo.min_quantity or False,
+                supplierinfo and supplierinfo.min_qty or False,
             'new_min_quantity':
-                partnerinfo and partnerinfo.min_quantity or False,
+                supplierinfo and supplierinfo.min_qty or False,
             'state': state,
             'price_variation': price_variation,
         }
--- account_invoice_supplierinfo_update/wizard/wizard_update_invoice_supplierinfo.py
+++ account_invoice_supplierinfo_update/wizard/wizard_update_invoice_supplierinfo.py
@@ -4,7 +4,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields
+from odoo import models, api, fields
 
 
 class WizardUpdateInvoiceSupplierinfo(models.TransientModel):
@@ -29,25 +29,15 @@ class WizardUpdateInvoiceSupplierinfo(models.TransientModel):
     def update_supplierinfo(self):
         self.ensure_one()
         supplierinfo_obj = self.env['product.supplierinfo']
-        partnerinfo_obj = self.env['pricelist.partnerinfo']
         for line in self.line_ids:
             supplierinfo = line.supplierinfo_id
-            partnerinfo = line.partnerinfo_id
-
             # Create supplierinfo if not exist
             if not supplierinfo:
                 supplierinfo_vals = line._prepare_supplierinfo()
                 supplierinfo = supplierinfo_obj.create(supplierinfo_vals)
-
-            partnerinfo_vals = line._prepare_partnerinfo(supplierinfo)
-
-            # Create partnerinfo if not exist
-            if not line.partnerinfo_id:
-                partnerinfo_obj.create(partnerinfo_vals)
-
-            # Update partnerinfo, if exist
             else:
-                partnerinfo.write(partnerinfo_vals)
+                supplierinfo.price = line.new_price
+                supplierinfo.min_qty = line.new_min_quantity
 
         # Mark the invoice as checked
         self.invoice_id.write({'supplierinfo_ok': True})
