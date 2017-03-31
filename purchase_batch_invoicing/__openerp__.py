407ba1eecf7572aba32aafde90c1e72bdb683201
Jairo Llopis <Yajo@users.noreply.github.com>
2017-03-31 14:00:12 +0200

    [ADD][purchase_batch_invoicing] Batch invoicing for purchase orders! (#180)
    This module extends the functionality of purchases to support batch invoicing
    purchase orders and to allow you to choose if you want them grouped by purchase
    order or by vendor.

---
+++ purchase_batch_invoicing/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Batch Invoicing",
+    "summary": "Make invoices for all ready purchase orders",
+    "version": "9.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://tecnativa.com/",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "external_dependencies": {
+        "python": [
+            "mock",  # FIXME Remove in Python >= 3.3
+        ],
+    },
+    "data": [
+        "data/ir_cron_data.xml",
+        "wizards/purchase_batch_invoicing_view.xml",
+        "views/purchase_order_view.xml",
+    ],
+    "images": [
+        "static/description/wizard.png",
+    ],
+}
