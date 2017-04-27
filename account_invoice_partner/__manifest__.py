33ebfc9170bc09cca0288b4c8a5f9e990c304778
gilles <meyomesse.gilles@gmail.com>
2017-04-27 15:38:20 +0200

    [10.0][MIG] account_invoice_partner

--- account_invoice_partner/__manifest__.py
+++ account_invoice_partner/__manifest__.py
@@ -1,30 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2012-2013 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012-2017 Therp BV (<http://therp.nl>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
-    "name": "Automatically select invoicing partner on invoice",
-    "version": "8.0.0.2.0",
-    "author": "Therp BV,Odoo Community Association (OCA)",
-    "category": 'Accounting & Finance',
-    'website': 'https://github.com/OCA/account-invoicing',
+    'name': "Automatically select invoicing partner on invoice",
+    'summary': """Replace the partner by an invoice contact if found""",
+    'author': "Therp BV,Odoo Community Association (OCA)",
+    'website': "https://odoo-community.org",
+    'category': "Accounting & Finance",
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': ['account'],
-    'installable': False,
+    'installable': True,
 }
--- account_invoice_partner/tests/test_account_invoice_partner.py
+++ account_invoice_partner/tests/test_account_invoice_partner.py
@@ -1,50 +1,67 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Account Invoice partner test module for OpenERP
-#    Copyright (C) 2015 ACSONE SA/NV (http://acsone.eu)
-#    @author Laurent Mignon <laurent.mignon@acsone.eu>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 ACSONE SA/NV (http://acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import openerp.tests.common as common
+from odoo.tests.common import SavepointCase
 
 
-class TestAccountInvoiceParner(common.TransactionCase):
+class TestAccountInvoiceParner(SavepointCase):
 
-    def setUp(self):
-        super(TestAccountInvoiceParner, self).setUp()
-        self.inv_model = self.env['account.invoice']
-        self.partner_model = self.env['res.partner']
-        self.partner_2 = self.ref('base.res_partner_2')
-        self.partner_address_3 = self.ref('base.res_partner_address_3')
+    @classmethod
+    def setUpClass(cls):
+        super(TestAccountInvoiceParner, cls).setUpClass()
+
+        # MODELS
+        cls.inv_model = cls.env['account.invoice']
+        cls.partner_model = cls.env['res.partner']
+
+        # INSTANCE
+        # partners
+        partners = cls.partner_model.search([
+            ('type', '!=', 'invoice'),
+            ('child_ids', '=', False)
+        ])
+        cls.partner = partners[0]
+        cls.partner_2 = partners[1]
+
+        # invoice
+        account_payable = cls.env['account.account'].create(
+            {'code': 'X1111', 'name': 'Sale - Test Payable Account',
+             'user_type_id': cls.env.ref(
+                 'account.data_account_type_payable').id,
+             'reconcile': True
+             })
+        journal = cls.env['account.journal'].create(
+            {'name': 'Purchase Journal - Test',
+             'code': 'STPJ',
+             'type': 'purchase'
+             })
+        invoice_vals = {
+            'name': 'TEST',
+            'type': 'in_invoice',
+            'partner_id': cls.partner.id,
+            'account_id': account_payable.id,
+            'journal_id': journal.id,
+        }
+        cls.invoice = cls.env['account.invoice'].create(invoice_vals)
 
     def test_0(self):
-        # partner_2 has no address defined for invoice
-        addr_ids = self.partner_model.search(
-            [('parent_id', '=', self.partner_2),
-             ('type', '=', 'invoice')])
-        self.assertFalse(addr_ids)
-        res = self.inv_model.onchange_partner_id(
-            invoice_type='in_invoice', partner_id=self.partner_2)
-        self.assertFalse('partner_id' in res['value'])
+        # these partners are differents
+        self.assertNotEqual(self.partner, self.partner_2)
 
-        # declare partner_address3 as invoice address
-        self.partner_model.browse(self.partner_address_3).write(
-            {'type': 'invoice'})
-        res = self.inv_model.onchange_partner_id(
-            invoice_type='in_invoice', partner_id=self.partner_2)
-        self.assertEqual(self.partner_address_3, res['value']['partner_id'])
+        # partner is define in invoice
+        self.assertEqual(self.invoice.partner_id, self.partner)
+
+        # partner has no address defined for invoice
+        res = self.invoice._onchange_partner_id()
+        self.assertFalse('value' in res)
+
+        # change partner type to define partner invoice address
+        self.partner_2.write({'type': 'invoice'})
+        self.partner.write({'child_ids': [(6, 0, self.partner_2.ids)]})
+        data = self.partner._convert_to_write(self.partner._cache)
+        self.assertEqual(data['child_ids'], [(6, 0, self.partner_2.ids)])
+
+        # test onchange function
+        res = self.invoice._onchange_partner_id()
+        self.assertEqual(self.invoice.partner_id.id, self.partner_2.id)
