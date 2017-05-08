1c9277f6bb715a1b739408a0decacafe0a471eae
Benjamin Willig <benjamin.willig@acsone.eu>
2017-05-08 11:44:49 +0200

    [MIG] account_invoice_merge_payment to 10.0

--- account_invoice_merge_payment/models/account_invoice.py
+++ account_invoice_merge_payment/models/account_invoice.py
@@ -1,31 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#     This file is part of account_invoice_merge_payment,
-#     an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     account_invoice_merge_payment is free software:
-#     you can redistribute it and/or modify it under the terms of the GNU
-#     Affero General Public License as published by the Free Software
-#     Foundation,either version 3 of the License, or (at your option) any
-#     later version.
-#
-#     account_invoice_merge_payment is distributed
-#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#     PURPOSE.  See the GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the GNU Affero General Public License
-#     along with account_invoice_merge_payment.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, api
+# Copyright 2015-2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api
 
 
 class AccountInvoice(models.Model):
+
     _inherit = 'account.invoice'
 
     @api.model
