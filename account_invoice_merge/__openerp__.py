3ec1fc6b5c6659bac0381f04474d77835ef5dbbc
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-05 18:01:03 +0200

    [FIX] account_invoice_merge: Don't fail on multiple invoices from SO
    If you generate more than one invoice from the SO, and you try to merge
    one of these invoices with another, then there's a key error, as there
    are invoice lines not included in the merging operation.

--- account_invoice_merge/__openerp__.py
+++ account_invoice_merge/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # © 2010-2011 Ian Li <ian.li@elico-corp.com>
 # © 2015 Cédric Pigeon <cedric.pigeon@acsone.eu>
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2016-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Invoice Merge Wizard',
-    'version': '8.0.2.0.0',
+    'version': '8.0.2.0.1',
     'category': 'Finance',
     'author': "Elico Corp, "
               "ACSONE A/V, "
