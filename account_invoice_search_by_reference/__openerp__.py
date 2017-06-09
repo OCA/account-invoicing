77d2716fdabfac8a7e1d945d490bdf9cd4f96834
aheficent <ahenriquez@eficent.com>
2017-06-09 16:48:45 +0200

    account_invoice_search_by_reference

---
+++ account_invoice_search_by_reference/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account invoice search by reference",
+    "version": "9.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "category": "Invoicing",
+    "website": "http://www.eficent.com/",
+    "license": "AGPL-3",
+    "depends": [
+        "account"
+    ],
+    "installable": True,
+    "auto_install": False,
+}
