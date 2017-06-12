e04fb664b0c0293c649720a5d0ccacddc47a6354
Luis M. Ontalba <luismaront@gmail.com>
2017-06-12 13:48:12 +0200

    [MIG] account_invoice_line_description: Migrated to 10.0

---
+++ account_invoice_line_description/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Agile Business Group sagl
+# (<http://www.agilebg.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': "Account invoice line description",
+    'version': '10.0.1.0.0',
+    'category': 'Generic Modules/Accounting',
+    'author':   "Agile Business Group, "
+                "Tecnativa, "
+                "Odoo Community Association (OCA)",
+    'website': 'http://www.agilebg.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'account',
+    ],
+    "data": [
+        'security/invoice_security.xml',
+        'wizards/res_config_view.xml',
+    ],
+    'installable': True,
+}
