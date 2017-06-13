bbeba161478d6129be23cfa2ad87ce12955227e0
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-13 14:28:57 +0530

    changed analytic user to partner

--- account_invoice_analytic_search/models/account_invoice_line.py
+++ account_invoice_analytic_search/models/account_invoice_line.py
@@ -12,7 +12,9 @@ class AccountInvoiceLine(models.Model):
 
     _inherit = "account.invoice.line"
 
-    account_analytic_user_id = fields.Many2one('res.users',
-                                               string='Project Manager',
-                                               store=True,
-                                               readonly=True)
+    account_analytic_partner_id = fields.Many2one('res.partner',
+                                                  string='Project Manager',
+                                                  related='account_analytic_id'
+                                                  '.partner_id',
+                                                  store=True,
+                                                  readonly=True)
