c6b118b4a5b51b3d5df899c64d2c4342cc1f9630
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-29 17:54:30 +0530

    [ADD] account_invoice_line_sequence v10 (#282)
    * [MIG] account_invoice_line_sequence v10
    
    * [IMP] post_init_hook

---
+++ account_invoice_line_sequence/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Invoice line with sequence number',
+    'version': '10.0.1.0.0',
+    "summary": "Adds sequence field on invoice lines to manage its order.",
+    'category': 'Accounting',
+    'author': "Camptocamp, "
+              "Eficent, "
+              "Serpent CS, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.camptocamp.com',
+    'data': ['views/account_invoice_view.xml',
+             'views/report_invoice.xml'],
+    'depends': ['account'],
+    'post_init_hook': 'post_init_hook',
+    'license': 'AGPL-3',
+    'installable': True,
+}
