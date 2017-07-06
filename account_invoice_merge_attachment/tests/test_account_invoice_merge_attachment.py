e27ae9d811a88f7615d7332d4a7fedf96b505832
Benjamin Willig <benjamin.willig@acsone.eu>
2017-07-06 11:18:37 +0200

    [10.0][MIG] account_invoice_merge_attachment migration

--- account_invoice_merge_attachment/tests/test_account_invoice_merge_attachment.py
+++ account_invoice_merge_attachment/tests/test_account_invoice_merge_attachment.py
@@ -1,28 +1,61 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestAccountInvoiceMergeAttachment(TransactionCase):
 
     def setUp(self):
         super(TestAccountInvoiceMergeAttachment, self).setUp()
-        self.invoice_demo = self.env.ref('account.invoice_1')
-        self.attach_obj = self.env['ir.attachment']
+        self.AttachmentObj = self.env['ir.attachment']
+        self.InvoiceObj = self.env['account.invoice']
 
-    def test_1(self):
-        invoice1 = self.invoice_demo.copy()
-        invoice2 = self.invoice_demo.copy()
-        self.attach_obj.create({
-            'name': 'Attach1', 'datas_fname': 'Attach1',
+        self.partner = self.env.ref('base.main_partner')
+        self.product = self.env.ref('product.product_product_8')
+        self.product_account = self.product.property_account_income_id or \
+            self.product.categ_id.property_account_income_categ_id
+
+    def create_invoice(self):
+        return self.InvoiceObj.create({
+            'partner_id': self.partner.id,
+            'name': "Test",
+            'invoice_line_ids': [
+                (0, 0, {
+                    'product_id': self.product.id,
+                    'name': self.product.name,
+                    'account_id': self.product_account.id,
+                    'quantity': 1,
+                    'price_unit': 1,
+                })
+            ]
+        })
+
+    def create_invoice_attachment(self, invoice):
+        return self.AttachmentObj.create({
+            'name': 'Attach',
+            'datas_fname': 'Attach',
             'datas': 'bWlncmF0aW9uIHRlc3Q=',
-            'res_model': 'account.invoice', 'res_id': invoice1.id})
+            'res_model': 'account.invoice',
+            'res_id': invoice.id
+        })
+
+    def test_merge_invoice_attachments(self):
+        invoice1 = self.create_invoice()
+        self.create_invoice_attachment(invoice1)
+
+        invoice2 = self.create_invoice()
+        self.create_invoice_attachment(invoice2)
+        self.create_invoice_attachment(invoice2)
+
         invoices = invoice1 + invoice2
         invoices_info = invoices.with_context(link_attachment=True).do_merge()
         self.assertTrue(len(invoices_info.keys()) == 1)
-        attach = self.attach_obj.search(
-            [('res_id', 'in', invoices_info.keys()),
-             ('res_model', '=', 'account.invoice')])
-        self.assertTrue(len(attach) == 1)
+        attach = self.AttachmentObj.search([
+            ('res_id', 'in', invoices_info.keys()),
+            ('res_model', '=', 'account.invoice')
+        ])
+        self.assertEquals(
+            len(attach), 3,
+            msg="Merged invoiced should have 3 attachments")
