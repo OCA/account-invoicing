aa714d12bb13905273bdd9ff4411a9095ce9a541
Benjamin Willig <benjamin.willig@acsone.eu>
2017-07-06 11:18:37 +0200

    [10.0][MIG] account_invoice_merge_attachment migration

--- account_invoice_merge_attachment/models/account_invoice.py
+++ account_invoice_merge_attachment/models/account_invoice.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
@@ -13,13 +13,19 @@ class AccountInvoice(models.Model):
     def do_merge(self, keep_references=True, date_invoice=False):
         invoices_info = super(AccountInvoice, self).do_merge(
             keep_references=keep_references, date_invoice=date_invoice)
+
         if self.env.context.get('link_attachment'):
-            attach_obj = self.env['ir.attachment']
+            AttachmentObj = self.env['ir.attachment']
             for new_invoice_id in invoices_info:
-                attachs = attach_obj.search(
-                    [('res_model', '=', 'account.invoice'),
-                     ('res_id', 'in', invoices_info[new_invoice_id])])
+                old_invoice_ids = invoices_info[new_invoice_id]
+                attachs = AttachmentObj.search([
+                    ('res_model', '=', self._name),
+                    ('res_id', 'in', old_invoice_ids)
+                ])
                 for attach in attachs:
-                    attach.copy(default={'res_id': new_invoice_id,
-                                         'name': attach.name})
+                    attach.copy(default={
+                        'res_id': new_invoice_id,
+                        'name': attach.name
+                    })
+
         return invoices_info
