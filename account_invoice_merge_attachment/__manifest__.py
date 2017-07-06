d9e5d7963f488a29b4f67289548250adcc39365e
Benjamin Willig <benjamin.willig@acsone.eu>
2017-07-06 11:18:37 +0200

    [10.0][MIG] account_invoice_merge_attachment migration

--- account_invoice_merge_attachment/__manifest__.py
+++ account_invoice_merge_attachment/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account Invoice Merge Attachment',
     'summary': """
         Consider attachment during invoice merge process""",
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'www.acsone.eu',
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://www.acsone.eu',
     'depends': [
         'account_invoice_merge',
         'document',
