74585648cdab6fcd5fd3d8c13f42c03c34f1b2b6
Enric Tobella <etobella@creublanca.es>
2017-07-06 17:24:49 +0200

    [ADD] Account Invoice Supplier Self invoice

---
+++ account_invoice_supplier_self_invoice/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Creu Blanca
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
+
+{
+    "name": "Purchase Self Invoice",
+    "version": "11.0.1.0.0",
+    "author": "Creu Blanca, "
+              "Odoo Community Association (OCA)",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/account_invoice_views.xml",
+        "views/report_self_invoice.xml"
+    ],
+    "installable": True,
+}
---
+++ account_invoice_supplier_self_invoice/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
+
+from . import res_partner
+from . import account_invoice
