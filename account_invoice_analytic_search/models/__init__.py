ab849f96530c3dbe16bdf510adecb5b244199cf5
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-07-26 11:35:14 +0530

    [MIG] account_invoice_analytic_search v10

--- account_invoice_analytic_search/models/__init__.py
+++ account_invoice_analytic_search/models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014-2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-#   (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import account_invoice_line
 from . import account_invoice
