ee4794b9525cf8b3bf4e65d14475864b66c9cab2
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-28 09:07:41 +0200

    Update __openerp__.py

--- purchase_stock_picking_return_invoicing/__openerp__.py
+++ purchase_stock_picking_return_invoicing/__openerp__.py
@@ -6,7 +6,7 @@
 {
     "name": "Purchase Stock Picking Return Invoicing",
     "summary": "Add an option to refund returned pickings",
-    "version": "9.0.1.0.0",
+    "version": "9.0.1.1.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "Eficent, Odoo Community Association (OCA)",
