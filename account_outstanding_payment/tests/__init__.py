f0da32411ecc0621b778740997207be39b5792a8
George Daramouskas <gdaramouskas@therp.nl>
2017-09-26 14:28:59 +0200

    Revert "[FIX] Renaming module"
    This reverts commit 932c98dac76a4fa21f6f9077a8a045155b679df1.

--- account_outstanding_payment/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
-# © 2017 Odoo SA <https://www.odoo.com>
-# © 2017 OCA <https://odoo-community.org>
-# License LGPL-3 (https://www.gnu.org/licenses/lgpl-3.0.en.html).
-from . import test_account_outstanding_payments
