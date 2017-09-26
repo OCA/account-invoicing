932c98dac76a4fa21f6f9077a8a045155b679df1
George Daramouskas <gdaramouskas@therp.nl>
2017-09-26 14:20:48 +0200

    [FIX] Renaming module

---
+++ account_outstanding_payment/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2017 Therp BV <http://therp.nl>
+# © 2017 Odoo SA <https://www.odoo.com>
+# © 2017 OCA <https://odoo-community.org>
+# License LGPL-3 (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+from . import account_invoice
+from . import account_config_settings
