7cfe5eaffbd9b0c8e77d1c381c98cd0fc8633fc0
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-07 15:11:52 +0530

    [MIG] product_customer_code_invoice v10

--- product_customer_code_invoice/__manifest__.py
+++ product_customer_code_invoice/__manifest__.py
@@ -1,45 +1,24 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Author: Nicola Malcontenti <nicola.malcontenti@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013-2017 Agile Business Group sagl
+#     (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Product Customer code for account invoice",
-    "version": "1.0",
+    "summary": """Based on product_customer_code, this module loads in every
+                account invoice the customer code defined in the product,""",
+    "version": "10.0.1.0.0",
     "author": "Agile Business Group,Odoo Community Association (OCA)",
     "website": "http://www.agilebg.com",
-    "license": "AGPL-3",
     "category": "Account",
+    "license": "AGPL-3",
     "depends": [
-        'base',
-        'product',
-        'account',
-        'product_customer_code'
+        "product",
+        "account",
+        "product_supplierinfo_for_customer"
     ],
-    "description": """
-    Based on product_customer_code,
-    this module loads in every account invoice
-    the customer code defined in the product,
-    """,
-    "demo": [],
     "data": [
-        'account_invoice_view.xml',
+        "views/account_invoice_view.xml",
     ],
-    'installable': False,
-    'active': False,
+    "installable": True,
 }
