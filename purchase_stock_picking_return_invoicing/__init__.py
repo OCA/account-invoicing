af1c0ef18a02287c65949ab15c8f838715c0f066
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-12-18 13:47:25 +0100

    [IMP] purchase_stock_picking_return_invoicing: Cleanup
    Remove purchase_open_qty dependency and cleanup module and tests.

--- purchase_stock_picking_return_invoicing/__init__.py
+++ purchase_stock_picking_return_invoicing/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services
-#           <contact@eficent.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
-from . import wizards
