421963f820baa18a542ad014b419e6bc4a4c0f0d
Jacques-Etienne Baudoux <je@bcim.be>
2018-02-20 12:59:42 +0100

    account_invoice_line_default_account: search based on xmlid as name is translatable

--- account_invoice_line_default_account/models/res_partner.py
+++ account_invoice_line_default_account/models/res_partner.py
@@ -2,7 +2,27 @@
 # Copyright 2012 Therp BV (<http://therp.nl>)
 # Copyright 2013-2018 BCIM SPRL (<http://www.bcim.be>)
 
-from odoo import models, fields
+from odoo import models, fields, api
+
+
+class AccountAccountType(models.Model):
+    _inherit = 'account.account.type'
+
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        # Support xmlid in domain
+        res = []
+        if operator == '=':
+            try:
+                o = self.env.ref(name)
+                if o._name == self._name:
+                    res += o.name_get()
+            except:
+                pass
+        if not res:
+            res = super(AccountAccountType, self).name_search(
+                name=name, args=args, operator=operator, limit=limit)
+        return res
 
 
 class ResPartner(models.Model):
@@ -11,7 +31,7 @@ class ResPartner(models.Model):
     property_account_income = fields.Many2one(
         'account.account',
         string='Default Income Account',
-        domain='''[('user_type_id', '=', 'Income')]''',
+        domain="[('user_type_id', '=', 'account.data_account_type_revenue')]",
         help='Default counterpart account for sales on invoice lines',
         company_dependent=True)
     auto_update_account_income = fields.Boolean(
@@ -22,7 +42,7 @@ class ResPartner(models.Model):
     property_account_expense = fields.Many2one(
         'account.account',
         string='Default Expense Account',
-        domain='''[('user_type_id', '=', 'Expenses')]''',
+        domain="[('user_type_id', '=', 'account.data_account_type_expenses')]",
         help='Default counterpart account for purchases on invoice lines',
         company_dependent=True)
     auto_update_account_expense = fields.Boolean(
