7a9a8aab5dc57768b495ccc805bce4c4a5d9b9c9
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2018-03-05 17:37:14 +0100

    Add module enabling to reuse supplier invoice references

---
+++ account_invoice_supplier_ref_reuse/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Reuse Supplier Invoice References',
+    'version': '11.0.1.0.0',
+    'summary': 'Makes it possible to reuse supplier invoice references',
+    'author': 'Camptocamp, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.camptocamp.com',
+    'license': 'AGPL-3',
+    'category': 'Accounting & Finance',
+    'depends': [
+        'account'
+    ],
+}
