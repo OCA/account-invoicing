fc1c7678e3919a05c9eae9266530bc917743f640
Raf Ven <raf.ven@dynapps.be>
2018-03-05 20:56:44 +0100

    [11.0][MIG] account_invoice_check_total: Migration to 11.0

--- account_invoice_check_total/__manifest__.py
+++ account_invoice_check_total/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Acsone SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     'name': 'Account Invoice Check Total',
     'summary': """
         Check if the verification total is equal to the bill's total""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Acsone SA/NV,Odoo Community Association (OCA)',
     'website': 'https://acsone.eu/',
@@ -14,7 +13,7 @@
         'account',
     ],
     'data': [
-        'views/account_config_settings.xml',
+        'views/res_config_settings.xml',
         'security/account_invoice_security.xml',
         'views/account_invoice.xml',
     ],
