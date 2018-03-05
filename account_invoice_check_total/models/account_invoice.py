aa5742ee7f7fe5bc1fdc6313b9ed1d17c80a26ca
Raf Ven <raf.ven@dynapps.be>
2018-03-05 20:56:44 +0100

    [11.0][MIG] account_invoice_check_total: Migration to 11.0

--- account_invoice_check_total/models/account_invoice.py
+++ account_invoice_check_total/models/account_invoice.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Acsone SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,7 +9,6 @@ GROUP_AICT = 'account_invoice_check_total.group_supplier_inv_check_total'
 
 
 class AccountInvoice(models.Model):
-
     _inherit = 'account.invoice'
 
     check_total = fields.Monetary(
@@ -23,17 +21,19 @@ class AccountInvoice(models.Model):
     def action_move_create(self):
         for inv in self:
             if self.env.user.has_group(GROUP_AICT):
-                if inv.type in ('in_invoice', 'in_refund') and\
-                    float_compare(
-                        inv.check_total,
-                        inv.amount_total,
-                        precision_rounding=inv.currency_id.rounding) != 0:
+                if inv.type in ('in_invoice', 'in_refund') and \
+                        float_compare(
+                            inv.check_total,
+                            inv.amount_total,
+                            precision_rounding=inv.currency_id.rounding
+                        ) != 0:
                     raise UserError(_(
                         'Please verify the price of the invoice!\n\
                         The encoded total does not match the computed total.'))
         return super(AccountInvoice, self).action_move_create()
 
     @api.model
+    # pylint: disable=too-many-arguments
     def _prepare_refund(self, invoice, date_invoice=None,
                         date=None, description=None, journal_id=None):
         vals = super(AccountInvoice, self)._prepare_refund(
