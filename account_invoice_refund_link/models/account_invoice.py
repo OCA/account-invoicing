287ef6023c18014433108e8ed622ecfa76056a51
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-03-29 13:20:10 +0200

    account_invoice_refund_link: Don't choke with standard fields
    Since v11, Odoo counts with a field called `refund_invoice_ids` of type
    one2many, which conflicts with the m2m field defined in this module.
    Moreover, these fields are not needed anymore.
    
    There is only 2 OCA modules using this module right now, and they
    don't need significant changes for this adaptation, but I will do it
    when this get merged.

--- account_invoice_refund_link/models/account_invoice.py
+++ account_invoice_refund_link/models/account_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# Copyright 2014-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2014-2018 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, fields
@@ -11,30 +11,15 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     refund_reason = fields.Text(string="Refund reason")
-    origin_invoice_ids = fields.Many2many(
-        comodel_name='account.invoice', column1='refund_invoice_id',
-        column2='original_invoice_id', relation='account_invoice_refunds_rel',
-        string="Original invoice", readonly=True,
-        help="Original invoice to which this refund invoice is referred to",
-        copy=False,
-    )
-    refund_invoice_ids = fields.Many2many(
-        comodel_name='account.invoice', column1='original_invoice_id',
-        column2='refund_invoice_id', relation='account_invoice_refunds_rel',
-        string="Refund invoices", readonly=True,
-        help="Refund invoices created from this invoice",
-        copy=False,
-    )
 
     @api.model
     def _prepare_refund(self, invoice, date_invoice=None, date=None,
                         description=None, journal_id=None):
-        """Add link in the refund to the origin invoice and origin lines."""
+        """Add link in the refund to the origin invoice lines."""
         res = super(AccountInvoice, self)._prepare_refund(
             invoice, date_invoice=date_invoice, date=date,
             description=description, journal_id=journal_id,
         )
-        res['origin_invoice_ids'] = [(6, 0, invoice.ids)]
         res['refund_reason'] = description
         refund_lines_vals = res['invoice_line_ids']
         for i, line in enumerate(invoice.invoice_line_ids):
