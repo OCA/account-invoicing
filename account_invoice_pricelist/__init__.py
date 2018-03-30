0a45d593356777dc0b162bccfe3e82f5f178ab00
Raphaël Valyi <rvalyi@akretion.com>
2018-03-30 13:01:17 -0300

    [MIG] account_invoice_pricelist: Migration to 10.0

--- account_invoice_pricelist/__init__.py
+++ account_invoice_pricelist/__init__.py
@@ -1,3 +1,3 @@
 # -*- encoding: utf-8 -*-
 
-from . import model
+from . import models
--- account_invoice_pricelist/models/__init__.py
+++ account_invoice_pricelist/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- encoding: utf-8 -*-
 
 from . import account_invoice
-from . import account_invoice_line
