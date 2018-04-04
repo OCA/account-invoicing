857d8377b16dfd3a6437bb529f96da4ec9f54ed4
xavierjimenez <xavier.jimenez@qubiq.es>
2018-04-04 17:32:58 +0200

    account_invoice_triple_discount

--- account_invoice_triple_discount/__manifest__.py
+++ account_invoice_triple_discount/__manifest__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 QubiQ (http://www.qubiq.es)
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Account Invoice Triple Discount',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting & Finance',
-    'author': 'Tecnativa, '
+    'author': 'QubiQ,'
+              'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': 'https://tecnativa.com',
+    'website': 'https://odoo-community.org',
     'license': 'AGPL-3',
     'summary': 'Manage triple discount on invoice lines',
     'depends': [
