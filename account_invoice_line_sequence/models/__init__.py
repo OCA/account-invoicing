bf2e9845fb16a5938e94d90dc2cff439ac6a09c2
amcor <alberto.martin@guadaltech.es>
2018-06-11 15:46:55 +0200

    [MIG] account_invoice_line_sequence: Migration to 11.0

--- account_invoice_line_sequence/models/__init__.py
+++ account_invoice_line_sequence/models/__init__.py
@@ -1,7 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-# Copyright 2017 Serpent Consulting Services Pvt. Ltd.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import invoice
