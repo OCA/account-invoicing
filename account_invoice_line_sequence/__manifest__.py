07bac7b7afa2105f4b6adc0df995bd6e901e061b
amcor <alberto.martin@guadaltech.es>
2018-06-11 15:46:55 +0200

    [MIG] account_invoice_line_sequence: Migration to 11.0

--- account_invoice_line_sequence/__manifest__.py
+++ account_invoice_line_sequence/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA - Damien Crier, Alexandre Fayolle
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
@@ -6,14 +5,14 @@
 
 {
     'name': 'Account Invoice line with sequence number',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     "summary": "Adds sequence field on invoice lines to manage its order.",
     'category': 'Accounting',
     'author': "Camptocamp, "
               "Eficent, "
               "Serpent CS, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/OCA/account-invoicing.git',
     'data': ['views/account_invoice_view.xml',
              'views/report_invoice.xml'],
     'depends': ['account'],
