1c696da112f22b56760930f6164be23fece1d2b7
amcor <alberto.martin@guadaltech.es>
2018-06-12 12:02:19 +0200

    account_invoice_pricelist: Migration to 11.0

--- account_invoice_pricelist/__manifest__.py
+++ account_invoice_pricelist/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Account - Pricelist on Invoices',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'summary': 'Add partner pricelist on invoices',
     'category': 'Accounting & Finance',
     'author': 'GRAP,'
               'Therp BV,'
               'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.grap.coop',
+    'website': 'https://github.com/OCA/account-invoicing',
     'license': 'AGPL-3',
     'depends': [
         'account',
