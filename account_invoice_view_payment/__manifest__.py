dd97e9e43423784d2867b6c2eb36846320729e2e
Achref Mhadhbi <mhadhbi.achref@gmail.com>
2018-06-29 13:58:10 +0200

    [MIG] account_invoice_view_payment: Migration to 11.0

--- account_invoice_view_payment/__manifest__.py
+++ account_invoice_view_payment/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #        (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Account Invoice View Payment",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'summary': "Access to the payment from an invoice",
     'author': 'Eficent,'
