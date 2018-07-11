0b1b9165fb050db9631263811aa98d17bb8b730f
Isaac Gallart Bochons <igallart@puntsistemes.es>
2018-07-11 12:12:44 +0200

    [9.0][FIX] purchase_batch_invoicing: No tax compute when invoice is created

--- purchase_batch_invoicing/__openerp__.py
+++ purchase_batch_invoicing/__openerp__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Batch Invoicing",
     "summary": "Make invoices for all ready purchase orders",
-    "version": "9.0.1.0.0",
+    "version": "9.0.1.0.1",
     "category": "Purchases",
     "website": "https://tecnativa.com/",
     "author": "Tecnativa, Odoo Community Association (OCA)",
