04ada49d588cb26999d4a241af2d8f17b3354088
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-27 19:50:07 +0200

    [FIX] do not use partner pricelist, if a precise pricelist is defined on the invoice

--- account_invoice_pricelist/model/account_invoice_line.py
+++ account_invoice_pricelist/model/account_invoice_line.py
@@ -25,15 +25,18 @@ class AccountInvoiceLine(models.Model):
         context = self._context
         partner = self.env['res.partner'].browse(partner_id)
         pricelist_id = context.get('pricelist_id', False)
-        if type in ('out_invoice', 'out_refund'):
-            # Customer Invoices
-            pricelist_id = partner.property_product_pricelist.id
-        elif type in ('in_invoice', 'in_refund'):
-            # Supplier Invoices
-            if partner._model._columns.get(
-                    'property_product_pricelist_purchase', False):
-                pricelist_id =\
-                    partner.property_product_pricelist_purchase.id
+        if not pricelist_id:
+            # If pricelist is not set on invoice, or not available in the
+            # context, use the pricelist of the partner
+            if type in ('out_invoice', 'out_refund'):
+                # Customer Invoices
+                pricelist_id = partner.property_product_pricelist.id
+            elif type in ('in_invoice', 'in_refund'):
+                # Supplier Invoices
+                if partner._model._columns.get(
+                        'property_product_pricelist_purchase', False):
+                    pricelist_id =\
+                        partner.property_product_pricelist_purchase.id
 
         if not pricelist_id:
             return res
