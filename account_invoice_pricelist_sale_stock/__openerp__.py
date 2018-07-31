b2cbf9511aee5ef3f19ecd56738b4dedbe7e7aed
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-31 18:59:29 +0200

    [IMP+FIX] use correct pricelist from purchase / sale order when generating invoices

--- account_invoice_pricelist_sale_stock/__openerp__.py
+++ account_invoice_pricelist_sale_stock/__openerp__.py
@@ -2,18 +2,17 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Invoice Pricelist Sale - Stock',
+    'name': 'OBSOLETE - Account Invoice Pricelist Sale - Stock',
     'summary': 'Set pricelist from SO in invoice created from picking.',
     'author': 'Therp BV,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'website': 'http://therp.nl',
     'category': 'Sales',
-    'version': '8.0.1.0.0',
+    'version': '8.0.2.0.0',
     'depends': [
-        'account_invoice_pricelist_sale',
-        'stock',
+        'account_invoice_pricelist_stock_account',
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': False,
     'application': False,
 }
---
+++ account_invoice_pricelist_stock_account/models/stock_picking.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import models, api
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def _get_invoice_vals(self, key, inv_type, journal_id, move):
+        """Set pricelist from sale / purchase order if set on picking."""
+        inv_vals = super(StockPicking, self)._get_invoice_vals(
+            key, inv_type, journal_id, move
+        )
+        pricelist = False
+        picking = move.picking_id
+        if inv_type in ['out_invoice', 'out_refund']:
+            # Get the pricelist of the sale order, if defined
+            if self._model._columns.get('sale_id', False):
+                pricelist = picking.sale_id.pricelist_id
+
+        elif inv_type in ['in_invoice', 'in_refund']:
+            # Get the pricelist of the purchase order, if defined
+            if move._model._columns.get('purchase_line_id', False):
+                pricelist = move.purchase_line_id.order_id.pricelist_id
+
+        if not pricelist and picking.partner_id:
+            # Set the default pricelist of the partner
+            pricelist = picking.partner_id._get_pricelist_for_invoice(inv_type)
+
+        inv_vals['pricelist_id'] = pricelist and pricelist.id
+
+        return inv_vals
