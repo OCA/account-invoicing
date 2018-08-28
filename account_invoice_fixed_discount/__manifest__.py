63ebfc0fbda4c94a4bfaa5dd2d8cd73a825ce0a3
mreficent <miquel.raich@eficent.com>
2018-08-28 16:31:00 +0200

    [MIG] account_invoice_fixed_discount: Migration to 11.0

--- account_invoice_fixed_discount/__manifest__.py
+++ account_invoice_fixed_discount/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Fixed Discount",
     "summary": "Allows to apply fixed amount discounts in invoices.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://odoo-community.org/",
     "author": "Eficent, Odoo Community Association (OCA)",
