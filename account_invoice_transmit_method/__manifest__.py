fbc002ffe823f80478ca868207b520656fc21f1e
tafaRU <alex.comba@agilebg.com>
2018-09-19 09:42:28 +0200

    [FIX] account_invoice_transmit_method: fix typo Replace company_dependant by company_dependent

--- account_invoice_transmit_method/__manifest__.py
+++ account_invoice_transmit_method/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Invoice Transmit Method',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Configure invoice transmit method (email, post, portal, ...)',
