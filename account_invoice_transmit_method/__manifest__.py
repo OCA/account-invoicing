c4a2730cd8e956c8b4713d43472f85a11f3b5365
Alexis de Lattre <alexis.delattre@akretion.com>
2018-10-22 23:49:38 +0200

    account_invoice_transmit_method: use base_view_inheritance_extension to inherit context in partner form view

--- account_invoice_transmit_method/__manifest__.py
+++ account_invoice_transmit_method/__manifest__.py
@@ -4,13 +4,13 @@
 
 {
     'name': 'Invoice Transmit Method',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Configure invoice transmit method (email, post, portal, ...)',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-invoicing',
-    'depends': ['account'],
+    'depends': ['account', 'base_view_inheritance_extension'],
     'data': [
         'security/ir.model.access.csv',
         'views/transmit_method.xml',
