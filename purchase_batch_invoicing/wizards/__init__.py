214820423b5780f5b40427f682d0b54a629a5840
David <david.vidal@tecnativa.com>
2018-10-29 12:19:49 +0100

    [MIG] purchase_batch_invoicing: Migration to 11.0

--- purchase_batch_invoicing/wizards/__init__.py
+++ purchase_batch_invoicing/wizards/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import purchase_batch_invoicing
