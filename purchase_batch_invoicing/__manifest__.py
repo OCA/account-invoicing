569396ddeeb8cb36eb214d6174585f0ef932e28d
David <david.vidal@tecnativa.com>
2018-10-29 12:19:49 +0100

    [MIG] purchase_batch_invoicing: Migration to 11.0

--- purchase_batch_invoicing/__manifest__.py
+++ purchase_batch_invoicing/__manifest__.py
@@ -1,22 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Batch Invoicing",
     "summary": "Make invoices for all ready purchase orders",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Purchases",
-    "website": "https://tecnativa.com/",
+    "website": "https://www.github.com/OCA/account-invoicing",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "purchase",
     ],
-    "external_dependencies": {
-        "python": [
-            "mock",  # FIXME Remove in Python >= 3.3
-        ],
-    },
     "data": [
         "data/ir_cron_data.xml",
         "wizards/purchase_batch_invoicing_view.xml",
