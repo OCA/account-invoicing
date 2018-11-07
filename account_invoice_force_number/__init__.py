31f40e023969fddc6209fe1e442f548402a70a75
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2018-11-07 12:03:27 +0000

    Update __init__.py
    py not needed for the module

--- account_invoice_force_number/__init__.py
+++ account_invoice_force_number/__init__.py
@@ -19,4 +19,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import invoice
+# from . import invoice
