3e083f9f8c4d67f0c6e3ebe66497fa2c63a14d3e
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-16 10:31:58 +0100

    12-mig-account_invoice_search_by_reference
    [UPD] Update account_invoice_search_by_reference.pot

--- account_invoice_search_by_reference/__manifest__.py
+++ account_invoice_search_by_reference/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account invoice search by reference",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, Odoo Community Association (OCA)",
     "category": "Invoicing",
     "website": "http://www.eficent.com/",
