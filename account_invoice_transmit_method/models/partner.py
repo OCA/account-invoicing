417e862a6ea8496368259d1c0f5465aad52ac687
JoshuaJan <popkar77@gmail.com>
2019-01-06 21:43:50 +0800

    account_invoice_transmit_method: Migration to 12.0

--- account_invoice_transmit_method/models/partner.py
+++ account_invoice_transmit_method/models/partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -11,14 +10,14 @@ class ResPartner(models.Model):
 
     customer_invoice_transmit_method_id = fields.Many2one(
         'transmit.method', string='Customer Invoice Transmission Method',
-        company_dependant=True, track_visibility='onchange',
+        track_visibility='onchange',
         domain=[('customer_ok', '=', True)], ondelete='restrict')
     customer_invoice_transmit_method_code = fields.Char(
         related='customer_invoice_transmit_method_id.code',
         string='Customer Invoice Transmission Method Code', readonly=True)
     supplier_invoice_transmit_method_id = fields.Many2one(
         'transmit.method', string='Vendor Invoice Reception Method',
-        company_dependant=True, track_visibility='onchange',
+        track_visibility='onchange',
         domain=[('supplier_ok', '=', True)], ondelete='restrict')
     supplier_invoice_transmit_method_code = fields.Char(
         related='supplier_invoice_transmit_method_id.code',
