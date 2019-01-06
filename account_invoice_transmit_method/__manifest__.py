012fdb3294f70969c9ccb37a60cafbb6d77dfb87
JoshuaJan <popkar77@gmail.com>
2019-01-06 21:43:50 +0800

    account_invoice_transmit_method: Migration to 12.0

--- account_invoice_transmit_method/__manifest__.py
+++ account_invoice_transmit_method/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Invoice Transmit Method',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Configure invoice transmit method (email, post, portal, ...)',
-    'author': 'Akretion,Odoo Community Association (OCA)',
+    'author': 'Akretion, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-invoicing',
     'depends': ['account'],
     'data': [
---
+++ account_invoice_transmit_method/readme/USAGE.rst
@@ -0,0 +1,6 @@
+On the form view of a parent Partner (not a Contact), in the *Accounting* tab, there are 2 fields:
+
+* Customer Invoice Transmission Method
+* Vendor Invoice Reception Method
+
+When you create an invoice, this value is automatically copied on the invoice (and can be modified).
