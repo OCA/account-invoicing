c840d2d06d195ec5063e258aa07053c8022ed6b8
Bhaviraj-Acespritech <bhaviraj,acespritech@gmail.com>
2019-01-19 12:37:27 +0530

    [MIG] account_invoice_view_payment v11 to v12

--- account_invoice_view_payment/__manifest__.py
+++ account_invoice_view_payment/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "Account Invoice View Payment",
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'summary': "Access to the payment from an invoice",
     'author': 'Eficent,'
