8f92b53f5040c0a426b25b1cfd36cc929b5a8699
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-27 21:48:20 +0530

    [MIG] account_invoice_refund_link v11 to v12

--- account_invoice_refund_link/models/account_invoice.py
+++ account_invoice_refund_link/models/account_invoice.py
@@ -33,16 +33,20 @@ class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
     origin_line_ids = fields.Many2many(
-        comodel_name='account.invoice.line', column1='refund_line_id',
-        column2='original_line_id', string="Original invoice line",
+        comodel_name='account.invoice.line',
+        column1='refund_line_id',
+        column2='original_line_id',
+        string="Original invoice line",
         relation='account_invoice_line_refunds_rel',
         help="Original invoice line to which this refund invoice line "
              "is referred to",
         copy=False,
     )
     refund_line_ids = fields.Many2many(
-        comodel_name='account.invoice.line', column1='original_line_id',
-        column2='refund_line_id', string="Refund invoice line",
+        comodel_name='account.invoice.line',
+        column1='original_line_id',
+        column2='refund_line_id',
+        string="Refund invoice line",
         relation='account_invoice_line_refunds_rel',
         help="Refund invoice lines created from this invoice line",
         copy=False,
