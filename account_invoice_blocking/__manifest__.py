61c581cc102b479ffd68e7a647cd1af80bfded1f
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-27 22:27:58 +0530

    [MIG] account_invoice_blocking v11 to v12

--- account_invoice_blocking/__manifest__.py
+++ account_invoice_blocking/__manifest__.py
@@ -4,12 +4,14 @@
 {
     'name': 'Account Invoice Blocking',
     'summary': 'Set a blocking (No Follow-up) flag on invoices',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'author': 'Acsone SA/NV,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/account-invoicing/',
-    'depends': ['account'],
+    'depends': [
+        'account'
+    ],
     'data': [
         'views/account_invoice.xml',
     ],
