cb7b9d465f19d5b8248be98f888bf1fb14457c9a
Simone <simone.rubino88@gmail.com>
2019-01-31 23:34:58 +0100

    [IMP] l10n_it_corrispettivi: Use Corrispettivi report for portal preview

---
+++ l10n_it_corrispettivi/controllers/main.py
@@ -0,0 +1,12 @@
+# Copyright 2019 Simone Rubino
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.addons.portal.controllers.portal import CustomerPortal
+
+
+class CorrispettiviPortal(CustomerPortal):
+
+    def _show_report(self, model, report_type, report_ref, download=False):
+        if model._name == 'account.invoice' and model.corrispettivo:
+            report_ref = 'l10n_it_corrispettivi.account_corrispettivi'
+        return super()._show_report(model, report_type, report_ref, download)
