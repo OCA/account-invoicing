9c51ad47e8ef366f56a1131025b859d921492f64
Jon Erik Ceberio <jonerikceberio@digital5.es>
2019-02-15 16:04:38 +0100

    [IMP] purchase_invoicing_no_zero_line: name change

--- purchase_invoicing_no_zero_line/__manifest__.py
+++ purchase_invoicing_no_zero_line/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Digital5 S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Purchase invoice no zero line",
+    "name": "Purchase invoicing no zero line",
     "summary": "Avoid the creation of zero quantity invoice lines from purchase",
     "version": "11.0.1.0.0",
     "category": "Purchase Management",
