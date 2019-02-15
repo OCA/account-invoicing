5c1769a353ae186f6480631cb15b74e3b47c2ace
Jon Erik Ceberio <jonerikceberio@digital5.es>
2019-02-15 16:31:28 +0100

    [FIX] purchase_invoicing_no_zero_line: minor fixes, translations

--- purchase_invoicing_no_zero_line/models/account_journal.py
+++ purchase_invoicing_no_zero_line/models/account_journal.py
@@ -2,7 +2,7 @@
 # Copyright 2019 Digital5 S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class AccountJournal(models.Model):
@@ -10,5 +10,6 @@ class AccountJournal(models.Model):
 
     avoid_zero_lines = fields.Boolean(
         string="Avoid zero lines?",
-        help="If this option is checked, the purchase invoices in this journal will have no lines with zero quantity"
+        help="If this option is checked, the purchase invoices in this journal"
+             " will have no lines with zero quantity"
     )
