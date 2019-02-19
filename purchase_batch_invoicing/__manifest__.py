0a812c5b7eb9d2827a127f1913122c3fc8eac634
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-02-19 13:08:14 +0100

    [11.0][FIX] purchase_batch_invoicing: When create invoice from batch wizard taxes are not computed

--- purchase_batch_invoicing/__manifest__.py
+++ purchase_batch_invoicing/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Purchase Batch Invoicing",
     "summary": "Make invoices for all ready purchase orders",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "category": "Purchases",
     "website": "https://www.github.com/OCA/account-invoicing",
     "author": "Tecnativa, Odoo Community Association (OCA)",
