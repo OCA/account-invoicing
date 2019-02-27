5b12df7ba7f22e6381c961e3893a07cc0975ef95
François Honoré <francois.honore@acsone.eu>
2019-02-27 16:35:55 +0100

    [10.0][MIG] Add stock_picking_invoicing (from existing open MR in 9.0) + [IMP] Improve way to inherit the group key (to create invoices) + [IMP] Improve links to invoices (M2M) + [IMP] Improve the code (about possibilities, functions, tests) and fix some bugs + [UPD] Update french translations

---
+++ stock_picking_invoicing/models/stock_picking.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, api, fields
+
+
+class StockPicking(models.Model):
+    _name = "stock.picking"
+    _inherit = [
+        _name,
+        "stock.invoice.state.mixin",
+    ]
+
+    invoice_ids = fields.Many2many(
+        comodel_name="account.invoice",
+        relation="stock_picking_account_invoice_m2m",
+        column1="picking_id",
+        column2="invoice_id",
+        readonly=True,
+        copy=False,
+    )
+
+    @api.multi
+    def set_to_be_invoiced(self):
+        """
+        Update invoice_state of current pickings to "2binvoiced".
+        :return: dict
+        """
+        self._set_as_2binvoiced()
+        return {}
+
+    @api.multi
+    def _set_as_2binvoiced(self):
+        """
+        Inherit to also update related moves.
+        :return: bool
+        """
+        self.mapped("move_lines")._set_as_2binvoiced()
+        return super(StockPicking, self)._set_as_2binvoiced()
+
+    @api.multi
+    def _set_as_invoiced(self):
+        """
+        Inherit to also update related moves.
+        :return: bool
+        """
+        self.mapped("move_lines")._set_as_invoiced()
+        return super(StockPicking, self)._set_as_invoiced()
+
+    @api.multi
+    def _get_partner_to_invoice(self):
+        self.ensure_one()
+        partner = self.partner_id
+        return partner.address_get(['invoice']).get('invoice')
