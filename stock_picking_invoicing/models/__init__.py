06a552db3ea5ebf0838fe43283b7b4fe981acd52
François Honoré <francois.honore@acsone.eu>
2019-02-27 16:35:55 +0100

    [10.0][MIG] Add stock_picking_invoicing (from existing open MR in 9.0) + [IMP] Improve way to inherit the group key (to create invoices) + [IMP] Improve links to invoices (M2M) + [IMP] Improve the code (about possibilities, functions, tests) and fix some bugs + [UPD] Update french translations

--- stock_picking_invoicing/models/__init__.py
+++ stock_picking_invoicing/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2016 <OCA>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import stock
+# Copyright (C) 2019-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import stock_invoice_state_mixin
 from . import account_invoice
+from . import account_invoice_line
+from . import stock_move
+from . import stock_picking
---
+++ stock_picking_invoicing/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to create invoices directly from picking, without having to
+use sale or purchase orders.
