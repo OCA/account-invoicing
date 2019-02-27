2760f9eea0da8b10e5836e2b2af4b535a98ca86f
François Honoré <francois.honore@acsone.eu>
2019-02-27 16:35:55 +0100

    [10.0][MIG] Add stock_picking_invoicing (from existing open MR in 9.0) + [IMP] Improve way to inherit the group key (to create invoices) + [IMP] Improve links to invoices (M2M) + [IMP] Improve the code (about possibilities, functions, tests) and fix some bugs + [UPD] Update french translations

---
+++ stock_picking_invoicing/models/stock_move.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019-Today: Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models, api, fields
+
+
+class StockMove(models.Model):
+    _name = "stock.move"
+    _inherit = [
+        _name,
+        "stock.invoice.state.mixin",
+    ]
+
+    invoice_line_ids = fields.Many2many(
+        comodel_name='account.invoice.line',
+        relation="stock_move_account_move_line_m2m",
+        column1="stock_move_id",
+        column2="invoice_line_id",
+        string='Invoice Line',
+        readonly=True,
+    )
+
+    @api.multi
+    def _get_taxes(self, fiscal_position):
+        """
+        Map product taxes based on given fiscal position
+        :param fiscal_position: account.fiscal.position recordset
+        :return: account.tax recordset
+        """
+        product = self.mapped("product_id")
+        product.ensure_one()
+        taxes = product.taxes_id
+        company_id = self.env.context.get(
+            'force_company', self.env.user.company_id.id)
+        my_taxes = taxes.filtered(lambda r: r.company_id.id == company_id)
+        return fiscal_position.map_tax(my_taxes)
+
+    @api.model
+    def _get_account(self, fiscal_position, account):
+        """
+        Map the given account with given fiscal position
+        :param fiscal_position: account.fiscal.position recordset
+        :param account: account.account recordset
+        :return: account.account recordset
+        """
+        return fiscal_position.map_account(account)
+
+    @api.multi
+    def _get_price_unit_invoice(self, inv_type, partner, qty=1):
+        """
+        Gets price unit for invoice
+        :param inv_type: str
+        :param partner: res.partner
+        :param qty: float
+        :return: float
+        """
+        product = self.mapped("product_id")
+        product.ensure_one()
+        if inv_type in ('in_invoice', 'in_refund'):
+            result = product.price
+        else:
+            # If partner given, search price in its sale pricelist
+            if partner and partner.property_product_pricelist:
+                product = product.with_context(
+                    partner=partner.id,
+                    quantity=qty,
+                    pricelist=partner.property_product_pricelist.id,
+                    uom=fields.first(self).product_uom.id
+                )
+                result = product.price
+            else:
+                result = product.lst_price
+        return result
