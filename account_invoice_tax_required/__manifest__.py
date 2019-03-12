d6c1d05657458c7fe0b7cdc9a6f9dd61f23c0202
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-03-12 13:08:31 +0100

    [FIX] account_invoice_tax_required: Fix tests

--- account_invoice_tax_required/__manifest__.py
+++ account_invoice_tax_required/__manifest__.py
@@ -3,11 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Tax required in invoice",
-<<<<<<< HEAD
     'version': "12.0.1.0.0",
-=======
-    'version': "11.0.1.0.1",
->>>>>>> 1d654401... [FIX] account_invoice_tax_required: Avoid gaps in sequence
     "author": "Camptocamp,Tecnativa,Odoo Community Association (OCA)",
     'website': "https://github.com/OCA/account-invoicing",
     'category': "Localization / Accounting",
