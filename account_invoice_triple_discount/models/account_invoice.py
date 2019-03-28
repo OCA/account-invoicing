a52df5b8c0e3fdeaeab87f0e7661bef88271357f
SimoRubi <simone.rubino@agilebg.com>
2019-03-28 14:03:23 +0100

    [FIX] account_invoice_triple_discount: Fix cache management during onchange Behavior before this commit:
    Create an invoice with one line: qty 1, unit price 100, discount 0, tax 22% -> tax amount is 22 OK
    Edit discount to 50 -> tax amount is still 22 KO
    Edit the discount back to 0 -> tax amount becomes 11 KO
    Behavior after this commit:
    
    Create an invoice with one line: qty 1, unit price 100, discount 0, tax 22% -> tax amount is 22 OK
    Edit discount to 50 -> tax amount is 11 OK
    Edit the discount back to 0 -> tax amount becomes 22 OK

--- account_invoice_triple_discount/models/account_invoice.py
+++ account_invoice_triple_discount/models/account_invoice.py
@@ -54,7 +54,9 @@ class AccountInvoiceLine(models.Model):
         Resetting discount2 and discount3 to 0.0 avoids issues if
         this method is called multiple times."""
         prev_values = dict()
-        self.invalidate_cache(['discount', 'discount2', 'discount3'], self.ids)
+        if not self.env.in_onchange:
+            self.invalidate_cache(
+                ['discount', 'discount2', 'discount3'], self.ids)
         for line in self:
             prev_values[line] = dict(
                 discount=line.discount,
