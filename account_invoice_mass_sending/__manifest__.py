0c42ac3b3d59e90725443722c3cb73f364c60059
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2019-03-29 15:18:02 +0100

    [12.0][ADD] account_invoice_mass_sending
    Add a new addon which allow to send invoice in mass mode

---
+++ account_invoice_mass_sending/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Mass Sending",
+    "description": """
+        This addon adds a mass sending feature on invoices.""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": [
+        # ODOO
+        "account",
+        # OCA
+        "queue_job",
+        "web_notify",
+    ],
+    "data": ["views/account_invoice_views.xml"],
+}
