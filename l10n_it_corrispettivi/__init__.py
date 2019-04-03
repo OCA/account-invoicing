31ed23798b38c3aacf5f4e250a46c2143882aa3a
SimoRubi <simone.rubino@agilebg.com>
2019-04-03 17:47:05 +0200

    [IMP] l10n_it_corrispettivi: Show the corrispettivi tree view as a result of refund wizard

--- l10n_it_corrispettivi/__init__.py
+++ l10n_it_corrispettivi/__init__.py
@@ -2,4 +2,5 @@
 
 from . import controllers
 from . import models
+from . import wizards
 
---
+++ l10n_it_corrispettivi/wizards/account_invoice_refund.py
@@ -0,0 +1,29 @@
+#  -*- coding: utf-8 -*-
+#  Copyright 2019 Simone Rubino - Agile Business Group
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+from odoo.tools.safe_eval import safe_eval
+
+
+class AccountInvoiceRefund(models.TransientModel):
+    _inherit = "account.invoice.refund"
+
+    @api.multi
+    def compute_refund(self, mode='refund'):
+        res = super(AccountInvoiceRefund, self).compute_refund(mode=mode)
+        if isinstance(res, dict):
+            invoice_ids_domain = res['domain'][-1]
+            invoices = self.env['account.invoice'].search([invoice_ids_domain])
+            if all(not inv.corrispettivo for inv in invoices):
+                return res
+
+            # Some of the created refunds are corrispettivi:
+            # show the corrispettivi tree view
+            corr_action = self.env.ref(
+                'l10n_it_corrispettivi.action_corrispettivi_tree1').read()[0]
+            corr_domain = safe_eval(corr_action['domain'])
+            corr_domain.append(invoice_ids_domain)
+            corr_action['domain'] = corr_domain
+            return corr_action
+        return res
