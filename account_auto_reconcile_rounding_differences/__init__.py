260712abc4bb470e247aa78d39525873f561ef6c
Wolfgang Pichler <wpichler@callino.at>
2019-05-06 09:58:20 +0200

    [FIX] Fixed travis errors / warnings

--- account_auto_reconcile_rounding_differences/__init__.py
+++ account_auto_reconcile_rounding_differences/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2019, Wolfgang Pichler, Callino
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import models
+from . import models
--- account_auto_reconcile_rounding_differences/models/__init__.py
+++ account_auto_reconcile_rounding_differences/models/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2019, Wolfgang Pichler, Callino
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import account_payment
-import company
-import account_move
-import res_config
+from . import account_payment
+from . import company
+from . import account_move
+from . import res_config
