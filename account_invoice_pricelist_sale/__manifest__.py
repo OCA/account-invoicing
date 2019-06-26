1157a450e3065576e7c37ec46bcc2d052d2b2319
David Beal <david.beal@akretion.com>
2019-06-26 12:41:37 +0200

    fixup! typo

--- account_invoice_pricelist_sale/__manifest__.py
+++ account_invoice_pricelist_sale/__manifest__.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Invoice Pricelist - Sale',
-    'summary': 'Module to fill pricelist from sales order in invoice.',
-    'author': 'Therp BV,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'http://therp.nl',
-    'category': 'Sales',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'account_invoice_pricelist',
-        'sale_management',
+    "name": "Account Invoice Pricelist - Sale",
+    "summary": "Module to fill pricelist from sales order in invoice.",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-invoicing",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "account_invoice_pricelist",
+        "sale_management",
     ],
-    'installable': True,
-    'auto_install': True,
-    'application': False,
+    "installable": True,
+    "auto_install": True,
 }
