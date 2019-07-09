fde07de67ae5b532d4e578d61ce6d204ea632bb9
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-07-09 17:48:55 +0200

    [12.0][MIG] purchase_stock_picking_return_invoicing

--- purchase_stock_picking_return_invoicing/__manifest__.py
+++ purchase_stock_picking_return_invoicing/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services
+# Copyright 2019 Eficent Business and IT Consulting Services
 # Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Purchase Stock Picking Return Invoicing",
     "summary": "Add an option to refund returned pickings",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "Eficent,"
@@ -16,7 +15,7 @@
     "installable": True,
     "development_status": "Mature",
     "depends": [
-        "purchase",
+        "purchase_stock",
     ],
     "data": [
         "views/purchase_view.xml",
