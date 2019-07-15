b0f4c9d323226e886173d2afc2cab197c97819e3
Magno Costa <magno.costa@akretion.com.br>
2019-07-15 15:27:22 -0300

    [MIG] stock_picking_invoicing: Migration to 12.0, changed version and removed unnecessary statement in python 3.

--- stock_picking_invoicing/models/__init__.py
+++ stock_picking_invoicing/models/__init__.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import stock_invoice_state_mixin
 from . import account_invoice
 from . import account_invoice_line
