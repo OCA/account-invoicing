7e6469839da0a443d2a31aefab1e08fb082a0dba
Magno Costa <magno.costa@akretion.com.br>
2019-07-15 15:27:22 -0300

    [MIG] stock_picking_invoicing: Migration to 12.0, changed version and removed unnecessary statement in python 3.

--- stock_picking_invoicing/models/stock_move.py
+++ stock_picking_invoicing/models/stock_move.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import models, api, fields
 
 
