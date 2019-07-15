df147ee8072a688adcad31ef6cf45557f6851e42
Magno Costa <magno.costa@akretion.com.br>
2019-07-15 15:27:22 -0300

    [MIG] stock_picking_invoicing: Migration to 12.0, changed version and removed unnecessary statement in python 3.

--- stock_picking_invoicing/__manifest__.py
+++ stock_picking_invoicing/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Stock Picking Invoicing",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Warehouse Management',
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/account-invoicing',
