a792b6ce2bb1f4d4c74d0f5344d7a718b4a94786
Magno Costa <magno.costa@akretion.com.br>
2019-07-15 15:27:22 -0300

    [MIG] stock_picking_invoicing: Migration to 12.0, changed version and removed unnecessary statement in python 3.

--- stock_picking_invoicing/models/account_invoice_line.py
+++ stock_picking_invoicing/models/account_invoice_line.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
 
 
