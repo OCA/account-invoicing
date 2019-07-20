95fa3b91811bba00def2d1187336e4889d34d14c
scampbell <scampbell@opensourceintegrators.com>
2019-07-20 14:11:47 -0700

    [ADD] account_brand

---
+++ account_brand/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Brand",
+    "summary": "Send branded invoices and refunds",
+    "version": "12.0.1.0.0",
+    "category": "Accounting Management",
+    "website": "https://github.com/OCA/account-invoicing",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        'account',
+        'partner_brand',
+    ],
+    "data": [
+        "views/account_invoice.xml",
+        "views/report_template.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["osi-scampbell"],
+}
---
+++ account_brand/readme/USAGE.rst
@@ -0,0 +1,6 @@
+To use this module, you need to:
+
+#. Go to Accounting > Customers > Invoices
+#. Select or create an invoice
+#. Enter the information and select the brand
+#. Print the PDF report. It includes the information of the brand.
