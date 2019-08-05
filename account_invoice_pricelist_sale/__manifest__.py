a55c80142b0edee81a86fbc50636d5647376ec14
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-06 00:50:59 +0200

    [PORT] account_invoice_pricelist_sale in 12.0

--- account_invoice_pricelist_sale/__manifest__.py
+++ account_invoice_pricelist_sale/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Invoice Pricelist - Sale',
     'summary': 'Module to fill pricelist from sales order in invoice.',
-    'author': 'Therp BV,Odoo Community Association (OCA)',
+    'author': 'Therp BV,GRAP,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
-    'website': 'http://therp.nl',
+    'website': 'https://github.com/OCA/account-invoicing',
     'category': 'Sales',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': [
         'account_invoice_pricelist',
         'sale',
