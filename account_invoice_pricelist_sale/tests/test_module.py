f88448fd0fcf6fcc9a8c7decdca09f8cc6618073
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-06 00:50:59 +0200

    [PORT] account_invoice_pricelist_sale in 12.0

---
+++ account_invoice_pricelist_sale/tests/test_module.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo.tests.common import TransactionCase
+
+
+class TestModule(TransactionCase):
+
+    def setUp(self):
+        super(TestModule, self).setUp()
+        self.partner = self.env.ref('base.res_partner_12')
+        self.product = self.env.ref('sale.advance_product_0')
+
+    def test_main(self):
+        # Create Pricelist
+        pricelist = self.env['product.pricelist'].create({
+            'name': 'Demo Pricelist',
+        })
+        # Create Product
+        order = self.env['sale.order'].create({
+            'partner_id': self.partner.id,
+            'pricelist_id': pricelist.id,
+            'order_line': [
+                (0, 0, {
+                    'name': self.product.name,
+                    'product_id': self.product.id,
+                    'product_uom_qty': 5,
+                    'product_uom': self.product.uom_id.id,
+                    'price_unit': self.product.list_price,
+                }),
+            ],
+        })
+        order.action_confirm()
+        invoice_id_list = order.action_invoice_create()
+        invoice = self.env['account.invoice'].browse(invoice_id_list)[0]
+        self.assertEqual(
+            invoice.pricelist_id, order.pricelist_id,
+            "Invoice Pricelist has not been recovered from sale order")
