f50e9a43855793eb9c9953efbfd25bae6e0d499d
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-20 14:29:05 +0200

    Update account_invoice_supplierinfo_update/__manifest__.py
    Co-Authored-By: Pierrick Brun <PierrickBrun@users.noreply.github.com>

--- account_invoice_supplierinfo_update/__manifest__.py
+++ account_invoice_supplierinfo_update/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': 'In the supplier invoice, automatically updates all products '
                'whose unit price on the line is different from '
                'the supplier price',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
     'website': 'https://github.com/OCA/account-invoicing',
     'author':
