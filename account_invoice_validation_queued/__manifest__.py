672a384b5c0b21ac8137bb0fe634d0fd77c1f356
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-08-30 01:20:41 +0200

    [ADD] account_invoice_validation_queued: Enqueue account invoice validation
    This module allows to enqueue in several jobs the account validation process
    to be executed in paralell on background, which is normally done serially and
    on foreground.
    
    Each invoice creates a job for performing the validation, but you will only be
    able to enqueue invoices for the same date.
    
    Installation
    ============
    
    This module depends on *queue_job* module that is hosted on
    https://github.com/OCA/queue.
    
    Configuration
    =============
    
    Jobs are enqueued in the channel ``root.account_invoice_validation_queued``,
    so you must adjust your
    `Odoo configuration <https://github.com/OCA/queue/tree/11.0/queue_job#configuration>`_
    according this.
    
    If you want to see queued jobs, you need "Job Queue / Job Queue Manager"
    permission in your user.
    
    Usage
    =====
    
    * Go to *Invoicing > Sales > Documents > Customer Invoices* or
      *Invoicing > Purchases > Documents > Vendor Bills*.
    * Mark at least one check on the left part of one draft invoice line in the
      list view.
    * Click on *Action > Confirm Draft Invoice*.
    * On the dialog popup that appears, click on "Enqueue Validation".
    * If any of the selected invoices are not in draft state, you will receive
      an error.
    * If any of the invoices is already enqueued, there will be a message saying
      so and avoiding to perform the process.
    * Once enqueued, and having the "Job Queue Manager" permission, you can go to
      the invoice, and see the tab "Validation Jobs". A list with all the jobs
      related to that invoice can be found there.
    
    Known issues / Roadmap
    ======================
    
    * Allow to enqueue for validating invoices of different date.
    * There's a chance that if you perform several enqueues of invoice validations
      from different dates, the order of the validated invoices, and thus, the
      number given for it, will result disordered.

---
+++ account_invoice_validation_queued/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Enqueue account invoice validation',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'author': 'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/account-invoicing',
+    'depends': [
+        'account',
+        'queue_job',
+    ],
+    'data': [
+        'views/queue_job_views.xml',
+        'views/account_invoice_views.xml',
+        'wizards/account_invoice_confirm_views.xml',
+    ],
+    'installable': True,
+}
