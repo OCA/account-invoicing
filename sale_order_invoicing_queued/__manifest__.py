2e0923cf3aec061e74e8496c75772f6baf8d1649
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-09 16:35:45 +0200

    [IMP] *_queued: Remove `cancel` method + use same user

--- sale_order_invoicing_queued/__manifest__.py
+++ sale_order_invoicing_queued/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Enqueue sales order invoicing',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'author': 'Tecnativa, '
