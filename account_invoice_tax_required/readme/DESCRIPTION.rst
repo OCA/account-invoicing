efba10e86426f0cad9dde588cb0c416ee5d39ae1
Juan Vicente Pascual <jvpascual@puntsistemes.es>
2019-10-06 10:20:26 +0200

    [MIG] account_invoice_tax_required: Migration to 13.0
    [MIG] account_invoice_tax_required: Migration to 13.0
    
    [MIG] account_invoice_tax_required: Migration to 13.0
    
    [MIG] account_invoice_tax_required: Migration to 13.0

--- account_invoice_tax_required/readme/DESCRIPTION.rst
+++ account_invoice_tax_required/readme/DESCRIPTION.rst
@@ -1,2 +1,2 @@
-This module adds functional a check on invoice
+This module adds a check on invoice
 to force user to set tax on invoice line.
--- account_invoice_tax_required/tests/__init__.py
+++ account_invoice_tax_required/tests/__init__.py
@@ -1,3 +1,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import test_account_invoice_tax_required
+from . import test_account_move_tax_required
