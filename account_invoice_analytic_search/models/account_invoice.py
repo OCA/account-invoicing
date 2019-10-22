430269581d3407eaadde4b7ff539a73c54f494e1
ahenriquez <ahenriquez@eficent.com>
2019-10-22 18:12:39 +0200

    [MIG]account_invoice_analytic_search to v12

--- account_invoice_analytic_search/models/account_invoice.py
+++ account_invoice_analytic_search/models/account_invoice.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -10,20 +9,21 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     @api.multi
-    @api.depends('invoice_line_ids.account_analytic_id')
+    @api.depends("invoice_line_ids.account_analytic_id")
     def _compute_analytic_accounts(self):
         for invoice in self:
-            invoice.account_analytic_ids =\
-                invoice.mapped('invoice_line_ids.account_analytic_id.id')
+            invoice.account_analytic_ids = invoice.mapped(
+                "invoice_line_ids.account_analytic_id.id"
+            )
 
     @api.multi
     def _search_analytic_accounts(self, operator, value):
-        return [('invoice_line_ids.account_analytic_id', operator, value)]
+        return [("invoice_line_ids.account_analytic_id", operator, value)]
 
     account_analytic_ids = fields.Many2many(
-        comodel_name='account.analytic.account',
-        compute='_compute_analytic_accounts',
-        search='_search_analytic_accounts',
-        string='Analytic Account',
-        readonly=True
+        comodel_name="account.analytic.account",
+        compute="_compute_analytic_accounts",
+        search="_search_analytic_accounts",
+        string="Analytic Account",
+        readonly=True,
     )
