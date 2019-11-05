fff9e3b4b121d14bf59b85082a1cc6bec897b466
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-05 01:06:48 +0100

    [MIG] sale_order_invoicing_queued: Migration to 13.0
    * Standard procedure
    * README updated
    * Manifest version updated
    * Adjusted code to v13

--- sale_order_invoicing_queued/models/sale_order.py
+++ sale_order_invoicing_queued/models/sale_order.py
@@ -3,29 +3,20 @@
 
 from odoo import fields, models
 
-try:  # The try can be removed in v12 as not needed
-    from odoo.addons.queue_job.job import job
-except ImportError:
-    import logging
-    import functools
-
-    logging.getLogger(__name__).debug("Can't `import queue_job`.")
-
-    def job(*argv, **kwargs):
-        return functools.partial
+from odoo.addons.queue_job.job import job
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     invoicing_job_ids = fields.Many2many(
-        comodel_name='queue.job',
-        column1='order_id',
-        column2='job_id',
+        comodel_name="queue.job",
+        column1="order_id",
+        column2="job_id",
         string="Invoicing Jobs",
         copy=False,
     )
 
-    @job(default_channel='root.sale_order_invoicing_queued')
+    @job(default_channel="root.sale_order_invoicing_queued")
     def create_invoices_job(self, final):
-        self.action_invoice_create(final=final)
+        self._create_invoices(final=final)
