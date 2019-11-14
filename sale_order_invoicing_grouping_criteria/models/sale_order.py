0bbaabd5bd15c7aee06222134692c7cf49e5e49d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-14 22:27:08 +0100

    [ADD] sale_order_invoicing_grouping_criteria: Sales order invoicing grouping criteria
    This module allows to use custom criteria for grouping sales orders to be
    invoiced.
    
    Default criteria for grouping (invoicing partner and currency used) will be
    always applied, as if not respected, there will be business inconsistencies,
    but you can add more fields to split the invoicing according them.
    
    Configuration
    =============
    
    For creating new grouping criteria:
    
    1. Go to *Invoicing > Configuration > Management > Invoicing Grouping Criteria*.
    2. Create or modify existing criteria, selecting fields belonging to "Sales
       Order" header model for grouping according them.
    3. Invoicing address and currency will always be applied with the selected
       ones.
    
    For setting a different grouping criteria for a specific customer:
    
    1. Go to *Invoicing > Sales > Master Data > Customers*.
    2. Open the desired customer.
    3. Go to *Invoicing* page.
    4. Set on "Sales Invoicing Grouping Criteria" the desired grouping
       criteria.
    
    For setting a different default grouping criteria than the standard for the
    whole company:
    
    1. Go to *Invoicing > Configuration> Settings*.
    2. Locate inside "Sales Order Invoicing" section, the field "Default
       Grouping Criteria".
    3. Introduce there the grouping criteria to be applied by default. If empty,
       the general default of invoicing address + currency will be applied.
    
    Usage
    =====
    
    1. Go to *Sales > Invoicing > Orders to Invoice*.
    2. Select sales orders whose invoicing you want to do.
    3. Click on *Action > Invoice Order*.
    4. Click on "Create and View Invoices" button.
    5. On that moment, the grouping criteria will be applied, and you will see
       different invoices if the criteria doesn't match for them.

---
+++ sale_order_invoicing_grouping_criteria/models/sale_order.py
@@ -0,0 +1,34 @@
+# Copyright 2019 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _get_sale_invoicing_group_key(self):
+        self.ensure_one()
+        group_key = [self.partner_invoice_id.id, self.currency_id.id]
+        criteria = (
+            self.partner_id.sale_invoicing_grouping_criteria_id or
+            self.company_id.default_sale_invoicing_grouping_criteria_id
+        )
+        for field in criteria.field_ids:
+            group_key.append(self[field.name])
+        return tuple(group_key)
+
+    def action_invoice_create(self, grouped=False, final=False):
+        """Slice the batch according grouping criteria."""
+        order_groups = {}
+        for order in self:
+            group_key = order._get_sale_invoicing_group_key()
+            if group_key not in order_groups:
+                order_groups[group_key] = order
+            else:
+                order_groups[group_key] += order
+        invoice_ids = []
+        for group in order_groups.values():
+            invoice_ids += super(SaleOrder, group).action_invoice_create(
+                grouped=grouped, final=final)
+        return invoice_ids
