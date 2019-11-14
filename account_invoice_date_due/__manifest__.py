75344aff661f6371bda28077218c62a69c2be492
Luis Torres <luis_t@vauxoo.com>
2019-11-14 01:04:51 +0000

    [ADD] account_invoice_date_due: Date Due now could be updated
    Allow update the date due in an invoice (Even if it's done). To avoid
    cancel the invoice and generate again

---
+++ account_invoice_date_due/__manifest__.py
@@ -0,0 +1,19 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Update Invoice's Due Date",
+    "version": "12.0.1.0.0",
+    "author": "Vauxoo,Odoo Community Association (OCA)",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "demo": [],
+    "data": [
+        "security/security.xml",
+        "wizards/update_due_date_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
