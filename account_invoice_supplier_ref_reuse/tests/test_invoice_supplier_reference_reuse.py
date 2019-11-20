590101f294ee113c4a82435db3e037e746a28023
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-20 09:11:30 -0500

    [IMP] account_invoice_supplier_ref_reuse: black, isort

--- account_invoice_supplier_ref_reuse/tests/test_invoice_supplier_reference_reuse.py
+++ account_invoice_supplier_ref_reuse/tests/test_invoice_supplier_reference_reuse.py
@@ -5,7 +5,6 @@ from odoo.tests.common import SavepointCase
 
 
 class TestInvoiceSupplierReferenceReuse(SavepointCase):
-
     @classmethod
     def setUpClass(self):
         super().setUpClass()
@@ -13,35 +12,49 @@ class TestInvoiceSupplierReferenceReuse(SavepointCase):
         # Inspired by TestAccountSupplierInvoice
         # https://github.com/odoo/odoo/blob/11.0/addons/account/tests/test_account_supplier_invoice.py
 
-        self.account_account = self.env['account.account']
+        self.account_account = self.env["account.account"]
         self.invoice_account = self.account_account.search(
-            [('user_type_id',
-              '=',
-              self.env.ref('account.data_account_type_receivable').id
-              )], limit=1).id
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    self.env.ref("account.data_account_type_receivable").id,
+                )
+            ],
+            limit=1,
+        ).id
         self.invoice_line_account = self.account_account.search(
-            [('user_type_id',
-              '=',
-              self.env.ref('account.data_account_type_expenses').id
-              )], limit=1).id
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    self.env.ref("account.data_account_type_expenses").id,
+                )
+            ],
+            limit=1,
+        ).id
 
-        self.invoice = self._create_invoice_with_reference(self, 'ABC123')
+        self.invoice = self._create_invoice_with_reference(self, "ABC123")
 
     def _create_invoice_with_reference(self, reference):
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.env.ref('base.res_partner_2').id,
-            'account_id': self.invoice_account,
-            'type': 'in_invoice',
-            'reference': reference
-        })
-        self.env['account.invoice.line'].create({
-            'product_id': self.env.ref('product.product_product_4').id,
-            'quantity': 1.0,
-            'price_unit': 100.0,
-            'invoice_id': invoice.id,
-            'name': 'product that cost 100',
-            'account_id': self.invoice_line_account,
-        })
+        invoice = self.env["account.invoice"].create(
+            {
+                "partner_id": self.env.ref("base.res_partner_2").id,
+                "account_id": self.invoice_account,
+                "type": "in_invoice",
+                "reference": reference,
+            }
+        )
+        self.env["account.invoice.line"].create(
+            {
+                "product_id": self.env.ref("product.product_product_4").id,
+                "quantity": 1.0,
+                "price_unit": 100.0,
+                "invoice_id": invoice.id,
+                "name": "product that cost 100",
+                "account_id": self.invoice_line_account,
+            }
+        )
         invoice.action_invoice_open()
         return invoice
 
