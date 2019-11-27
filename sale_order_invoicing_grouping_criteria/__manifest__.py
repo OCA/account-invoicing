01f1fa248b62412d70a960ed53d7ffd4cc039556
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-27 17:18:26 +0100

    [IMP] sale_order_invoicing_grouping_criteria: black, isort

--- sale_order_invoicing_grouping_criteria/__manifest__.py
+++ sale_order_invoicing_grouping_criteria/__manifest__.py
@@ -2,23 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Sales order invoicing grouping criteria',
-    'version': '12.0.1.0.0',
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-invoicing',
-    'depends': [
-        'sale',
+    "name": "Sales order invoicing grouping criteria",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": ["sale"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config_settings_views.xml",
+        "views/res_partner_views.xml",
+        "views/sale_invoicing_grouping_criteria_views.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_config_settings_views.xml',
-        'views/res_partner_views.xml',
-        'views/sale_invoicing_grouping_criteria_views.xml',
-    ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['pedrobaeza'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["pedrobaeza"],
 }
