5ffe7226e0050bcda2eb3a1ad9f49c1ab1537b79
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-11-27 17:34:12 +0100

    [MIG] sale_order_invoicing_grouping_criteria: Migration to 13.0
    - Standard procedure
    - Adapt code

--- sale_order_invoicing_grouping_criteria/__manifest__.py
+++ sale_order_invoicing_grouping_criteria/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "Sales order invoicing grouping criteria",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
-    "depends": ["sale"],
+    "depends": ["sale_management"],
     "data": [
         "security/ir.model.access.csv",
         "views/res_config_settings_views.xml",
@@ -16,6 +16,6 @@
         "views/sale_invoicing_grouping_criteria_views.xml",
     ],
     "installable": True,
-    "development_status": "Beta",
+    "development_status": "Production/Stable",
     "maintainers": ["pedrobaeza"],
 }
