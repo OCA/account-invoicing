2fd66c07675550af16351025ffedec94d1021110
kongrattapong <rattapong.chok@gmail.com>
2019-12-16 10:53:04 +0700

    [IMP] account_invoice_line_description: black, isort

--- account_invoice_line_description/wizards/res_config.py
+++ account_invoice_line_description/wizards/res_config.py
@@ -1,12 +1,11 @@
-# Copyright 2015 Agile Business Group sagl
-# (<http://www.agilebg.com>)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# Copyright 2015 Agile Business Group sagl (https://www.agilebg.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     group_use_product_description_per_inv_line = fields.Boolean(
         """Allow using only the product description on the
@@ -14,5 +13,5 @@ class ResConfigSettings(models.TransientModel):
         implied_group="account_invoice_line_description."
         "group_use_product_description_per_inv_line",
         help="""Allows you to use only product description on the
-        invoice lines."""
+        invoice lines.""",
     )
