b5106798e0f045d796559bf84858a17af199d387
newtratip <tharathip.chaweewongphan@gmail.com>
2019-12-17 16:28:10 +0700

    [13.0][MIG] account_invoice_search_by_reference
    [UPD] Update account_invoice_search_by_reference.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_invoice_search_by_reference
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_invoice_search_by_reference/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_invoice_search_by_reference
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_invoice_search_by_reference/

--- account_invoice_search_by_reference/tests/__init__.py
+++ account_invoice_search_by_reference/tests/__init__.py
@@ -1,1 +1,4 @@
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from . import test_account_invoice_search_by_reference
