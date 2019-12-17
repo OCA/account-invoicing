1ad27dff01ed3502f8d1461b2bc6e2e66f9c78f0
kongrattapong <rattapong.chok@gmail.com>
2019-12-17 09:15:49 +0700

    [IMP] account_invoice_view_payment: black, isort

--- account_invoice_view_payment/readme/CONTRIBUTORS.rst
+++ account_invoice_view_payment/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
 * Jordi Ballester <jordi.ballester@eficent.com>
 * Miquel Raïch <miquel.raich@eficent.com>
 * Achraf Mhadhbi <machraf@bloopark.de>
+* Rattapong Chokmasermkul <rattapongc@ecosoft.co.th>
--- account_invoice_view_payment/wizards/account_register_payments.py
+++ account_invoice_view_payment/wizards/account_register_payments.py
@@ -1,8 +1,8 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
-#        (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+#        (https://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import api, models, _
+from odoo import _, api, models
 
 
 class AccountRegisterPayments(models.TransientModel):
@@ -10,20 +10,20 @@ class AccountRegisterPayments(models.TransientModel):
 
     @api.multi
     def create_payment_and_open(self):
-        payment_model = self.env['account.payment']
+        payment_model = self.env["account.payment"]
         payments = payment_model
         for payment_vals in self.get_payments_vals():
             payments += payment_model.create(payment_vals)
         payments.post()
         res = {
-            'domain': [('id', 'in', payments.ids), ('state', '=', 'posted')],
-            'views': [(False, 'form')],
-            'name': _('Payments'),
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'account.payment',
-            'view_id': False,
-            'context': False,
-            'type': 'ir.actions.act_window'
+            "domain": [("id", "in", payments.ids), ("state", "=", "posted")],
+            "views": [(False, "form")],
+            "name": _("Payments"),
+            "view_type": "form",
+            "view_mode": "tree,form",
+            "res_model": "account.payment",
+            "view_id": False,
+            "context": False,
+            "type": "ir.actions.act_window",
         }
         return res
