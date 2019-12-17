b41307f9fb9569652dab67f670e1aa4f489ed32a
newtratip <tharathip.chaweewongphan@gmail.com>
2019-12-17 16:28:10 +0700

    [13.0][MIG] account_invoice_search_by_reference
    [UPD] Update account_invoice_search_by_reference.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_invoice_search_by_reference
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_invoice_search_by_reference/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_invoice_search_by_reference
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_invoice_search_by_reference/

--- account_invoice_search_by_reference/tests/test_account_invoice_search_by_reference.py
+++ account_invoice_search_by_reference/tests/test_account_invoice_search_by_reference.py
@@ -1,5 +1,6 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo.tests.common import TransactionCase
 
 
@@ -8,8 +9,8 @@ class TestAccountInvoiceSearchByReference(TransactionCase):
         super(TestAccountInvoiceSearchByReference, self).setUp()
         self.par_model = self.env["res.partner"]
         self.acc_model = self.env["account.account"]
-        self.inv_model = self.env["account.invoice"]
-        self.inv_line_model = self.env["account.invoice.line"]
+        self.inv_model = self.env["account.move"]
+        self.inv_line_model = self.env["account.move.line"]
 
         self.partner1 = self._create_partner()
 
@@ -24,17 +25,25 @@ class TestAccountInvoiceSearchByReference(TransactionCase):
             limit=1,
         )
 
-        self.invoice_line1 = self._create_inv_line(self.invoice_account.id)
+        self.invoice1 = self._create_invoice(self.partner1)
 
-        self.invoice1 = self._create_invoice(self.partner1, "Test", self.invoice_line1)
+        self.invoice_line1 = self._create_inv_line(
+            self.invoice_account.id, self.invoice1.id
+        )
 
     def _create_partner(self):
         partner = self.par_model.create(
-            {"name": "Test Partner", "supplier": True, "company_type": "company"}
+            {"name": "Test Partner", "supplier_rank": 1, "company_type": "company"}
         )
         return partner
 
-    def _create_inv_line(self, account_id):
+    def _create_invoice(self, partner):
+        invoice = self.inv_model.create(
+            {"partner_id": partner.id, "ref": "Test reference"}
+        )
+        return invoice
+
+    def _create_inv_line(self, account_id, move_id):
         inv_line = self.inv_line_model.create(
             {
                 "name": "test invoice line",
@@ -42,26 +51,18 @@ class TestAccountInvoiceSearchByReference(TransactionCase):
                 "quantity": 1.0,
                 "price_unit": 3.0,
                 "product_id": self.env.ref("product.product_product_8").id,
+                "move_id": move_id,
             }
         )
         return inv_line
 
-    def _create_invoice(self, partner, name, inv_line):
-        invoice = self.inv_model.create(
-            {
-                "partner_id": partner.id,
-                "name": name,
-                "reference": "Test reference",
-                "invoice_line_ids": [(4, inv_line.id)],
-            }
-        )
-        return invoice
-
     def test_account_invoice_method(self):
-        self.invoice1.action_invoice_open()
-        self.invoice1.write({"reference": "Test"})
-        check_method = self.invoice1.name_search(
-            name="INV", operator="ilike", args=[("id", "in", self.invoice1.ids)]
+        check_method1 = self.invoice1.name_search(
+            name="TEST", operator="ilike", args=[("id", "in", self.invoice1.ids)]
         )
-        self.assertEqual(check_method[0][0], self.invoice1.id)
-        self.assertEqual(check_method[0][1][-4:], self.invoice1.name)
+        self.assertEqual(check_method1[0][0], self.invoice1.id)
+        self.invoice1.action_post()
+        check_method2 = self.invoice1.name_search(
+            name="MISC", operator="ilike", args=[("id", "in", self.invoice1.ids)]
+        )
+        self.assertEqual(check_method2[0][0], self.invoice1.id)
