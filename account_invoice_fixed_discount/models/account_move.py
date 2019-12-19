1ba6670f2d3fd83e562603c433525ce971afcdbe
kongrattapong <rattapong.chok@gmail.com>
2019-12-19 14:08:19 +0700

    [MIG] account_invoice_fixed_discount: Migration to 13.0

---
+++ account_invoice_fixed_discount/models/account_move.py
@@ -0,0 +1,89 @@
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    discount_fixed = fields.Float(
+        string="Discount (Fixed)",
+        digits="Product Price",
+        default=0.00,
+        help="Fixed amount discount.",
+    )
+
+    @api.onchange("discount")
+    def _onchange_discount(self):
+        if self.discount:
+            self.discount_fixed = 0.0
+
+    @api.onchange("discount_fixed")
+    def _onchange_discount_fixed(self):
+        if self.discount_fixed:
+            self.discount = 0.0
+
+    @api.constrains("discount", "discount_fixed")
+    def _check_only_one_discount(self):
+        for rec in self:
+            for line in rec:
+                if line.discount and line.discount_fixed:
+                    raise ValidationError(
+                        _("You can only set one type of discount per line.")
+                    )
+
+    @api.onchange("quantity", "discount", "price_unit", "tax_ids", "discount_fixed")
+    def _onchange_price_subtotal(self):
+        return super(AccountMoveLine, self)._onchange_price_subtotal()
+
+    @api.model
+    def _get_price_total_and_subtotal_model(
+        self,
+        price_unit,
+        quantity,
+        discount,
+        currency,
+        product,
+        partner,
+        taxes,
+        move_type,
+    ):
+        if self.discount_fixed != 0:
+            discount = ((self.discount_fixed) / price_unit) * 100 or 0.00
+        return super(AccountMoveLine, self)._get_price_total_and_subtotal_model(
+            price_unit, quantity, discount, currency, product, partner, taxes, move_type
+        )
+
+    @api.model
+    def _get_fields_onchange_balance_model(
+        self, quantity, discount, balance, move_type, currency, taxes
+    ):
+        if self.discount_fixed != 0:
+            discount = ((self.discount_fixed) / self.price_unit) * 100 or 0.00
+        return super(AccountMoveLine, self)._get_fields_onchange_balance_model(
+            quantity, discount, balance, move_type, currency, taxes
+        )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        prev_discount = []
+        for vals in vals_list:
+            if vals.get("discount_fixed"):
+                prev_discount.append(
+                    {"discount_fixed": vals.get("discount_fixed"), "discount": 0.00}
+                )
+                fixed_discount = (
+                    vals.get("discount_fixed") / vals.get("price_unit")
+                ) * 100
+                vals.update({"discount": fixed_discount, "discount_fixed": 0.00})
+            elif vals.get("discount"):
+                prev_discount.append({"discount": vals.get("discount")})
+        res = super(AccountMoveLine, self).create(vals_list)
+        i = 0
+        for rec in res:
+            if rec.discount and prev_discount:
+                rec.write(prev_discount[i])
+                i += 1
+        return res
