5027be3bbd62e458e2d925d015e8e5dd34a148e8
Magno Costa <magno.costa@akretion.com.br>
2019-12-19 11:42:14 -0300

    [12.0][MIG][REF] Included dependency of module stock_picking_invoice_link from repository https://github.com/OCA/stock-logistics-workflow.git .

--- stock_picking_invoicing/__manifest__.py
+++ stock_picking_invoicing/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Stock Picking Invoicing",
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': 'Warehouse Management',
     'author': "Agile Business Group,Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/account-invoicing',
@@ -10,6 +10,7 @@
     "depends": [
         "stock",
         "account",
+        "stock_picking_invoice_link",
     ],
     "data": [
         "wizards/stock_invoice_onshipping_view.xml",
---
+++ stock_picking_invoicing/readme/HISTORY.rst
@@ -0,0 +1,11 @@
+12.0.1.0.0 (2019-07-16)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+ * Migration to version 12.0 .
+
+
+12.0.2.0.0 (2019-12-19)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+ * Included dependency from module stock_picking_invoice_link
+   from repository https://github.com/OCA/stock-logistics-workflow.git.
