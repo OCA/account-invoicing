9e7a55c66ecf510cfcf4c973277231c1a4bdef45
Giovanni <giovanni@gslab.it>
2020-01-08 10:49:56 +0100

    [12.0][FIX] l10n_it_corrispettivi, fix account report

--- l10n_it_corrispettivi/__manifest__.py
+++ l10n_it_corrispettivi/__manifest__.py
@@ -1,6 +1,7 @@
 # Copyright 2016 Lorenzo Battistini - Agile Business Group
 # Copyright 2018-2019 Simone Rubino - Agile Business Group
 # Copyright 2019 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# Copyright 2020 Giovanni Serra - GSLab.it
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     'name': 'Italian Localization - Ricevute',
