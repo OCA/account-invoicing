25587f61893fea56ab137c852599009706b8a6e8
kongrattapong <rattapong.chok@gmail.com>
2020-01-20 17:07:22 +0700

    [IMP] account_menu_invoice_refund: black, isort

--- account_menu_invoice_refund/__manifest__.py
+++ account_menu_invoice_refund/__manifest__.py
@@ -2,22 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Accunt Menu - Invoice & Refund',
-    'version': '12.0.1.0.0',
-    'summary': 'New invoice menu that combine invoices and refunds',
-    'category': 'Accounting & Finance',
-    'author': 'Ecosoft, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/account-invoicing/',
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'views/account_invoice_view.xml',
-    ],
-    'installable': True,
-    'development_status': 'beta',
-    'maintainers': ['kittiu'],
-    'post_load': 'post_load_hook',
+    "name": "Accunt Menu - Invoice & Refund",
+    "version": "12.0.1.0.0",
+    "summary": "New invoice menu that combine invoices and refunds",
+    "category": "Accounting & Finance",
+    "author": "Ecosoft, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-invoicing/",
+    "depends": ["account"],
+    "data": ["views/account_invoice_view.xml"],
+    "installable": True,
+    "development_status": "beta",
+    "maintainers": ["kittiu"],
+    "post_load": "post_load_hook",
 }
