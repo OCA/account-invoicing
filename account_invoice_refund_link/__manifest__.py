5cd0749fdbfcbaddbe6210d7a4a48742678a5268
ernestotejeda <ernesto.tejeda87@gmail.com>
2020-01-21 08:49:50 -0500

    [MIG] account_invoice_refund_link: Migration to 13.0

--- account_invoice_refund_link/__manifest__.py
+++ account_invoice_refund_link/__manifest__.py
@@ -4,12 +4,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Link refund invoice with original",
-    "summary": "Link refund invoice with its original invoice",
-    "version": "12.0.1.0.0",
+    "name": "Show links between refunds and their originator invoices",
+    "version": "13.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "Pexego, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Pexego, Tecnativa, Odoo Community Association (OCA)",
     "installable": True,
     "post_init_hook": "post_init_hook",
     "depends": ["account"],
---
+++ account_invoice_refund_link/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module shows the links between refunds and their original invoices in the
+invoice form and also keep track of refund lines and their original invoice
+lines.
