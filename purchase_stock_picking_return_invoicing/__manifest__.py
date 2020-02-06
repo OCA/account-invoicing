c8f75222a23931ed9b87d77721449e17d4e77e73
kittiu <kittiu@gmail.com>
2020-02-06 09:56:23 +0700

    [13.0][IMP] purchase_stock_picking_return_invoicing: black, isort

--- purchase_stock_picking_return_invoicing/__manifest__.py
+++ purchase_stock_picking_return_invoicing/__manifest__.py
@@ -5,23 +5,14 @@
 {
     "name": "Purchase Stock Picking Return Invoicing",
     "summary": "Add an option to refund returned pickings",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "Eficent,"
-              "Tecnativa,"
-              "Odoo Community Association (OCA)",
+    "author": "Eficent," "Tecnativa," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "development_status": "Mature",
-    "depends": [
-        "purchase_stock",
-    ],
-    "data": [
-        "views/account_invoice_view.xml",
-        "views/purchase_view.xml",
-    ],
-    "maintainers": [
-        'pedrobaeza',
-    ],
+    "depends": ["purchase_stock"],
+    "data": ["views/account_invoice_view.xml", "views/purchase_view.xml"],
+    "maintainers": ["pedrobaeza"],
 }
