631abf11ea7be1fc177b3547b6acb4f84b340e43
kittiu <kittiu@gmail.com>
2020-02-10 11:53:34 +0700

    [13.0][MIG] purchase_stock_picking_return_invoicing

--- purchase_stock_picking_return_invoicing/models/account_invoice.py
+++ purchase_stock_picking_return_invoicing/models/account_invoice.py
@@ -6,43 +6,17 @@ from odoo import api, models
 from odoo.tools import float_is_zero
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
-    @api.onchange("purchase_id")
-    def purchase_order_change(self):
+    @api.onchange("purchase_vendor_bill_id", "purchase_id")
+    def _onchange_purchase_auto_complete(self):
         """Remove lines with qty=0 when making refunds."""
-        res = super().purchase_order_change()
+        res = super()._onchange_purchase_auto_complete()
         if self.type == "in_refund":
-            self.invoice_line_ids -= self.invoice_line_ids.filtered(
+            self.line_ids -= self.invoice_line_ids.filtered(
                 lambda x: float_is_zero(
-                    x.quantity, precision_rounding=x.uom_id.rounding
+                    x.quantity, precision_rounding=x.product_uom_id.rounding
                 )
             )
         return res
-
-    def _prepare_invoice_line_from_po_line(self, line):
-        data = super()._prepare_invoice_line_from_po_line(line)
-        if line.product_id.purchase_method == "receive":
-            # This formula proceeds from the simplification of full expression:
-            # qty_received + qty_returned - (qty_invoiced + qty_refunded) -
-            # (qty_returned - qty_refunded)
-            qty = line.qty_received - line.qty_invoiced
-            data["quantity"] = qty
-        if self.type == "in_refund":
-            invoice_line = self.env["account.invoice.line"]
-            data["quantity"] *= -1.0
-            data["account_id"] = (
-                invoice_line.with_context(
-                    {"journal_id": self.journal_id.id, "type": "in_invoice"}
-                )._default_account(),
-            )
-            account = invoice_line.get_invoice_line_account(
-                "in_invoice",
-                line.product_id,
-                self.purchase_id.fiscal_position_id,
-                self.env.user.company_id,
-            )
-            if account:
-                data["account_id"] = account.id
-        return data
