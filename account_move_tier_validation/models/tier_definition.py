c848af53edcebed90c3f9eafb3ed52e3e6cd4155
Pedro Gonzalez <pedro.gonzalez@pesol.es>
2020-02-14 12:44:42 +0100

    [ADD] 13.0 account_move_tier_validation

---
+++ account_move_tier_validation/models/tier_definition.py
@@ -0,0 +1,14 @@
+# Copyright <2020> PESOL <info@pesol.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import api, models
+
+
+class TierDefinition(models.Model):
+    _inherit = "tier.definition"
+
+    @api.model
+    def _get_tier_validation_model_names(self):
+        res = super(TierDefinition, self)._get_tier_validation_model_names()
+        res.append("account.move")
+        return res
