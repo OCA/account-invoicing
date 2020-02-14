f12b839cb66a60f4bac5948226722960dc6d20cb
Pedro Gonzalez <pedro.gonzalez@pesol.es>
2020-02-14 12:44:42 +0100

    [ADD] 13.0 account_move_tier_validation

---
+++ account_move_tier_validation/models/account_move.py
@@ -0,0 +1,11 @@
+# Copyright <2020> PESOL <info@pesol.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _name = "account.move"
+    _inherit = ["account.move", "tier.validation"]
+    _state_from = ["draft"]
+    _state_to = ["posted", None]
---
+++ account_move_tier_validation/readme/CONTRIBUTORS.rst
@@ -0,0 +1,1 @@
+* Odoo Perú <info@odooperu.pe>
