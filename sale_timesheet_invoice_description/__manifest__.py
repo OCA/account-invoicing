3be3826cce59ffa2c997c481978a9cd1cb0bf8d8
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-24 17:18:09 +0100

    [MIG] sale_timesheet_invoice_description: Migration to 13.0

--- sale_timesheet_invoice_description/__manifest__.py
+++ sale_timesheet_invoice_description/__manifest__.py
@@ -1,14 +1,15 @@
 # Copyright 2016 Tecnativa - Carlos Dauden
 # Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2020 Tecnativa - Manuel Calero
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Timesheet details invoice",
     "summary": "Add timesheet details in invoice line",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://www.tecnativa.com",
     "depends": ["sale_timesheet"],
     "data": ["views/sale_view.xml", "views/res_config_view.xml"],
