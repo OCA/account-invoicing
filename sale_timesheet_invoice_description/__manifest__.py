eee11dde5841ecdde886a3772694e9db088b4331
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-24 14:52:40 +0100

    [IMP] sale_timesheet_invoice_description: black, isort

--- sale_timesheet_invoice_description/__manifest__.py
+++ sale_timesheet_invoice_description/__manifest__.py
@@ -3,20 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Timesheet details invoice',
-    'summary': 'Add timesheet details in invoice line',
-    'version': '12.0.1.0.0',
-    'category': 'Sales Management',
-    'license': 'AGPL-3',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
-    'depends': [
-        'sale_timesheet',
-    ],
-    'data': [
-        'views/sale_view.xml',
-        'views/res_config_view.xml',
-    ],
-    'installable': True,
+    "name": "Timesheet details invoice",
+    "summary": "Add timesheet details in invoice line",
+    "version": "12.0.1.0.0",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
+    "depends": ["sale_timesheet"],
+    "data": ["views/sale_view.xml", "views/res_config_view.xml"],
+    "installable": True,
 }
