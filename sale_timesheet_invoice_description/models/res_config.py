8568627f36487ede944927162cd6aa8cc52c66c1
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-24 14:52:40 +0100

    [IMP] sale_timesheet_invoice_description: black, isort

--- sale_timesheet_invoice_description/models/res_config.py
+++ sale_timesheet_invoice_description/models/res_config.py
@@ -4,14 +4,14 @@ from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     default_timesheet_invoice_description = fields.Selection(
-        selection='_get_timesheet_invoice_description',
+        selection="_get_timesheet_invoice_description",
         string="Timesheet Invoice Description",
-        default_model='sale.order'
+        default_model="sale.order",
     )
 
     @api.model
     def _get_timesheet_invoice_description(self):
-        return self.env['sale.order']._get_timesheet_invoice_description()
+        return self.env["sale.order"]._get_timesheet_invoice_description()
