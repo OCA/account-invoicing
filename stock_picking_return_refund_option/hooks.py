0e1d29bc6e0e34335039ef22bcedf20a3b9de36b
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-25 23:34:03 +0100

    [IMP] stock_picking_return_refund_option: black, isort

--- stock_picking_return_refund_option/hooks.py
+++ stock_picking_return_refund_option/hooks.py
@@ -1,12 +1,14 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, SUPERUSER_ID, tools
+from odoo import SUPERUSER_ID, api, tools
 
 
 def pre_init_hook(cr):
-    if not tools.config.options.get('without_demo', False):
+    if not tools.config.options.get("without_demo", False):
         env = api.Environment(cr, SUPERUSER_ID, {})
-        env['ir.module.module'].search([
-            ('name', 'in', ['purchase_stock', 'sale_stock']),
-            ('state', '!=', 'installed'),
-        ]).write({'state': 'to install'})
+        env["ir.module.module"].search(
+            [
+                ("name", "in", ["purchase_stock", "sale_stock"]),
+                ("state", "!=", "installed"),
+            ]
+        ).write({"state": "to install"})
