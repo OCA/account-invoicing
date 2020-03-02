6d953163b262df33d5476347d91901c4e2bebfe0
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-02 16:15:53 +0100

    account_invoice_transmit_method: Black and other reformatting

--- account_invoice_transmit_method/models/transmit_method.py
+++ account_invoice_transmit_method/models/transmit_method.py
@@ -6,21 +6,19 @@ from odoo import fields, models
 
 
 class TransmitMethod(models.Model):
-    _name = 'transmit.method'
-    _description = 'Transmit Method of a document'
+    _name = "transmit.method"
+    _description = "Transmit Method of a document"
 
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string="Name", required=True)
     code = fields.Char(
-        string='Code', copy=False,
+        string="Code",
+        copy=False,
         help="Do not modify the code of an existing Transmit Method "
-        "because it may be used to identify a particular transmit method.")
-    customer_ok = fields.Boolean(
-        string='Selectable on Customers', default=True)
-    supplier_ok = fields.Boolean(
-        string='Selectable on Vendors', default=True)
+        "because it may be used to identify a particular transmit method.",
+    )
+    customer_ok = fields.Boolean(string="Selectable on Customers", default=True)
+    supplier_ok = fields.Boolean(string="Selectable on Vendors", default=True)
 
-    _sql_constraints = [(
-        'code_unique',
-        'unique(code)',
-        'This transmit method code already exists!'
-    )]
+    _sql_constraints = [
+        ("code_unique", "unique(code)", "This transmit method code already exists!")
+    ]
