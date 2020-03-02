13c60f0c790b1ea57ee6d3817e0d69f1b8250828
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-02 16:15:53 +0100

    account_invoice_transmit_method: Black and other reformatting

--- account_invoice_transmit_method/models/partner.py
+++ account_invoice_transmit_method/models/partner.py
@@ -6,25 +6,36 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     customer_invoice_transmit_method_id = fields.Many2one(
-        'transmit.method', string='Customer Invoice Transmission Method',
-        company_dependant=True, tracking=True,
-        ondelete='restrict')
+        "transmit.method",
+        string="Customer Invoice Transmission Method",
+        company_dependant=True,
+        tracking=True,
+        ondelete="restrict",
+    )
     customer_invoice_transmit_method_code = fields.Char(
-        related='customer_invoice_transmit_method_id.code',
-        string='Customer Invoice Transmission Method Code', readonly=True)
+        related="customer_invoice_transmit_method_id.code",
+        string="Customer Invoice Transmission Method Code",
+        readonly=True,
+    )
     supplier_invoice_transmit_method_id = fields.Many2one(
-        'transmit.method', string='Vendor Invoice Reception Method',
-        company_dependant=True, tracking=True,
-        ondelete='restrict')
+        "transmit.method",
+        string="Vendor Invoice Reception Method",
+        company_dependant=True,
+        tracking=True,
+        ondelete="restrict",
+    )
     supplier_invoice_transmit_method_code = fields.Char(
-        related='supplier_invoice_transmit_method_id.code',
-        string='Vendor Invoice Reception Method Code', readonly=True)
+        related="supplier_invoice_transmit_method_id.code",
+        string="Vendor Invoice Reception Method Code",
+        readonly=True,
+    )
 
     @api.model
     def _commercial_fields(self):
         return super()._commercial_fields() + [
-            'customer_invoice_transmit_method_id',
-            'supplier_invoice_transmit_method_id']
+            "customer_invoice_transmit_method_id",
+            "supplier_invoice_transmit_method_id",
+        ]
--- account_invoice_transmit_method/tests/test_invoice_transmit_method.py
+++ account_invoice_transmit_method/tests/test_invoice_transmit_method.py
@@ -6,22 +6,23 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountInvoiceTransmitMethod(TransactionCase):
-
     def test_create_invoice(self):
-        post_method = self.env.ref('account_invoice_transmit_method.post')
-        partner1 = self.env['res.partner'].create({
-            'is_company': True,
-            'name': 'Old School Company',
-            'customer_invoice_transmit_method_id': post_method.id,
-        })
-        sale_journal = self.env['account.journal'].create({
-            'code': 'XYZZZ',
-            'name': 'sale journal (test)',
-            'type': 'sale',
-        })
-        inv1 = self.env['account.move'].create({
-            'partner_id': partner1.id,
-            'type': 'out_invoice',
-            'journal_id': sale_journal.id,
-        })
+        post_method = self.env.ref("account_invoice_transmit_method.post")
+        partner1 = self.env["res.partner"].create(
+            {
+                "is_company": True,
+                "name": "Old School Company",
+                "customer_invoice_transmit_method_id": post_method.id,
+            }
+        )
+        sale_journal = self.env["account.journal"].create(
+            {"code": "XYZZZ", "name": "sale journal (test)", "type": "sale"}
+        )
+        inv1 = self.env["account.move"].create(
+            {
+                "partner_id": partner1.id,
+                "type": "out_invoice",
+                "journal_id": sale_journal.id,
+            }
+        )
         self.assertEqual(inv1.transmit_method_id, post_method)
