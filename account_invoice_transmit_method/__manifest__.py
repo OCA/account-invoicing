88a845c9d77f9b9fde41000a661bba01e2b8831d
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-02 16:14:21 +0100

    [MIG] account_invoice_transmit_method to v13

--- account_invoice_transmit_method/__manifest__.py
+++ account_invoice_transmit_method/__manifest__.py
@@ -1,19 +1,21 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017-2020 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Invoice Transmit Method',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
+    'version': '13.0.1.0.0',
+    'category': 'Accounting/Accounting',
     'license': 'AGPL-3',
     'summary': 'Configure invoice transmit method (email, post, portal, ...)',
     'author': 'Akretion, Odoo Community Association (OCA)',
+    'maintainers': ['alexis-via'],
     'website': 'https://github.com/OCA/account-invoicing',
     'depends': ['account', 'base_view_inheritance_extension'],
     'data': [
         'security/ir.model.access.csv',
         'views/transmit_method.xml',
-        'views/account_invoice.xml',
+        'views/account_move.xml',
         'views/partner.xml',
         'data/transmit_method.xml',
     ],
