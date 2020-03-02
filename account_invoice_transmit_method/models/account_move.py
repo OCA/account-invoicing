2a41ade03629c012ae5a71bd8111abca535640bc
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-02 16:14:21 +0100

    [MIG] account_invoice_transmit_method to v13

--- account_invoice_transmit_method/models/account_move.py
+++ account_invoice_transmit_method/models/account_move.py
@@ -1,24 +1,23 @@
-# © 2017 Akretion (http://www.akretion.com)
+# Copyright 2017-2020 Akretion France (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     transmit_method_id = fields.Many2one(
         'transmit.method', string='Transmission Method',
-        track_visibility='onchange', ondelete='restrict')  # domain in the view
+        tracking=True, ondelete='restrict')  # domain in the view
     # Field used to match specific invoice transmit method
     # to show/display fields/buttons, add constraints, etc...
     transmit_method_code = fields.Char(
-        related='transmit_method_id.code', readonly=True, store=True)
+        related='transmit_method_id.code', store=True)
 
     @api.onchange('partner_id', 'company_id')
-    def _onchange_partner_id(self):
-        res = super(AccountInvoice, self)._onchange_partner_id()
+    def _transmit_method_partner_change(self):
         if self.partner_id and self.type:
             if self.type in ('out_invoice', 'out_refund'):
                 self.transmit_method_id = self.partner_id.\
@@ -28,7 +27,6 @@ class AccountInvoice(models.Model):
                     supplier_invoice_transmit_method_id.id or False
         else:
             self.transmit_method_id = False
-        return res
 
     @api.model
     def create(self, vals):
@@ -43,4 +41,4 @@ class AccountInvoice(models.Model):
             else:
                 vals['transmit_method_id'] =\
                     partner.supplier_invoice_transmit_method_id.id or False
-        return super(AccountInvoice, self).create(vals)
+        return super().create(vals)
