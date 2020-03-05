a9dec3bf18b561d5987b653d4d4d5d96bc6f2fac
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-05 10:12:18 +0100

    [IMP] sale_order_invoicing_grouping_criter: Make use of new core hook
    Odoo now includes a hook for obtaining the grouping criteria, so we are making use
    of it for cleaning some code and being compatible with other developments.
    
    We also clarifies in documentation the new company grouping criteria also introduced
    by Odoo, as v13 allows to see several companies at the same time.

--- sale_order_invoicing_grouping_criteria/__manifest__.py
+++ sale_order_invoicing_grouping_criteria/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sales order invoicing grouping criteria",
-    "version": "13.0.1.0.1",
+    "version": "13.0.2.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
--- sale_order_invoicing_grouping_criteria/models/sale_order.py
+++ sale_order_invoicing_grouping_criteria/models/sale_order.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2019-2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -7,29 +7,13 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _get_sale_invoicing_group_key(self):
-        self.ensure_one()
-        group_key = [self.partner_invoice_id.id, self.currency_id.id]
+    def _get_invoice_grouping_keys(self):
+        """Inject the extra grouping criteria."""
+        group_key = super()._get_invoice_grouping_keys()
         criteria = (
             self.partner_id.sale_invoicing_grouping_criteria_id
             or self.company_id.default_sale_invoicing_grouping_criteria_id
         )
         for field in criteria.field_ids:
-            group_key.append(self[field.name])
-        return tuple(group_key)
-
-    def _create_invoices(self, grouped=False, final=False):
-        """Slice the batch according grouping criteria."""
-        order_groups = {}
-        for order in self:
-            group_key = order._get_sale_invoicing_group_key()
-            if group_key not in order_groups:
-                order_groups[group_key] = order
-            else:
-                order_groups[group_key] += order
-        invoices = self.env["account.move"].browse()
-        for group in order_groups.values():
-            invoices += super(SaleOrder, group)._create_invoices(
-                grouped=grouped, final=final
-            )
-        return invoices
+            group_key.append(field.name)
+        return group_key
