15d6c033126957da785b44396c14daf329556c50
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-07 10:11:49 +0200

    [FIX] account_global_discount
    Module title incorrect

--- account_global_discount/__manifest__.py
+++ account_global_discount/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Tecnativa S.L. - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Base Global Discount',
+    'name': 'Account Global Discount',
     'version': '11.0.1.0.0',
     'category': 'Accounting',
     'author': 'Tecnativa,'
