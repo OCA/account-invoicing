a830bb3639d67afc9d07f3cbf4d5549fc6bfc17d
kong <rattapong.chok@gmail.com>
2020-04-09 18:34:40 +0700

    [MIG] account_billing: Migration to 13.0

--- account_billing/models/account_move.py
+++ account_billing/models/account_move.py
@@ -1,11 +1,11 @@
 # Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     billing_ids = fields.Many2many(
         comodel_name="account.billing",
@@ -14,7 +14,6 @@ class AccountInvoice(models.Model):
         help="Relationship between invoice and billing",
     )
 
-    @api.multi
     def _compute_billing_ids(self):
         BillLine = self.env["account.billing.line"]
         for rec in self:
