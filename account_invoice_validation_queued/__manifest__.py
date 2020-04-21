4e81ff4466091fb565cbdb0c74cc6cfbe5fa0b1b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-21 10:48:38 +0200

    [IMP] : black, isort, prettier

--- account_invoice_validation_queued/__manifest__.py
+++ account_invoice_validation_queued/__manifest__.py
@@ -2,23 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Enqueue account invoice validation',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-invoicing',
-    'depends': [
-        'account',
-        'queue_job',
+    "name": "Enqueue account invoice validation",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": ["account", "queue_job",],
+    "data": [
+        "views/queue_job_views.xml",
+        "views/account_invoice_views.xml",
+        "wizards/account_invoice_confirm_views.xml",
     ],
-    'data': [
-        'views/queue_job_views.xml',
-        'views/account_invoice_views.xml',
-        'wizards/account_invoice_confirm_views.xml',
-    ],
-    'installable': True,
-    'development_status': 'Production/Stable',
-    'maintainers': ['pedrobaeza'],
+    "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["pedrobaeza"],
 }
