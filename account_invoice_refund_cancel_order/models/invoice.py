fc09d43a2939b9f7887850948945a1ae4acf63b6
Marc Poch <mpoch@planetatic.com>
2020-04-23 12:11:48 +0200

    [FIX]account_invoice_refund_cancel_order: Fixed when cancelling a sale order with invoices partially payed.

--- account_invoice_refund_cancel_order/models/invoice.py
+++ account_invoice_refund_cancel_order/models/invoice.py
@@ -4,6 +4,7 @@
 
 from odoo import models, api, _
 from datetime import date
+from odoo.tools.float_utils import float_round
 
 from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
@@ -13,6 +14,33 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def refund_cancel_order(self, order_name):
+        def automatic_payment(
+                invoice, payment_type, payment_journal_id, amount):
+            if not payment_journal_id or amount == 0.0:
+                # If there is no  payment journal,
+                # there is no need to pay nothing
+                return False
+            payment_method = payment_method_obj.search([
+                ('code', '=', 'manual'),
+                ('payment_type', '=', payment_type),
+            ])
+
+            payment = payment_obj.create({
+                'journal_id': payment_journal_id,
+                'payment_date': today_str,
+                'amount': amount,
+                'communication': description,
+                'payment_method_id': payment_method.id,
+                'payment_type': payment_type,
+                'partner_type': 'customer',
+                'partner_id': invoice.partner_id.id,
+                'currency_id': invoice.currency_id.id,
+                'invoice_ids': [(6, False, [invoice.id])],
+                'company_id': invoice.company_id.id,
+            })
+            payment.post()
+            return True
+
         payment_method_obj = self.env['account.payment.method']
         payment_obj = self.env['account.payment']
 
@@ -42,36 +70,27 @@ class AccountInvoice(models.Model):
         # validate refund invoice:
         refund.action_invoice_open()
 
-        if self.state == 'paid':
-            # automatic pay:
-            payment_method = payment_method_obj.search([
-                ('code', '=', 'manual'),
-                ('payment_type', '=', 'inbound'),
-            ])
-            # get the payment journal:
+        if self.state == 'paid' or self.residual != self.amount_total:
+            # get payment journal:
             original_payments = payment_obj.search([
                 ('invoice_ids', 'in', [self.id]),
             ])
-            journal_id = original_payments[0].journal_id.id
-            payment = payment_obj.create({
-                'journal_id': journal_id,
-                'payment_date': today_str,
-                'amount': refund.amount_total,
-                'communication': description,
-                'payment_method_id': payment_method.id,
-                'payment_type': 'outbound',
-                'partner_type': 'customer',
-                'partner_id': refund.partner_id.id,
-                'currency_id': refund.currency_id.id,
-                'invoice_ids': [(6, False, [refund.id])],
-                'company_id': refund.company_id.id,
-            })
-            payment.post()
+            payment_journal_id = False
+            if original_payments:
+                payment_journal_id = original_payments[0].journal_id.id
+            if self.state == 'open':
+                # this invoice has a partial payment, pay the missing quantity
+                precision = self.env.user.company_id.currency_id.decimal_places
+                amount = float_round(
+                    self.residual, precision_digits=precision)
+                automatic_payment(self, 'inbound', payment_journal_id, amount)
+            # Pay back the refund invoice:
+            automatic_payment(
+                refund, 'outbound', payment_journal_id, refund.amount_total)
         else:
             # reconcile invoices each other:
             for tmpline in refund.move_id.line_ids:
                 if tmpline.account_id.id == self.account_id.id:
                     to_reconcile_lines += tmpline
             to_reconcile_lines.filtered(lambda l: not l.reconciled).reconcile()
-
         return True
