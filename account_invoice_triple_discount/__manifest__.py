377f188a40da78edeb2f554e4579cc111f806403
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-12 14:39:58 +0200

    [MIG] account_invoice_triple_discount to v13

--- account_invoice_triple_discount/__manifest__.py
+++ account_invoice_triple_discount/__manifest__.py
@@ -3,13 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Invoice Triple Discount",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting & Finance",
-    "author": "QubiQ," "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "QubiQ, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "summary": "Manage triple discount on invoice lines",
     "depends": ["account"],
-    "data": ["views/account_invoice_view.xml"],
+    "data": ["report/invoice.xml", "views/account_move.xml"],
     "installable": True,
 }
