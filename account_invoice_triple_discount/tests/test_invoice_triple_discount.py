46e1204b6fca6cac893ea022b409708f70685657
Benjamin Willig <benjamin.willig@acsone.eu>
2020-06-12 14:39:58 +0200

    [MIG] account_invoice_triple_discount to v13

--- account_invoice_triple_discount/tests/test_invoice_triple_discount.py
+++ account_invoice_triple_discount/tests/test_invoice_triple_discount.py
@@ -2,84 +2,119 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests import SavepointCase
+from odoo.tests.common import Form
 
 
 class TestInvoiceTripleDiscount(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestInvoiceTripleDiscount, cls).setUpClass()
-        cls.partner = cls.env['res.partner'].create({'name': 'Test'})
-        cls.tax = cls.env['account.tax'].create({
-            'name': 'TAX 15%',
-            'amount_type': 'percent',
-            'type_tax_use': 'purchase',
-            'amount': 15.0,
-        })
-        cls.account_type = cls.env['account.account.type'].create({
-            'name': 'Test',
-            'type': 'receivable',
-        })
-        cls.account = cls.env['account.account'].create({
-            'name': 'Test account',
-            'code': 'TEST',
-            'user_type_id': cls.account_type.id,
-            'reconcile': True,
-        })
-        cls.invoice = cls.env['account.invoice'].create({
-            'name': "Test Customer Invoice",
-            'journal_id': cls.env['account.journal'].search(
-                [('type', '=', 'sale')])[0].id,
-            'partner_id': cls.partner.id,
-            'account_id': cls.account.id,
-        })
-        cls.invoice_line = cls.env['account.invoice.line']
-        cls.invoice_line1 = cls.invoice_line.create({
-            'invoice_id': cls.invoice.id,
-            'name': 'Line 1',
-            'price_unit': 200.0,
-            'account_id': cls.account.id,
-            'invoice_line_tax_ids': [(6, 0, [cls.tax.id])],
-            'quantity': 1,
-        })
+        cls.Account = cls.env["account.account"]
+        cls.AccountMove = cls.env["account.move"]
+        cls.AccountTax = cls.env["account.tax"]
+        cls.AccountType = cls.env["account.account.type"]
+        cls.Partner = cls.env["res.partner"]
+        cls.Journal = cls.env["account.journal"]
+
+        cls.partner = cls.Partner.create({"name": "test"})
+        cls.tax = cls.AccountTax.create(
+            {
+                "name": "TAX 15%",
+                "amount_type": "percent",
+                "type_tax_use": "purchase",
+                "amount": 15.0,
+            }
+        )
+        cls.account_type = cls.AccountType.create(
+            {"name": "Test", "type": "receivable", "internal_group": "income"}
+        )
+        cls.account = cls.Account.create(
+            {
+                "name": "Test account",
+                "code": "TEST",
+                "user_type_id": cls.account_type.id,
+                "reconcile": True,
+            }
+        )
+        cls.sale_journal = cls.Journal.search([("type", "=", "sale")], limit=1)
+
+    def create_simple_invoice(self, amount):
+        invoice_form = Form(
+            self.AccountMove.with_context(
+                default_type="out_invoice", default_journal_id=self.sale_journal.id,
+            )
+        )
+        invoice_form.partner_id = self.partner
+
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.name = "Line 1"
+            line_form.quantity = 1
+            line_form.price_unit = amount
+            line_form.tax_ids.clear()
+            line_form.tax_ids.add(self.tax)
+
+        invoice = invoice_form.save()
+        return invoice
 
     def test_01_discounts(self):
         """ Tests multiple discounts in line with taxes """
+        invoice = self.create_simple_invoice(200)
+
+        invoice_form = Form(invoice)
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = 50.0
+        invoice_form.save()
+
+        invoice_line = invoice.invoice_line_ids[0]
+
         # Adds a first discount
-        self.invoice_line1.discount = 50.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 115.0)
+        self.assertEqual(invoice.amount_total, 115.0)
+
         # Adds a second discount over the price calculated before
-        self.invoice_line1.discount2 = 40.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 69.0)
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount2 = 40.0
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 69.0)
+
         # Adds a third discount over the price calculated before
-        self.invoice_line1.discount3 = 50.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 34.5)
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount3 = 50.0
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 34.5)
+
         # Deletes first discount
-        self.invoice_line1.discount = 0.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 69.0)
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = 0
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 69)
+
         # Charge 5% over price:
-        self.invoice_line1.discount = -5.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 72.45)
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = -5
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 72.45)
+
+        self.assertEqual(invoice_line.price_unit, 200)
 
     def test_02_discounts_multiple_lines(self):
-        """ Tests multiple lines with mixed taxes """
-        self.invoice_line2 = self.invoice_line.create({
-            'invoice_id': self.invoice.id,
-            'name': 'Line 1',
-            'price_unit': 500.0,
-            'account_id': self.account.id,
-            'quantity': 1,
-        })
-        self.assertEqual(self.invoice_line2.price_subtotal, 500.0)
-        self.invoice_line2.discount3 = 50.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice_line2.price_subtotal, 250.0)
-        self.assertEqual(self.invoice.amount_total, 480.0)
-        self.invoice_line1.discount = 50.0
-        self.invoice._onchange_invoice_line_ids()
-        self.assertEqual(self.invoice.amount_total, 365.0)
+        invoice = self.create_simple_invoice(200)
+        invoice_form = Form(invoice)
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.name = "Line 2"
+            line_form.quantity = 1
+            line_form.price_unit = 500
+            line_form.tax_ids.clear()
+        invoice_form.save()
+
+        invoice_line2 = invoice.invoice_line_ids[1]
+        self.assertEqual(invoice_line2.price_subtotal, 500.0)
+
+        with invoice_form.invoice_line_ids.edit(1) as line_form:
+            line_form.discount3 = 50.0
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 480.0)
+
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = 50.0
+        invoice_form.save()
+        self.assertEqual(invoice.amount_total, 365.0)
