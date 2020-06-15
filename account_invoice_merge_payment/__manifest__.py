970aeaf23595cb403f31da04c040f3497cd21d13
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-06-15 17:48:54 +0200

    [MIG] account_invoice_merge_payment: Migration to 12.0

--- account_invoice_merge_payment/__manifest__.py
+++ account_invoice_merge_payment/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -8,7 +7,7 @@
     'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
     'website': "http://www.acsone.eu",
     'category': 'Invoicing & Payments',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'account_invoice_merge',
