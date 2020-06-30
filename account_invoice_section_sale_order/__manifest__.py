3b2bd31268b15d19b80f6d9a77528e27acf28da6
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-06-30 09:41:14 +0200

    [13.0][ADD] account_invoice_section_sale_order
    Description from the readme :
    
    When invoicing multiple sale orders at the same time, sale orders may be grouped
    by customer into a single invoice. Unfortunately when this happens, it is hard
    to know which invoice line belongs to which sale order.
    
    This module helps by grouping invoicing lines into sections with the name of the
    targeted sale order.
    This is only done when an invoice targets multiple sale order.

---
+++ account_invoice_section_sale_order/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Acccount Invoice Section Sale Order",
+    "version": "13.0.1.0.0",
+    "summary": "For invoices targetting multiple sale order add"
+    "sections with sale order name.",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "depends": ["account", "sale"],
+}
