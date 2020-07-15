d9cc19f2308a33e84241bd10cba9cd070da5d9bb
manu <manuel.regidor@sygel.es>
2020-07-15 16:00:54 +0200

    [MIG]account_invoice_pricelist_sale: Migration to 13.0

--- account_invoice_pricelist_sale/tests/test_module.py
+++ account_invoice_pricelist_sale/tests/test_module.py
@@ -2,7 +2,6 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
 from odoo.tests.common import TransactionCase
 
 
