c57901f7059d5743b97a8a92642e69b9edd38f53
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-20 09:56:49 +0200

    [IMP] account_global_discount: Deprecated use of currency compute
    Replaced by more explicit `_convert` method.

--- account_global_discount/__manifest__.py
+++ account_global_discount/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Global Discount',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Accounting',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
