247d20004c81cb0058a7dbd6a1253955f39afb05
manu <manuel.regidor@sygel.es>
2020-08-21 16:13:09 +0200

    [MIG]account_invoice_pricelist: Migration to 13.0

--- account_invoice_pricelist/tests/test_account_invoice_pricelist.py
+++ account_invoice_pricelist/tests/test_account_invoice_pricelist.py
@@ -3,42 +3,46 @@
 from odoo.tests.common import SavepointCase
 
 
-class TestAccountInvoicePricelist(SavepointCase):
+class TestAccountMovePricelist(SavepointCase):
     @classmethod
-    def setUpClass(self):
-        super(TestAccountInvoicePricelist, self).setUpClass()
-        self.AccountInvoice = self.env["account.invoice"]
-        self.ProductPricelist = self.env["product.pricelist"]
-        self.FiscalPosition = self.env["account.fiscal.position"]
-        self.fiscal_position = self.FiscalPosition.create(
-            {"name": "Test Fiscal Position", "active": True,}
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.AccountMove = cls.env["account.move"]
+        cls.ProductPricelist = cls.env["product.pricelist"]
+        cls.FiscalPosition = cls.env["account.fiscal.position"]
+        cls.fiscal_position = cls.FiscalPosition.create(
+            {"name": "Test Fiscal Position", "active": True}
         )
-        self.journal_sale = self.env["account.journal"].create(
-            {"name": "Test sale journal", "type": "sale", "code": "TEST_SJ",}
+        cls.journal_sale = cls.env["account.journal"].create(
+            {"name": "Test sale journal", "type": "sale", "code": "TEST_SJ"}
         )
-        self.at_receivable = self.env["account.account.type"].create(
-            {"name": "Test receivable account", "type": "receivable",}
+        cls.at_receivable = cls.env["account.account.type"].create(
+            {
+                "name": "Test receivable account",
+                "type": "receivable",
+                "internal_group": "income",
+            }
         )
-        self.a_receivable = self.env["account.account"].create(
+        cls.a_receivable = cls.env["account.account"].create(
             {
                 "name": "Test receivable account",
                 "code": "TEST_RA",
-                "user_type_id": self.at_receivable.id,
+                "user_type_id": cls.at_receivable.id,
                 "reconcile": True,
             }
         )
-        self.partner = self.env["res.partner"].create(
+        cls.partner = cls.env["res.partner"].create(
             {
                 "name": "Test Partner",
                 "property_product_pricelist": 1,
-                "property_account_receivable_id": self.a_receivable.id,
-                "property_account_position_id": self.fiscal_position.id,
+                "property_account_receivable_id": cls.a_receivable.id,
+                "property_account_position_id": cls.fiscal_position.id,
             }
         )
-        self.product = self.env["product.template"].create(
-            {"name": "Product Test", "list_price": 100.00,}
+        cls.product = cls.env["product.template"].create(
+            {"name": "Product Test", "list_price": 100.00}
         )
-        self.sale_pricelist_id = self.ProductPricelist.create(
+        cls.sale_pricelist = cls.ProductPricelist.create(
             {
                 "name": "Test Sale pricelist",
                 "item_ids": [
@@ -49,24 +53,22 @@ class TestAccountInvoicePricelist(SavepointCase):
                             "applied_on": "1_product",
                             "compute_price": "fixed",
                             "fixed_price": 300.00,
-                            "product_tmpl_id": self.product.id,
+                            "product_tmpl_id": cls.product.id,
                         },
                     )
                 ],
             }
         )
-        self.invoice = self.AccountInvoice.create(
+        cls.invoice = cls.AccountMove.create(
             {
-                "partner_id": self.partner.id,
-                "account_id": self.a_receivable.id,
+                "partner_id": cls.partner.id,
                 "type": "out_invoice",
                 "invoice_line_ids": [
                     (
                         0,
                         0,
                         {
-                            "account_id": self.a_receivable.id,
-                            "product_id": self.product.product_variant_ids[:1].id,
+                            "product_id": cls.product.product_variant_ids[:1].id,
                             "name": "Test line",
                             "quantity": 1.0,
                             "price_unit": 100.00,
@@ -76,9 +78,8 @@ class TestAccountInvoicePricelist(SavepointCase):
                         0,
                         0,
                         {
-                            "account_id": self.a_receivable.id,
-                            "product_id": self.product.product_variant_ids[:2].id,
-                            "name": "Test line",
+                            "product_id": cls.product.product_variant_ids[:2].id,
+                            "name": "Test line 2",
                             "quantity": 1.0,
                             "price_unit": 100.00,
                         },
@@ -95,7 +96,7 @@ class TestAccountInvoicePricelist(SavepointCase):
         )
 
     def test_account_invoice_change_pricelist(self):
-        self.invoice.pricelist_id = self.sale_pricelist_id.id
+        self.invoice.pricelist_id = self.sale_pricelist.id
         self.invoice.button_update_prices_from_pricelist()
         invoice_line = self.invoice.invoice_line_ids[:1]
         self.assertEqual(invoice_line.price_unit, 300.00)
