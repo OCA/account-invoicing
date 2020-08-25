2e61b84e2a3f72dd8685d42b845bf116b0d7073a
Saran440 <saranl@ecosoft.co.th>
2020-08-25 10:02:42 +0700

    [IMP] account_invoice_change_currency: black, isort, prettier

--- account_invoice_change_currency/__manifest__.py
+++ account_invoice_change_currency/__manifest__.py
@@ -1,20 +1,18 @@
 # Copyright 2017 Komit <http://komit-consulting.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Invoice - Change Currency',
-    'version': '12.0.1.1.0',
-    'category': 'Accounting & Finance',
-    'summary': 'Allows to change currency of Invoice by wizard',
-    'author': 'Komit Consulting, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-invoicing',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Account Invoice - Change Currency",
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "summary": "Allows to change currency of Invoice by wizard",
+    "author": "Komit Consulting, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "data/ir_server_actions.xml",
+        "data/message_subtype.xml",
+        "views/account_invoice.xml",
     ],
-    'data': [
-        'data/ir_server_actions.xml',
-        'data/message_subtype.xml',
-        'views/account_invoice.xml',
-    ],
-    "installable": True
+    "installable": True,
 }
