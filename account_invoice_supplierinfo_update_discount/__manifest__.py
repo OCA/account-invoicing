8612d94be8eb6b2f79222cd90bc27a6ad3103fbc
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-09-02 14:11:46 +0200

    [MIG] account_invoice_supplierinfo_update_discount from 8.0 to 12.0

--- account_invoice_supplierinfo_update_discount/__manifest__.py
+++ account_invoice_supplierinfo_update_discount/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -8,14 +7,15 @@
     'summary': 'In the supplier invoice, automatically update all products '
                'whose discount on the line is different from '
                'the supplier discount',
-    'version': '8.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting & Finance',
-    'website': 'http://odoo-community.org',
+    'website': 'https://github.com/OCA/account-invoicing',
     'author': 'GRAP,Odoo Community Association (OCA)',
+    'maintainers': ['legalsylvain'],
     'license': 'AGPL-3',
     'depends': [
         'account_invoice_supplierinfo_update',
-        'product_supplierinfo_discount',
+        'purchase_discount',
     ],
     'installable': True,
     'auto_install': True,
