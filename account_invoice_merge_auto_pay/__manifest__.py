95428580386f5f3c5f48463be0bb76047da58258
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-09-17 15:03:16 +0200

    [FIX] pep8

--- account_invoice_merge_auto_pay/__manifest__.py
+++ account_invoice_merge_auto_pay/__manifest__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: Commown (https://commown.coop)
 # @author: Florent Cayré
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,14 +6,14 @@
     "name": "Account invoice merge auto pay",
     "category": "accounting",
     "version": "12.0.1.0.0",
-    "author": "Commown SCIC SAS",
+    "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://commown.coop",
 
     "depends": [
         "account_invoice_merge_auto",
         "contract_payment_auto",
-        "queue_job",        
+        "queue_job",
     ],
     "installable": True,
 }
