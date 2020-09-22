bb97bc65969659e52288de5c45462252fe2b9500
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-09-22 11:39:44 +0200

    [13.0][FIX] Add acconut_invoicing_mode
    Add few changes from reviews.
    Use sudo to post the invoice.

--- account_invoice_base_invoicing_mode/models/sale_order.py
+++ account_invoice_base_invoicing_mode/models/sale_order.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    invoicing_mode = fields.Selection(
-        related="partner_invoice_id.invoicing_mode", readonly=True
-    )
+    invoicing_mode = fields.Selection(related="partner_invoice_id.invoicing_mode")
