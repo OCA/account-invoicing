b714a4680b1ec70fecabed119b7d5790d25805c1
Ethan Hildick <hildickethan@gmail.com>
2020-09-24 15:29:37 +0200

    [MIG] account_menu_invoice_refund: Migration to 13.0

--- account_menu_invoice_refund/__manifest__.py
+++ account_menu_invoice_refund/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Accunt Menu - Invoice & Refund",
-    "version": "12.0.2.0.0",
+    "name": "Account Menu - Invoice & Refund",
+    "version": "13.0.1.0.0",
     "summary": "New invoice menu that combine invoices and refunds",
     "category": "Accounting & Finance",
     "author": "Ecosoft, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-invoicing/",
-    "depends": ["account",],
-    "data": ["views/account_invoice_view.xml",],
+    "depends": ["account"],
+    "data": ["views/account_invoice_view.xml"],
     "installable": True,
     "development_status": "beta",
     "maintainers": ["kittiu"],
