c211c4c610a422578e498057b8eb83fbd35eb3ac
mreficent <miquel.raich@eficent.com>
2020-09-30 10:37:59 +0200

    [UPD] Eficent -> ForgeFlow

--- account_invoice_fixed_discount/__manifest__.py
+++ account_invoice_fixed_discount/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 {
@@ -7,7 +7,7 @@
     "version": "13.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
