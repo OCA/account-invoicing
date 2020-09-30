b9799d75d4f7eea290b9578988cab130a80e6339
mreficent <miquel.raich@eficent.com>
2020-09-30 10:37:59 +0200

    [UPD] Eficent -> ForgeFlow

--- account_invoice_fixed_discount/models/account_move.py
+++ account_invoice_fixed_discount/models/account_move.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo import _, api, fields, models
