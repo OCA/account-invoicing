b01d00ea2fe9f53ee684460e175d2b9692255fa4
Lindsay <lindsay.marion@acsone.eu>
2020-10-07 16:23:32 +0200

    [IMP] sale_restocking_fee_invoicinf: Add hook to filter out some product where creating restocking fee

--- sale_stock_restocking_fee_invoicing/models/stock_move.py
+++ sale_stock_restocking_fee_invoicing/models/stock_move.py
@@ -25,9 +25,7 @@ class StockMove(models.Model):
         result = super(StockMove, self).action_done()
         move_ids_to_charge_by_so = defaultdict(list)
         for move in self:
-            if not move.charge_restocking_fee:
-                continue
-            if not move.origin_returned_move_id:
+            if not move._is_restocking_fee_chargeable():
                 continue
             so = move.procurement_id.sale_line_id.order_id
             move_ids_to_charge_by_so[so].append(move.id)
@@ -36,3 +34,12 @@ class StockMove(models.Model):
                 self.browse(move_ids_to_charge)
             )
         return result
+
+    @api.multi
+    def _is_restocking_fee_chargeable(self):
+        self.ensure_one()
+        if not self.charge_restocking_fee:
+            return False
+        if not self.origin_returned_move_id:
+            return False
+        return True
