256e8fb3be2b035f2f26add3c8c3869385eaea3a
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-08 08:58:14 -0400

    [IMP] account_invoice_check_total: black, isort, prettier

--- account_invoice_check_total/models/res_config_settings.py
+++ account_invoice_check_total/models/res_config_settings.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
+
 from ..models.account_invoice import GROUP_AICT
 
 
 class AccountConfigSettings(models.TransientModel):
 
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     group_supplier_inv_check_total = fields.Boolean(
-        string="Check Total on Vendor Bills",
-        implied_group=GROUP_AICT)
+        string="Check Total on Vendor Bills", implied_group=GROUP_AICT
+    )
