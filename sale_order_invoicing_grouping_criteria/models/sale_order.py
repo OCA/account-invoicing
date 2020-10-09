d212a18abba0b6e987ebacfe450bbc7c5f4aa383
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-10-09 20:46:09 +0200

    [FIX] sale_order_invoicing_grouping_criteria: Don't inject grouping criteria, but slice super calls
    It turns out that the grouping criteria is applied over the invoice vals dictionary
    list instead on sales order so you can't group by any field in sales order,
    including the trick of slicing by ID for not grouping at all.
    
    We then restore and adapt the v12 code, that slices the recordset in pieces
    according the grouping criteria with fields obtained in the sales orders.

--- sale_order_invoicing_grouping_criteria/models/sale_order.py
+++ sale_order_invoicing_grouping_criteria/models/sale_order.py
@@ -7,13 +7,34 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _get_invoice_grouping_keys(self):
-        """Inject the extra grouping criteria."""
-        group_key = super()._get_invoice_grouping_keys()
+    def _get_sale_invoicing_group_key(self):
+        """Prepare extended grouping criteria for sales orders."""
+        self.ensure_one()
+        group_key = [
+            self.company_id.id,
+            self.partner_invoice_id.id,
+            self.currency_id.id,
+        ]
         criteria = (
             self.partner_id.sale_invoicing_grouping_criteria_id
             or self.company_id.default_sale_invoicing_grouping_criteria_id
         )
         for field in criteria.field_ids:
-            group_key.append(field.name)
-        return group_key
+            group_key.append(self[field.name])
+        return tuple(group_key)
+
+    def _create_invoices(self, grouped=False, final=False):
+        """Slice the batch according grouping criteria."""
+        order_groups = {}
+        for order in self:
+            group_key = order._get_sale_invoicing_group_key()
+            if group_key not in order_groups:
+                order_groups[group_key] = order
+            else:
+                order_groups[group_key] += order
+        moves = self.env["account.move"]
+        for group in order_groups.values():
+            moves += super(SaleOrder, group)._create_invoices(
+                grouped=grouped, final=final
+            )
+        return moves
