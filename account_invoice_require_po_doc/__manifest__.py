585c347f4cfe8bfa9a229ff8b8f1687867e74bc4
Khalid <m.khalid@serpentcs.com>
2020-11-10 18:38:02 +0530

    [ADD] account_invoice_require_po_doc

---
+++ account_invoice_require_po_doc/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Require PO or Sales Documentation",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": """Open Source Integrators,
+        Serpent Consulting Services
+        Odoo Community Association (OCA)""",
+    "website": "https://github.com/OCA/account-invoicing",
+    "category": "Accounting & Finance",
+    "depends": ["sale_require_po_doc"],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
---
+++ account_invoice_require_po_doc/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Daniel Reis <dreis@opensourceintegrators.com>
+* Mohammad Khalid <m.khalid@serpentcs.com>
