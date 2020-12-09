7eca7f2d797f484d6f162a34f34b004765164cc0
Alfredo Zamora <alfredo.zamora@agilebg.com>
2020-12-09 17:01:17 +0100

    [IMP] stock_picking_invoicing_incoterm: black, isort, prettier

--- stock_picking_invoicing_incoterm/__openerp__.py
+++ stock_picking_invoicing_incoterm/__openerp__.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2015 Agile Business Group sagl
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': "Stock Picking Invoicing Incoterm",
-    'version': '8.0.1.0.0',
-    'category': 'Warehouse Management',
-    'author': "Agile Business Group, Odoo Community Association (OCA)",
-    'website': 'http://www.agilebg.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_stock',
+    "name": "Stock Picking Invoicing Incoterm",
+    "version": "8.0.1.0.0",
+    "category": "Warehouse Management",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_stock",
     ],
-    'data': [
-        'views/account_invoice_view.xml',
-        'views/stock_view.xml',
+    "data": [
+        "views/account_invoice_view.xml",
+        "views/stock_view.xml",
     ],
-    'test': [
-        'test/stock_picking_invoicing_incoterm.yml',
+    "test": [
+        "test/stock_picking_invoicing_incoterm.yml",
     ],
-    'installable': True
+    "installable": True,
 }
--- stock_picking_invoicing_incoterm/models/__init__.py
+++ stock_picking_invoicing_incoterm/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2015 Agile Business Group sagl
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
