f9b090d3c20710e3be8195f2ec6037c44b2bb759
Alfredo Zamora <alfredo.zamora@agilebg.com>
2020-12-09 18:01:06 +0100

    [MIG] stock_picking_invoicing_incoterm: Migration to 14.0

--- stock_picking_invoicing_incoterm/models/__init__.py
+++ stock_picking_invoicing_incoterm/models/__init__.py
@@ -1,5 +1,4 @@
-# Copyright 2014-2015 Agile Business Group sagl
+# Copyright 2014-2020 Agile Business Group sagl
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import account_invoice
 from . import stock
