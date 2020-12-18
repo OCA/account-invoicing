dd27d923c5605e360a6ec6fba2573dcf1390ec35
Rad0van <radovan@skolnik.info>
2020-12-18 16:51:49 +0100

    [IMP] stock_picking_invoicing: black, isort, prettier

--- stock_picking_invoicing/__manifest__.py
+++ stock_picking_invoicing/__manifest__.py
@@ -1,25 +1,19 @@
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Stock Picking Invoicing",
-    'version': '12.0.2.0.1',
-    'category': 'Warehouse Management',
-    'author': "Agile Business Group,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-invoicing',
-    'license': 'AGPL-3',
-    "depends": [
-        "stock",
-        "account",
-        "stock_picking_invoice_link",
-    ],
+    "name": "Stock Picking Invoicing",
+    "version": "12.0.2.0.1",
+    "category": "Warehouse Management",
+    "author": "Agile Business Group,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["stock", "account", "stock_picking_invoice_link",],
     "data": [
         "wizards/stock_invoice_onshipping_view.xml",
         "views/stock_move.xml",
         "views/stock_picking.xml",
         "views/stock_picking_type.xml",
     ],
-    "demo": [
-        "demo/stock_picking_demo.xml"
-    ],
-    'installable': True,
+    "demo": ["demo/stock_picking_demo.xml"],
+    "installable": True,
 }
