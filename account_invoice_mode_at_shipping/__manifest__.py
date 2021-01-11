da338123f77405ba1a125b693986cad4a44add2e
Tran Thanh Phuc <phuc@trobz.com>
2021-01-11 21:24:24 +0700

    [MIG] account_invoice_mode: Migration to 14.0

--- account_invoice_mode_at_shipping/__manifest__.py
+++ account_invoice_mode_at_shipping/__manifest__.py
@@ -2,11 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Invoice Mode At Shipping",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": "Create invoices automatically when goods are shipped.",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
+    "data": [
+        "data/queue_job_data.xml",
+    ],
     "depends": ["account", "account_invoice_base_invoicing_mode", "queue_job", "stock"],
 }
