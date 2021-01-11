7927fc84fd973d57101cead35b4f6698409a4c3b
Tran Thanh Phuc <phuc@trobz.com>
2021-01-11 21:24:24 +0700

    [MIG] account_invoice_mode: Migration to 14.0

--- account_invoice_base_invoicing_mode/models/account_invoice.py
+++ account_invoice_base_invoicing_mode/models/account_invoice.py
@@ -3,13 +3,9 @@
 
 from odoo import models
 
-from odoo.addons.queue_job.job import job, related_action
-
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @job(default_channel="root.invoice_validation")
-    @related_action(action="related_action_open_invoice")
     def _validate_invoice(self):
         return self.sudo().action_post()
