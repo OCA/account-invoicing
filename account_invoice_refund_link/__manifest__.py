268290d85ee5bcb0cf2a616bf21a6fcff3d59948
João Marques <joao.marques@tecnativa.com>
2021-02-01 07:44:30 +0000

    [MIG] account_invoice_refund_link: Migration to 14.0
    TT26438

--- account_invoice_refund_link/__manifest__.py
+++ account_invoice_refund_link/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2014-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Show links between refunds and their originator invoices",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "Pexego, Tecnativa, Odoo Community Association (OCA)",
