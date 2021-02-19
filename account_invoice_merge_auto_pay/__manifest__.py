b4e2f56bda7cb6cd00ca338fbbde6c69cd323ea9
Florent Cayré <florent@commown.fr>
2021-02-19 06:47:19 +0100

    [FIX] Add missing dependency to account_invoice_merge_payment
    This module is required to add payment_method_id to the merges invoices.

--- account_invoice_merge_auto_pay/__manifest__.py
+++ account_invoice_merge_auto_pay/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 - Today: Commown (https://commown.coop)
+# Copyright (C) 2021 - Commown SCIC (https://commown.coop)
 # @author: Florent Cayré
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -11,6 +11,7 @@
     "website": "https://commown.coop",
 
     "depends": [
+        "account_invoice_merge_payment",
         "account_invoice_merge_auto",
         "contract_payment_auto",
         "queue_job",
