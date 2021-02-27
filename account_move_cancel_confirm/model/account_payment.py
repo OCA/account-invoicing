06494e64723391a2f88c279f7dc6bc55f2385c5f
Kitti U <kittiu@gmail.com>
2021-02-27 15:17:32 +0700

    [14.0][ADD] account_move_cancel_confirm

---
+++ account_move_cancel_confirm/model/account_payment.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class AccountPayment(models.Model):
+    _name = "account.payment"
+    _inherit = ["account.payment", "base.cancel.confirm"]
+
+    _has_cancel_reason = "optional"  # ["no", "optional", "required"]
+
+    def action_cancel(self):
+        if not self.filtered("cancel_confirm"):
+            return self.open_cancel_confirm_wizard()
+        return super().action_cancel()
+
+    def action_draft(self):
+        self.clear_cancel_confirm_data()
+        return super().action_draft()
---
+++ account_move_cancel_confirm/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+When user click to cancel invoice and journal entry, a confirmation wizard will be show, with reason as optional.
