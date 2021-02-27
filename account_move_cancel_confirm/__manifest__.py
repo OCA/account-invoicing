6ea4f90df187ce4150ef68057669d2c9ea0b2c4a
Kitti U <kittiu@gmail.com>
2021-02-27 15:17:32 +0700

    [ADD] account_move_cancel_confirm

---
+++ account_move_cancel_confirm/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Cancel Confirm",
+    "version": "16.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "category": "Usability",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": ["base_cancel_confirm", "account"],
+    "installable": True,
+    "maintainers": ["kittiu"],
+}
