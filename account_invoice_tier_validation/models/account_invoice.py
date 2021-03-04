26cd6a0149bc3bba848ba6e5a954b7e739510909
Andrea <a.stirpe@onestein.nl>
2021-03-04 18:10:13 +0100

    [12.0][FIX] allow opening invoice after tier validation

--- account_invoice_tier_validation/models/account_invoice.py
+++ account_invoice_tier_validation/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright 2020 ForgeFlow S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
@@ -9,3 +9,19 @@ class AccountInvoice(models.Model):
     _inherit = ["account.invoice", "tier.validation"]
     _state_from = ["draft"]
     _state_to = ["open", "paid"]
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super()._get_under_validation_exceptions()
+        if self.env.context.get("invoice_tier_validation_allow_write"):
+            res += ["move_id", "date", "move_name", "date_due", "date_invoice"]
+        return res
+
+    @api.multi
+    def _check_allow_write_under_validation(self, vals):
+        allow_write = all([invoice.state == "draft" for invoice in self])
+        ctx = self.env.context.copy()
+        ctx["invoice_tier_validation_allow_write"] = allow_write
+        res = super(AccountInvoice, self.with_context(ctx))\
+            ._check_allow_write_under_validation(vals)
+        return res
