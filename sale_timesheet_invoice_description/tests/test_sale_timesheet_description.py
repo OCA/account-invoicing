9f822ca6288c011f3f02052e58bc58adbcf1b3d6
clementmbr <clement.mombereau@akretion.com.br>
2021-03-24 23:26:29 +0100

    [MIG] sale_timesheet_invoice_description: Migration to 14.0
    - **Changes on tests :** now the timesheet needs to be linked to a
    project task and this task is linked to the sale.order.line.
    
    - **Changes on sale.py :** we cannot anymore find the timesheet related
    to our future invoice line checking its create_date as v14 allows
    to invoice only timesheets between two dates.
    So I decided to refactor completely the way to match an invoice line
    and its timesheet adding a `_link_timesheets_to_invoice_line` method
    inspired by the native `_link_timesheets_to_invoice`

--- sale_timesheet_invoice_description/tests/test_sale_timesheet_description.py
+++ sale_timesheet_invoice_description/tests/test_sale_timesheet_description.py
@@ -2,6 +2,8 @@
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from datetime import datetime
+
 from odoo.tests import common
 
 
@@ -13,17 +15,31 @@ class TestSaleTimesheetDescription(common.SavepointCase):
         cls.env["res.lang"].load_lang("en_US")
         cls.env.user.lang = "en_US"
         cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
+        cls.analytic_account = cls.env["account.analytic.account"].create(
+            {"name": "Test analytic account"}
+        )
+        cls.project = cls.env["project.project"].create(
+            {
+                "name": "Test project",
+                "analytic_account_id": cls.analytic_account.id,
+                "allow_timesheets": True,
+                "allow_billable": True,
+            }
+        )
         cls.product_uom = cls.env.ref("uom.product_uom_unit")
         cls.product = cls.env["product.product"].create(
             {
                 "name": "Test product",
-                "service_type": "timesheet",
+                "type": "service",
                 "uom_id": cls.product_uom.id,
+                "service_type": "timesheet",
+                "invoice_policy": "delivery",
+                # Task created on cls.project when the cls.product is ordered
+                "service_tracking": "task_global_project",
+                "project_id": cls.project.id,
             }
         )
-        cls.analytic_account = cls.env["account.analytic.account"].create(
-            {"name": "Test analytic account"}
-        )
+
         cls.sale_order = cls.env["sale.order"].create(
             {
                 "partner_id": cls.partner.id,
@@ -31,28 +47,32 @@ class TestSaleTimesheetDescription(common.SavepointCase):
                 "partner_shipping_id": cls.partner.id,
                 "pricelist_id": cls.partner.property_product_pricelist.id,
                 "analytic_account_id": cls.analytic_account.id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": cls.product.name,
-                            "product_id": cls.product.id,
-                            "product_uom_qty": 10.5,
-                            "product_uom": cls.product.uom_id.id,
-                            "price_unit": cls.product.list_price,
-                        },
-                    )
-                ],
             }
         )
-        cls.analytic_line = cls.env["account.analytic.line"].create(
+        cls.so_line = cls.env["sale.order.line"].create(
             {
-                "account_id": cls.analytic_account.id,
-                "date": "2017-08-04",
+                "name": cls.product.name,
+                "product_id": cls.product.id,
+                "product_uom_qty": 10.5,
+                "product_uom": cls.product.uom_id.id,
+                "price_unit": cls.product.list_price,
+                "order_id": cls.sale_order.id,
+            },
+        )
+        cls.so_line.product_id_change()
+        # confirm SO, task is created
+        cls.sale_order.action_confirm()
+        cls.task = cls.env["project.task"].search(
+            [("sale_line_id", "=", cls.so_line.id)]
+        )
+        # Add cls.timesheet to this task
+        cls.timesheet = cls.env["account.analytic.line"].create(
+            {
+                "project_id": cls.project.id,
+                "task_id": cls.task.id,
+                "date": datetime.strptime("2017-08-04", "%Y-%m-%d"),
                 "name": "Test description 1234567890",
                 "product_uom_id": cls.product_uom.id,
-                "so_line": cls.sale_order.order_line[0].id,
                 "unit_amount": 10.5,
                 "user_id": cls.env.user.id,
             }
@@ -60,19 +80,19 @@ class TestSaleTimesheetDescription(common.SavepointCase):
 
     def _test_sale_time_description(self, desc_option, expected):
         self.sale_order.timesheet_invoice_description = desc_option
-        self.sale_order.action_confirm()
+
         invoice = self.sale_order.with_context(
             test_timesheet_description=True
         )._create_invoices()
         self.assertEqual(invoice.line_ids[0].name, expected)
 
-        # trigger the creation of another invoice
-        for line in self.sale_order.order_line:
-            line.product_uom_qty += 1
-        invoice = self.sale_order.with_context(
+        # Add a new timesheet to the same invoiced sale.order.line
+        self.timesheet2 = self.timesheet.copy()
+
+        invoice2 = self.sale_order.with_context(
             test_timesheet_description=True
         )._create_invoices()
-        self.assertEqual(invoice.line_ids[0].name, expected)
+        self.assertEqual(invoice2.line_ids[0].name, expected)
 
     def test_sale_timesheet_description_000(self):
         self._test_sale_time_description("000", "Test product")
@@ -80,22 +100,22 @@ class TestSaleTimesheetDescription(common.SavepointCase):
     def test_sale_timesheet_description_111(self):
         self._test_sale_time_description(
             "111",
-            "Test product\n" "2017-08-04 - 10.5 Units - Test description 1234567890",
+            "Test product\n2017-08-04 - 10.5 Units - Test description 1234567890",
         )
 
     def test_sale_timesheet_description_101(self):
         self._test_sale_time_description(
-            "101", "Test product\n" "2017-08-04 - Test description 1234567890"
+            "101", "Test product\n2017-08-04 - Test description 1234567890"
         )
 
     def test_sale_timesheet_description_001(self):
         self._test_sale_time_description(
-            "001", "Test product\n" "Test description 1234567890"
+            "001", "Test product\nTest description 1234567890"
         )
 
     def test_sale_timesheet_description_011(self):
         self._test_sale_time_description(
-            "011", "Test product\n" "10.5 Units - Test description 1234567890"
+            "011", "Test product\n10.5 Units - Test description 1234567890"
         )
 
     def test_settings(self):
@@ -111,3 +131,41 @@ class TestSaleTimesheetDescription(common.SavepointCase):
             }
         )
         self.assertEqual(sale_order.timesheet_invoice_description, "101")
+
+    def test_two_timesheets_same_date(self):
+        self.sale_order.timesheet_invoice_description = "111"
+        description = "2017-08-04 - 10.5 Units - Test description 1234567890"
+        expected = "Test product" + 2 * ("\n" + description)
+
+        # Add a new timesheet with the same date to the same invoiced sale.order.line
+        self.timesheet2 = self.timesheet.copy()
+
+        invoice = self.sale_order.with_context(
+            test_timesheet_description=True
+        )._create_invoices(start_date="2017-01-01", end_date="2018-01-01")
+
+        self.assertEqual(invoice.invoice_line_ids[0].name, expected)
+
+    def test_two_timesheets_different_dates(self):
+        self.sale_order.timesheet_invoice_description = "111"
+        expected = "Test product\n2017-08-04 - 10.5 Units - Test description 1234567890"
+
+        # Add a new timesheet to the same invoiced sale.order.line (i.e. with the same
+        # task and projet) but with different date
+        self.env["account.analytic.line"].create(
+            {
+                "project_id": self.project.id,
+                "task_id": self.task.id,
+                "date": datetime.strptime("2018-08-04", "%Y-%m-%d"),
+                "name": "Timesheet 2",
+                "product_uom_id": self.product_uom.id,
+                "unit_amount": 10.5,
+                "user_id": self.env.user.id,
+            }
+        )
+
+        invoice = self.sale_order.with_context(
+            test_timesheet_description=True
+        )._create_invoices(start_date="2017-01-01", end_date="2018-01-01")
+
+        self.assertEqual(invoice.invoice_line_ids[0].name, expected)
