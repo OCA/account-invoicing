e5f8f006156971c14d7b7fa82a947f9a4a3b1580
João Marques <joao.marques@tecnativa.com>
2021-04-06 12:24:31 +0100

    [MIG] account_invoice_date_due: Migration to 14.0
    TT28729

--- account_invoice_date_due/models/account_move.py
+++ account_invoice_date_due/models/account_move.py
@@ -17,6 +17,22 @@ class AccountMove(models.Model):
         if self.invoice_date_due_payment_term:
             self.invoice_date_due = self.invoice_date_due_payment_term
 
+    def _compute_amount(self):
+        # Avoid recomputation of amount fields when we are only changing the
+        # invoice's due date
+        if self.env.context.get("bypass_compute_amount"):
+            return
+        return super()._compute_amount()
+
+    def onchange(self, values, field_name, field_onchange):
+        obj = self
+        if field_name == "invoice_date_due" and self.state == "posted":
+            # A change in the invoice date due would trigger a recompute of the
+            # amount fields. We want to avoid that to prevent inconsistencies with
+            # the payment state.
+            obj = self.with_context(bypass_compute_amount=True)
+        return super(AccountMove, obj).onchange(values, field_name, field_onchange)
+
     def write(self, vals):
         res = super().write(vals)
         # Propagate due date to move lines
