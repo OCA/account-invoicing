094f92bfe38f36a0672ce508a34dcc8d1d3d10dd
Víctor Martínez <victor.martinez@tecnativa.com>
2021-04-27 17:17:14 +0200

    [FIX] purchase_batch_invoicing: (Adapt to 13.0) remove from invoice lines with no quantity

--- purchase_batch_invoicing/models/account_move.py
+++ purchase_batch_invoicing/models/account_move.py
@@ -1,19 +1,20 @@
 # Copyright 2017 Eficent Business and IT Consulting Services
 # Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # Copyright 2021 Sergio Corato <https://github.com/sergiocorato>
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
-    @api.onchange('purchase_id')
-    def purchase_order_change(self):
+    @api.onchange("purchase_vendor_bill_id", "purchase_id")
+    def _onchange_purchase_auto_complete(self):
         """Remove lines with qty=0 if option selected making invoices."""
-        res = super().purchase_order_change()
-        if self._context.get('exclude_zero_qty', False):
+        res = super()._onchange_purchase_auto_complete()
+        if self._context.get("exclude_zero_qty", False):
             self.invoice_line_ids -= self.invoice_line_ids.filtered(
                 lambda x: x.quantity == 0.0
             )
