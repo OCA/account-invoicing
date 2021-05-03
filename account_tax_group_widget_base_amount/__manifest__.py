ea2c092aff7e7611fa2189e0b67c1f441084beab
david <david.vidal@tecnativa.com>
2021-05-03 17:31:05 +0200

    [ADD] account_tax_group_widget_base_amount: New module
    This module adds the base value to the tax group widget used in invoices and receipts
    as it's shown in reports.
    
    TT29538
    
    [UPD] Update account_tax_group_widget_base_amount.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_tax_group_widget_base_amount
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_tax_group_widget_base_amount/

---
+++ account_tax_group_widget_base_amount/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Tax Group Widget Base Amount",
+    "summary": "Adds base to tax group widget as it's put in the report",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-invoicing",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "qweb": ["static/src/xml/tax_group.xml"],
+}
---
+++ account_tax_group_widget_base_amount/readme/ROADMAP.rst
@@ -0,0 +1,4 @@
+* With the refactor of the tax group widget in v15, hopefully this module won't be
+  necessary. As for the moment that change is still in developement, check when the
+  moment to migrate to such version arrives.
+  (See https://github.com/odoo/odoo/pull/63338#issuecomment-821035897)
