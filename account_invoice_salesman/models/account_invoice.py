7c328113f4cca4ad2015d8202a7bacdd47ed4f82
elmonitor <info@elmonitor.net>
2021-05-18 17:17:34 -0500

    Remove unnecesary imports

--- account_invoice_salesman/models/account_invoice.py
+++ account_invoice_salesman/models/account_invoice.py
@@ -2,9 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api, _
-from odoo.exceptions import UserError
-from odoo.tools import config
-
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
