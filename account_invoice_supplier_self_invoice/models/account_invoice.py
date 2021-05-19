1bafad9dabf2d8750901f4034cc2f6ffd6ce3f87
Alba Riera <alba.riera@creublanca.es>
2021-05-19 13:40:34 +0200

    [IMP] account_invoice_supplier_self_invoice: black, isort, prettier

--- account_invoice_supplier_self_invoice/models/account_invoice.py
+++ account_invoice_supplier_self_invoice/models/account_invoice.py
@@ -5,15 +5,13 @@ from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
-    self_invoice_number = fields.Char(
-        readonly=True
-    )
-    set_self_invoice = fields.Boolean('Set self invoice')
-    can_self_invoice = fields.Boolean(related='partner_id.self_invoice')
+    self_invoice_number = fields.Char(readonly=True)
+    set_self_invoice = fields.Boolean("Set self invoice")
+    can_self_invoice = fields.Boolean(related="partner_id.self_invoice")
 
-    @api.onchange('partner_id', 'company_id')
+    @api.onchange("partner_id", "company_id")
     def _onchange_partner_id(self):
         res = super(AccountInvoice, self)._onchange_partner_id()
         self.set_self_invoice = self.partner_id.self_invoice
@@ -24,8 +22,11 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self).invoice_validate()
         for invoice in self:
             partner = invoice.partner_id
-            if partner.self_invoice and invoice.type in 'in_invoice' and \
-                    invoice.set_self_invoice:
+            if (
+                partner.self_invoice
+                and invoice.type in "in_invoice"
+                and invoice.set_self_invoice
+            ):
                 sequence = partner.self_invoice_sequence_id
                 invoice.self_invoice_number = sequence.with_context(
                     ir_sequence_date=invoice.date
@@ -35,7 +36,7 @@ class AccountInvoice(models.Model):
     @api.model
     def create(self, vals):
         onchanges = {
-            '_onchange_partner_id': ['set_self_invoice'],
+            "_onchange_partner_id": ["set_self_invoice"],
         }
         for onchange_method, changed_fields in onchanges.items():
             if any(f not in vals for f in changed_fields):
@@ -49,6 +50,7 @@ class AccountInvoice(models.Model):
         return super(AccountInvoice, self).create(vals)
 
     def action_view_account_invoice_self(self):
-        report = self.env['ir.actions.report']._get_report_from_name(
-            'account_invoice_supplier_self_invoice.report_invoice_self')
+        report = self.env["ir.actions.report"]._get_report_from_name(
+            "account_invoice_supplier_self_invoice.report_invoice_self"
+        )
         return report.report_action(self)
