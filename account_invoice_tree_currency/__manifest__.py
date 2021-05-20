441ea85912e4bf4e3547bd33fc072efec11e9007
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-05-20 17:13:34 +0200

    [MIG] account_invoice_tree_currency: Migration to 14.0

--- account_invoice_tree_currency/__manifest__.py
+++ account_invoice_tree_currency/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Invoice Tree Currency",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "summary": "Show currencies in the invoice tree view",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
     "depends": ["account"],
