c192080136c32e2dd2e0d86fecc896e267b8bf31
Alba Riera <alba.riera@creublanca.es>
2021-05-21 14:01:46 +0200

    [MIG] account_invoice_supplier_self_invoice: Migration to 13.0

--- account_invoice_supplier_self_invoice/tests/test_self_invoice.py
+++ account_invoice_supplier_self_invoice/tests/test_self_invoice.py
@@ -8,13 +8,16 @@ class TestSelfInvoice(common.TransactionCase):
     def setUp(self):
         res = super(TestSelfInvoice, self).setUp()
         self.partner = self.env["res.partner"].create(
-            {"name": "Partner", "supplier": True}
+            {"name": "Partner", "supplier_rank": 1}
         )
         self.simple_partner = self.env["res.partner"].create(
-            {"name": "Partner", "supplier": True}
+            {"name": "Partner", "supplier_rank": 1}
         )
         main_company = self.env.ref("base.main_company")
-        self.invoice = self.env["account.invoice"].create(
+        main_company.external_report_layout_id = self.env.ref(
+            "web.report_layout_standard"
+        ).view_id
+        self.invoice = self.env["account.move"].create(
             {
                 "company_id": main_company.id,
                 "partner_id": self.simple_partner.id,
@@ -30,14 +33,14 @@ class TestSelfInvoice(common.TransactionCase):
                 "user_type_id": self.env.ref("account.data_account_type_revenue").id,
             }
         )
-        self.env["account.invoice.line"].create(
+        self.env["account.move.line"].create(
             {
-                "invoice_id": self.invoice.id,
+                "move_id": self.invoice.id,
                 "product_id": product.id,
                 "quantity": 1,
                 "account_id": account.id,
                 "name": "Test product",
-                "price_unit": 20,
+                # "price_unit": 20,
             }
         )
         self.invoice._onchange_invoice_line_ids()
@@ -57,7 +60,7 @@ class TestSelfInvoice(common.TransactionCase):
 
     def test_none_self_invoice(self):
         self.assertFalse(self.invoice.self_invoice_number)
-        self.invoice.action_invoice_open()
+        self.invoice.action_post()
         self.assertFalse(self.invoice.self_invoice_number)
 
     def test_self_invoice(self):
@@ -68,5 +71,18 @@ class TestSelfInvoice(common.TransactionCase):
         self.invoice._onchange_partner_id()
         self.assertTrue(self.invoice.can_self_invoice)
         self.assertTrue(self.invoice.set_self_invoice)
-        self.invoice.action_invoice_open()
+        self.invoice.action_post()
         self.assertTrue(self.invoice.self_invoice_number)
+
+    def test_action_view_account_invoice_self(self):
+        self.partner.set_self_invoice()
+        self.invoice.partner_id = self.partner
+        self.invoice._onchange_partner_id()
+        self.invoice.action_post()
+        self.assertTrue(self.invoice.self_invoice_number)
+        report = self.invoice.action_view_account_invoice_self()
+        self.assertTrue(report)
+        self.assertEqual(report["type"], "ir.actions.report")
+        self.assertEqual(report["name"], "Self Invoice")
+        self.assertEqual(len(report["context"]["active_ids"]), 1)
+        self.assertEqual(report["context"]["active_ids"][0], self.invoice.id)
