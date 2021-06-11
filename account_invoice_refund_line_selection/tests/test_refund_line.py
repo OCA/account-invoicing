7e6179d113a31d7beea2f982a95ebeac8af7abb5
Enric Tobella <etobella@creublanca.es>
2021-06-11 12:15:01 +0200

    [MIG] account_invoice_refund_line_selection: Migration to 13.0

--- account_invoice_refund_line_selection/tests/test_refund_line.py
+++ account_invoice_refund_line_selection/tests/test_refund_line.py
@@ -1,72 +1,104 @@
 # Copyright 2019 Creu Blanca
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo.tests import common
+from odoo.addons.account.tests.account_test_savepoint import AccountTestInvoicingCommon
 
 
-class TestInvoiceRefundLine(common.SavepointCase):
-    filter_refund = "refund"
-
+class TestInvoiceRefundLine(AccountTestInvoicingCommon):
     @classmethod
-    def setUpClass(cls):
-        super(TestInvoiceRefundLine, cls).setUpClass()
-        cls.partner = cls.env["res.partner"].create({"name": "Test partner",})
-        default_line_account = cls.env["account.account"].search(
-            [
-                ("internal_type", "=", "other"),
-                ("deprecated", "=", False),
-                ("company_id", "=", cls.env.user.company_id.id),
-            ],
-            limit=1,
-        )
-        cls.invoice_lines = [
-            (
-                0,
-                False,
-                {
-                    "name": "Test description #1",
-                    "account_id": default_line_account.id,
-                    "quantity": 1.0,
-                    "price_unit": 100.0,
-                },
-            ),
-            (
-                0,
-                False,
-                {
-                    "name": "Test description #2",
-                    "account_id": default_line_account.id,
-                    "quantity": 2.0,
-                    "price_unit": 25.0,
-                },
-            ),
-        ]
-        cls.invoice = cls.env["account.invoice"].create(
-            {
-                "partner_id": cls.partner.id,
-                "type": "out_invoice",
-                "invoice_line_ids": cls.invoice_lines,
-            }
-        )
-        cls.invoice.action_invoice_open()
-        cls.refund_reason = "The refund reason"
-        cls.refund = (
-            cls.env["account.invoice.refund"]
-            .with_context(active_ids=cls.invoice.ids)
-            .create(
-                {
-                    "filter_refund": "refund_lines",
-                    "description": cls.refund_reason,
-                    "line_ids": [(6, 0, cls.invoice.invoice_line_ids[0].ids)],
-                }
-            )
-        )
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+        cls.in_invoice = cls.init_invoice("in_invoice")
+        cls.in_invoice.post()
+        cls.out_invoice = cls.init_invoice("out_invoice")
+        cls.out_invoice.post()
 
-    def test_refund_line(self):
-        self.refund.invoice_refund()
-        self.assertTrue(self.invoice.refund_invoice_ids)
-        refund = self.invoice.refund_invoice_ids[0]
-        self.assertTrue(len(refund.invoice_line_ids), 1)
-        self.assertTrue(
-            refund.invoice_line_ids[0].name, self.invoice.invoice_line_ids[0].name
+    def test_partial_refund_in_invoice(self):
+        reversal = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_id=self.in_invoice.id,
+                active_model=self.in_invoice._name,
+                active_ids=self.in_invoice.ids,
+            )
+            .create({})
         )
+        self.assertEqual(
+            reversal.selectable_invoice_lines_ids, self.in_invoice.invoice_line_ids,
+        )
+        line = self.in_invoice.invoice_line_ids[0]
+        reversal.write({"refund_method": "refund_lines", "line_ids": [(4, line.id)]})
+        action = reversal.reverse_moves()
+        refund = self.env[action["res_model"]].browse(action["res_id"])
+        self.assertTrue(refund)
+        self.assertEqual(refund._name, "account.move")
+        self.assertEqual(1, len(refund.invoice_line_ids))
+        self.assertEqual(line.product_id, refund.invoice_line_ids.product_id)
+        self.assertNotEqual(
+            (self.in_invoice.invoice_line_ids - line).product_id,
+            refund.invoice_line_ids.product_id,
+        )
+        self.assertNotEqual(refund.amount_total, self.out_invoice.amount_total)
+
+    def test_total_refund_in_invoice(self):
+        """Checking the old functionality"""
+        reversal = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_id=self.in_invoice.id,
+                active_model=self.in_invoice._name,
+                active_ids=self.in_invoice.ids,
+            )
+            .create({})
+        )
+        action = reversal.reverse_moves()
+        refund = self.env[action["res_model"]].browse(action["res_id"])
+        self.assertTrue(refund)
+        self.assertEqual(refund._name, "account.move")
+        self.assertEqual(2, len(refund.invoice_line_ids))
+        self.assertEqual(refund.amount_total, self.in_invoice.amount_total)
+
+    def test_partial_refund_out_invoice(self):
+        reversal = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_id=self.out_invoice.id,
+                active_model=self.out_invoice._name,
+                active_ids=self.out_invoice.ids,
+            )
+            .create({})
+        )
+        self.assertEqual(
+            reversal.selectable_invoice_lines_ids, self.out_invoice.invoice_line_ids,
+        )
+        line = self.out_invoice.invoice_line_ids[0]
+        reversal.write({"refund_method": "refund_lines", "line_ids": [(4, line.id)]})
+        action = reversal.reverse_moves()
+        refund = self.env[action["res_model"]].browse(action["res_id"])
+        self.assertTrue(refund)
+        self.assertEqual(refund._name, "account.move")
+        self.assertEqual(1, len(refund.invoice_line_ids))
+        self.assertEqual(line.product_id, refund.invoice_line_ids.product_id)
+        self.assertNotEqual(
+            (self.out_invoice.invoice_line_ids - line).product_id,
+            refund.invoice_line_ids.product_id,
+        )
+        self.assertNotEqual(refund.amount_total, self.out_invoice.amount_total)
+
+    def test_total_refund_out_invoice(self):
+        """Checking the old functionality"""
+        reversal = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_id=self.out_invoice.id,
+                active_model=self.out_invoice._name,
+                active_ids=self.out_invoice.ids,
+            )
+            .create({})
+        )
+        action = reversal.reverse_moves()
+        refund = self.env[action["res_model"]].browse(action["res_id"])
+        self.assertTrue(refund)
+        self.assertEqual(refund._name, "account.move")
+        self.assertEqual(2, len(refund.invoice_line_ids))
+        self.assertEqual(refund.amount_total, self.out_invoice.amount_total)
