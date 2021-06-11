a79dc3679d177fdf5dc8f6c49e5370454167d199
Enric Tobella <etobella@creublanca.es>
2021-06-11 11:03:52 +0200

    [MIG] account_invoice_blocking: Migration to 13.0

--- account_invoice_blocking/__manifest__.py
+++ account_invoice_blocking/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Account Invoice Blocking",
     "summary": "Set a blocking (No Follow-up) flag on invoices",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting",
     "author": "Acsone SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing/",
     "depends": ["account"],
-    "data": ["views/account_invoice.xml",],
+    "data": ["views/account_move.xml"],
 }
