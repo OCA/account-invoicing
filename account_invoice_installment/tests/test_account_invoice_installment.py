655334ed6014b45a25c6be348165f84c78772eaf
Eder Brito <britoederr@gmail.com>
2021-06-17 22:41:17 +0000

    correcting tests

--- account_invoice_installment/tests/test_account_invoice_installment.py
+++ account_invoice_installment/tests/test_account_invoice_installment.py
@@ -4,78 +4,78 @@ from odoo.tests import common
 
 
 class TestAccountInvoiceInstallment(common.TransactionCase):
-    def create_in_invoice(self):
-        account200 = self.env["account.account"].create(
-            {
-                "code": "200",
-                "name": "Account 200",
-                "user_type_id": self.env.ref("account.data_account_type_payable").id,
-                "reconcile": True,
-            }
-        )
-        partner_id = self.env.ref("base.res_partner_12").id
-        move_vals = {
-            "move_type": "in_invoice",
-            "partner_id": partner_id,
-            "line_ids": [
-                (
-                    0,
-                    0,
-                    {
-                        "name": "Test product",
-                        "quantity": 1,
-                        "price_unit": 450,
-                        "account_id": account200.id,
-                    },
-                )
-            ],
-        }
-        in_invoice = (
-            self.env["account.move"]
-            .with_context(default_move_type="in_invoice")
-            .create(move_vals)
-        )
-        return in_invoice
-
-    def create_out_invoice(self):
-        account100 = self.env["account.account"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        account100 = cls.env["account.account"].create(
             {
                 "code": "100",
                 "name": "Account 100",
-                "user_type_id": self.env.ref("account.data_account_type_receivable").id,
+                "user_type_id": cls.env.ref("account.data_account_type_receivable").id,
                 "reconcile": True,
             }
         )
-        partner_id = self.env.ref("base.res_partner_12").id
-        move_vals = {
-            "move_type": "in_invoice",
-            "partner_id": partner_id,
-            "line_ids": [
-                (
-                    0,
-                    0,
-                    {
-                        "name": "Test product",
-                        "quantity": 1,
-                        "price_unit": 450,
-                        "account_id": account100.id,
-                    },
-                )
-            ],
-        }
-        out_invoice = (
-            self.env["account.move"]
-            .with_context(default_move_type="out_invoice")
-            .create(move_vals)
+        account200 = cls.env["account.account"].create(
+            {
+                "code": "200",
+                "name": "Account 200",
+                "user_type_id": cls.env.ref("account.data_account_type_payable").id,
+                "reconcile": True,
+            }
         )
-        return out_invoice
+        cls.partner_id = cls.env.ref("base.res_partner_12").id
+        cls.env["account.journal"].create(
+            {
+                "name": "Journal 1",
+                "code": "J1",
+                "type": "sale",
+                "company_id": cls.env.user.company_id.id,
+            }
+        )
+        cls.invoice_lines = [
+            (
+                0,
+                False,
+                {
+                    "name": "Test description #1",
+                    "account_id": account100.id,
+                    "quantity": 1.0,
+                    "price_unit": 100.0,
+                },
+            ),
+            (
+                0,
+                False,
+                {
+                    "name": "Test description #2",
+                    "account_id": account200.id,
+                    "quantity": 1.0,
+                    "price_unit": 200.0,
+                },
+            ),
+        ]
+        cls.invoice_in = cls.env["account.move"].create(
+            {
+                "partner_id": cls.partner.id,
+                "move_type": "in_invoice",
+                "invoice_line_ids": cls.invoice_lines,
+            }
+        )
 
-    def test_compute(self):
-        in_invoice = self.create_in_invoice()
-        out_invoice = self.create_out_invoice()
+        cls.invoice_in._compute_receivable_move_line_ids()
+        cls.invoice_in.action_post()
 
-        out_invoice._compute_receivable_move_line_ids()
-        in_invoice._compute_payable_move_line_ids()
+        cls.invoice_out = cls.env["account.move"].create(
+            {
+                "partner_id": cls.partner.id,
+                "move_type": "out_invoice",
+                "invoice_line_ids": cls.invoice_lines,
+            }
+        )
 
-        self.isAssert(out_invoice.receivable_move_line_ids)
-        self.isAssert(in_invoice.payable_move_line_ids)
+        cls.invoice_out._compute_payable_move_line_ids()
+        cls.invoice_out.action_post()
+
+    def _test_installment(self):
+        self.assertTrue(self.invoice_out.receivable_move_line_ids)
+        self.assertTrue(self.invoice_in.payable_move_line_ids)
