08af3e21b748e5b2fcd4c7b2b357dc92b52b9b3a
Enric Tobella <etobella@creublanca.es>
2021-06-21 11:15:35 +0200

    [MIG] account_invoice_alternate_payer: Migration to 13.0

--- account_invoice_alternate_payer/models/account_payment.py
+++ account_invoice_alternate_payer/models/account_payment.py
@@ -3,48 +3,31 @@
 
 from odoo import api, models
 
-from odoo.addons.account.models.account_payment import MAP_INVOICE_TYPE_PARTNER_TYPE
 
-
-class AccountRegisterPayments(models.TransientModel):
-    _inherit = "account.register.payments"
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
 
     @api.model
-    def default_get(self, fields):
-        rec = super(AccountRegisterPayments, self).default_get(fields)
+    def default_get(self, default_fields):
+        rec = super().default_get(default_fields)
+        if self.env.context.get("active_model") != "account.move":
+            return rec
         active_ids = self._context.get("active_ids")
-        invoices = self.env["account.invoice"].browse(active_ids)
-
-        # Look if we are mixin multiple alternate_payer or
-        # customer invoices with vendor bills
-        payer = invoices[0].alternate_payer_id or invoices[0].commercial_partner_id
-        multi = any(
-            inv.alternate_payer_id or inv.commercial_partner_id != payer
-            for inv in invoices
+        invoices = (
+            self.env["account.move"]
+            .browse(active_ids)
+            .filtered(lambda move: move.is_invoice(include_receipts=True))
         )
-        payer_id = (
-            invoices[0].alternate_payer_id.id or invoices[0].commercial_partner_id.id
-        )
-        rec["partner_id"] = False if multi else payer_id
-        rec["multi"] = multi
+        if invoices and invoices[0].alternate_payer_id:
+            rec.update({"partner_id": invoices[0].alternate_payer_id.id})
         return rec
 
-    @api.multi
-    def _groupby_invoices(self):
-        # We have to re-group because the old groups are not valid anymore
-        super(AccountRegisterPayments, self)._groupby_invoices()
-        results = {}
-        for inv in self.invoice_ids:
-            payer = inv.alternate_payer_id.id or inv.commercial_partner_id.id
-            key = (payer, MAP_INVOICE_TYPE_PARTNER_TYPE[inv.type])
-            if key not in results:
-                results[key] = self.env["account.invoice"]
-            results[key] += inv
-        return results
 
-    @api.multi
+class AccountPaymentRegister(models.TransientModel):
+    _inherit = "account.payment.register"
+
     def _prepare_payment_vals(self, invoices):
-        res = super(AccountRegisterPayments, self)._prepare_payment_vals(invoices)
+        res = super()._prepare_payment_vals(invoices)
         payer_id = (
             invoices[0].alternate_payer_id.id or invoices[0].commercial_partner_id.id
         )
