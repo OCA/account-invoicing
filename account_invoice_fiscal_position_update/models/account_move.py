49774032a7fc1f3e9d5a157ff2bfd1f3c9c6348d
Alexis de Lattre <alexis.delattre@akretion.com>
2021-06-23 20:52:11 +0200

    [MIG] account_invoice_fiscal_position_update to v14

--- account_invoice_fiscal_position_update/models/account_move.py
+++ account_invoice_fiscal_position_update/models/account_move.py
@@ -1,5 +1,5 @@
-# Copyright 2011-2014 Julius Network Solutions SARL <contact@julius.fr>
-# Copyright 2014 Akretion (http://www.akretion.com)
+# Copyright 2011-2020 Julius Network Solutions SARL <contact@julius.fr>
+# Copyright 2014-2020 Akretion France (http://www.akretion.com)
 # @author Mathieu Vatel <mathieu _at_ julius.fr>
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -21,22 +21,21 @@ class AccountMove(models.Model):
     @api.onchange("fiscal_position_id")
     def fiscal_position_change(self):
         """Updates taxes and accounts on all invoice lines"""
-        self.ensure_one()
         res = {}
         lines_without_product = []
         fp = self.fiscal_position_id
-        inv_type = self.type
-        for line in self.invoice_line_ids:
+        for line in self.invoice_line_ids.filtered(lambda l: not l.display_type):
             if line.product_id:
                 account = line._get_computed_account()
-                product = line.with_context(force_company=self.company_id.id).product_id
-                if inv_type in ("out_invoice", "out_refund"):
+                # we could use _get_computed_taxes()
+                product = line.with_company(self.company_id.id).product_id
+                if self.is_sale_document(include_receipts=True):
                     # M2M fields don't have an option 'company_dependent=True'
                     # so we need per-company post-filtering
                     taxes = product.taxes_id.filtered(
                         lambda tax: tax.company_id == self.company_id
                     )
-                else:
+                elif self.is_purchase_document(include_receipts=True):
                     taxes = product.supplier_taxes_id.filtered(
                         lambda tax: tax.company_id == self.company_id
                     )
@@ -47,10 +46,13 @@ class AccountMove(models.Model):
                     taxes = fp.map_tax(taxes)
 
                 line.tax_ids = [(6, 0, taxes.ids)]
-
+                line._onchange_mark_recompute_taxes()
                 line.account_id = account.id
             else:
                 lines_without_product.append(line.name)
+
+        self.with_context(check_move_validity=False)._recompute_dynamic_lines()
+        self._check_balanced()
 
         if lines_without_product:
             res["warning"] = {"title": _("Warning")}
@@ -65,7 +67,7 @@ class AccountMove(models.Model):
                 res["warning"]["message"] = _(
                     "The following invoice lines were not updated "
                     "to the new Fiscal Position because they don't have a "
-                    "Product: - %s You should update the Account and the "
+                    "Product:\n - %s\nYou should update the Account and the "
                     "Taxes of these invoice lines manually."
-                ) % ("- ".join(lines_without_product))
+                ) % ("\n- ".join(lines_without_product))
         return res
