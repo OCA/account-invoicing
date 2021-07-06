bb162bf0e3fbd95d3a81371999fac1bfea3a6d2c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-07-06 17:23:04 +0200

    [FIX+REF] account_global_discount: Include taxes in global discount move lines
    Previously, no taxes were populated as base taxes for global discount
    move lines, which means that the tax reports were incorrect.
    
    The global discount lines + move lines has been injected other way for
    avoiding inconsistencies, and the rest of the features have been adapted
    according this.
    
    A migration script is provided as well for filling taxes in existing
    global discount move lines.
    
    [UPD] Update account_global_discount.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-invoicing-13.0/account-invoicing-13.0-account_global_discount
    Translate-URL: https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_global_discount/

--- account_global_discount/__manifest__.py
+++ account_global_discount/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright 2019 Tecnativa S.L. - David Vidal
-# Copyright 2020 Tecnativa - Pedro M. Baeza
+# Copyright 2020-2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Global Discount",
-    "version": "13.0.1.1.1",
+    "version": "13.0.2.0.0",
     "category": "Accounting",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-backend",
