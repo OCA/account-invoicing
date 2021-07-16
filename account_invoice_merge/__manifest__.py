488d22ef89bd38ac2dbd279f5ec5388f35cf5f1a
François Honoré <francois.honore@acsone.eu>
2021-07-16 10:14:02 +0200

    [IMP] account_invoice_merge: black, isort, prettier

--- account_invoice_merge/__manifest__.py
+++ account_invoice_merge/__manifest__.py
@@ -7,16 +7,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Invoice Merge',
-    'version': '12.0.1.0.1',
-    'category': 'Finance',
-    'summary': "Merge invoices in draft",
-    'author': "Elico Corp,Odoo Community Association (OCA)",
-    'website': 'http://www.openerp.net.cn',
-    'license': 'AGPL-3',
-    'depends': ['account'],
-    'data': [
-        'wizard/invoice_merge_view.xml',
-    ],
-    'installable': True,
+    "name": "Account Invoice Merge",
+    "version": "13.0.1.0.1",
+    "category": "Finance",
+    "summary": "Merge invoices in draft",
+    "author": "Elico Corp,Odoo Community Association (OCA)",
+    "website": "http://www.openerp.net.cn",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": ["wizard/invoice_merge_view.xml",],
+    "installable": True,
 }
