1bca63194d313e93e11f769dcd7a1609b5eb2b75
Mateu Griful <mateu.griful@forgeflow.com>
2021-07-20 12:31:17 +0200

    [ADD] account_move_tier_validation_approver

---
+++ account_move_tier_validation_approver/models/res_company.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    require_approver_in_vendor_bills = fields.Boolean(
+        string="Require approver in vendor bills"
+    )
+    validation_approver_tier_definition_id = fields.Many2one(
+        comodel_name="tier.definition", string="Bill approval tier definition"
+    )
