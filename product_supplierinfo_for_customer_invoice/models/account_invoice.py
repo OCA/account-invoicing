fff4e4be09ab18c5a0879da5a324167692f9af68
Lois Rilo <lois.rilo@forgeflow.com>
2021-07-22 12:11:54 +0200

    [IMP] product_supplierinfo_for_customer_invoice: black, isort, prettier

--- product_supplierinfo_for_customer_invoice/models/account_invoice.py
+++ product_supplierinfo_for_customer_invoice/models/account_invoice.py
@@ -6,25 +6,24 @@ from odoo import api, fields, models
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
     @api.multi
-    @api.depends('product_id')
+    @api.depends("product_id")
     def _get_product_customer_code(self):
-        product_customerinfo_obj = self.env['product.customerinfo']
-        for line in self.filtered(
-                lambda il: il.invoice_id.partner_id.customer):
+        product_customerinfo_obj = self.env["product.customerinfo"]
+        for line in self.filtered(lambda il: il.invoice_id.partner_id.customer):
             product = line.product_id
-            code_id = product_customerinfo_obj.search([
-                ('product_tmpl_id', '=', product.product_tmpl_id.id),
-                ('name', '=', line.invoice_id.partner_id.id)
-            ], limit=1)
-            line.product_customer_code = code_id.product_code or ''
+            code_id = product_customerinfo_obj.search(
+                [
+                    ("product_tmpl_id", "=", product.product_tmpl_id.id),
+                    ("name", "=", line.invoice_id.partner_id.id),
+                ],
+                limit=1,
+            )
+            line.product_customer_code = code_id.product_code or ""
 
     product_customer_code = fields.Char(
-        compute='_get_product_customer_code',
-        string='Product Customer Code',
+        compute="_get_product_customer_code", string="Product Customer Code",
     )
-    partner_is_customer = fields.Boolean(
-        related='invoice_id.partner_id.customer',
-    )
+    partner_is_customer = fields.Boolean(related="invoice_id.partner_id.customer",)
