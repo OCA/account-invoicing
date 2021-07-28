OCA/account-invoicing

commit 415bb0b4c2eea0435fbd6431dad8b239216c90dd

[IMP] account_invoice_date_due: Refactor and add tests
Don't use the wizard, but make the invoice_date_due editable for a specific group
Still, propagate the change to the invoice lines
Add tests

TT28875

account_invoice_date_due/models/account_move.py
@@ -1,0 +1,30 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    # Show invoice date due even when payment term is defined
+    invoice_date_due_payment_term = fields.Date(
+        related="invoice_date_due", string="Due Date Payment Term"
+    )
+
+    @api.onchange("invoice_date_due_payment_term")
+    def _onchange_invoice_date_due_payment_term(self):
+        """Propagate from Payment term due date to original field"""
+        if self.invoice_date_due_payment_term:
+            self.invoice_date_due = self.invoice_date_due_payment_term
+
+    def write(self, vals):
+        res = super().write(vals)
+        # Propagate due date to move lines
+        # that correspont to the receivable/payable account
+        if "invoice_date_due" in vals and self.state == "posted":
+            payment_term_lines = self.line_ids.filtered(
+                lambda line: line.account_id.user_type_id.type
+                in ("receivable", "payable")
+            )
+            payment_term_lines.write({"date_maturity": vals["invoice_date_due"]})
+        return res

account_invoice_date_due/__init__.py
@@ -1,3 +1,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import wizards
+from . import models

account_invoice_date_due/__manifest__.py
@@ -4,13 +4,13 @@
     "name": "Update Invoice's Due Date",
     "version": "13.0.1.0.0",
     "author": "Vauxoo,Odoo Community Association (OCA)",
-    "maintainers": ["luisg123v"],
+    "maintainers": ["luisg123v", "joao-p-marques"],
     "category": "Accounting",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "depends": ["account"],
     "demo": [],
-    "data": ["security/security.xml", "wizards/update_due_date_views.xml"],
+    "data": ["security/security.xml", "views/account_move_date_due.xml"],
     "installable": True,
     "auto_install": False,
 }

account_invoice_date_due/models/__init__.py
@@ -1,3 +1,3 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import update_due_date
+from . import account_move

account_invoice_date_due/static/description/index.html
@@ -1,0 +1,451 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="generator" content="Docutils: http://docutils.sourceforge.net/" />
+<title>Update Invoice's Due Date</title>
+<style type="text/css">
+
+/*
+:Author: David Goodger (goodger@python.org)
+:Id: $Id: html4css1.css 7952 2016-07-26 18:15:59Z milde $
+:Copyright: This stylesheet has been placed in the public domain.
+
+Default cascading style sheet for the HTML output of Docutils.
+
+See http://docutils.sf.net/docs/howto/html-stylesheets.html for how to
+customize this style sheet.
+*/
+
+/* used to remove borders from tables and images */
+.borderless, table.borderless td, table.borderless th {
+  border: 0 }
+
+table.borderless td, table.borderless th {
+  /* Override padding for "table.docutils td" with "! important".
+     The right padding separates the table cells. */
+  padding: 0 0.5em 0 0 ! important }
+
+.first {
+  /* Override more specific margin styles with "! important". */
+  margin-top: 0 ! important }
+
+.last, .with-subtitle {
+  margin-bottom: 0 ! important }
+
+.hidden {
+  display: none }
+
+.subscript {
+  vertical-align: sub;
+  font-size: smaller }
+
+.superscript {
+  vertical-align: super;
+  font-size: smaller }
+
+a.toc-backref {
+  text-decoration: none ;
+  color: black }
+
+blockquote.epigraph {
+  margin: 2em 5em ; }
+
+dl.docutils dd {
+  margin-bottom: 0.5em }
+
+object[type="image/svg+xml"], object[type="application/x-shockwave-flash"] {
+  overflow: hidden;
+}
+
+/* Uncomment (and remove this text!) to get bold-faced definition list terms
+dl.docutils dt {
+  font-weight: bold }
+*/
+
+div.abstract {
+  margin: 2em 5em }
+
+div.abstract p.topic-title {
+  font-weight: bold ;
+  text-align: center }
+
+div.admonition, div.attention, div.caution, div.danger, div.error,
+div.hint, div.important, div.note, div.tip, div.warning {
+  margin: 2em ;
+  border: medium outset ;
+  padding: 1em }
+
+div.admonition p.admonition-title, div.hint p.admonition-title,
+div.important p.admonition-title, div.note p.admonition-title,
+div.tip p.admonition-title {
+  font-weight: bold ;
+  font-family: sans-serif }
+
+div.attention p.admonition-title, div.caution p.admonition-title,
+div.danger p.admonition-title, div.error p.admonition-title,
+div.warning p.admonition-title, .code .error {
+  color: red ;
+  font-weight: bold ;
+  font-family: sans-serif }
+
+/* Uncomment (and remove this text!) to get reduced vertical space in
+   compound paragraphs.
+div.compound .compound-first, div.compound .compound-middle {
+  margin-bottom: 0.5em }
+
+div.compound .compound-last, div.compound .compound-middle {
+  margin-top: 0.5em }
+*/
+
+div.dedication {
+  margin: 2em 5em ;
+  text-align: center ;
+  font-style: italic }
+
+div.dedication p.topic-title {
+  font-weight: bold ;
+  font-style: normal }
+
+div.figure {
+  margin-left: 2em ;
+  margin-right: 2em }
+
+div.footer, div.header {
+  clear: both;
+  font-size: smaller }
+
+div.line-block {
+  display: block ;
+  margin-top: 1em ;
+  margin-bottom: 1em }
+
+div.line-block div.line-block {
+  margin-top: 0 ;
+  margin-bottom: 0 ;
+  margin-left: 1.5em }
+
+div.sidebar {
+  margin: 0 0 0.5em 1em ;
+  border: medium outset ;
+  padding: 1em ;
+  background-color: #ffffee ;
+  width: 40% ;
+  float: right ;
+  clear: right }
+
+div.sidebar p.rubric {
+  font-family: sans-serif ;
+  font-size: medium }
+
+div.system-messages {
+  margin: 5em }
+
+div.system-messages h1 {
+  color: red }
+
+div.system-message {
+  border: medium outset ;
+  padding: 1em }
+
+div.system-message p.system-message-title {
+  color: red ;
+  font-weight: bold }
+
+div.topic {
+  margin: 2em }
+
+h1.section-subtitle, h2.section-subtitle, h3.section-subtitle,
+h4.section-subtitle, h5.section-subtitle, h6.section-subtitle {
+  margin-top: 0.4em }
+
+h1.title {
+  text-align: center }
+
+h2.subtitle {
+  text-align: center }
+
+hr.docutils {
+  width: 75% }
+
+img.align-left, .figure.align-left, object.align-left, table.align-left {
+  clear: left ;
+  float: left ;
+  margin-right: 1em }
+
+img.align-right, .figure.align-right, object.align-right, table.align-right {
+  clear: right ;
+  float: right ;
+  margin-left: 1em }
+
+img.align-center, .figure.align-center, object.align-center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+table.align-center {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.align-left {
+  text-align: left }
+
+.align-center {
+  clear: both ;
+  text-align: center }
+
+.align-right {
+  text-align: right }
+
+/* reset inner alignment in figures */
+div.align-right {
+  text-align: inherit }
+
+/* div.align-center * { */
+/*   text-align: left } */
+
+.align-top    {
+  vertical-align: top }
+
+.align-middle {
+  vertical-align: middle }
+
+.align-bottom {
+  vertical-align: bottom }
+
+ol.simple, ul.simple {
+  margin-bottom: 1em }
+
+ol.arabic {
+  list-style: decimal }
+
+ol.loweralpha {
+  list-style: lower-alpha }
+
+ol.upperalpha {
+  list-style: upper-alpha }
+
+ol.lowerroman {
+  list-style: lower-roman }
+
+ol.upperroman {
+  list-style: upper-roman }
+
+p.attribution {
+  text-align: right ;
+  margin-left: 50% }
+
+p.caption {
+  font-style: italic }
+
+p.credits {
+  font-style: italic ;
+  font-size: smaller }
+
+p.label {
+  white-space: nowrap }
+
+p.rubric {
+  font-weight: bold ;
+  font-size: larger ;
+  color: maroon ;
+  text-align: center }
+
+p.sidebar-title {
+  font-family: sans-serif ;
+  font-weight: bold ;
+  font-size: larger }
+
+p.sidebar-subtitle {
+  font-family: sans-serif ;
+  font-weight: bold }
+
+p.topic-title {
+  font-weight: bold }
+
+pre.address {
+  margin-bottom: 0 ;
+  margin-top: 0 ;
+  font: inherit }
+
+pre.literal-block, pre.doctest-block, pre.math, pre.code {
+  margin-left: 2em ;
+  margin-right: 2em }
+
+pre.code .ln { color: grey; } /* line numbers */
+pre.code, code { background-color: #eeeeee }
+pre.code .comment, code .comment { color: #5C6576 }
+pre.code .keyword, code .keyword { color: #3B0D06; font-weight: bold }
+pre.code .literal.string, code .literal.string { color: #0C5404 }
+pre.code .name.builtin, code .name.builtin { color: #352B84 }
+pre.code .deleted, code .deleted { background-color: #DEB0A1}
+pre.code .inserted, code .inserted { background-color: #A3D289}
+
+span.classifier {
+  font-family: sans-serif ;
+  font-style: oblique }
+
+span.classifier-delimiter {
+  font-family: sans-serif ;
+  font-weight: bold }
+
+span.interpreted {
+  font-family: sans-serif }
+
+span.option {
+  white-space: nowrap }
+
+span.pre {
+  white-space: pre }
+
+span.problematic {
+  color: red }
+
+span.section-subtitle {
+  /* font-size relative to parent (h1..h6 element) */
+  font-size: 80% }
+
+table.citation {
+  border-left: solid 1px gray;
+  margin-left: 1px }
+
+table.docinfo {
+  margin: 2em 4em }
+
+table.docutils {
+  margin-top: 0.5em ;
+  margin-bottom: 0.5em }
+
+table.footnote {
+  border-left: solid 1px black;
+  margin-left: 1px }
+
+table.docutils td, table.docutils th,
+table.docinfo td, table.docinfo th {
+  padding-left: 0.5em ;
+  padding-right: 0.5em ;
+  vertical-align: top }
+
+table.docutils th.field-name, table.docinfo th.docinfo-name {
+  font-weight: bold ;
+  text-align: left ;
+  white-space: nowrap ;
+  padding-left: 0 }
+
+/* "booktabs" style (no vertical lines) */
+table.docutils.booktabs {
+  border: 0px;
+  border-top: 2px solid;
+  border-bottom: 2px solid;
+  border-collapse: collapse;
+}
+table.docutils.booktabs * {
+  border: 0px;
+}
+table.docutils.booktabs th {
+  border-bottom: thin solid;
+  text-align: left;
+}
+
+h1 tt.docutils, h2 tt.docutils, h3 tt.docutils,
+h4 tt.docutils, h5 tt.docutils, h6 tt.docutils {
+  font-size: 100% }
+
+ul.auto-toc {
+  list-style-type: none }
+
+</style>
+</head>
+<body>
+<div class="document" id="update-invoice-s-due-date">
+<h1 class="title">Update Invoice’s Due Date</h1>
+
+<!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! This file is generated by oca-gen-addon-readme !!
+!! changes will be overwritten.                   !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -->
+<p><a class="reference external" href="https://odoo-community.org/page/development-status"><img alt="Beta" src="https://img.shields.io/badge/maturity-Beta-yellow.png" /></a> <a class="reference external" href="http://www.gnu.org/licenses/agpl-3.0-standalone.html"><img alt="License: AGPL-3" src="https://img.shields.io/badge/licence-AGPL--3-blue.png" /></a> <a class="reference external" href="https://github.com/OCA/account-invoicing/tree/13.0/account_invoice_date_due"><img alt="OCA/account-invoicing" src="https://img.shields.io/badge/github-OCA%2Faccount--invoicing-lightgray.png?logo=github" /></a> <a class="reference external" href="https://translation.odoo-community.org/projects/account-invoicing-13-0/account-invoicing-13-0-account_invoice_date_due"><img alt="Translate me on Weblate" src="https://img.shields.io/badge/weblate-Translate%20me-F47D42.png" /></a> <a class="reference external" href="https://runbot.odoo-community.org/runbot/95/13.0"><img alt="Try me on Runbot" src="https://img.shields.io/badge/runbot-Try%20me-875A7B.png" /></a></p>
+<p>This module sets the invoice’s due date field as editable (Even if the invoice state is done and a Payment Term is set).</p>
+<p>When the field is edited, the invoice’s due date is updated, along with the
+related invoice lines.</p>
+<p><strong>Table of contents</strong></p>
+<div class="contents local topic" id="contents">
+<ul class="simple">
+<li><a class="reference internal" href="#configuration" id="id1">Configuration</a></li>
+<li><a class="reference internal" href="#usage" id="id2">Usage</a></li>
+<li><a class="reference internal" href="#bug-tracker" id="id3">Bug Tracker</a></li>
+<li><a class="reference internal" href="#credits" id="id4">Credits</a><ul>
+<li><a class="reference internal" href="#authors" id="id5">Authors</a></li>
+<li><a class="reference internal" href="#contributors" id="id6">Contributors</a></li>
+<li><a class="reference internal" href="#maintainers" id="id7">Maintainers</a></li>
+</ul>
+</li>
+</ul>
+</div>
+<div class="section" id="configuration">
+<h1><a class="toc-backref" href="#id1">Configuration</a></h1>
+<p>To be able to edit this field, the user needs to have access to the group
+<tt class="docutils literal">Allow to change due date</tt>.</p>
+</div>
+<div class="section" id="usage">
+<h1><a class="toc-backref" href="#id2">Usage</a></h1>
+<p>To edit the invoice’s due date, we have several scenarios:</p>
+<ul class="simple">
+<li>A Payment Term is set. In this case, no extra field will be shown while in
+draft, as the standard flow allows us to edit the Payment Term directly, or
+delete it and set a due date manually.
+Once the invoice is posted, a new field will be shown in the view above the
+Payment Term, allowing the due date to be changed.</li>
+<li>No Payment Term is set, and the due date is set manually. In this case, the
+due date is editable while in draft, as with the standard Odoo flow. However,
+once posted, the field will remain editable.</li>
+</ul>
+<p>(All the above are considering the user belongs to the security group refered in
+“Configuration”)</p>
+</div>
+<div class="section" id="bug-tracker">
+<h1><a class="toc-backref" href="#id3">Bug Tracker</a></h1>
+<p>Bugs are tracked on <a class="reference external" href="https://github.com/OCA/account-invoicing/issues">GitHub Issues</a>.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed
+<a class="reference external" href="https://github.com/OCA/account-invoicing/issues/new?body=module:%20account_invoice_date_due%0Aversion:%2013.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**">feedback</a>.</p>
+<p>Do not contact contributors directly about support or help with technical issues.</p>
+</div>
+<div class="section" id="credits">
+<h1><a class="toc-backref" href="#id4">Credits</a></h1>
+<div class="section" id="authors">
+<h2><a class="toc-backref" href="#id5">Authors</a></h2>
+<ul class="simple">
+<li>Vauxoo</li>
+</ul>
+</div>
+<div class="section" id="contributors">
+<h2><a class="toc-backref" href="#id6">Contributors</a></h2>
+<ul class="simple">
+<li>Luis González &lt;<a class="reference external" href="mailto:lgonzalez&#64;vauxoo.com">lgonzalez&#64;vauxoo.com</a>&gt;</li>
+<li>Luis Torres &lt;<a class="reference external" href="mailto:luis_t&#64;vauxoo.com">luis_t&#64;vauxoo.com</a>&gt;</li>
+<li><a class="reference external" href="https://www.tecnativa.com">Tecnativa</a>:<ul>
+<li>João Marques</li>
+</ul>
+</li>
+</ul>
+</div>
+<div class="section" id="maintainers">
+<h2><a class="toc-backref" href="#id7">Maintainers</a></h2>
+<p>This module is maintained by the OCA.</p>
+<a class="reference external image-reference" href="https://odoo-community.org"><img alt="Odoo Community Association" src="https://odoo-community.org/logo.png" /></a>
+<p>OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.</p>
+<p>Current <a class="reference external" href="https://odoo-community.org/page/maintainer-role">maintainers</a>:</p>
+<p><a class="reference external" href="https://github.com/luisg123v"><img alt="luisg123v" src="https://github.com/luisg123v.png?size=40px" /></a> <a class="reference external" href="https://github.com/joao-p-marques"><img alt="joao-p-marques" src="https://github.com/joao-p-marques.png?size=40px" /></a></p>
+<p>This module is part of the <a class="reference external" href="https://github.com/OCA/account-invoicing/tree/13.0/account_invoice_date_due">OCA/account-invoicing</a> project on GitHub.</p>
+<p>You are welcome to contribute. To learn how please visit <a class="reference external" href="https://odoo-community.org/page/Contribute">https://odoo-community.org/page/Contribute</a>.</p>
+</div>
+</div>
+</div>
+</body>
+</html>

account_invoice_date_due/tests/__init__.py
@@ -1,0 +1,3 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_account_invoice_date_due

account_invoice_date_due/tests/test_account_invoice_date_due.py
@@ -1,0 +1,134 @@
+# Copyright 2021 Tecnativa - João Marques
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from datetime import datetime, timedelta
+
+from odoo import fields
+from odoo.tests import Form, common
+
+
+class TestAccountInvoiceDateDue(common.TransactionCase):
+    def setUp(self):
+        super().setUp()
+        # Create new user allowed to change invoice due date
+        group = self.env.ref("account_invoice_date_due.allow_to_change_due_date")
+        acc_group = self.env.ref("account.group_account_manager")
+        self.user_w_access = self.env["res.users"].create(
+            {
+                "name": "Test User w/ access",
+                "login": "user_w_access",
+                "groups_id": [(6, 0, [group.id, acc_group.id])],
+            }
+        )
+        # Create new user not allowed to change invoice due date
+        self.user_wo_access = self.env["res.users"].create(
+            {
+                "name": "Test User wo/ access",
+                "login": "user_wo_access",
+                "groups_id": [(6, 0, [acc_group.id])],
+            }
+        )
+        account100 = self.env["account.account"].create(
+            {
+                "code": "100",
+                "name": "Account 100",
+                "user_type_id": self.env.ref("account.data_account_type_receivable").id,
+                "reconcile": True,
+            }
+        )
+        account300 = self.env["account.account"].create(
+            {
+                "code": "300",
+                "name": "Account 300",
+                "user_type_id": self.env.ref(
+                    "account.data_account_type_other_income"
+                ).id,
+            }
+        )
+        move_form = Form(self.env["account.move"])
+        move_form.date = fields.Date.today()
+        with move_form.invoice_line_ids.new() as line_form:
+            line_form.name = "move test"
+            line_form.debit = 0.0
+            line_form.credit = 1000.0
+            line_form.account_id = account300
+        with move_form.invoice_line_ids.new() as line_form:
+            line_form.name = "move test"
+            line_form.debit = 1000.0
+            line_form.credit = 0.0
+            line_form.account_id = account100
+        self.move = move_form.save()
+
+    def test_invoice_date_due_is_editable(self):
+        move_edit_form = Form(self.move.with_user(self.user_w_access))
+        ten_days_from_now = fields.Date.to_string(datetime.today() + timedelta(days=10))
+        move_edit_form.invoice_date_due = ten_days_from_now
+        self.move = move_edit_form.save()
+        self.assertEquals(
+            fields.Date.to_string(self.move.invoice_date_due), ten_days_from_now
+        )
+        # Post and should remain editable
+        self.move.action_post()
+        move_edit_form = Form(self.move.with_user(self.user_w_access))
+        twenty_days_from_now = fields.Date.to_string(
+            datetime.today() + timedelta(days=20)
+        )
+        move_edit_form.invoice_date_due = twenty_days_from_now
+        self.move = move_edit_form.save()
+        # Check that the change has been propagated to the corresponding invoice line
+        self.assertEquals(
+            len(
+                self.move.line_ids.filtered(
+                    lambda l: fields.Date.to_string(l.date_maturity)
+                    == twenty_days_from_now
+                    and l.account_id.user_type_id.type in ("receivable", "payable")
+                )
+            ),
+            1,
+        )
+
+    def test_invoice_date_due_is_editable_w_payment_term(self):
+        move_edit_form = Form(self.move.with_user(self.user_w_access))
+        move_edit_form.invoice_payment_term_id = self.env.ref(
+            "account.account_payment_term_15days"
+        )
+        self.move = move_edit_form.save()
+        self.assertFalse(self.move.invoice_date_due)
+        # Post and should remain editable even w/ payment term
+        twenty_days_from_now = fields.Date.to_string(
+            datetime.today() + timedelta(days=20)
+        )
+        self.move.action_post()
+        move_edit_form = Form(self.move.with_user(self.user_w_access))
+        move_edit_form.invoice_date_due_payment_term = twenty_days_from_now
+        self.move = move_edit_form.save()
+        # Check that the change has been propagated to the corresponding invoice line
+        self.assertEquals(
+            len(
+                self.move.line_ids.filtered(
+                    lambda l: fields.Date.to_string(l.date_maturity)
+                    == twenty_days_from_now
+                    and l.account_id.user_type_id.type in ("receivable", "payable")
+                )
+            ),
+            1,
+        )
+
+    def test_invoice_date_due_is_not_editable_for_user_wo_access(self):
+        move_edit_form = Form(self.move.with_user(self.user_wo_access))
+        ten_days_from_now = fields.Date.to_string(datetime.today() + timedelta(days=10))
+        move_edit_form.invoice_date_due = ten_days_from_now
+        self.move = move_edit_form.save()
+        self.assertEquals(
+            fields.Date.to_string(self.move.invoice_date_due), ten_days_from_now
+        )  # Should be editable while in draft
+        # Post and should not be editable for this user
+        self.move.action_post()
+        move_edit_form = Form(self.move.with_user(self.user_wo_access))
+        twenty_days_from_now = fields.Date.to_string(
+            datetime.today() + timedelta(days=20)
+        )
+        with self.assertRaisesRegex(
+            AssertionError, r"can't\swrite\son\sreadonly\sfield.*"
+        ):
+            move_edit_form.invoice_date_due = twenty_days_from_now

account_invoice_date_due/views/account_move_date_due.xml
@@ -1,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!-- License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). -->
+<odoo>
+    <record id="view_move_form" model="ir.ui.view">
+        <field name="name">account.move.form</field>
+        <field name="model">account.move</field>
+        <field name="inherit_id" ref="account.view_move_form" />
+        <field name="arch" type="xml">
+            <field name="invoice_date" position="after">
+                <field
+                    name="invoice_date_due_payment_term"
+                    string="Due Date"
+                    groups="account_invoice_date_due.allow_to_change_due_date"
+                    attrs="{'readonly': [('state', 'not in', ['posted', 'draft'])], 'invisible': ['|', ('state', '=', 'draft'), ('invoice_payment_term_id', '=', False)]}"
+                />
+            </field>
+            <field name="invoice_date_due" position="attributes">
+                <attribute
+                    name="groups"
+                >!account_invoice_date_due.allow_to_change_due_date</attribute>
+            </field>
+            <field name="invoice_date_due" position="after">
+                <field
+                    name="invoice_date_due"
+                    groups="account_invoice_date_due.allow_to_change_due_date"
+                    attrs="{'readonly': [('state', 'not in', ['posted', 'draft'])], 'invisible': [('invoice_payment_term_id', '!=', False)]}"
+                />
+            </field>
+        </field>
+    </record>
+</odoo>

commit 7a1e22b60bfa2f3b39f81e8821a8052eb687a506

[FIX] account_invoice_date_due: restore multi-record handling in write()
415bb0b4c2eea0435fbd6431dad8b239216c90dd introduced a bug disallowing to write() in a multi-record set.
The bug is reproduced when doing mass edits.
This restores that possibility.

TT31218

account_invoice_date_due/models/account_move.py
@@ -21,10 +21,11 @@ class AccountMove(models.Model):
         res = super().write(vals)
         # Propagate due date to move lines
         # that correspont to the receivable/payable account
-        if "invoice_date_due" in vals and self.state == "posted":
-            payment_term_lines = self.line_ids.filtered(
-                lambda line: line.account_id.user_type_id.type
-                in ("receivable", "payable")
-            )
-            payment_term_lines.write({"date_maturity": vals["invoice_date_due"]})
+        if "invoice_date_due" in vals:
+            for record in self.filtered(lambda r: r.state == "posted"):
+                payment_term_lines = record.line_ids.filtered(
+                    lambda line: line.account_id.user_type_id.type
+                    in ("receivable", "payable")
+                )
+                payment_term_lines.write({"date_maturity": vals["invoice_date_due"]})
         return res