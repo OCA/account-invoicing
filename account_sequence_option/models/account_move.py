4c98453b242fc9e9f73608e9dfbc15d1213e559d
Kitti U <kittiu@gmail.com>
2021-07-30 15:04:29 +0700

    Fix problem when use with Payment

--- account_sequence_option/models/account_move.py
+++ account_sequence_option/models/account_move.py
@@ -17,32 +17,32 @@ class AccountMove(models.Model):
 
     @api.depends("posted_before", "state", "journal_id", "date")
     def _compute_name(self):
+        options = self.env["sequence.option"].get_model_options(self._name)
         # On post, get the sequence option
-        for rec in self.filtered(
-            lambda l: l.name in (False, "/") and l.state == "posted"
-        ):
-            sequence = self.env["sequence.option"].get_sequence(rec)
-            if sequence:
-                rec.name = sequence.next_by_id(sequence_date=rec.date)
-                rec.sequence_option = True
+        if options:
+            for rec in self.filtered(
+                lambda l: l.name in (False, "/") and l.state == "posted"
+            ):
+                sequence = self.env["sequence.option"].get_sequence(
+                    rec, options=options
+                )
+                if sequence:
+                    rec.name = sequence.next_by_id(sequence_date=rec.date)
+                    rec.sequence_option = True
 
         # Call super()
         super()._compute_name()
-
         # On draft, odoo may still suggest the 1st new number too, remove it.
-        for rec in self.filtered(
-            lambda l: l.name not in (False, "/") and l.state == "draft"
-        ):
-            sequence = self.env["sequence.option"].get_sequence(rec)
-            if sequence:
+        if options:
+            for rec in self.filtered(
+                lambda l: l.name not in (False, "/") and l.state == "draft"
+            ):
                 rec.name = "/"
 
     # Bypass constrains if sequence is defined
     def _constrains_date_sequence(self):
-        for record in self:
-            sequence = self.env["sequence.option"].get_sequence(record)
-            if not sequence:
-                record.super()._constrains_date_sequence()
+        records = self.filtered(lambda l: self.env["sequence.option"].get_sequence(l))
+        return super(AccountMove, self - records)._constrains_date_sequence()
 
     def _get_last_sequence_domain(self, relaxed=False):
         (where_string, param) = super()._get_last_sequence_domain(relaxed=relaxed)
