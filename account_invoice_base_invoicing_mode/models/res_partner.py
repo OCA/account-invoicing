d4b78255d7885434e8cff4c05227ceb5d43a4d07
SilvioC2C <silvio.gregorini@camptocamp.com>
2021-08-05 11:22:04 +0200

    [FIX] account_invoice_base_invoicing_mode: Add 'invoicing_mode' and 'one_invoice_per_order' to res.partner's _commercial_fields()

--- account_invoice_base_invoicing_mode/models/res_partner.py
+++ account_invoice_base_invoicing_mode/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -13,3 +13,10 @@ class ResPartner(models.Model):
         default=False,
         help="Do not group sale order into one invoice.",
     )
+
+    @api.model
+    def _commercial_fields(self):
+        return super()._commercial_fields() + [
+            "invoicing_mode",
+            "one_invoice_per_order",
+        ]
