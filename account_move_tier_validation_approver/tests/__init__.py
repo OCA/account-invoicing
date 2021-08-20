a5c28489e5d78be6481461b2b52f0b4c8645a5f4
Mateu Griful <mateu.griful@forgeflow.com>
2021-08-20 12:37:55 +0200

    [14.0][MIG] account_move_tier_validation_approver

--- account_move_tier_validation_approver/tests/__init__.py
+++ account_move_tier_validation_approver/tests/__init__.py
@@ -1,3 +1,3 @@
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_account_move_tier_validation_approver
