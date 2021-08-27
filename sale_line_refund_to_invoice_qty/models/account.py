7002c66e38b0d71febf41558fd78b4879c51a073
AaronHForgeFlow <ahenriquez@eficent.com>
2021-08-27 08:58:47 +0200

    [IMP]sale_line_refund_to_invoice_qty: do not use negative booleans

--- sale_line_refund_to_invoice_qty/models/account.py
+++ sale_line_refund_to_invoice_qty/models/account.py
@@ -7,22 +7,21 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _reverse_move_vals(self, default_values, cancel=True):
-        # Set the sale_qty_not_to_reinvoice based on the boolean from the
+        # Set the sale_qty_to_reinvoice based on the boolean from the
         # reversal wizard
         move_vals = super(AccountMove, self)._reverse_move_vals(
             default_values, cancel=cancel
         )
-        if self.env.context.get("sale_qty_not_to_reinvoice", False):
+        if self.env.context.get("sale_qty_to_reinvoice", False):
             for vals in move_vals["line_ids"]:
-                vals[2].update({"sale_qty_not_to_reinvoice": True})
+                vals[2].update({"sale_qty_to_reinvoice": True})
         return move_vals
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    sale_qty_not_to_reinvoice = fields.Boolean(
-        string="Sale qty not to reinvoice",
-        help="If marked, the quantity in this move line will not be considered "
-        "as quantity to be reinvoiced in the related sales order line.",
+    sale_qty_to_reinvoice = fields.Boolean(
+        string="Sale qty to reinvoice",
+        help="Leave it marked if you will reinvoice the same sale order line",
     )
