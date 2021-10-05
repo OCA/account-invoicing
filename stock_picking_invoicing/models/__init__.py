5ffea7bd421caad7174d41d4604c2a81b0069790
Magno Costa <magno.costa@akretion.com.br>
2021-10-04 23:04:45 -0300

    [MIG] Object account.invoice was merged in account.move .

--- stock_picking_invoicing/models/__init__.py
+++ stock_picking_invoicing/models/__init__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import stock_invoice_state_mixin
-from . import account_invoice
+from . import account_move
 from . import stock_move
 from . import stock_picking
 from . import stock_picking_type
