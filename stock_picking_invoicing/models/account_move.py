06a75434253a07eebcac4bd9d23277ed77aeb3aa
Magno Costa <magno.costa@akretion.com.br>
2021-10-04 23:52:52 -0300

    [MIG] Method don't exist anymore and work without it.

--- stock_picking_invoicing/models/account_move.py
+++ stock_picking_invoicing/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountInvoice(models.Model):
@@ -42,26 +42,3 @@ class AccountInvoice(models.Model):
         self.mapped("invoice_line_ids.move_line_ids")._set_as_2binvoiced()
         pickings._set_as_2binvoiced()
         return super().unlink()
-
-    @api.model
-    def _prepare_refund(
-        self, invoice, date_invoice=None, date=None, description=None, journal_id=None
-    ):
-        """
-        Inherit to put link picking of the invoice into the new refund
-        :param invoice: self recordset
-        :param date_invoice: str
-        :param date: str
-        :param description: str
-        :param journal_id: int
-        :return: dict
-        """
-        result = super()._prepare_refund(
-            invoice=invoice,
-            invoice_date=date_invoice,
-            date=date,
-            description=description,
-            journal_id=journal_id,
-        )
-        result.update({"picking_ids": [(6, False, invoice.picking_ids.ids)]})
-        return result
