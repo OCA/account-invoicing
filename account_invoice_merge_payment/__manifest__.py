d310c844a4d1df62058a457622af9e9d6f04766c
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-10-14 15:47:30 +0200

    [IMP] account_invoice_merge_payment: black, isort, prettier

--- account_invoice_merge_payment/__manifest__.py
+++ account_invoice_merge_payment/__manifest__.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "account_invoice_merge_payment",
-    'summary': """
+    "name": "account_invoice_merge_payment",
+    "summary": """
         Use invoice merge regarding fields on Account Payment Partner""",
-    'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
-    'website': "http://www.acsone.eu",
-    'category': 'Invoicing & Payments',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_invoice_merge',
-        'account_payment_partner',
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "category": "Invoicing & Payments",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_merge",
+        "account_payment_partner",
     ],
-    'auto_install': True,
-    'installable': True,
+    "auto_install": True,
+    "installable": True,
 }
