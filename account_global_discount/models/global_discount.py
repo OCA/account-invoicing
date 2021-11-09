c7ee1a3eda2125ce0411ab9591a2bb04bbf4d290
Omar (Comunitea) <omar@comunitea.com>
2021-11-09 20:56:26 +0100

    [IMP] account_global_discount: securization for multi-company

--- account_global_discount/models/global_discount.py
+++ account_global_discount/models/global_discount.py
@@ -5,15 +5,18 @@ from odoo import fields, models
 
 class GlobalDiscount(models.Model):
     _inherit = "global.discount"
+    _check_company_auto = True
 
     account_id = fields.Many2one(
         comodel_name="account.account",
         string="Account",
         domain="[('user_type_id.type', 'not in', ['receivable', 'payable'])]",
+        check_company=True,
     )
     account_analytic_id = fields.Many2one(
         comodel_name="account.analytic.account",
         string="Analytic account",
+        check_company=True,
     )
 
     def _get_global_discount_vals(self, base, account_id=False, **kwargs):
