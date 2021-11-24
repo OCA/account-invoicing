948d83f8b83c9179dbd92571cac2e01ce7bd7705
OriolVForgeFlow <oriol.villamayor@forgeflow.com>
2021-11-24 10:54:01 +0100

    [15.0][MIG] account_invoice_tree_currency: Migration to 15.0

--- account_invoice_refund_link/__manifest__.py
+++ account_invoice_refund_link/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Show links between refunds and their originator invoices",
+    "name": "Show links between refunds and their originator invoices.",
     "version": "15.0.1.0.0",
     "development_status": "Mature",
     "category": "Accounting & Finance",
