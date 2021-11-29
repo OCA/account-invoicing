c8d6ab64110c3037e0a24f166d96caa8a86c3a5f
Akim Juillerat <akim.juillerat@camptocamp.com>
2021-11-29 09:41:06 +0100

    account_invoice_section_sale_order: Custom section name
    Refactor preparation of sections
    
    Refactor to allow different section grouping
    
    Add field to allow different possibilities of grouping invoice lines from sale order

--- account_invoice_section_sale_order/models/sale_order.py
+++ account_invoice_section_sale_order/models/sale_order.py
@@ -1,57 +1,67 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+from collections import OrderedDict
 
 from odoo import models
+from odoo.tools.safe_eval import safe_eval, time
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _create_invoices(self, grouped=False, final=False, date=None):
-        """Add sections by sale order in the invoice line.
+        """Add sections by groups in the invoice line.
 
-        Order the invoicing lines by sale order and add lines section with
-        the sale order name.
-        Only do this for invoices targetting multiple sale order
+        Order the invoicing lines by groups and add lines section with
+        the group name.
+        Only do this for invoices targetting multiple groups
         """
         invoice_ids = super()._create_invoices(grouped=grouped, final=final, date=date)
         for invoice in invoice_ids:
-            if len(invoice.line_ids.mapped("sale_line_ids.order_id.id")) == 1:
+            if (
+                len(invoice.line_ids.mapped(invoice.line_ids._get_section_grouping()))
+                == 1
+            ):
                 continue
-            so = None
             sequence = 10
+            move_lines = invoice._get_ordered_invoice_lines()
+            # Group move lines according to their sale order
+            section_grouping_matrix = OrderedDict()
+            for move_line in move_lines:
+                group = move_line._get_section_group()
+                section_grouping_matrix.setdefault(group, []).append(move_line.id)
+            # Prepare section lines for each group
             section_lines = []
-            lines = self._get_ordered_invoice_lines(invoice)
-            for line in lines:
-                if line.sale_line_ids.order_id and so != line.sale_line_ids.order_id:
-                    so = line.sale_line_ids.order_id
+            for group, move_line_ids in section_grouping_matrix.items():
+                if group:
                     section_lines.append(
                         (
                             0,
                             0,
                             {
-                                "name": so._get_saleorder_section_name(),
+                                "name": group._get_invoice_section_name(),
                                 "display_type": "line_section",
                                 "sequence": sequence,
                             },
                         )
                     )
                     sequence += 10
-                line.sequence = sequence
-                sequence += 10
+                for move_line in self.env["account.move.line"].browse(move_line_ids):
+                    move_line.sequence = sequence
+                    sequence += 10
             invoice.line_ids = section_lines
-
         return invoice_ids
 
-    def _get_ordered_invoice_lines(self, invoice):
-        return invoice.line_ids.sorted(
-            key=lambda r: r.sale_line_ids.order_id.id
-        ).filtered(lambda r: not r.exclude_from_invoice_tab)
-
-    def _get_saleorder_section_name(self):
+    def _get_invoice_section_name(self):
         """Returns the text for the section name."""
         self.ensure_one()
-        if self.client_order_ref:
+        naming_scheme = (
+            self.partner_invoice_id.invoice_section_name_scheme
+            or self.company_id.invoice_section_name_scheme
+        )
+        if naming_scheme:
+            return safe_eval(naming_scheme, {"object": self, "time": time})
+        elif self.client_order_ref:
             return "{} - {}".format(self.name, self.client_order_ref or "")
         else:
             return self.name
--- account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
+++ account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
@@ -1,7 +1,14 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import mock
 
+from odoo.exceptions import UserError
 from odoo.tests.common import SavepointCase
+
+SECTION_GROUPING_FUNCTION = "odoo.addons.account_invoice_section_sale_order.models.account_move.AccountMoveLine._get_section_grouping"  # noqa
+SECTION_NAME_FUNCTION = (
+    "odoo.addons.base.models.res_users.Users._get_invoice_section_name"
+)
 
 
 class TestInvoiceGroupBySaleOrder(SavepointCase):
@@ -10,7 +17,9 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
         super().setUpClass()
         cls.partner_1 = cls.env.ref("base.res_partner_1")
         cls.product_1 = cls.env.ref("product.product_product_1")
+        cls.product_2 = cls.env.ref("product.product_product_2")
         cls.product_1.invoice_policy = "order"
+        cls.product_2.invoice_policy = "order"
         cls.order1_p1 = cls.env["sale.order"].create(
             {
                 "partner_id": cls.partner_1.id,
@@ -34,7 +43,7 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
                         0,
                         {
                             "name": "order 1 line 2",
-                            "product_id": cls.product_1.id,
+                            "product_id": cls.product_2.id,
                             "price_unit": 20,
                             "product_uom_qty": 1,
                             "product_uom": cls.product_1.uom_id.id,
@@ -66,7 +75,7 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
                         0,
                         {
                             "name": "order 2 line 2",
-                            "product_id": cls.product_1.id,
+                            "product_id": cls.product_2.id,
                             "price_unit": 20,
                             "product_uom_qty": 1,
                             "product_uom": cls.product_1.uom_id.id,
@@ -80,12 +89,15 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
     def test_create_invoice(self):
         """ Check invoice is generated  with sale order sections."""
         result = {
-            0: "".join([self.order1_p1.name, " - ", self.order1_p1.client_order_ref]),
-            1: "order 1 line 1",
-            2: "order 1 line 2",
-            3: self.order2_p1.name,
-            4: "order 2 line 1",
-            5: "order 2 line 2",
+            10: (
+                "".join([self.order1_p1.name, " - ", self.order1_p1.client_order_ref]),
+                "line_section",
+            ),
+            20: ("order 1 line 1", False),
+            30: ("order 1 line 2", False),
+            40: (self.order2_p1.name, "line_section"),
+            50: ("order 2 line 1", False),
+            60: ("order 2 line 2", False),
         }
         invoice_ids = (self.order1_p1 + self.order2_p1)._create_invoices()
         lines = (
@@ -93,8 +105,9 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
             .line_ids.sorted("sequence")
             .filtered(lambda r: not r.exclude_from_invoice_tab)
         )
-        for idx, line in enumerate(lines):
-            self.assertEqual(line.name, result[idx])
+        for line in lines:
+            self.assertEqual(line.name, result[line.sequence][0])
+            self.assertEqual(line.display_type, result[line.sequence][1])
 
     def test_create_invoice_no_section(self):
         """Check invoice for only one sale order
@@ -107,3 +120,54 @@ class TestInvoiceGroupBySaleOrder(SavepointCase):
             lambda r: r.display_type == "line_section"
         )
         self.assertEqual(len(line_sections), 0)
+
+    def test_unknown_invoice_section_grouping_value(self):
+        """Check an error is raised when invoice_section_grouping value is
+        unknown
+        """
+        mock_company_section_grouping = mock.patch.object(
+            type(self.env.company),
+            "invoice_section_grouping",
+            new_callable=mock.PropertyMock,
+        )
+        with mock_company_section_grouping as mocked_company_section_grouping:
+            mocked_company_section_grouping.return_value = "unknown"
+            with self.assertRaises(UserError):
+                (self.order1_p1 + self.order2_p1)._create_invoices()
+
+    def test_custom_grouping_by_sale_order_user(self):
+        """Check custom grouping by sale order user.
+
+        By mocking account.move.line_get_section_grouping and creating
+        res.users.get_invoice_section_name, this test ensures custom grouping
+        is possible by redefining these functions"""
+        demo_user = self.env.ref("base.user_demo")
+        admin_user = self.env.ref("base.partner_admin")
+        orders = self.order1_p1 + self.order2_p1
+        orders.write({"user_id": admin_user.id})
+        sale_order_3 = self.order1_p1.copy({"user_id": demo_user.id})
+        sale_order_3.order_line[0].name = "order 3 line 1"
+        sale_order_3.order_line[1].name = "order 3 line 2"
+        sale_order_3.action_confirm()
+
+        with mock.patch(
+            SECTION_GROUPING_FUNCTION
+        ) as mocked_get_section_grouping, mock.patch(
+            SECTION_NAME_FUNCTION, create=True
+        ) as mocked_get_invoice_section_name:
+            mocked_get_section_grouping.return_value = "sale_line_ids.order_id.user_id"
+            mocked_get_invoice_section_name.return_value = "Mocked value from ResUsers"
+            invoice = (orders + sale_order_3)._create_invoices()
+            result = {
+                10: ("Mocked value from ResUsers", "line_section"),
+                20: ("order 1 line 1", False),
+                30: ("order 1 line 2", False),
+                40: ("order 2 line 1", False),
+                50: ("order 2 line 2", False),
+                60: ("Mocked value from ResUsers", "line_section"),
+                70: ("order 3 line 1", False),
+                80: ("order 3 line 2", False),
+            }
+            for line in invoice.invoice_line_ids.sorted("sequence"):
+                self.assertEqual(line.name, result[line.sequence][0])
+                self.assertEqual(line.display_type, result[line.sequence][1])
