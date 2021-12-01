0562959ebbdc3b4a837227d03df1437643b4fbcc
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-12-01 16:34:14 +0100

    account_invoice_section_sale_order fix create_invoice
    The function signature as changed in v14

--- account_invoice_section_sale_order/models/sale_order.py
+++ account_invoice_section_sale_order/models/sale_order.py
@@ -7,14 +7,14 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _create_invoices(self, grouped=False, final=False):
+    def _create_invoices(self, grouped=False, final=False, date=None):
         """Add sections by sale order in the invoice line.
 
         Order the invoicing lines by sale order and add lines section with
         the sale order name.
         Only do this for invoices targetting multiple sale order
         """
-        invoice_ids = super()._create_invoices(grouped=grouped, final=final)
+        invoice_ids = super()._create_invoices(grouped=grouped, final=final, date=date)
         for invoice in invoice_ids:
             if len(invoice.line_ids.mapped("sale_line_ids.order_id.id")) == 1:
                 continue
