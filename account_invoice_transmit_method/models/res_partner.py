fccdde77b83de5d82201a8c6d68dad8cde4d5eb8
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-12-13 15:40:18 -0300

    [MIG] account_invoice_transmit_method: Migration to 15.0

--- account_invoice_transmit_method/models/res_partner.py
+++ account_invoice_transmit_method/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright 2017-2020 Akretion France (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
