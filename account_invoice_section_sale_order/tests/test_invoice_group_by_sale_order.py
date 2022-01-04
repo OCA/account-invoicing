2ad877da0b75bce317d4bc6624cba317955c0c67
Jeroen Evens <jeroen.evens@dynapps.be>
2022-01-04 16:30:46 +0100

    [mig] Odoo 15 fixes

--- account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
+++ account_invoice_section_sale_order/tests/test_invoice_group_by_sale_order.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestInvoiceGroupBySaleOrder(SavepointCase):
+class TestInvoiceGroupBySaleOrder(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
