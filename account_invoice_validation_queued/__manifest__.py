f876a307bcedf39d942e63a925b42edb3bbe4b4f
iwkse <iwkse@claws-mail.org>
2022-01-25 17:52:01 +0100

    [14.0][MIG] account_invoice_validation_queued

--- account_invoice_validation_queued/__manifest__.py
+++ account_invoice_validation_queued/__manifest__.py
@@ -4,13 +4,14 @@
 
 {
     "name": "Enqueue account invoice validation",
-    "version": "13.0.2.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "depends": ["account", "queue_job"],
     "data": [
+        "data/queue_job.xml",
         "views/queue_job_views.xml",
         "views/account_invoice_views.xml",
         "wizards/validate_account_move_view.xml",
--- account_invoice_validation_queued/readme/USAGE.rst
+++ account_invoice_validation_queued/readme/USAGE.rst
@@ -4,8 +4,6 @@
    list view.
 #. Click on *Action > Confirm Draft Invoice*.
 #. On the dialog popup that appears, click on "Enqueue Validation".
-#. If any of the selected invoices are not in draft state, you will receive
-   an error.
 #. If any of the invoices is already enqueued, there will be a message saying
    so and avoiding to perform the process.
 #. Once enqueued, and having the "Job Queue Manager" permission, you can go to
