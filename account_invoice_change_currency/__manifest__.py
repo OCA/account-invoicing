80d671f5e1389d9ae53cc7087d71f075beb77f49
rolandojduartem <rolando@vauxoo.com>
2022-02-01 22:17:14 -0600

    [MIG] account_invoice_change_currency: Migration to 14.0
    **Changes**
    
    Now, the account_invoice_change_currency avoid the mail tracking using account.move, res.currency
    and new fields to handle the different calculations.
    
    **New fields**
    
    - original_currency_id: Store the base currency when the invoice is created or the conversion is called for the first time. This is used to calculate conversion from this currency.
    - is_original_currency: Check if current currency is the base currency. This is used to hide custom rate field in the form view.
    - original_price_unit: Store price unit from every line when the invoice is created or the conversion is called for the first time to use it to convert the amount in the new currency.

--- account_invoice_change_currency/__manifest__.py
+++ account_invoice_change_currency/__manifest__.py
@@ -2,17 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice - Change Currency",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "summary": "Allows to change currency of Invoice by wizard",
-    "author": "Komit Consulting, Odoo Community Association (OCA)",
+    "author": "Vauxoo, Komit Consulting, Odoo Community Association (OCA)",
+    "maintainers": ["luisg123v"],
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "depends": ["account"],
     "data": [
-        "data/ir_server_actions.xml",
-        "data/message_subtype.xml",
-        "views/account_invoice.xml",
+        "views/account_move_views.xml",
     ],
     "installable": True,
 }
