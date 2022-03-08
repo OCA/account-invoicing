bfab8bf2c1993eaf32231ce494457ee632c9380a
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-03-08 12:29:26 -0500

    [IMP] account_invoice_supplierinfo_update: black, isort, prettier

--- account_invoice_supplierinfo_update/models/account_invoice.py
+++ account_invoice_supplierinfo_update/models/account_invoice.py
@@ -3,29 +3,34 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     # Column Section
     supplier_partner_id = fields.Many2one(
-        comodel_name='res.partner', string="Supplier Partner",
-        compute='_compute_supplier_partner_id')
+        comodel_name="res.partner",
+        string="Supplier Partner",
+        compute="_compute_supplier_partner_id",
+    )
 
     supplierinfo_ok = fields.Boolean(
-        string='Supplier Informations Checked', copy=False,
+        string="Supplier Informations Checked",
+        copy=False,
         help="Checked if the check of supplierinfo has been done.\n"
         " - Uncheck this box, if you want to check again this invoice\n"
-        " - Check this box, if you want to mark this invoice as checked")
+        " - Check this box, if you want to mark this invoice as checked",
+    )
 
     # Compute Section
     @api.multi
     def _compute_supplier_partner_id(self):
         for invoice in self:
-            invoice.supplier_partner_id =\
+            invoice.supplier_partner_id = (
                 invoice.partner_id.commercial_partner_id or invoice.partner_id
+            )
 
     # Custom Section
     @api.multi
@@ -51,8 +56,7 @@ class AccountInvoice(models.Model):
                     continue
 
             # Propose updating, if needed
-            lines.append((0, 0, line._prepare_supplier_wizard_line(
-                supplierinfo)))
+            lines.append((0, 0, line._prepare_supplier_wizard_line(supplierinfo)))
         return lines
 
     # View Section
@@ -62,22 +66,23 @@ class AccountInvoice(models.Model):
         lines_for_update = self._get_update_supplierinfo_lines()
         if lines_for_update:
             ctx = {
-                'default_line_ids': lines_for_update,
-                'default_invoice_id': self.id,
+                "default_line_ids": lines_for_update,
+                "default_invoice_id": self.id,
             }
 
             view_form = self.env.ref(
-                'account_invoice_supplierinfo_update.'
-                'view_wizard_update_invoice_supplierinfo_form')
+                "account_invoice_supplierinfo_update."
+                "view_wizard_update_invoice_supplierinfo_form"
+            )
             return {
-                'name': _("Update supplier informations of products"),
-                'type': 'ir.actions.act_window',
-                'view_mode': 'form',
-                'res_model': 'wizard.update.invoice.supplierinfo',
-                'views': [(view_form.id, 'form')],
-                'view_id': view_form.id,
-                'target': 'new',
-                'context': ctx,
+                "name": _("Update supplier informations of products"),
+                "type": "ir.actions.act_window",
+                "view_mode": "form",
+                "res_model": "wizard.update.invoice.supplierinfo",
+                "views": [(view_form.id, "form")],
+                "view_id": view_form.id,
+                "target": "new",
+                "context": ctx,
             }
         else:
-            self.write({'supplierinfo_ok': True})
+            self.write({"supplierinfo_ok": True})
