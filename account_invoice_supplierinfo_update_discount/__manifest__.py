06552acc0470a18a97c4bc6e135f2308c7c32607
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-03-09 15:03:58 -0500

    [MIG] account_invoice_supplierinfo_update_discount: Migration to 13.0

--- account_invoice_supplierinfo_update_discount/__manifest__.py
+++ account_invoice_supplierinfo_update_discount/__manifest__.py
@@ -7,14 +7,14 @@
     "summary": "In the supplier invoice, automatically update all products "
     "whose discount on the line is different from "
     "the supplier discount",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "GRAP,Odoo Community Association (OCA)",
+    "author": "GRAP, Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
     "license": "AGPL-3",
-    "depends": ["account_invoice_supplierinfo_update", "purchase_discount",],
+    "depends": ["account_invoice_supplierinfo_update", "purchase_discount"],
     "installable": True,
     "auto_install": True,
-    "data": ["wizard/wizard_update_invoice_supplierinfo.xml",],
+    "data": ["wizard/wizard_update_invoice_supplierinfo.xml"],
 }
--- account_invoice_supplierinfo_update_discount/tests/test_module.py
+++ account_invoice_supplierinfo_update_discount/tests/test_module.py
@@ -1,61 +1,32 @@
-# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# Copyright 2018 - Today: GRAP (http://www.grap.coop)
+# Copyright Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from datetime import datetime
-
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestModule(TransactionCase):
     def setUp(self):
         super().setUp()
-        self.AccountInvoice = self.env["account.invoice"]
+        self.AccountMove = self.env["account.move"]
         self.WizardUpdate = self.env["wizard.update.invoice.supplierinfo"]
         self.SupplierInfo = self.env["product.supplierinfo"]
-
         self.product1 = self.env.ref("product.product_product_4b")
-        unit = self.env.ref("uom.product_uom_unit")
-        account_id = (
-            self.env["account.account"]
-            .search([("user_type_id.type", "=", "payable")], limit=1)
-            .id
-        )
-        journal_id = (
-            self.env["account.journal"].search([("type", "=", "purchase")], limit=1).id
-        )
-        product_account_id = self.env.ref("account.demo_coffee_machine_account").id
-
-        self.invoice = self.AccountInvoice.create(
-            {
-                "journal_id": journal_id,
-                "partner_id": self.env.ref("base.res_partner_12").id,
-                "account_id": account_id,
-                "date_invoice": "%s-01-01" % datetime.now().year,
-                "invoice_line_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "product_id": self.product1.id,
-                            "name": "iPad Retina Display",
-                            "quantity": 10.0,
-                            "price_unit": 400.0,
-                            "uom_id": unit.id,
-                            "account_id": product_account_id,
-                            "discount": 10.0,
-                        },
-                    )
-                ],
-            }
-        )
+        invoice_form = Form(self.AccountMove.with_context(default_type="in_invoice"))
+        invoice_form.partner_id = self.env.ref("base.res_partner_12")
+        with invoice_form.invoice_line_ids.new() as line_form:
+            line_form.product_id = self.product1
+            line_form.quantity = 10.0
+            line_form.price_unit = 400.0
+            line_form.discount = 10.0
+        self.invoice = invoice_form.save()
 
     # Test Section
     def test_discount(self):
         # Launch and confirm Wizard
         lines_for_update = self.invoice._get_update_supplierinfo_lines()
         wizard = self.WizardUpdate.with_context(
-            default_line_ids=lines_for_update, default_invoice_id=self.invoice.id
+            default_line_ids=lines_for_update, default_invoice_id=self.invoice.id,
         ).create({})
         wizard.update_supplierinfo()
 
