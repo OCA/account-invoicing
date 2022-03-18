bf869ed67ca952d96421c60627c921e2171d66d9
vvrossem <vvrossem@gmail.com>
2022-03-18 12:09:47 +0100

    [MIG][15.0] account_move_tier_validation

--- account_move_tier_validation/tests/test_tier_validation.py
+++ account_move_tier_validation/tests/test_tier_validation.py
@@ -9,7 +9,7 @@ from .tier_validation_tester import TierValidationTester
 
 
 @tagged("post_install", "-at_install")
-class TestAccountTierValidation(common.SavepointCase):
+class TestAccountTierValidation(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestAccountTierValidation, cls).setUpClass()
@@ -56,7 +56,7 @@ class TestAccountTierValidation(common.SavepointCase):
     @classmethod
     def tearDownClass(cls):
         teardown_test_model(cls.env, [TierValidationTester])
-        super(TestAccountTierValidation, cls).tearDownClass()
+        return super(TestAccountTierValidation, cls).tearDownClass()
 
     def test_01_tier_definition_models(self):
         """When the user can validate all future reviews, it is not needed
