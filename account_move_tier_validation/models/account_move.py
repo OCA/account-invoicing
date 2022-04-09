7ce66ab0bb6d3af4455aa81456f9b301f73e8306
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-04-09 20:28:38 +0200

    [IMP] account_move_tier_validation: Use field messages for review, validation, rejection

--- account_move_tier_validation/models/account_move.py
+++ account_move_tier_validation/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright <2020> PESOL <info@pesol.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo import models
+from odoo import _, models
 
 
 class AccountMove(models.Model):
@@ -9,3 +9,15 @@ class AccountMove(models.Model):
     _inherit = ["account.move", "tier.validation"]
     _state_from = ["draft"]
     _state_to = ["posted"]
+
+    def _get_to_validate_message_name(self):
+        name = super(AccountMove, self)._get_to_validate_message_name()
+        if self.move_type == "in_invoice":
+            name = _("Bill")
+        elif self.move_type == "in_refund":
+            name = _("Refund")
+        elif self.move_type == "out_invoice":
+            name = _("Invoice")
+        elif self.move_type == "out_refund":
+            name = _("Credit Note")
+        return name
