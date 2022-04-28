1c36b76ada9f851268286a196fc50ceecc8668c3
Marco Colombo <marco.colombo@gmail.com>
2022-04-28 21:30:46 +0200

    [IMP] account_invoice_fix_tax_rounding: black, isort, prettier

--- account_invoice_fix_tax_rounding/models/res_company.py
+++ account_invoice_fix_tax_rounding/models/res_company.py
@@ -2,9 +2,10 @@ from odoo import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     tax_max_diff_global_rounding_method = fields.Float(
-        default=0.0, required=True,
-        string='Tax calculation max diff amount for rounding method'
+        default=0.0,
+        required=True,
+        string="Tax calculation max diff amount for rounding method",
     )
