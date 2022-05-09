37a7859f43f7f9689997784ae9800eaf9217e31a
dsolanki <dhara.solanki@initos.com>
2022-05-09 18:24:30 +0530

    [15.0][MIG]account_invoice_pdf_export

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -11,6 +11,7 @@ setuptools.setup(
         'odoo-addon-account_invoice_check_total>=15.0dev,<15.1dev',
         'odoo-addon-account_invoice_date_due>=15.0dev,<15.1dev',
         'odoo-addon-account_invoice_fiscal_position_update>=15.0dev,<15.1dev',
+        'odoo-addon-account_invoice_pdf_export>=15.0dev,<15.1dev',
         'odoo-addon-account_invoice_refund_link>=15.0dev,<15.1dev',
         'odoo-addon-account_invoice_search_by_reference>=15.0dev,<15.1dev',
         'odoo-addon-account_invoice_tax_note>=15.0dev,<15.1dev',
