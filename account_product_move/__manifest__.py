bf09d7ce6255d07749caea915dfb3365e9d3415a
ntsirintanis <ntsirintanis@therp.nl>
2022-05-25 15:00:16 +0200

    [WIP] some review corrections

--- account_product_move/__manifest__.py
+++ account_product_move/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Account Move Journal Template",
-    "summary": "Template for Journal Entries and Items, per product.",
+    "name": "Account Product Move",
+    "summary": "Extra Journal Entries and Items, per product.",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Therp BV, Odoo Community Association (OCA)",
@@ -12,9 +12,9 @@
     "depends": ["account"],
     "data": [
         "security/ir.model.access.csv",
-        "views/account_move_journal_template.xml",
+        "views/account_product_move.xml",
         "views/account_move_views.xml",
         "views/product_views.xml",
     ],
-    "demo": ["demo/account_move_journal_template.xml"],
+    "demo": ["demo/account_product_move.xml"],
 }
