d52b51ed7e8ec0151f1297bfd1294d43931594d7
Juany Davila <juanyadavilap@gmail.com>
2022-06-04 15:37:36 -0500

    [MIG] product_supplierinfo_for_customer_invoice: Migration to 15.0

--- product_supplierinfo_for_customer_invoice/tests/test_product_supplierinfo_for_customer_invoice.py
+++ product_supplierinfo_for_customer_invoice/tests/test_product_supplierinfo_for_customer_invoice.py
@@ -1,10 +1,10 @@
 # Copyright 2021 ForgeFlow
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestProductSupplierinfoForCustomerInvoice(SavepointCase):
+class TestProductSupplierinfoForCustomerInvoice(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
