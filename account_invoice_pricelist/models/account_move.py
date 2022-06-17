8e268bbd09818028e2559304fef84d614221bad5
ferran-73 <ferran@studio73.es>
2022-06-17 12:22:09 +0200

    [FIX] account_invoice_pricelist: float division by zero

--- account_invoice_pricelist/models/account_move.py
+++ account_invoice_pricelist/models/account_move.py
@@ -155,6 +155,8 @@ class AccountMoveLine(models.Model):
         return product[field_name] * uom_factor * cur_factor, currency_id
 
     def _calculate_discount(self, base_price, final_price):
+        if not base_price:
+            return 0.0
         discount = (base_price - final_price) / base_price * 100
         if (discount < 0 and base_price > 0) or (discount > 0 and base_price < 0):
             discount = 0.0
