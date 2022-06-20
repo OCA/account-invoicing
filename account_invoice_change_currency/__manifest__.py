1fd17ac7ecd2220014e7193fde860d1a11e769c9
Rolando Duarte [Vauxoo] <90422721+rolandojduartem@users.noreply.github.com>
2022-06-20 12:43:54 -0600

    [IMP] account_invoice_change_currency: Speed up module installation. (#1185)
    Initializing column custom_rate on table account_move for the improvement in performance to avoid long duration in databases with thousand of moves using a hook.
    
    account_invoice_change_currency 15.0.2.0.0

--- account_invoice_change_currency/__manifest__.py
+++ account_invoice_change_currency/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice - Change Currency",
-    "version": "15.0.1.0.0",
+    "version": "15.0.2.0.0",
     "category": "Accounting & Finance",
     "summary": "Allows to change currency of Invoice by wizard",
     "author": "Vauxoo, Komit Consulting, Odoo Community Association (OCA)",
@@ -13,5 +13,6 @@
     "data": [
         "views/account_move_views.xml",
     ],
+    "pre_init_hook": "pre_init_hook",
     "installable": True,
 }
