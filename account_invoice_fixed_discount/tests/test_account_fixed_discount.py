22ae28f3deb4c938c2013162b16bb9df1b357114
Jakub Hnilica <j.hnilica147@gmail.com>
2022-06-21 08:32:12 +0200

    [MIG] account_invoice_fixed_discount: Migration to 15.0

--- account_invoice_fixed_discount/tests/test_account_fixed_discount.py
+++ account_invoice_fixed_discount/tests/test_account_fixed_discount.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo.exceptions import ValidationError
-from odoo.tests import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestInvoiceFixedDiscount(SavepointCase):
+class TestInvoiceFixedDiscount(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestInvoiceFixedDiscount, cls).setUpClass()
