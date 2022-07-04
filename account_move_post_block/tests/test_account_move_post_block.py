ab7262ccbcbdd397c621c47c22d6b13d06723153
Juany Davila <juanyadavilap@gmail.com>
2022-07-03 21:25:07 -0500

    [IMP] account_move_post_block: black, isort, prettier

--- account_move_post_block/tests/test_account_move_post_block.py
+++ account_move_post_block/tests/test_account_move_post_block.py
@@ -48,7 +48,7 @@ class TestAccountMovePostBlock(TransactionCase):
         )
 
     def _create_user(self, login, groups, company):
-        """ Create a user."""
+        """Create a user."""
         group_ids = [group.id for group in groups]
         user = self.users_obj.with_context({"no_reset_password": True}).create(
             {
