3e9e29cebc92232d78d5a4dbee3babf0d5e9971c
AnnaPForgeFlow <anna.puig@forgeflow.com>
2022-07-15 12:56:56 +0200

    [MIG] account_invoice_tier_validation_approver: Migration to 13.0

--- account_invoice_tier_validation_approver/__manifest__.py
+++ account_invoice_tier_validation_approver/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Account Invoice Tier Validation Approver",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Accounting",
     "license": "LGPL-3",
