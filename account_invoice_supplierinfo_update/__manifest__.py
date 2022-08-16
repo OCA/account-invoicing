100ab9966d773bd5721bfdc54384fb2ac01a2491
Luis D. Lafaurie <luis.lafaurie@tecnativa.com>
2022-08-16 19:06:55 +0200

    [MIG] account_invoice_supplierinfo_update: Migration to 14.0

--- account_invoice_supplierinfo_update/__manifest__.py
+++ account_invoice_supplierinfo_update/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": "In the supplier invoice, automatically updates all products "
     "whose unit price on the line is different from "
     "the supplier price",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "Akretion, GRAP, Odoo Community Association (OCA)",
@@ -16,8 +16,8 @@
     "installable": True,
     "depends": ["account"],
     "data": [
+        "security/ir.model.access.csv",
         "views/account_invoice_view.xml",
         "wizard/wizard_update_invoice_supplierinfo.xml",
     ],
-    "images": ["static/description/main_screenshot.png"],
 }
--- account_invoice_supplierinfo_update/tests/test_account_invoice_supplierinfo_update.py
+++ account_invoice_supplierinfo_update/tests/test_account_invoice_supplierinfo_update.py
@@ -1,39 +1,60 @@
 # Copyright 2016 Chafique DELLI @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import tagged
 from odoo.tests.common import Form, TransactionCase
 
 
+@tagged("post_install", "-at_install")
 class Tests(TransactionCase):
     def setUp(self):
         super(Tests, self).setUp()
         self.wizard_obj = self.env["wizard.update.invoice.supplierinfo"]
         self.supplierinfo_obj = self.env["product.supplierinfo"]
         self.invoice_model = self.env["account.move"]
+        unit = self.env.ref("uom.product_uom_unit")
         journal_model = self.env["account.journal"]
         self.journal = journal_model.search([("type", "=", "purchase")], limit=1)
-        self.product1 = self.env.ref("product.product_product_4b")
-        self.product2 = self.env.ref("product.product_delivery_01")
-        unit = self.env.ref("uom.product_uom_unit")
+        self.product1 = self.env["product.product"].create(
+            {"name": "Test product 1", "uom_id": unit.id}
+        )
+        self.product2 = self.env["product.product"].create(
+            {"name": "Test product 2", "uom_id": unit.id}
+        )
+        # We need to explicitly set the account on lines with no product, but also on
+        # the rest due to a glitch not assigning such account when setting the product
+        self.account = self.env["account.account"].search(
+            [
+                (
+                    "user_type_id",
+                    "=",
+                    self.env.ref("account.data_account_type_expenses").id,
+                ),
+                ("company_id", "=", self.env.company.id),
+            ],
+            limit=1,
+        )
+        self.vendor = self.env["res.partner"].create({"name": "Test vendor"})
         self.currency = self.env.ref("base.GBP")
         self.journal.write({"currency_id": self.currency.id})
-
         invoice_form = Form(self.invoice_model.with_context(default_type="in_invoice"))
-        invoice_form.partner_id = self.env.ref("base.res_partner_12")
+        invoice_form.partner_id = self.vendor
         invoice_form.journal_id = self.journal
         with invoice_form.invoice_line_ids.new() as line_form:
             line_form.product_id = self.product1
+            line_form.account_id = self.account
             line_form.quantity = 10.0
-            line_form.product_uom_id = unit
             line_form.price_unit = 400.0
         with invoice_form.invoice_line_ids.new() as line_form:
             line_form.name = "line without product"
             line_form.quantity = 1.0
+            line_form.account_id = self.account
             line_form.product_uom_id = unit
             line_form.price_unit = 35.0
         with invoice_form.invoice_line_ids.new() as line_form:
             line_form.product_id = self.product2
+            line_form.account_id = self.account
             line_form.quantity = 1.0
-            line_form.product_uom_id = unit
             line_form.price_unit = 10.0
         self.invoice = invoice_form.save()
 
@@ -46,15 +67,15 @@ class Tests(TransactionCase):
         line_ids = vals_wizard.get("default_line_ids", {})
         invoice_id = vals_wizard.get("default_invoice_id", {})
 
-        self.assertEquals(len(line_ids), 2)
-        self.assertEquals(line_ids[0][2]["current_price"], False)
-        self.assertEquals(line_ids[0][2]["new_price"], 400.0)
+        self.assertEqual(len(line_ids), 2)
+        self.assertEqual(line_ids[0][2]["current_price"], False)
+        self.assertEqual(line_ids[0][2]["new_price"], 400.0)
 
         # Create and launch update process
         wizard = self.wizard_obj.create(
             {"line_ids": line_ids, "invoice_id": invoice_id}
         )
-        self.assertEquals(wizard.line_ids[1].new_price, 10.0)
+        self.assertEqual(wizard.line_ids[1].new_price, 10.0)
         wizard.update_supplierinfo()
 
         supplierinfos1 = self.supplierinfo_obj.search(
@@ -67,10 +88,10 @@ class Tests(TransactionCase):
                 ),
             ]
         )
-        self.assertEquals(len(supplierinfos1), 1)
+        self.assertEqual(len(supplierinfos1), 1)
         self.assertEqual(supplierinfos1.currency_id, self.currency)
 
-        self.assertEquals(supplierinfos1.price, 400.0)
+        self.assertEqual(supplierinfos1.price, 400.0)
 
         supplierinfos2 = self.supplierinfo_obj.search(
             [
@@ -82,9 +103,9 @@ class Tests(TransactionCase):
                 ),
             ]
         )
-        self.assertEquals(len(supplierinfos2), 1)
+        self.assertEqual(len(supplierinfos2), 1)
 
-        self.assertEquals(supplierinfos2.price, 10.0)
+        self.assertEqual(supplierinfos2.price, 10.0)
 
     def test_update_pricelist_supplierinfo_uom_conversion(self):
         """Price is converted to the product's purchase UOM"""
@@ -102,7 +123,7 @@ class Tests(TransactionCase):
 
         # Prices are converted to the purchase UOM.
         # 33 per unit equals 396 per dozen
-        self.assertEquals(line.new_price, 396.0)
+        self.assertEqual(line.new_price, 396.0)
 
         wizard.update_supplierinfo()
         supplierinfo = self.supplierinfo_obj.search(
@@ -111,5 +132,5 @@ class Tests(TransactionCase):
                 ("product_tmpl_id", "=", self.product1.product_tmpl_id.id),
             ]
         )
-        self.assertEquals(supplierinfo.price, 396.0)
+        self.assertEqual(supplierinfo.price, 396.0)
         self.assertTrue(invoice_line._is_correct_price(supplierinfo))
