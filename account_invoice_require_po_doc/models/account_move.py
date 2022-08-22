3ec722c40629743dc9678ad6db3cc38931438710
Hardik-OSI <hsuthar@opensourceintegrators.com>
2022-08-22 15:29:47 +0530

    [15.0][FIX]pre-commit

--- account_invoice_require_po_doc/models/account_move.py
+++ account_invoice_require_po_doc/models/account_move.py
@@ -4,20 +4,27 @@
 from odoo import _, models
 from odoo.exceptions import ValidationError
 
+
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     def action_post(self):
-        sale = self.env['sale.order'].search(
-            [('name', '=', self.invoice_origin)],
-            limit=1)
+        sale = self.env["sale.order"].search(
+            [("name", "=", self.invoice_origin)], limit=1
+        )
         for order in sale:
             if order.customer_need_po and not order.client_order_ref:
                 raise ValidationError(
-                    _("You can not confirm sale order without \
-                                    Customer reference."))
+                    _(
+                        "You can not confirm sale order without \
+                                    Customer reference."
+                    )
+                )
             if order.sale_doc and not order.sale_document_option:
                 raise ValidationError(
-                    _("You can not confirm sale order without \
-                                    Sale Documentation."))
+                    _(
+                        "You can not confirm sale order without \
+                                    Sale Documentation."
+                    )
+                )
         return super(AccountMove, self).action_post()
