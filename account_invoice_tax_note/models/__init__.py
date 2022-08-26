525d86e1e6e13185c58a56656d7e3e97cf822f16
François Degrave <f.degrave@oncodna.com>
2022-08-26 13:00:48 +0200

    [BACKPORT] account_invoice_tax_note from 11.0 version
    Works "as is"

---
+++ account_invoice_tax_note/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import account_invoice
+from . import account_tax_group
