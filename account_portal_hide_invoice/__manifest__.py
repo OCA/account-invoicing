a36fc96104914d8ab698bb6cc1bbc049f1f9fd75
Freni Patel <fpatel@opensourceintegrators.com>
2022-09-06 15:17:19 +0530

    [15.0][MIG] account_portal_hide_invoice

--- account_portal_hide_invoice/__manifest__.py
+++ account_portal_hide_invoice/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Website Hide Invoice",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": """Open Source Integrators,
         Serpent Consulting Services,
