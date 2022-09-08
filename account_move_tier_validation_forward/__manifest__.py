f24364c3a5faac3db86ec5690b2ce1c73ecfdbf7
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-09-08 11:18:43 +0530

    [IMP] account_move_tier_validation_forward: black, isort, prettier

--- account_move_tier_validation_forward/__manifest__.py
+++ account_move_tier_validation_forward/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account Move Tier Validation - Forward Option",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "ForgeFlow,Odoo Community Association (OCA)",
