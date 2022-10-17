f2a92e4ef0caae16b823a630a4f2e5f4ed5c40f4
Eric Antones <eantones@nuobit.com>
2022-10-17 15:24:08 +0200

    [IMP] purchase_stock_picking_return_invoicing_force_invoiced: black, isort, prettier

--- purchase_stock_picking_return_invoicing_force_invoiced/__manifest__.py
+++ purchase_stock_picking_return_invoicing_force_invoiced/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services
 # Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,12 +5,11 @@
 {
     "name": "Purchase Stock Picking Return Invoicing Force Invoiced",
     "summary": "Glue module between purchase_force_invoiced "
-               "and purchase_stock_picking_return_invoicing",
+    "and purchase_stock_picking_return_invoicing",
     "version": "12.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "Eficent,"
-              "Odoo Community Association (OCA)",
+    "author": "Eficent," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "auto_install": True,
@@ -21,6 +19,6 @@
         "purchase_stock_picking_return_invoicing",
     ],
     "maintainers": [
-        'jbeficent',
+        "jbeficent",
     ],
 }
