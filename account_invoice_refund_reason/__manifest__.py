4b7d25947a491ff077e82264e9d1ff3a04c8fe72
Jaume Bernaus <jaume.bernaus@forgeflow.com>
2022-10-19 17:08:26 +0200

    [13.0][IMP] account_invoice_refund_reason: black, isort, prettier

--- account_invoice_refund_reason/__manifest__.py
+++ account_invoice_refund_reason/__manifest__.py
@@ -3,23 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Refund Reason',
-    'version': '12.0.1.0.1',
+    "name": "Account Invoice Refund Reason",
+    "version": "12.0.1.0.1",
     "summary": "Account Invoice Refund Reason.",
-    'category': 'Accounting',
-    'author':
-        "Open Source Integrators, "
-        "Serpent CS, "
-        "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-invoicing',
-    'data': [
-        'security/ir.model.access.csv',
-        'data/account.invoice.refund.reason.csv',
-        'views/account_invoice_view.xml',
-        'views/account_invoice_refund_reason_view.xml',
-        'wizard/account_invoice_refund_view.xml'],
-    'depends': ['account'],
-    'license': 'AGPL-3',
-    'development_status': 'Beta',
-    'maintainers': ['max3903'],
+    "category": "Accounting",
+    "author": "Open Source Integrators, "
+    "Serpent CS, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/account.invoice.refund.reason.csv",
+        "views/account_invoice_view.xml",
+        "views/account_invoice_refund_reason_view.xml",
+        "wizard/account_invoice_refund_view.xml",
+    ],
+    "depends": ["account"],
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
 }
