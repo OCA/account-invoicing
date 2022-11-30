d52cceb59da145da7ad740a475e3b8af510bdfe1
Stefan Rijnhart <stefan@opener.am>
2022-11-30 21:12:50 +0100

    [MIG] partner_invoicing_mode_monthly: Migration to 16.0

--- partner_invoicing_mode_monthly/__manifest__.py
+++ partner_invoicing_mode_monthly/__manifest__.py
@@ -2,16 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Invoicing Mode Monthly",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Create invoices automatically on a monthly basis.",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
     "depends": [
-        "account",
         "partner_invoicing_mode",
-        "queue_job",
         "sale_stock",
     ],
     "data": [
