fc8bf1a0a95e764381b99fe91d5254473cb80727
Miquel Raïch <miquel.raich@forgeflow.com>
2022-12-21 16:01:45 +0100

    [MIG] account_invoice_origin_link: Migration to 14.0

--- account_invoice_origin_link/__manifest__.py
+++ account_invoice_origin_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Invoice Origin Link",
     "summary": "Add clickable link in invoice source document.",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
