80babd9ebd6440d7c0474e10efd8fcc7efc4cd00
sergiocorato <sergiocorato@gmail.com>
2022-12-23 17:36:15 +0100

    [12.0][FIX] view created payment

--- account_invoice_view_payment/wizards/account_register_payments.py
+++ account_invoice_view_payment/wizards/account_register_payments.py
@@ -15,15 +15,11 @@ class AccountRegisterPayments(models.TransientModel):
         for payment_vals in self.get_payments_vals():
             payments += payment_model.create(payment_vals)
         payments.post()
-        res = {
-            'domain': [('id', 'in', payments.ids), ('state', '=', 'posted')],
-            'views': [(False, 'form')],
-            'name': _('Payments'),
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'account.payment',
-            'view_id': False,
-            'context': False,
-            'type': 'ir.actions.act_window'
-        }
+        res = dict(
+            domain=[('id', 'in', payments.ids)],
+            name=_('Payments'),
+            view_mode='tree,form',
+            res_model='account.payment',
+            type='ir.actions.act_window',
+        )
         return res
