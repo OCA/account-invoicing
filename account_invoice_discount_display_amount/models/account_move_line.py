46f9e6b0f387c468d4ed3fa0c0ea5e53e4457bd6
Alexei Rivera <alexeirivera87@gmail.com>
2023-01-20 09:40:20 -0500

    [MIG] account_invoice_discount_display_amount: Migration to 15.0

--- account_invoice_discount_display_amount/models/account_move_line.py
+++ account_invoice_discount_display_amount/models/account_move_line.py
@@ -7,30 +7,27 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    discount_total = fields.Monetary(
-        compute="_compute_discount_amount", string="Discount Total", store=True
-    )
+    discount_total = fields.Monetary(compute="_compute_discount_amount", store=True)
     price_total_no_discount = fields.Monetary(
         compute="_compute_discount_amount", string="Total Without Discount", store=True
     )
 
     @api.depends("discount", "price_total")
     def _compute_discount_amount(self):
-        invoice_lines_discount = self.filtered(
-            lambda a: a.discount and not a.exclude_from_invoice_tab
-        )
-
-        # Invoice lines with discount
+        invoice_lines_discount = self.filtered(lambda a: not a.exclude_from_invoice_tab)
         for line in invoice_lines_discount:
-            taxes = line.tax_ids.compute_all(
-                line.price_unit,
-                line.move_id.currency_id,
-                line.quantity,
-                product=line.product_id,
-                partner=line.partner_id,
-            )
-            price_total_no_discount = taxes["total_included"]
-            discount_total = price_total_no_discount - line.price_total
+            price_total_no_discount = line.price_total
+            discount_total = 0
+            if line.discount:
+                taxes = line.tax_ids.compute_all(
+                    line.price_unit,
+                    line.move_id.currency_id,
+                    line.quantity,
+                    product=line.product_id,
+                    partner=line.partner_id,
+                )
+                price_total_no_discount = taxes["total_included"]
+                discount_total = price_total_no_discount - line.price_total
             line.update(
                 {
                     "discount_total": discount_total,
@@ -38,8 +35,7 @@ class AccountMoveLine(models.Model):
                 }
             )
 
-        # Lines without a discount and those that are not invoice lines
-        # are excluded
+        # Lines that are not invoice lines are excluded
         (self - invoice_lines_discount).update(
             {
                 "discount_total": 0.0,
