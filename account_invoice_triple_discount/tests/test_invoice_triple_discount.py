9ab7cade8312a5476aa9103520dac0465764da5a
David Ramia <david.ramia@processcontrol.es>
2023-02-14 21:29:55 +0100

    [FIX][16.0] account_invoice_triple_discount : Error when print reports

--- account_invoice_triple_discount/tests/test_invoice_triple_discount.py
+++ account_invoice_triple_discount/tests/test_invoice_triple_discount.py
@@ -161,4 +161,28 @@ class TestInvoiceTripleDiscount(TransactionCase):
             line_form.discount = 15.0
         invoice_form.save()
 
-        self.assertEqual(invoice.amount_tax, 177.61)
+    def test_05_has_discount(self):
+        """
+        Tests has_discount
+        """
+        invoice = self.create_simple_invoice(0)
+        invoice_form = Form(invoice)
+
+        self.assertFalse(invoice._has_discount())
+
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = 50.0
+        invoice_form.save()
+        self.assertTrue(invoice._has_discount())
+
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount = 0
+            line_form.discount2 = 50.0
+        invoice_form.save()
+        self.assertTrue(invoice._has_discount())
+
+        with invoice_form.invoice_line_ids.edit(0) as line_form:
+            line_form.discount2 = 0
+            line_form.discount3 = 50.0
+        invoice_form.save()
+        self.assertTrue(invoice._has_discount())
