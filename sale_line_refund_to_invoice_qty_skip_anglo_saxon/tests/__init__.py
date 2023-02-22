306e11b9b34459b0ae0e36d730e9b28b34f8b161
Christopher Ormaza <chris.ormaza@forgeflow.com>
2023-02-22 10:21:35 -0500

    [14.0][ADD] sale_line_refund_to_invoice_qty_skip_anglo_saxon

---
+++ sale_line_refund_to_invoice_qty_skip_anglo_saxon/tests/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# Part of ForgeFlow. See LICENSE file for full copyright and licensing details.
+from . import test_sale_line_refund_to_invoice_qty_skip_anglo_saxon
