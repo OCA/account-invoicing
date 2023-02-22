5a08c796191fb0844787aeb99e13f6c316b64098
Luis Ernesto García Medina <ernesto.garcia@tecnativa.com>
2023-02-22 08:45:41 -0600

    [MIG] sale_invoicing_date_selection: Migration to 15.0

--- sale_invoicing_date_selection/tests/test_sale_invoicing_date_selection.py
+++ sale_invoicing_date_selection/tests/test_sale_invoicing_date_selection.py
@@ -1,10 +1,10 @@
 # Copyright 2022 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields
-from odoo.tests import Form, SavepointCase
+from odoo.tests import Form, TransactionCase
 
 
-class TestSaleInvoicingDateSelection(SavepointCase):
+class TestSaleInvoicingDateSelection(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
