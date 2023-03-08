acb831e985af2239b642f1b0b8232c295435d3bb
Rémy Taymans <remytms@tsmail.eu>
2023-03-08 16:52:56 +0100

    [IMP] account_invoice_negative_total: increase test coverage

--- account_invoice_negative_total/tests/test_account_invoice_open.py
+++ account_invoice_negative_total/tests/test_account_invoice_open.py
@@ -21,10 +21,11 @@ class TestInvoiceNegativeAmount(TransactionCase):
                 "reconcile": False,
             }
         )
-        self.journal = self.env["account.journal"].search(
-            [("type", "=", "sale")])[0]
+        self.journal = self.env["account.journal"].search([("type", "=", "sale")])[0]
 
-        self.invoice_line = self.env["account.invoice.line"].create(
+    def get_negative_invoice(self):
+        """Create negative invoice"""
+        invoice_line = self.env["account.invoice.line"].create(
             {
                 "name": "Line",
                 "price_unit": 1000.0,
@@ -32,34 +33,19 @@ class TestInvoiceNegativeAmount(TransactionCase):
                 "quantity": -1,
             }
         )
-
-        self.invoice = self.env["account.invoice"].create(
+        return self.env["account.invoice"].create(
             {
                 "name": "Test Customer Invoice",
                 "journal_id": self.journal.id,
                 "partner_id": self.partner.id,
                 "account_id": self.account.id,
-                "invoice_line_ids": [(4, self.invoice_line.id)],
+                "invoice_line_ids": [(4, invoice_line.id)],
             }
         )
 
-    def test_invoice_has_negative_amount(self):
-        self.assertEqual(
-            float_compare(
-                self.invoice.amount_total,
-                0.0,
-                precision_rounding=self.invoice.currency_id.rounding,
-            ),
-            -1,
-        )
-
-    def test_action_invoice_open_without_permission(self):
-        with self.assertRaises(UserError):
-            self.invoice.action_invoice_open()
-
-    def test_action_invoice_open_with_permissions(self):
-        # create a user with the right permission group
-        self.user_with_permission = self.env["res.users"].create(
+    def get_user_with_permission(self):
+        """Create a user with the right permission group"""
+        return self.env["res.users"].create(
             {
                 "name": "test",
                 "login": "test",
@@ -69,23 +55,84 @@ class TestInvoiceNegativeAmount(TransactionCase):
                         6,
                         0,
                         [
+                            self.env.ref("account.group_account_manager").id,
                             self.env.ref(
-                                "account.group_account_manager"
-                                ).id,
-                            self.env.ref(
-                                "account_invoice_negative_total.group_validate_invoice_negative_total_amount" # noqa
+                                "account_invoice_negative_total"
+                                ".group_validate_invoice_negative_total_amount"
                             ).id,
                         ],
                     )
                 ],
             }
         )
-        self.permission_env = self.env["account.invoice"].sudo(
-            self.user_with_permission
+
+    def test_invoice_has_negative_amount(self):
+        invoice = self.get_negative_invoice()
+        self.assertEqual(
+            float_compare(
+                invoice.amount_total,
+                0.0,
+                precision_rounding=invoice.currency_id.rounding,
+            ),
+            -1,
         )
+
+    def test_action_invoice_open_without_permission(self):
+        """Test opening invoice with negative amount without permission"""
+        with self.assertRaises(UserError):
+            self.get_negative_invoice().action_invoice_open()
+
+    def test_action_invoice_open_with_permissions(self):
+        """Test opening invoice with negative amount with permission"""
+        invoice = self.get_negative_invoice()
+        self.user_with_permission = self.get_user_with_permission()
+
         # check that the user can open the invoice with a negative amount
         self.assertTrue(
-            self.permission_env.browse(self.invoice.id).action_invoice_open()
+            self.env["account.invoice"]
+            .sudo(self.user_with_permission)
+            .browse(invoice.id)
+            .action_invoice_open()
         )
-        # reset the state of the invoice for further tests
-        self.permission_env.browse(self.invoice.id).state = "draft"
+
+    def test_action_invoice_open_empty_vendor(self):
+        """Test that action_invoice_open raise error when vendor is empty."""
+        invoice = self.get_negative_invoice()
+        invoice.partner_id = False
+        self.user_with_permission = self.get_user_with_permission()
+
+        with self.assertRaises(UserError):
+            (
+                self.env["account.invoice"]
+                .sudo(self.user_with_permission)
+                .browse(invoice.id)
+                .action_invoice_open()
+            )
+
+    def test_action_invoice_open_not_draft_state(self):
+        """Test that action_invoice_open raise error when state is not draft."""
+        invoice = self.get_negative_invoice()
+        invoice.state = "cancel"
+        self.user_with_permission = self.get_user_with_permission()
+
+        with self.assertRaises(UserError):
+            (
+                self.env["account.invoice"]
+                .sudo(self.user_with_permission)
+                .browse(invoice.id)
+                .action_invoice_open()
+            )
+
+    def test_action_invoice_open_empty_account(self):
+        """Test that action_invoice_open raise error when account_id is not set"""
+        invoice = self.get_negative_invoice()
+        invoice.account_id = False
+        self.user_with_permission = self.get_user_with_permission()
+
+        with self.assertRaises(UserError):
+            (
+                self.env["account.invoice"]
+                .sudo(self.user_with_permission)
+                .browse(invoice.id)
+                .action_invoice_open()
+            )
