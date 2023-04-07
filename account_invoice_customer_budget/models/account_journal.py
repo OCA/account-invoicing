0d183fdc99af47e7a88f278d8e294e30c8ffb3df
chafique.delli <chafique.delli@akretion.com>
2023-04-07 17:43:12 +0200

    [FIX] pre-commit

--- account_invoice_customer_budget/models/account_journal.py
+++ account_invoice_customer_budget/models/account_journal.py
@@ -2,7 +2,6 @@
 # @author: Mourad EL HADJ MIMOUNE <mourad.elhadj.mimounee@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
 
 from odoo import fields, models
 
@@ -10,6 +9,8 @@ from odoo import fields, models
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    is_budget = fields.Boolean("Is a customer budget",
-                               help="Check this box if the journal is used for manage customer budget.\n"
-                                    "The journal must have a sale type")
+    is_budget = fields.Boolean(
+        "Is a customer budget",
+        help="Check this box if the journal is used for manage customer budget.\n"
+        "The journal must have a sale type",
+    )
