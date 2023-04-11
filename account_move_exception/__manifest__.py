3b7445419380ab80702e78a18155f48461c9fc10
dsolanki <dhara.solanki@initos.com>
2023-04-11 10:47:34 +0530

    [IMP] account_move_exception: pre-commit stuff

--- account_move_exception/__manifest__.py
+++ account_move_exception/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Move Exception",
     "summary": "Custom exceptions on account move",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Generic Modules/Account",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
