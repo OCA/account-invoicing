a5bc44152a18c05522ecfb02687b177d25169e21
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-18 22:01:01 +0200

    [IMP] account_invoice_supplierinfo_update_triple_discount: black, isort, prettier

--- account_invoice_supplierinfo_update_triple_discount/__manifest__.py
+++ account_invoice_supplierinfo_update_triple_discount/__manifest__.py
@@ -3,27 +3,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Invoice - Triple Discount Supplier Info Update',
-    'summary': 'In the supplier invoice, automatically update all products '
-               'whose discounts on the line is different from '
-               'the supplier discounts',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting & Finance',
-    'website': 'https://github.com/OCA/account-invoicing',
-    'author': 'GRAP,Odoo Community Association (OCA)',
-    'maintainers': ['legalsylvain'],
-    'license': 'AGPL-3',
-    'depends': [
-        'account_invoice_triple_discount',
-        'account_invoice_supplierinfo_update_discount',
-        'purchase_triple_discount',
+    "name": "Account Invoice - Triple Discount Supplier Info Update",
+    "summary": "In the supplier invoice, automatically update all products "
+    "whose discounts on the line is different from "
+    "the supplier discounts",
+    "version": "12.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-invoicing",
+    "author": "GRAP,Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_triple_discount",
+        "account_invoice_supplierinfo_update_discount",
+        "purchase_triple_discount",
     ],
-    'installable': True,
-    'auto_install': True,
-    'data': [
-        'wizard/wizard_update_invoice_supplierinfo.xml',
+    "installable": True,
+    "auto_install": True,
+    "data": [
+        "wizard/wizard_update_invoice_supplierinfo.xml",
     ],
-    'images': [
-        'static/description/wizard_form.png',
+    "images": [
+        "static/description/wizard_form.png",
     ],
 }
