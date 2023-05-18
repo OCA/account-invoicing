0ac5e72fafcc13ea652a2a3ca2beca93f610a279
Aungkokolin1997 <lin@quartile.co>
2023-05-18 14:47:39 +0630

    [ADD] stock_picking_accounting_date

---
+++ stock_picking_accounting_date/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Accounting Date",
+    "version": "16.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "category": "Stock",
+    "license": "AGPL-3",
+    "depends": ["stock_account"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+    "installable": True,
+}
