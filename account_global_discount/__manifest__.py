fa2a3b0b4266ac6698cafb32265c19d8b74246a8
rafaferri <rafa.ferri@studio73.es>
2023-06-21 17:30:52 +0200

    [16.0][IMP] account_global_discount: Remake amount global discount

--- account_global_discount/__manifest__.py
+++ account_global_discount/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Global Discount",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Accounting",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
