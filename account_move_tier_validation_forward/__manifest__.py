38ce75e88405700fb93c73bb41b50a4274783c4f
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-06-22 18:09:45 +0200

    [MIG] account_move_tier_validation_forward: Migration to 16.0

--- account_move_tier_validation_forward/__manifest__.py
+++ account_move_tier_validation_forward/__manifest__.py
@@ -2,15 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account Move Tier Validation - Forward Option",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-invoicing",
     "author": "ForgeFlow,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "application": False,
     "installable": True,
     "auto_install": True,
     "depends": ["base_tier_validation_forward", "account_move_tier_validation"],
-    "data": ["views/account_move_view.xml"],
 }
