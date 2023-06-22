ec26e7c144bd5f8b589aee7d4aa2df7638736942
Christopher Ormaza <chris.ormaza@forgeflow.com>
2023-06-22 11:27:17 -0500

    [16.0][MIG] account_invoice_alternate_payer

--- account_invoice_alternate_payer/models/account_move.py
+++ account_invoice_alternate_payer/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import json
+from contextlib import contextmanager
 
 from odoo import _, api, fields, models
 from odoo.tools import float_is_zero
@@ -23,35 +23,58 @@ class AccountMove(models.Model):
 
     @api.depends("commercial_partner_id", "alternate_payer_id")
     def _compute_bank_partner_id(self):
-        super(
-            AccountMove,
-            self.filtered(lambda r: not r.alternate_payer_id or not r.is_outbound()),
-        )._compute_bank_partner_id()
-        for move in self:
+        super_moves = self.filtered(
+            lambda r: not r.alternate_payer_id or not r.is_outbound()
+        )
+        for move in self - super_moves:
             if move.is_outbound() and move.alternate_payer_id:
                 move.bank_partner_id = move.alternate_payer_id
+        return super(
+            AccountMove,
+            super_moves,
+        )._compute_bank_partner_id()
 
     @api.onchange("alternate_payer_id")
     def _onchange_alternate_payer_id(self):
         return self._onchange_partner_id()
 
-    def _recompute_payment_terms_lines(self):
-        super()._recompute_payment_terms_lines()
-        for invoice in self:
-            if invoice.alternate_payer_id:
-                invoice.line_ids.filtered(
-                    lambda r: r.account_id.user_type_id.type
-                    in ("receivable", "payable")
-                ).update({"partner_id": invoice.alternate_payer_id.id})
+    @contextmanager
+    def _sync_dynamic_line(
+        self,
+        existing_key_fname,
+        needed_vals_fname,
+        needed_dirty_fname,
+        line_type,
+        container,
+    ):
+        records_with_alternate_player = container.get("records").filtered(
+            lambda x: x.alternate_payer_id
+        )
+        with super()._sync_dynamic_line(
+            existing_key_fname,
+            needed_vals_fname,
+            needed_dirty_fname,
+            line_type,
+            container,
+        ):
+            if line_type == "payment_term" and records_with_alternate_player:
+                for invoice in container.get("records").filtered(
+                    lambda x: x.alternate_payer_id
+                ):
+                    payment_term_lines = invoice.line_ids.filtered(
+                        lambda x: x.display_type == "payment_term"
+                    )
+                    payment_term_lines.write(
+                        {
+                            "partner_id": invoice.alternate_payer_id.id,
+                        }
+                    )
+            yield
 
     def _compute_payments_widget_to_reconcile_info(self):
-        super(
-            AccountMove, self.filtered(lambda r: not r.alternate_payer_id)
-        )._compute_payments_widget_to_reconcile_info()
-        for move in self:
-            if not self.alternate_payer_id:
-                continue
-            move.invoice_outstanding_credits_debits_widget = json.dumps(False)
+        super_moves = self.filtered(lambda r: not r.alternate_payer_id)
+        for move in self - super_moves:
+            move.invoice_outstanding_credits_debits_widget = False
             move.invoice_has_outstanding = False
             if (
                 move.state != "posted"
@@ -60,8 +83,8 @@ class AccountMove(models.Model):
             ):
                 continue
             pay_term_line_ids = move.line_ids.filtered(
-                lambda line: line.account_id.user_type_id.type
-                in ("receivable", "payable")
+                lambda line: line.account_id.account_type
+                in ("asset_receivable", "liability_payable")
             )
 
             domain = [
@@ -86,37 +109,39 @@ class AccountMove(models.Model):
             info = {"title": "", "outstanding": True, "content": [], "move_id": move.id}
             lines = self.env["account.move.line"].search(domain)
             currency_id = move.currency_id
-            if len(lines) != 0:
-                for line in lines:
-                    # get the outstanding residual value in invoice currency
-                    if line.currency_id and line.currency_id == move.currency_id:
-                        amount_to_show = abs(line.amount_residual_currency)
-                    else:
-                        currency = line.company_id.currency_id
-                        amount_to_show = currency._convert(
-                            abs(line.amount_residual),
-                            move.currency_id,
-                            move.company_id,
-                            line.date or fields.Date.today(),
-                        )
-                    if float_is_zero(
-                        amount_to_show, precision_rounding=move.currency_id.rounding
-                    ):
-                        continue
-                    info["content"].append(
-                        {
-                            "journal_name": line.ref or line.move_id.name,
-                            "amount": amount_to_show,
-                            "currency": currency_id.symbol,
-                            "id": line.id,
-                            "position": currency_id.position,
-                            "digits": [69, move.currency_id.decimal_places],
-                            "payment_date": fields.Date.to_string(line.date),
-                        }
+            for line in lines:
+                # get the outstanding residual value in invoice currency
+                if line.currency_id and line.currency_id == move.currency_id:
+                    amount_to_show = abs(line.amount_residual_currency)
+                else:
+                    currency = line.company_id.currency_id
+                    amount_to_show = currency._convert(
+                        abs(line.amount_residual),
+                        move.currency_id,
+                        move.company_id,
+                        line.date or fields.Date.today(),
                     )
-                info["title"] = type_payment
-                move.invoice_outstanding_credits_debits_widget = json.dumps(info)
-                move.invoice_has_outstanding = True
+                if float_is_zero(
+                    amount_to_show, precision_rounding=move.currency_id.rounding
+                ):
+                    continue
+                info["content"].append(
+                    {
+                        "journal_name": line.ref or line.move_id.name,
+                        "amount": amount_to_show,
+                        "currency": currency_id.symbol,
+                        "id": line.id,
+                        "position": currency_id.position,
+                        "digits": [69, move.currency_id.decimal_places],
+                        "payment_date": fields.Date.to_string(line.date),
+                    }
+                )
+            info["title"] = type_payment
+            move.invoice_outstanding_credits_debits_widget = info
+            move.invoice_has_outstanding = True
+        return super(
+            AccountMove, super_moves
+        )._compute_payments_widget_to_reconcile_info()
 
 
 class AccountMoveLine(models.Model):
@@ -125,8 +150,12 @@ class AccountMoveLine(models.Model):
 
     def write(self, values):
         # CHECK ME: this change to commercial partner when find difference between main partner
-        # https://github.com/odoo/odoo/blob/0f90852818a334d66ed8a6781f5abdc022f76ff7/addons/account/models/account_move.py#L2695 # noqa: B950
+        # https://github.com/odoo/odoo/blob/16.0/addons/account/models/account_move.py#L3431
         if "partner_id" in values and len(values.keys()) == 1:
-            lines_to_skip = self.filtered(lambda x: x.move_id.alternate_payer_id)
+            lines_to_skip = self.filtered(
+                lambda x: x.move_id.alternate_payer_id
+                and x.display_type == "payment_term"
+                and x.partner_id == x.move_id.alternate_payer_id
+            )
             return super(AccountMoveLine, self - lines_to_skip).write(values)
-        return super(AccountMoveLine, self).write(values)
+        return super().write(values)
