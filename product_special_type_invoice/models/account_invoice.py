e388c3566cbbc134133e2fc55640060866e525f8
François Honoré <francois.honore@acsone.eu>
2023-07-12 14:06:33 +0200

    [IMP] product_special_type_invoice: pre-commit stuff

--- product_special_type_invoice/models/account_invoice.py
+++ product_special_type_invoice/models/account_invoice.py
@@ -1,51 +1,54 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from operator import add
-from odoo import api, fields, models, _
+
+from odoo import api, fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
 class AccountInvoice(models.Model):
 
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     extra_discount_amount = fields.Float(
-        compute='_compute_special_fields',
-        digits=dp.get_precision('Account'),
+        compute="_compute_special_fields",
+        digits=dp.get_precision("Account"),
         store=True,
     )
     advance_amount = fields.Float(
-        compute='_compute_special_fields',
-        digits=dp.get_precision('Account'),
+        compute="_compute_special_fields",
+        digits=dp.get_precision("Account"),
         store=True,
     )
     delivery_amount = fields.Float(
-        compute='_compute_special_fields',
-        digits=dp.get_precision('Account'),
+        compute="_compute_special_fields",
+        digits=dp.get_precision("Account"),
         store=True,
     )
     fees_amount = fields.Float(
-        compute='_compute_special_fields',
-        digits=dp.get_precision('Account'),
+        compute="_compute_special_fields",
+        digits=dp.get_precision("Account"),
         store=True,
     )
 
     @api.model
     def _get_special_fields(self):
         return {
-            'discount': 'extra_discount_amount',
-            'advance': 'advance_amount',
-            'delivery': 'delivery_amount',
-            'fee': 'fees_amount',
+            "discount": "extra_discount_amount",
+            "advance": "advance_amount",
+            "delivery": "delivery_amount",
+            "fee": "fees_amount",
         }
 
-    @api.depends('invoice_line_ids.product_id.special_type',
-                 'invoice_line_ids.price_unit',
-                 'invoice_line_ids.discount',
-                 'invoice_line_ids.price_subtotal')
+    @api.depends(
+        "invoice_line_ids.product_id.special_type",
+        "invoice_line_ids.price_unit",
+        "invoice_line_ids.discount",
+        "invoice_line_ids.price_subtotal",
+    )
     def _compute_special_fields(self):
-        """ Compute Discount and Advances amounts (sum of all the products of
+        """Compute Discount and Advances amounts (sum of all the products of
         these types)
         You can have several types amounts that are added to the same field
         depending on the grouping in '_get_special_fields' (overriding it)
@@ -61,6 +64,8 @@ class AccountInvoice(models.Model):
                     [
                         line.price_subtotal
                         for line in invoice.invoice_line_ids
-                        if line.product_id and
-                        line.product_id.special_type == special_type
-                    ], 0.0)
+                        if line.product_id
+                        and line.product_id.special_type == special_type
+                    ],
+                    0.0,
+                )
--- product_special_type_invoice/tests/test_product_special_invoice.py
+++ product_special_type_invoice/tests/test_product_special_invoice.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,55 +5,53 @@ from odoo.tests import common
 
 
 class TestProductSpecialInvoice(common.TransactionCase):
-
     def setUp(self):
         super(TestProductSpecialInvoice, self).setUp()
-        self.product_obj = self.env['product.product']
-        self.line_obj = self.env['account.invoice.line']
-        self.invoice_obj = self.env['account.invoice']
-        self.agrolait = self.env.ref('base.res_partner_2')
-        self.product_3 = self.env.ref('product.product_product_3')
-        self.product_2 = self.env.ref('product.product_product_2')
-        self.product_1 = self.env.ref('product.product_product_1')
-        self.product_1.special_type = 'fee'
+        self.product_obj = self.env["product.product"]
+        self.line_obj = self.env["account.invoice.line"]
+        self.invoice_obj = self.env["account.invoice"]
+        self.agrolait = self.env.ref("base.res_partner_2")
+        self.product_3 = self.env.ref("product.product_product_3")
+        self.product_2 = self.env.ref("product.product_product_2")
+        self.product_1 = self.env.ref("product.product_product_1")
+        self.product_1.special_type = "fee"
         vals = {
-            'name': 'Advance Product',
-            'special_type': 'advance',
-            'type': 'service',
+            "name": "Advance Product",
+            "special_type": "advance",
+            "type": "service",
         }
         self.advance = self.product_obj.create(vals)
 
         vals = {
-            'name': 'Delivery Product',
-            'special_type': 'delivery',
-            'type': 'service',
-            'list_price': 30.0,
+            "name": "Delivery Product",
+            "special_type": "delivery",
+            "type": "service",
+            "list_price": 30.0,
         }
         self.delivery = self.product_obj.create(vals)
 
         vals = {
-            'name': 'Discount Product',
-            'special_type': 'discount',
-            'type': 'service',
-            'list_price': '-15.0',
+            "name": "Discount Product",
+            "special_type": "discount",
+            "type": "service",
+            "list_price": "-15.0",
         }
         self.discount = self.product_obj.create(vals)
 
     def _create_line(self, product, qty):
         vals = {
-            'product_id': product.id,
-            'quantity': qty,
-            'invoice_id': self.invoice.id,
+            "product_id": product.id,
+            "quantity": qty,
+            "invoice_id": self.invoice.id,
         }
         line = self.line_obj.new(vals)
         line._onchange_product_id()
-        values = line._convert_to_write(
-            {name: line[name] for name in line._cache})
+        values = line._convert_to_write({name: line[name] for name in line._cache})
         return self.line_obj.create(values)
 
     def _create_invoice(self):
         vals = {
-            'partner_id': self.agrolait.id,
+            "partner_id": self.agrolait.id,
         }
         self.invoice = self.invoice_obj.create(vals)
 
@@ -68,25 +65,25 @@ class TestProductSpecialInvoice(common.TransactionCase):
         self.line3 = self._create_line(self.advance, 1.0)
         self.line3 = self._create_line(self.advance, 1.0)
 
-        self.assertEquals(
+        self.assertEqual(
             307.5,
             self.invoice.fees_amount,
         )
-        self.assertEquals(
+        self.assertEqual(
             30.0,
             self.invoice.delivery_amount,
         )
-        self.assertEquals(
+        self.assertEqual(
             -15.0,
             self.invoice.extra_discount_amount,
         )
-        self.assertEquals(
+        self.assertEqual(
             2.0,
             self.invoice.advance_amount,
         )
         # (38.25 * 5 = 191.25) + (450.0 * 3 = 1350) + 307.5 - 15.0
         # + 30.0 +  2.0
-        self.assertEquals(
+        self.assertEqual(
             1865.75,
             self.invoice.amount_untaxed,
         )
