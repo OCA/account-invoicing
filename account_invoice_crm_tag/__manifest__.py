89d7bfa6dd282b0de8070bdac6e73e37e9846e2b
Stefan <stefan.ungureanu@tecnativa.com>
2023-07-14 09:14:07 +0200

    [ADD] account_invoice_crm_tag: CRM tags on invoices
    TT44457
    
    Co-Authored-By: Pedro M. Baeza <pedro.baeza@tecnativa.com>

---
+++ account_invoice_crm_tag/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Tecnativa - Stefan Ungureanu
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Invoice CRM Tag",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/account_invoice_views.xml",
+        "report/account_invoice_report.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
