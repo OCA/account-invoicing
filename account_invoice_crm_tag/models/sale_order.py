a85ff1ec781288124eafc99116a7bc8b9cebcfbb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-08-03 09:11:24 +0200

    [FIX+IMP] account_invoice_crm_tag: Proper line tag propagation + colors
    - Transfer properly CRM tags also to invoice lines, as although being a
      related stored, it's not correctly saved due to the trick/duality of
      invoice_line_ids/line_ids.
    - Put color definition in many2many_tags widget.
    
    TT44457

--- account_invoice_crm_tag/models/sale_order.py
+++ account_invoice_crm_tag/models/sale_order.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Tecnativa - Stefan Ungureanu
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
 
@@ -11,3 +12,15 @@ class SaleOrder(models.Model):
         if self.tag_ids:
             vals["crm_tag_ids"] = [(6, 0, self.tag_ids.ids)]
         return vals
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_invoice_line(self, **optional_values):
+        """Transfer CRM tags also to invoice lines, as although being a related stored,
+        it's not correctly saved due to the trick/duality of invoice_line_ids/line_ids.
+        """
+        res = super()._prepare_invoice_line(**optional_values)
+        res["crm_tag_ids"] = [(6, 0, self.order_id.tag_ids.ids)]
+        return res
