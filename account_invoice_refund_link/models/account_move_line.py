ec999721860c76a83aed8c1d0f463cc49264fc38
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-08-07 12:03:06 +0200

    [IMP] account_invoice_refund_link: Make more reliable the linking
    Hooking on a high level method like `_reverse_move_vals` (although still
    being private), makes that other modules hooking into it may alter the
    number of returned lines (like for example,
    account_invoice_refund_line_selection).
    
    We choose the low-level `copy_data` method that is used in such method
    for linking the refund lines with their origin ones, avoiding the
    problem.

--- account_invoice_refund_link/models/account_move_line.py
+++ account_invoice_refund_link/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# Copyright 2014-2018 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2014-2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -24,3 +24,13 @@ class AccountInvoiceLine(models.Model):
         help="Refund invoice lines created from this invoice line",
         copy=False,
     )
+
+    def copy_data(self, default=None):
+        """Link refund lines with the original ones when copying move lines from the
+        `_reverse_move_vals` method.
+        """
+        res = super().copy_data(default=default)
+        if self.env.context.get("link_origin_line"):
+            for line, values in zip(self, res):
+                values["origin_line_id"] = line.id
+        return res
