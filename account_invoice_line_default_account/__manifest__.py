9b38459a1527d608afd9bf36137016bcfa67a8c4
Denis Roussel <denis.roussel@acsone.eu>
2023-09-06 12:15:27 +0200

    [IMP] account_invoice_line_default_account: pre-commit stuff

--- account_invoice_line_default_account/__manifest__.py
+++ account_invoice_line_default_account/__manifest__.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2012 Therp BV (<http://therp.nl>)
 # Copyright 2013-2018 BCIM SPRL (<http://www.bcim.be>)
 # Copyright 2022 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Account Invoice Line Default Account',
-    'version': '12.0.1.0.1',
-    'depends': [
-        'account'
+    "name": "Account Invoice Line Default Account",
+    "version": "12.0.1.0.1",
+    "depends": ["account"],
+    "author": "Therp BV,BCIM,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "data": [
+        "views/res_partner.xml",
     ],
-    'author': 'Therp BV,BCIM,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-invoicing',
-    'license': 'AGPL-3',
-    'category': 'Accounting',
-    'data': [
-        'views/res_partner.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
