dcf199871e65f1b1740bd0c0a14d2ee2d9ed4716
sergio-teruel <sergio.teruel@tecnativa.com>
2023-09-06 11:33:13 +0200

    [FIX] account_invoice_mass_sending: Do not check company logo in tests

--- account_invoice_mass_sending/tests/test_account_invoice_mass_sending.py
+++ account_invoice_mass_sending/tests/test_account_invoice_mass_sending.py
@@ -111,6 +111,7 @@ class TestAccountInvoiceMassSending(TransactionCase):
             wizard = self.wizard_obj.with_context(
                 active_ids=self.invoices.ids,
                 active_model=self.first_eligible_invoice._name,
+                discard_logo_check=True,
             ).create({})
             wizard.enqueue_invoices()
             trap.assert_jobs_count(2)
@@ -121,6 +122,7 @@ class TestAccountInvoiceMassSending(TransactionCase):
             wizard = self.wizard_obj.with_context(
                 active_ids=self.invoice_in_progress.ids,
                 active_model=self.invoice_in_progress._name,
+                discard_logo_check=True,
             ).create({})
             wizard.enqueue_invoices()
             trap.assert_jobs_count(0)
@@ -136,6 +138,7 @@ class TestAccountInvoiceMassSending(TransactionCase):
             wizard = self.wizard_obj.with_context(
                 active_ids=self.invoices.ids,
                 active_model=self.first_eligible_invoice._name,
+                discard_logo_check=True,
             ).create({})
             wizard.enqueue_invoices()
             self.assertTrue(self.first_eligible_invoice.sending_in_progress)
