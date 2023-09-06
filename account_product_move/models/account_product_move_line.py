9bb79b27736b02e01d6ba52e435b580a58860421
Ronald Portier (Therp BV) <ronald@therp.nl>
2023-09-06 13:42:58 +0200

    [IMP] Amount in extra moves can also be based on cost price
    There is also a change where debit and credit in extra move definition are
    always in the currency of the specific account.product.move.line.

--- account_product_move/models/account_product_move_line.py
+++ account_product_move/models/account_product_move_line.py
@@ -1,13 +1,18 @@
-# Copyright 2022 Therp BV <https://therp.nl>
+# Copyright 2022-2023 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AccountProductMoveLine(models.Model):
     _name = "account.product.move.line"
     _description = "Items for journal entry for given products"
 
-    move_id = fields.Many2one(comodel_name="account.product.move")
+    def _compute_effective_currency_id(self):
+        """Effective currency is specified on line or taken from company."""
+        for line in self:
+            line.effective_currency_id = line.currency_id or line.company_currency_id
+
     company_id = fields.Many2one(
         related="move_id.company_id", store=True, readonly=True
     )
@@ -17,63 +22,93 @@ class AccountProductMoveLine(models.Model):
         readonly=True,
         help="Utility field to express amount currency",
     )
-    debit = fields.Monetary(default=0.0, currency_field="company_currency_id")
-    credit = fields.Monetary(default=0.0, currency_field="company_currency_id")
+    currency_id = fields.Many2one("res.currency", string="Currency")
+    amount_currency = fields.Monetary(
+        string="Amount in Currency",
+        copy=True,
+        help="DEPRECATED, but still needed for data migration",
+    )
+    effective_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        compute="_compute_effective_currency_id",
+        string="Effective Currency",
+        readonly=True,
+        help="Utility field to express amount currency",
+    )
+    move_id = fields.Many2one(comodel_name="account.product.move")
     account_id = fields.Many2one(
         comodel_name="account.account",
         ondelete="cascade",
         required=True,
         check_company=True,
     )
-    currency_id = fields.Many2one("res.currency", string="Currency")
-    amount_currency = fields.Monetary(
-        string="Amount in Currency",
+    debit = fields.Monetary(default=0.0, currency_field="effective_currency_id")
+    credit = fields.Monetary(default=0.0, currency_field="effective_currency_id")
+    percentage_debit = fields.Float(
+        string="Debit as % of cost",
+        digits=(5, 2),
         copy=True,
-        help="The amount expressed in an optional other currency"
-        " if it is a multi-currency entry.",
+        help="Instead of, or in addition to, a fixed amount debit per unit"
+        " product a percentage can be taken from the product standard price.",
+    )
+    percentage_credit = fields.Float(
+        string="Credit as % of cost",
+        digits=(5, 2),
+        copy=True,
+        help="Instead of, or in addition to, a fixed amount credit per unit"
+        " product a percentage can be taken from the product standard price.",
     )
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        """Make sure debit and credit reflect currency fields."""
-        result = super().create(vals_list)
-        result._recompute_debit_credit_from_amount_currency()
-        return result
-
-    def write(self, vals):
-        """Make sure debit and credit reflect currency fields."""
-        result = super().write(vals)
-        self._recompute_debit_credit_from_amount_currency()
-        return result
-
-    @api.onchange("currency_id", "amount_currency")
-    def _onchange_amount_currency(self):
-        """If currency fields change, change debit/credit."""
-        self._recompute_debit_credit_from_amount_currency()
-
-    def _recompute_debit_credit_from_amount_currency(self):
-        """Recompute the debit/credit based on amount_currency/currency_id and date."""
+    @api.constrains("debit", "credit", "percentage_debit", "percentage_credit")
+    def _check_debit_credit(self):
+        """Do not allow to mix debit and credit."""
         for line in self:
-            company_currency = line.company_id.currency_id
-            if (
-                not company_currency
-                or not line.currency_id
-                or line.currency_id == company_currency
+            if (line.debit or line.percentage_debit) and (
+                line.credit or line.percentage_credit
             ):
-                # if no specific currency, leave debit and credit as is.
+                raise ValidationError(
+                    _("You can not mix debit and credit in one line.")
+                )
+
+    @api.constrains(
+        "currency_id", "company_currency_id", "percentage_debit", "percentage_credit",
+    )
+    def _check_no_percentage_currency(self):
+        """Do not use percentages of cost price with currency conversion."""
+        for line in self:
+            if not line.currency_id or (line.currency_id == line.company_currency_id):
                 continue
-            balance = line.currency_id._convert(
-                line.amount_currency,
-                company_currency,
-                line.company_id,
-                fields.Date.today(),
-            )
-            if balance > 0:
-                debit = balance
-                credit = 0.0
-            else:
-                debit = 0.0
-                credit = -balance
-            if debit != line.debit or credit != line.credit:
-                # Doing a write only on unequal values prevents infinite recursion.
-                line.write({"debit": debit, "credit": credit})
+            if line.percentage_debit or line.percentage_credit:
+                raise ValidationError(
+                    _("You can only use percentages with the company currency.")
+                )
+
+    def _complete_line_vals(self, line, vals):
+        """Return dict to complete vals for move line to be created."""
+        self.ensure_one()
+        vals.update({"name": self.move_id.name, "account_id": self.account_id.id})
+        quantity = line.quantity
+        invoice_type = line.move_id.type
+        standard_price = line.product_id.standard_price
+        if self.debit or self.percentage_debit:
+            credit = 0.0
+            debit = quantity * self.debit
+            if self.percentage_debit:
+                debit += quantity * self.percentage_debit * 0.01 * standard_price
+        else:
+            debit = 0.0
+            credit = quantity * self.credit
+            if self.percentage_credit:
+                credit += quantity * self.percentage_credit * 0.01 * standard_price
+        if invoice_type == "out_refund":
+            # Reverse amounts for credit note.
+            debit = 0.0 - debit
+            credit = 0.0 - credit
+        if self.effective_currency_id == self.company_currency_id:
+            vals["debit"] = debit
+            vals["credit"] = credit
+            vals["currency_id"] = False
+        else:
+            vals["amount_currency"] = debit - credit
+            vals["currency_id"] = self.currency_id.id
+        return vals
--- account_product_move/readme/DESCRIPTION.rst
+++ account_product_move/readme/DESCRIPTION.rst
@@ -1,2 +1,9 @@
 Create extra Journal entries and items when posting out-invoices, that depend on
 the products in the invoice lines.
+
+The extra journal entries created will remain linked to the original invoice, and
+the status of the extra moves will depend on the status of the invoice.
+
+Journal items can have a fixed amount debit or credit per unit of product
+invoiced on a line, or the amount debit or credit can depend on the costprice
+of the product.
