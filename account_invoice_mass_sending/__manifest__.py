0419d70fefa00863227a0fb3c6d3f9d391ac9801
sergio-teruel <sergio.teruel@tecnativa.com>
2023-09-06 10:43:28 +0200

    [MIG] account_invoice_mass_sending: Migration to v15.0

--- account_invoice_mass_sending/__manifest__.py
+++ account_invoice_mass_sending/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Invoice Mass Sending",
     "summary": """
         This addon adds a mass sending feature on invoices.""",
-    "version": "14.0.2.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA), Open Net Sàrl",
     "website": "https://github.com/OCA/account-invoicing",
