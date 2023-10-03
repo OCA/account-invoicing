ce92be7cce25a1c9f129a9df2eea23c35e6d31db
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-10-03 23:30:58 +0200

    [IMP] account_move_substate: documentation

--- account_move_substate/__manifest__.py
+++ account_move_substate/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2021 Ecosoft (<http://ecosoft.co.th>)
+# Copyright 2023 360ERP (<https://360ERP.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Move Sub State",
     "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
-    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "author": "360ERP, Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
     "license": "AGPL-3",
     "depends": ["account", "base_substate"],
