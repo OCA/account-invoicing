46964ba56ad2d419eff2cfdfaad858afc90c6c42
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2023-10-17 14:44:05 +0530

    [MIG] account_invoice_refund_reason_skip_anglo_saxon: Migration to 16.0

--- account_invoice_refund_reason_skip_anglo_saxon/__manifest__.py
+++ account_invoice_refund_reason_skip_anglo_saxon/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of ForgeFlow. See LICENSE file for full copyright and licensing details.
 {
     "name": "Account Invoice Refund Reason Skip Anglo Saxon",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Account Invoice Refund Reason.",
     "category": "Accounting",
     "author": "ForgeFlow S.L, " "Odoo Community Association (OCA)",
