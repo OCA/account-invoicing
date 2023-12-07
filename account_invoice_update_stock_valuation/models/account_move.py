e0dbdaaf54ef0115a8e3df2370a9b38f87d83bc6
chafique.delli <chafique.delli@akretion.com>
2023-12-07 20:41:08 +0100

    refactoring and improvements

--- account_invoice_update_stock_valuation/models/account_move.py
+++ account_invoice_update_stock_valuation/models/account_move.py
@@ -2,79 +2,90 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    svl_warn_msg = fields.Text(compute="_compute_svl_warn_msg")
+    all_stock_valuation_ok = fields.Boolean(
+        string="All stock valuation is ok ?",
+        compute="_compute_all_stock_valuation_ok",
+        store=True,
+    )
 
-    @api.depends("invoice_line_ids.stock_valuation_ok")
-    def _compute_svl_warn_msg(self):
+    @api.depends("state", "invoice_line_ids", "invoice_line_ids.stock_valuation_ok")
+    def _compute_all_stock_valuation_ok(self):
         for invoice in self:
-            svl_warn_msg = False
-            if invoice.state == "draft" and invoice.invoice_line_ids.filtered(
-                lambda l: not l.stock_valuation_ok
-            ):
-                svl_warn_msg = _(
-                    "Warning: in this invoice, there are lines that have a unit price "
-                    "that is different from that of the purchase order ! "
-                    "The stock valuation as well as the purchase price of the product "
-                    "will be updated when the invoice is validated."
+            invoice.all_stock_valuation_ok = not (
+                invoice.state == "draft"
+                and invoice.invoice_line_ids.filtered(
+                    lambda l: not l.stock_valuation_ok
                 )
-            invoice.svl_warn_msg = svl_warn_msg
+            )
 
     def action_post(self):
-        res = super().action_post()
         for invoice in self:
             if invoice.move_type == "in_invoice":
                 for line in invoice.invoice_line_ids.filtered(
                     lambda l: not l.stock_valuation_ok
                 ):
-                    line._update_product_standard_price()
                     line._create_in_svl()
-        return res
+                    line._update_product_standard_price()
+        return super().action_post()
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     stock_valuation_ok = fields.Boolean(
-        string="Stock valuation is ok ?", compute="_compute_stock_valuation_ok"
+        string="Stock valuation is ok ?",
+        compute="_compute_stock_valuation_ok",
+        store=True,
     )
 
-    @api.depends("price_unit", "purchase_line_id", "purchase_line_id.price_unit")
+    @api.depends(
+        "move_id.state",
+        "price_unit",
+        "product_uom_id",
+        "purchase_line_id",
+        "purchase_line_id.price_unit",
+        "purchase_line_id.product_uom",
+    )
     def _compute_stock_valuation_ok(self):
         for line in self:
-            stock_valuation_ok = True
             svl = (
                 self.env["stock.valuation.layer"]
                 .sudo()
                 .search(
                     [
                         ("product_id", "=", line.product_id.id),
+                        ("unit_cost", "=", line.price_unit),
+                        ("quantity", "=", line.quantity),
                         ("account_move_id", "=", line.move_id.id),
                         ("company_id", "=", line.company_id.id),
                     ]
                 )
             )
-            if (
-                not svl
+            if line.product_uom_id == line.purchase_line_id.product_uom:
+                line_price_unit = line.price_unit
+            else:
+                line_price_unit = line.product_uom_id._compute_price(
+                    price=line.price_unit, to_unit=line.purchase_line_id.product_uom
+                )
+            line.stock_valuation_ok = not (
+                line.move_id.state == "draft"
+                and not svl
                 and line.purchase_line_id
-                and line.price_unit != line.purchase_line_id.price_unit
-            ):
-                stock_valuation_ok = False
-            line.stock_valuation_ok = stock_valuation_ok
+                and line_price_unit != line.purchase_line_id.price_unit
+            )
 
     def _prepare_common_svl_vals(self):
         self.ensure_one()
         return {
             "account_move_id": self.move_id.id,
             "company_id": self.company_id.id,
-            "product_id": self.product_id.id,
-            "description": "%s - %s" % (self.move_id.name, self.product_id.name)
-            or self.product_id.name,
+            "description": "%s - %s" % (self.move_id.name, self.product_id.name),
         }
 
     def _create_in_svl(self):
@@ -84,6 +95,17 @@ class AccountMoveLine(models.Model):
                 line.quantity, line.price_unit
             )
             svl_vals.update(line._prepare_common_svl_vals())
+            if line.product_uom_id != line.product_id.uom_po_id:
+                svl_vals["quantity"] = line.product_uom_id._compute_quantity(
+                    qty=line.quantity,
+                    to_unit=line.product_id.uom_po_id,
+                    round=True,
+                    rounding_method="UP",
+                    raise_if_failure=True,
+                )
+                svl_vals["unit_cost"] = line.product_uom_id._compute_price(
+                    price=line.price_unit, to_unit=line.product_id.uom_po_id
+                )
         return self.env["stock.valuation.layer"].sudo().create(svl_vals)
 
     def _update_product_standard_price(self):
@@ -99,5 +121,5 @@ class AccountMoveLine(models.Model):
             # Update the standard price in case of AVCO
             if line.product_id.categ_id.property_cost_method == "average":
                 line.product_id.with_context(disable_auto_svl=True).standard_price = (
-                    added_value_svls + line.price_unit
-                ) / (total_qty_svls + line.quantity)
+                    added_value_svls / total_qty_svls
+                )
--- account_invoice_update_stock_valuation/tests/test_account_invoice_update_stock_valuation.py
+++ account_invoice_update_stock_valuation/tests/test_account_invoice_update_stock_valuation.py
@@ -11,23 +11,25 @@ class TestAccountInvoiceUpdateStockValuation(SavepointCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.partner = cls.env.ref("base.res_partner_1")
-        cls.product_6 = cls.env.ref("product.product_product_6")
-        cls.product_7 = cls.env.ref("product.product_product_7")
+        cls.product = cls.env.ref("product.product_product_6")
+        cls.unit = cls.env.ref("uom.product_uom_unit")
+        cls.dozen = cls.env.ref("uom.product_uom_dozen")
 
     def test_update_stock_valuation_from_invoice(self):
-        products = self.product_6
-        purchase = self._create_purchase_order(self.partner, products)
+        purchase = self._create_purchase_order(
+            self.partner, self.product, qty=1, uom=self.unit
+        )
         invoice = self._create_invoice_from_purchase(purchase)
         invoice_line = invoice.invoice_line_ids[0].with_context(
             check_move_validity=False
         )
         invoice_line.price_unit += 10.0
         invoice._recompute_dynamic_lines()
-        self.assertEqual(invoice_line.stock_valuation_ok, False)
+        self.assertFalse(invoice_line.stock_valuation_ok)
         invoice.invoice_date = fields.Date.today()
         invoice.action_post()
         invoice_line._compute_stock_valuation_ok()
-        self.assertEqual(invoice_line.stock_valuation_ok, True)
+        self.assertTrue(invoice_line.stock_valuation_ok)
         svl = self.env["stock.valuation.layer"].search(
             [
                 ("product_id", "=", invoice_line.product_id.id),
@@ -38,17 +40,85 @@ class TestAccountInvoiceUpdateStockValuation(SavepointCase):
         )
         self.assertTrue(svl)
 
-    def _create_purchase_order(self, partner, products):
-        order_form = Form(self.env["purchase.order"])
-        order_form.partner_id = partner
-        for product in products:
-            with order_form.order_line.new() as line_form:
-                line_form.product_id = product
-        return order_form.save()
+    def test_update_stock_valuation_invoice_different_uom_purchase(self):
+        # purchase order with 12 units
+        # invoice with 1 Dozen
+        purchase = self._create_purchase_order(
+            self.partner, self.product, qty=12, uom=self.unit
+        )
+        invoice = self._create_invoice_from_purchase(purchase)
+        invoice_line = invoice.invoice_line_ids[0].with_context(
+            check_move_validity=False
+        )
+        invoice_line.update(
+            {
+                "price_unit": (invoice_line.price_unit * 12.0) + 10.0,
+                "quantity": 1,
+                "product_uom_id": self.dozen.id,
+            }
+        )
+        invoice._recompute_dynamic_lines()
+        self.assertFalse(invoice_line.stock_valuation_ok)
+        invoice.invoice_date = fields.Date.today()
+        invoice.action_post()
+        invoice_line._compute_stock_valuation_ok()
+        self.assertTrue(invoice_line.stock_valuation_ok)
+        svl = self.env["stock.valuation.layer"].search(
+            [
+                ("product_id", "=", invoice_line.product_id.id),
+                ("quantity", "=", 12.0),
+                ("uom_id", "=", self.unit.id),
+                ("value", "=", invoice_line.price_unit),
+                ("account_move_id", "=", invoice.id),
+                ("company_id", "=", invoice_line.company_id.id),
+            ]
+        )
+        self.assertTrue(svl)
+        self.assertEqual(svl.unit_cost, 750.83)
+
+    def test_update_stock_valuation_invoice_same_uom_purchase(self):
+        # purchase order with 1 Dozen
+        # invoice with 1 Dozen
+        purchase = self._create_purchase_order(
+            self.partner, self.product, qty=1, uom=self.dozen
+        )
+        invoice = self._create_invoice_from_purchase(purchase)
+        invoice_line = invoice.invoice_line_ids[0].with_context(
+            check_move_validity=False
+        )
+        invoice_line.price_unit += 10.0
+        invoice._recompute_dynamic_lines()
+        self.assertFalse(invoice_line.stock_valuation_ok)
+        invoice.invoice_date = fields.Date.today()
+        invoice.action_post()
+        invoice_line._compute_stock_valuation_ok()
+        self.assertTrue(invoice_line.stock_valuation_ok)
+        svl = self.env["stock.valuation.layer"].search(
+            [
+                ("product_id", "=", invoice_line.product_id.id),
+                ("quantity", "=", 12.0),
+                ("uom_id", "=", self.unit.id),
+                ("value", "=", invoice_line.price_unit),
+                ("account_move_id", "=", invoice.id),
+                ("company_id", "=", invoice_line.company_id.id),
+            ]
+        )
+        self.assertTrue(svl)
+        self.assertEqual(svl.unit_cost, 750.83)
+
+    def _create_purchase_order(self, partner, product, qty, uom):
+        po = Form(self.env["purchase.order"])
+        po.partner_id = partner
+        with po.order_line.new() as po_line:
+            po_line.product_id = product
+            po_line.product_qty = qty
+            po_line.product_uom = uom
+        return po.save()
 
     def _create_invoice_from_purchase(self, purchase):
         purchase.button_confirm()
-        purchase.picking_ids.move_lines.move_line_ids.qty_done = 1
+        purchase.picking_ids.move_lines.move_line_ids.qty_done = 1.0
         purchase.picking_ids._action_done()
+        purchase.order_line.qty_received = 1.0
         purchase.action_create_invoice()
         return purchase.invoice_ids[0].with_context(check_move_validity=False)
