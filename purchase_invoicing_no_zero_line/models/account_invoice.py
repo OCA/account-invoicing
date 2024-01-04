f919ab94df224a6626dd631d524b57ec46d5b656
Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
2024-01-04 15:51:13 +0100

    [FIX] purchase_invoicing_no_zero_line: _set_additional_fields incompatibility

--- purchase_invoicing_no_zero_line/models/account_invoice.py
+++ purchase_invoicing_no_zero_line/models/account_invoice.py
@@ -23,17 +23,15 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     @api.model
-    def new(self, values={}, ref=None):
-        """Avoid zero quantity lines."""
-        if (
-            self._context.get("avoid_zero_lines", False) and
-            "quantity" in values
-        ):
-            if "uom_id" in values:
-                uom_id = values["uom_id"]
+    def create(self, vals):
+        if self._context.get("avoid_zero_lines"):
+            if vals.get("uom_id", False):
+                uom_id = vals["uom_id"]
             else:
                 uom_id = self.env["product.template"]._get_default_uom_id()
             uom = self.env["product.uom"].browse(uom_id)
-            if float_is_zero(values["quantity"], precision_rounding=uom.rounding):
+            if float_is_zero(
+                vals["quantity"], precision_rounding=uom.rounding
+            ):
                 return self.env["account.invoice.line"]
-        return super().new(values=values, ref=ref)
+        return super().create(vals)
