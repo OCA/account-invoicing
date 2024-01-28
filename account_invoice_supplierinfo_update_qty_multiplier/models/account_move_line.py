5b575fdba8762b4d61b971cd5d1cacd47b854270
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-28 21:55:01 +0100

    [MIG] account_invoice_supplierinfo_update_qty_multiplier: Migration to 16.0

--- account_invoice_supplierinfo_update_qty_multiplier/models/account_move_line.py
+++ account_invoice_supplierinfo_update_qty_multiplier/models/account_move_line.py
@@ -2,13 +2,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.invoice.line"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
-    @api.multi
     def _prepare_supplier_wizard_line(self, supplierinfo):
         res = super()._prepare_supplier_wizard_line(supplierinfo)
         res["current_multiplier_qty"] = res["new_multiplier_qty"] = (
--- account_invoice_supplierinfo_update_qty_multiplier/wizard/wizard_update_invoice_supplierinfo_line.py
+++ account_invoice_supplierinfo_update_qty_multiplier/wizard/wizard_update_invoice_supplierinfo_line.py
@@ -2,19 +2,16 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class WizardUpdateInvoiceSupplierinfoLine(models.TransientModel):
     _inherit = "wizard.update.invoice.supplierinfo.line"
 
-    current_multiplier_qty = fields.Float(
-        string="Current Multiplier Qty", readonly=True
-    )
+    current_multiplier_qty = fields.Float(readonly=True)
 
-    new_multiplier_qty = fields.Float(string="New Multiplier Qty", required=True)
+    new_multiplier_qty = fields.Float(required=True)
 
-    @api.multi
     def _prepare_supplierinfo_update(self):
         res = super()._prepare_supplierinfo_update()
         res["multiplier_qty"] = self.new_multiplier_qty
