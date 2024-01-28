c966c1fd5df138483c83a77194b8847935cfb59d
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-28 21:55:00 +0100

    [IMP] account_invoice_supplierinfo_update_qty_multiplier: pre-commit execution

--- account_invoice_supplierinfo_update_qty_multiplier/models/account_invoice_line.py
+++ account_invoice_supplierinfo_update_qty_multiplier/models/account_invoice_line.py
@@ -6,12 +6,12 @@ from odoo import api, models
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
     @api.multi
     def _prepare_supplier_wizard_line(self, supplierinfo):
         res = super()._prepare_supplier_wizard_line(supplierinfo)
-        res['current_multiplier_qty'] = res['new_multiplier_qty'] = (
+        res["current_multiplier_qty"] = res["new_multiplier_qty"] = (
             supplierinfo and supplierinfo.multiplier_qty
         )
         return res
--- account_invoice_supplierinfo_update_qty_multiplier/wizard/wizard_update_invoice_supplierinfo_line.py
+++ account_invoice_supplierinfo_update_qty_multiplier/wizard/wizard_update_invoice_supplierinfo_line.py
@@ -6,20 +6,16 @@ from odoo import api, fields, models
 
 
 class WizardUpdateInvoiceSupplierinfoLine(models.TransientModel):
-    _inherit = 'wizard.update.invoice.supplierinfo.line'
+    _inherit = "wizard.update.invoice.supplierinfo.line"
 
     current_multiplier_qty = fields.Float(
-        string='Current Multiplier Qty',
-        readonly=True
+        string="Current Multiplier Qty", readonly=True
     )
 
-    new_multiplier_qty = fields.Float(
-        string='New Multiplier Qty',
-        required=True
-    )
+    new_multiplier_qty = fields.Float(string="New Multiplier Qty", required=True)
 
     @api.multi
     def _prepare_supplierinfo_update(self):
         res = super()._prepare_supplierinfo_update()
-        res['multiplier_qty'] = self.new_multiplier_qty
+        res["multiplier_qty"] = self.new_multiplier_qty
         return res
