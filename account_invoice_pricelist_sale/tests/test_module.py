7766391c6402e0cd8664a9aecd7b02feb3a70bdc
Aleksander Milinkevich <77437506+milinsoft@users.noreply.github.com>
2024-01-31 17:42:49 +0100

    Update test_module.py

--- account_invoice_pricelist_sale/tests/test_module.py
+++ account_invoice_pricelist_sale/tests/test_module.py
@@ -2,8 +2,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import TransactionCase
 from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
 
 
 @tagged("post_install", "-at_install")
