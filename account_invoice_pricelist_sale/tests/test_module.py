efb6a14473a44692c9c091c7474104b8fafd1577
Aleksander Milinkevich <77437506+milinsoft@users.noreply.github.com>
2024-01-31 17:35:31 +0100

    Update test_module.py
    FIX BUG IN TEST

--- account_invoice_pricelist_sale/tests/test_module.py
+++ account_invoice_pricelist_sale/tests/test_module.py
@@ -3,8 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
 
 
+@tagged("post_install", "-at_install")
 class TestModule(TransactionCase):
     def setUp(self):
         super().setUp()
