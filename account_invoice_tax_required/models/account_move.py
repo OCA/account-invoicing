07ef0a761398ec07297ce675cd71394ebda5d106
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-02-01 08:04:37 +0100

    [IMP] account_invoice_tax_required: Improve error message for lack of tax
    The customer is included in the error message of the line containing a
    product without tax and an action is added to go directly to the list of
    invoices containing those lines.
    
    In addition, the method call is changed so that it receives and checks
    all the invoices instead of doing it one at a time.
    
    TT47521

--- account_invoice_tax_required/models/account_move.py
+++ account_invoice_tax_required/models/account_move.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import SUPERUSER_ID, _, models
-from odoo.exceptions import UserError
+from odoo.exceptions import RedirectWarning
 from odoo.tools import config
 
 
@@ -14,8 +14,10 @@ class AccountMove(models.Model):
 
     def _test_invoice_line_tax(self):
         errors = []
+        invoice_error_ids = []
         error_template = _(
-            "Invoice %(invoice)s has a line with product %(product)s with no taxes"
+            "Invoice %(invoice)s for customer %(customer)s "
+            "has a line with product %(product)s with no taxes"
         )
         for invoice_line in self.mapped("invoice_line_ids").filtered(
             lambda x: x.display_type not in ("line_section", "line_note")
@@ -23,13 +25,41 @@ class AccountMove(models.Model):
             if not invoice_line.tax_ids:
                 error_string = error_template % {
                     "invoice": invoice_line.move_id.name,
+                    "customer": invoice_line.partner_id.name,
                     "product": invoice_line.name,
                 }
+                invoice_error_ids.append(invoice_line.move_id.id)
                 errors.append(error_string)
         if errors:
-            raise UserError(  # pylint: disable=C8107
-                "%(message)s\n%(errors)s"
-                % {"message": _("No Taxes Defined!"), "errors": "\n".join(errors)}
+            invoice_error_ids = list(set(invoice_error_ids))
+            action_error = {
+                "name": _("Invoices with Missing Taxes"),
+                "res_model": "account.move",
+                "type": "ir.actions.act_window",
+                "search_view_id": [
+                    self.env.ref("account.view_account_move_filter").id,
+                    "search",
+                ],
+            }
+            if len(invoice_error_ids) == 1:
+                action_error["view_mode"] = "form"
+                action_error["res_id"] = invoice_error_ids[0]
+                action_error["views"] = [
+                    [self.env.ref("account.view_move_form").id, "form"],
+                ]
+            else:
+                action_error["view_mode"] = "tree"
+                action_error["domain"] = [("id", "in", invoice_error_ids)]
+                action_error["views"] = [
+                    [self.env.ref("account.view_move_tree").id, "list"],
+                    [self.env.ref("account.view_move_form").id, "form"],
+                ]
+            error_msg = "%(message)s\n%(errors)s" % {
+                "message": _("No Taxes Defined!"),
+                "errors": "\n".join(errors),
+            }
+            raise RedirectWarning(
+                error_msg, action_error, _("Show invoices with lines without taxes")
             )
 
     def _post(self, soft=True):
@@ -50,7 +80,6 @@ class AccountMove(models.Model):
                 config["test_enable"],
             )
         )
-        for move in self:
-            if move.move_type != "entry" and (force_test or not skip_test):
-                move._test_invoice_line_tax()
+        if force_test or not skip_test:
+            self.filtered(lambda m: m.is_invoice())._test_invoice_line_tax()
         return super()._post(soft=soft)
