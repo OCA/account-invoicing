01e4d58d3eb0381495d5aba0e2a9aa2114c03931
Ruchir Shukla <ruchir@bizzappdev.com>
2024-04-17 16:31:18 +0530

    [MIG][15.0]account_move_line_accounting_description:Migration to 15.0

--- account_move_line_accounting_description/models/account_move.py
+++ account_move_line_accounting_description/models/account_move.py
@@ -7,12 +7,13 @@ from odoo import api, fields, models
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    external_name = fields.Char(string="External Name")
+    external_name = fields.Char()
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
-        super()._onchange_product_id()
+        res = super()._onchange_product_id()
         for line in self:
             line.external_name = line.name
             if line.product_id.accounting_description:
                 line.name = line.product_id.accounting_description
+        return res
