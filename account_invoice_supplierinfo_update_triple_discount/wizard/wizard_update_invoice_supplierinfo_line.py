a7c9b91d359a168d5667bdb6045831b2adc5a521
Denis Roussel <denis.roussel@acsone.eu>
2024-06-13 11:30:33 +0200

    [FIX] account_invoice_supplierinfo_update_triple_discount: Don't use 'get_precision' in digits attribute

--- account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo_line.py
+++ account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo_line.py
@@ -4,26 +4,24 @@
 
 from odoo import fields, models
 
-import odoo.addons.decimal_precision as dp
-
 
 class WizardUpdateInvoiceSupplierinfoLine(models.TransientModel):
     _inherit = "wizard.update.invoice.supplierinfo.line"
 
     current_discount2 = fields.Float(
-        string="Current Disc. 2 (%)", readonly=True, digits=dp.get_precision("Discount")
+        string="Current Disc. 2 (%)", readonly=True, digits="Discount"
     )
 
     new_discount2 = fields.Float(
-        string="New Disc. 2 (%)", required=True, digits=dp.get_precision("Discount")
+        string="New Disc. 2 (%)", required=True, digits="Discount"
     )
 
     current_discount3 = fields.Float(
-        string="Current Disc. 3 (%)", readonly=True, digits=dp.get_precision("Discount")
+        string="Current Disc. 3 (%)", readonly=True, digits="Discount"
     )
 
     new_discount3 = fields.Float(
-        string="New Disc. 3 (%)", required=True, digits=dp.get_precision("Discount")
+        string="New Disc. 3 (%)", required=True, digits="Discount"
     )
 
     def _prepare_supplierinfo_update(self):
