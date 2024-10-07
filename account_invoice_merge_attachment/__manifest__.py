bdbd997cdedef51104a2cdc8d5c6711f6ce47938
Florent Cayré <florent@commown.coop>
2024-10-07 11:18:29 +0200

    [IMP] account_invoice_merge_attachment: pre-commit stuff

--- account_invoice_merge_attachment/__manifest__.py
+++ account_invoice_merge_attachment/__manifest__.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Invoice Merge Attachment',
-    'summary': """
+    "name": "Account Invoice Merge Attachment",
+    "summary": """
         Consider attachment during invoice merge process""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://www.acsone.eu',
-    'depends': [
-        'account_invoice_merge',
-        'document',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": [
+        "account_invoice_merge",
+        "document",
     ],
-    'data': [
-        'wizards/invoice_merge.xml',
+    "data": [
+        "wizards/invoice_merge.xml",
     ],
-    'auto_install': True,
+    "auto_install": True,
 }
