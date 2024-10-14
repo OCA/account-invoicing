2c328559cb56025a0a3ab014a621b13b79fd8799
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-10-14 09:48:29 +0200

    [MIG] stock_picking_invoicing_incoterm: Migration to 17.0

--- stock_picking_invoicing_incoterm/__manifest__.py
+++ stock_picking_invoicing_incoterm/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Stock Picking Invoicing Incoterm",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Inventory/Inventory",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
