58e887a660bbb572e92f0bc39caf5d5e65ae15c8
louck <luc.parent@commown.coop>
2024-10-18 17:35:09 +0200

    [IMP] account_invoice_merge_payment: pre-commit stuff

--- account_invoice_merge_payment/__manifest__.py
+++ account_invoice_merge_payment/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2015-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Account Invoice Merge Payment",
-    'summary': """
+    "name": "Account Invoice Merge Payment",
+    "summary": """
         Use invoice merge regarding fields on Account Payment Partner""",
-    'author': "ACSONE SA/NV,Odoo Community Association (OCA)",
-    'website': "http://www.acsone.eu",
-    'category': 'Invoicing & Payments',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'account_invoice_merge',
-        'account_payment_partner',
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "category": "Invoicing & Payments",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account_invoice_merge",
+        "account_payment_partner",
     ],
-    'auto_install': True,
-    'installable': True,
+    "auto_install": True,
+    "installable": True,
 }
