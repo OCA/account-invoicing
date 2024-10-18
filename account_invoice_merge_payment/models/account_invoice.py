33e2970870c712796a84d98ea02309c816aa9fb0
louck <luc.parent@commown.coop>
2024-10-18 17:35:09 +0200

    [IMP] account_invoice_merge_payment: pre-commit stuff

--- account_invoice_merge_payment/models/account_invoice.py
+++ account_invoice_merge_payment/models/account_invoice.py
@@ -1,21 +1,21 @@
 # Copyright 2015-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class AccountInvoice(models.Model):
 
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.model
     def _get_invoice_key_cols(self):
         return super(AccountInvoice, self)._get_invoice_key_cols() + [
-            'payment_mode_id',
+            "payment_mode_id",
         ]
 
     @api.model
     def _get_first_invoice_fields(self, invoice):
         res = super(AccountInvoice, self)._get_first_invoice_fields(invoice)
-        res.update({'payment_mode_id': invoice.payment_mode_id.id})
+        res.update({"payment_mode_id": invoice.payment_mode_id.id})
         return res
