6b14cbc2b0a655beca6a5bfbb68bedb641ae3b49
sygel <alberto.martinez@sygel.es>
2024-11-14 18:14:20 +0100

    Migration to v12 :3 uwu

--- account_invoice_warn_message/models/account_invoice.py
+++ account_invoice_warn_message/models/account_invoice.py
@@ -4,22 +4,20 @@
 from odoo import api, fields, models
 
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
+class AccountInvoice(models.Model):
+
+    _inherit = "account.invoice"
 
     invoice_warn_msg = fields.Text(compute="_compute_invoice_warn_msg")
 
     @api.depends(
-        "move_type",
-        "state",
-        "partner_id.invoice_warn",
-        "partner_id.parent_id.invoice_warn",
+        "state", "state", "partner_id.invoice_warn", "partner_id.parent_id.invoice_warn"
     )
     def _compute_invoice_warn_msg(self):
         for rec in self:
             if (
                 rec.partner_id
-                and rec.move_type in ("out_invoice", "out_refund")
+                and rec.type in ("out_invoice", "out_refund")
                 and rec.state == "draft"
             ):
                 if (
@@ -29,7 +27,7 @@ class AccountMove(models.Model):
                     rec.invoice_warn_msg = rec.partner_id.parent_id.invoice_warn_msg
                     if rec.partner_id.invoice_warn == "warning":
                         rec.invoice_warn_msg += "\n%s" % rec.partner_id.invoice_warn_msg
-                    continue
+                        continue
                 elif rec.partner_id.invoice_warn == "warning":
                     rec.invoice_warn_msg = rec.partner_id.invoice_warn_msg
                     continue
--- account_invoice_warn_message/readme/CONTRIBUTORS.md
+++
@@ -1,2 +0,0 @@
-- Héctor Villarreal \<<hector.villarreal@forgeflow.com>\>
-- Manuel Regidor \<<manuel.regidor@sygel.es>\>
