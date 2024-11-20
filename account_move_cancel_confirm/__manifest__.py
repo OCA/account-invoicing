433a49c212131c2c9973e0c4e8afdd6ad56e5fc7
theerayut <theerayutattajak@gmail.com>
2024-11-20 15:25:07 +0700

    [MIG] account_move_cancel_confirm: Migration to 15.0

--- account_move_cancel_confirm/__manifest__.py
+++ account_move_cancel_confirm/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Account Move Cancel Confirm",
-    "version": "16.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "category": "Usability",
     "license": "AGPL-3",
