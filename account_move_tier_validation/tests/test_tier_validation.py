80b9b34f68d2e9fa9c3779c26ccb6c0c4a790e0c
arosales <arosales>
2025-02-26 15:59:59 -0300

    [18.0][MIG] account_move_tier_validation: Migration to 18.0

--- account_move_tier_validation/tests/test_tier_validation.py
+++ account_move_tier_validation/tests/test_tier_validation.py
@@ -1,67 +1,57 @@
 # Copyright 2018 ForgeFlow S.L.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields
+from odoo import Command, fields
 from odoo.exceptions import ValidationError
 from odoo.tests import Form
-from odoo.tests.common import TransactionCase, tagged
+from odoo.tests.common import new_test_user, tagged
 
-from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+from odoo.addons.base.tests.common import BaseCommon
 
 
 @tagged("post_install", "-at_install")
-class TestAccountTierValidation(TransactionCase):
+class TestAccountTierValidation(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        cls.group_system = cls.env.ref("base.group_system")
+        cls.group_account_manager = cls.env.ref("account.group_account_manager")
+        cls.test_user_1 = new_test_user(
+            cls.env,
+            name="John",
+            login="test1",
+            groups="base.group_system,account.group_account_manager",
+        )
+        cls.test_user_2 = new_test_user(
+            cls.env,
+            name="Mike",
+            login="test2",
+            groups="base.group_system,account.group_account_manager",
+        )
+        cls.account_move_model = cls.env["ir.model"]._get("account.move")
 
     def test_01_tier_definition_models(self):
         res = self.env["tier.definition"]._get_tier_validation_model_names()
         self.assertIn("account.move", res)
 
     def test_02_form(self):
-        for _type in ("in_invoice", "out_invoice", "in_refund", "out_refund"):
+        for move_type in ("in_invoice", "out_invoice", "in_refund", "out_refund"):
             self.env["tier.definition"].create(
                 {
-                    "model_id": self.env["ir.model"]
-                    .search([("model", "=", "account.move")])
-                    .id,
-                    "definition_domain": f"[('move_type', '=', '{_type}')]",
+                    "model_id": self.account_move_model.id,
+                    "definition_domain": f"[('move_type', '=', '{move_type}')]",
                 }
             )
             with Form(
-                self.env["account.move"].with_context(default_move_type=_type)
+                self.env["account.move"].with_context(default_move_type=move_type)
             ) as form:
                 form.save()
                 self.assertTrue(form.hide_post_button)
 
     def test_03_move_post(self):
-        group_ids = [
-            self.env.ref("base.group_system").id,
-            self.env.ref("account.group_account_manager").id,
-        ]
-        self.test_user_1 = self.env["res.users"].create(
-            {
-                "name": "John",
-                "login": "test1",
-                "email": "john@test.com",
-                "groups_id": [(6, 0, group_ids)],
-            }
-        )
-        self.test_user_2 = self.env["res.users"].create(
-            {
-                "name": "Mike",
-                "login": "test2",
-                "email": "mike@test.com",
-                "groups_id": [(6, 0, group_ids)],
-            }
-        )
         self.env["tier.definition"].create(
             {
-                "model_id": self.env["ir.model"]
-                .search([("model", "=", "account.move")])
-                .id,
+                "model_id": self.account_move_model.id,
                 "definition_domain": "[('move_type', '=', 'out_invoice')]",
                 "reviewer_id": self.test_user_1.id,
             }
@@ -72,9 +62,11 @@ class TestAccountTierValidation(TransactionCase):
             {
                 "move_type": "out_invoice",
                 "partner_id": partner.id,
-                "invoice_date_due": fields.Date.from_string("2024-01-01"),
+                "invoice_date_due": fields.Date.to_date("2024-01-01"),
                 "invoice_line_ids": [
-                    (0, 0, {"product_id": product.id, "quantity": 1, "price_unit": 30})
+                    Command.create(
+                        {"product_id": product.id, "quantity": 1, "price_unit": 30}
+                    )
                 ],
             }
         )
@@ -83,8 +75,9 @@ class TestAccountTierValidation(TransactionCase):
         invoice.invalidate_model()
         invoice.validate_tier()
         with self.assertRaisesRegex(
-            ValidationError, self.env._("You are not allowed to write those fields")
+            ValidationError, "You are not allowed to write those fields"
         ):
             invoice._post()
         # Calls _post method by passing context skip_validation_check set to True
         invoice.action_post()
+        self.assertEqual(invoice.state, "posted")
