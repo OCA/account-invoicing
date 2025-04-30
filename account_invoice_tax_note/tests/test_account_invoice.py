75db3a7b5f82a7ad5d192ee36697bdaf01c031ce
Qlasta <kas.asta@gmail.com>
2025-04-30 16:00:06 +0300

    [17.0][MIG] account_invoice_tax_note: Migration to 17.0

--- account_invoice_tax_note/tests/test_account_invoice.py
+++ account_invoice_tax_note/tests/test_account_invoice.py
@@ -11,49 +11,50 @@ class TestAccountInvoice(TransactionCase):
     Tests for account.invoice
     """
 
-    def setUp(self):
-        super().setUp()
-        tax_group_obj = self.env["account.tax.group"]
-        tax_obj = self.env["account.tax"]
-        invoice_obj = self.env["account.move"]
-        self.product1 = self.env.ref("product.product_product_7")
-        self.product2 = self.env.ref("product.product_product_8")
-        customer = self.env.ref("base.res_partner_2")
-        self.tax_group1 = tax_group_obj.create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        tax_group_obj = cls.env["account.tax.group"]
+        tax_obj = cls.env["account.tax"]
+        invoice_obj = cls.env["account.move"]
+        cls.product1 = cls.env.ref("product.product_product_7")
+        cls.product2 = cls.env.ref("product.product_product_8")
+        customer = cls.env.ref("base.res_partner_2")
+        cls.tax_group1 = tax_group_obj.create(
             {"name": "Secret Taxes", "sequence": 20, "report_note": str(uuid4())}
         )
-        self.tax_group2 = tax_group_obj.create(
+        cls.tax_group2 = tax_group_obj.create(
             {"name": "Public taxes", "sequence": 30, "report_note": str(uuid4())}
         )
-        self.tax1 = tax_obj.create(
+        cls.tax1 = tax_obj.create(
             {
                 "name": "TVA 1",
                 "type_tax_use": "sale",
                 "amount_type": "percent",
                 "amount": "35",
                 "description": "Top secret",
-                "tax_group_id": self.tax_group1.id,
+                "tax_group_id": cls.tax_group1.id,
             }
         )
-        self.tax2 = tax_obj.create(
+        cls.tax2 = tax_obj.create(
             {
                 "name": "TVA 2",
                 "type_tax_use": "sale",
                 "amount_type": "percent",
                 "amount": "22",
                 "description": "Hello",
-                "tax_group_id": self.tax_group2.id,
+                "tax_group_id": cls.tax_group2.id,
             }
         )
-        taxes = self.tax1 | self.tax2
-        self.product1.write({"taxes_id": [(6, False, self.tax1.ids)]})
-        self.product2.write({"taxes_id": [(6, False, self.tax2.ids)]})
-        account = self.env["account.account"].search(
-            [("account_type", "=", "income"), ("company_id", "=", self.env.company.id)],
+        taxes = cls.tax1 | cls.tax2
+        cls.product1.write({"taxes_id": [(6, False, cls.tax1.ids)]})
+        cls.product2.write({"taxes_id": [(6, False, cls.tax2.ids)]})
+        account = cls.env["account.account"].search(
+            [("account_type", "=", "income"), ("company_id", "=", cls.env.company.id)],
             limit=1,
         )
-        account.write({"tax_ids": [(4, self.tax1.id, False), (4, self.tax2.id, False)]})
-        journal = self.env["account.journal"].create(
+        account.write({"tax_ids": [(4, cls.tax1.id, False), (4, cls.tax2.id, False)]})
+        journal = cls.env["account.journal"].create(
             {"name": "Sale journal - Test", "code": "SJ-TT", "type": "sale"}
         )
         invoice_lines1 = [
@@ -61,18 +62,18 @@ class TestAccountInvoice(TransactionCase):
                 0,
                 False,
                 {
-                    "name": self.product1.display_name,
-                    "product_id": self.product1.id,
+                    "name": cls.product1.display_name,
+                    "product_id": cls.product1.id,
                     "quantity": 3,
-                    "product_uom_id": self.product1.uom_id.id,
-                    "price_unit": self.product1.standard_price,
+                    "product_uom_id": cls.product1.uom_id.id,
+                    "price_unit": cls.product1.standard_price,
                     "account_id": account.id,
-                    "tax_ids": self.tax1.ids,
+                    "tax_ids": cls.tax1.ids,
                 },
             )
         ]
         # Invoice 1 must have 1 tax group only
-        self.invoice1 = invoice_obj.create(
+        cls.invoice1 = invoice_obj.create(
             {
                 "partner_id": customer.id,
                 "move_type": "out_invoice",
@@ -87,31 +88,31 @@ class TestAccountInvoice(TransactionCase):
                 0,
                 False,
                 {
-                    "name": self.product1.display_name,
-                    "product_id": self.product1.id,
+                    "name": cls.product1.display_name,
+                    "product_id": cls.product1.id,
                     "quantity": 3,
-                    "product_uom_id": self.product1.uom_id.id,
-                    "price_unit": self.product1.standard_price,
+                    "product_uom_id": cls.product1.uom_id.id,
+                    "price_unit": cls.product1.standard_price,
                     "account_id": account.id,
-                    "tax_ids": self.tax1.ids,
+                    "tax_ids": cls.tax1.ids,
                 },
             ),
             (
                 0,
                 False,
                 {
-                    "name": self.product1.display_name,
-                    "product_id": self.product2.id,
+                    "name": cls.product1.display_name,
+                    "product_id": cls.product2.id,
                     "quantity": 3,
-                    "product_uom_id": self.product2.uom_id.id,
-                    "price_unit": self.product2.standard_price,
+                    "product_uom_id": cls.product2.uom_id.id,
+                    "price_unit": cls.product2.standard_price,
                     "account_id": account.id,
                     "tax_ids": taxes.ids,
                 },
             ),
         ]
         # Invoice 2 must have more than 1 tax group
-        self.invoice2 = invoice_obj.create(
+        cls.invoice2 = invoice_obj.create(
             {
                 "partner_id": customer.id,
                 "move_type": "out_invoice",
