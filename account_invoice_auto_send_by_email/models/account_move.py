92a5bfc762473537288779c2784461be0c91e0bc
Michael Tietz <mtietz@mt-software.de>
2025-05-24 15:12:53 +0200

    [IMP] account_invoice_auto_send_by_email: Ensure that only moves with transmit_method_code mail getting send Create mail wizard with correct values

--- account_invoice_auto_send_by_email/models/account_move.py
+++ account_invoice_auto_send_by_email/models/account_move.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Camptocamp SA
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import _, api, models
@@ -13,16 +14,24 @@ class AccountMove(models.Model):
             description = "Send invoice {} by email".format(invoice.name)
             invoice.with_delay(description=description)._execute_invoice_sent_wizard()
 
+    def _prepare_invoice_sent_wizard_vals(self):
+        return {"is_email": True, "composition_mode": "mass_mail"}
+
     def _execute_invoice_sent_wizard(self, options=None):
         self.ensure_one()
         if self.is_move_sent:
             return _("This invoice has already been sent.")
+        if self.transmit_method_code != "mail":
+            return _("This invoice should not send by mail")
         res = self.action_invoice_sent()
         wiz_ctx = res["context"] or {}
         wiz_ctx["active_model"] = self._name
         wiz_ctx["active_ids"] = self.ids
-        wiz = self.env["account.invoice.send"].with_context(**wiz_ctx).create({})
-        wiz.write(wiz._get_ui_options())
+        wiz = (
+            self.env["account.invoice.send"]
+            .with_context(**wiz_ctx)
+            .create(self._prepare_invoice_sent_wizard_vals())
+        )
         return wiz.send_and_print_action()
 
     @api.model
