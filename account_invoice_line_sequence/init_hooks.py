af986ab7689083f530d0294a3d31284fc9d19123
bo-eska <betulo@eska.biz>
2025-05-28 16:21:26 +0300

    pre-commit fixes

--- account_invoice_line_sequence/init_hooks.py
+++ account_invoice_line_sequence/init_hooks.py
@@ -2,10 +2,6 @@
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import SUPERUSER_ID
-from odoo.api import Environment
-
-
 def post_init_hook(env):
     """
     Fetches all invoice and resets the sequence of their invoice line
