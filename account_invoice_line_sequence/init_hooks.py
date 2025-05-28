8b5c701d4ea584082f618a72400bb6a0e10e12ab
bo-eska <betulo@eska.biz>
2025-05-28 16:23:39 +0300

    pre-commit fixes

--- account_invoice_line_sequence/init_hooks.py
+++ account_invoice_line_sequence/init_hooks.py
@@ -2,6 +2,7 @@
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+
 def post_init_hook(env):
     """
     Fetches all invoice and resets the sequence of their invoice line
