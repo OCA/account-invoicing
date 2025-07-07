1a06956a23cb87fcdfcd5a7d481fe2797949ec79
sergiocorato <sergiocorato@gmail.com>
2025-07-07 10:37:11 +0200

    [IMP] account_invoice_supplierinfo_update_triple_discount: black, isort, prettier

--- account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo.py
+++ account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 
 
 class WizardUpdateInvoiceSupplierinfo(models.TransientModel):
-    _inherit = 'wizard.update.invoice.supplierinfo'
+    _inherit = "wizard.update.invoice.supplierinfo"
 
     display_discount2 = fields.Boolean(
         compute="_compute_display_discount2",
@@ -19,15 +19,13 @@ class WizardUpdateInvoiceSupplierinfo(models.TransientModel):
     @api.depends("line_ids.current_discount2", "line_ids.new_discount2")
     def _compute_display_discount2(self):
         for wizard in self:
-            wizard.display_discount2 = (
-                any(wizard.mapped("line_ids.current_discount2"))
-                or any(wizard.mapped("line_ids.new_discount2"))
-            )
+            wizard.display_discount2 = any(
+                wizard.mapped("line_ids.current_discount2")
+            ) or any(wizard.mapped("line_ids.new_discount2"))
 
     @api.depends("line_ids.current_discount3", "line_ids.new_discount3")
     def _compute_display_discount3(self):
         for wizard in self:
-            wizard.display_discount3 = (
-                any(wizard.mapped("line_ids.current_discount3"))
-                or any(wizard.mapped("line_ids.new_discount3"))
-            )
+            wizard.display_discount3 = any(
+                wizard.mapped("line_ids.current_discount3")
+            ) or any(wizard.mapped("line_ids.new_discount3"))
--- account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo_line.py
+++ account_invoice_supplierinfo_update_triple_discount/wizard/wizard_update_invoice_supplierinfo_line.py
@@ -3,31 +3,32 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
+
 import odoo.addons.decimal_precision as dp
 
 
 class WizardUpdateInvoiceSupplierinfoLine(models.TransientModel):
-    _inherit = 'wizard.update.invoice.supplierinfo.line'
+    _inherit = "wizard.update.invoice.supplierinfo.line"
 
     current_discount2 = fields.Float(
-        string='Current Disc. 2 (%)', readonly=True,
-        digits=dp.get_precision('Discount'))
+        string="Current Disc. 2 (%)", readonly=True, digits=dp.get_precision("Discount")
+    )
 
     new_discount2 = fields.Float(
-        string='New Disc. 2 (%)', required=True,
-        digits=dp.get_precision('Discount'))
+        string="New Disc. 2 (%)", required=True, digits=dp.get_precision("Discount")
+    )
 
     current_discount3 = fields.Float(
-        string='Current Disc. 3 (%)', readonly=True,
-        digits=dp.get_precision('Discount'))
+        string="Current Disc. 3 (%)", readonly=True, digits=dp.get_precision("Discount")
+    )
 
     new_discount3 = fields.Float(
-        string='New Disc. 3 (%)', required=True,
-        digits=dp.get_precision('Discount'))
+        string="New Disc. 3 (%)", required=True, digits=dp.get_precision("Discount")
+    )
 
     @api.multi
     def _prepare_supplierinfo_update(self):
         res = super()._prepare_supplierinfo_update()
-        res['discount2'] = self.new_discount2
-        res['discount3'] = self.new_discount3
+        res["discount2"] = self.new_discount2
+        res["discount3"] = self.new_discount3
         return res
