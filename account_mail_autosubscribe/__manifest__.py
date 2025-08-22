e97060bfb92a247502b1a6fa3acafa8474cadc6a
Lansana Barry Sow <lbarry@apsl.net>
2025-08-22 09:13:51 +0200

    [MIG] account_mail_autosubscribe: Migration to 17.0

--- account_mail_autosubscribe/__manifest__.py
+++ account_mail_autosubscribe/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Mail Autosubscribe",
     "summary": "Automatically subscribe partners to their company's invoices",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "license": "AGPL-3",
