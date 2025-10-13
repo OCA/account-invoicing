877d9f55be18da321a83447ca565e2dbda8c2db4
sergiocorato <sergiocorato@gmail.com>
2025-10-13 14:31:23 +0200

    [14.0][IMP] account_invoice_section_sale_order add ability to localize datetime

--- account_invoice_section_sale_order/models/sale_order.py
+++ account_invoice_section_sale_order/models/sale_order.py
@@ -1,6 +1,10 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+import re
 from collections import OrderedDict
+from datetime import datetime
+
+import pytz
 
 from odoo import models
 from odoo.tools.safe_eval import safe_eval, time
@@ -69,6 +73,18 @@ class SaleOrder(models.Model):
             invoice.line_ids = section_lines
         return invoices
 
+    def localize(self, date_to_localize=None, strftime_var="%m-%d-%Y"):
+        tz = self.env.user.tz
+        if tz:
+            local_tz = pytz.timezone(tz)
+        else:
+            local_tz = pytz.utc
+        return (
+            pytz.utc.localize(date_to_localize)
+            .astimezone(local_tz)
+            .strftime(strftime_var)
+        )
+
     def _get_invoice_section_name(self):
         """Returns the text for the section name."""
         self.ensure_one()
@@ -76,6 +92,28 @@ class SaleOrder(models.Model):
             self.partner_invoice_id.invoice_section_name_scheme
             or self.company_id.invoice_section_name_scheme
         )
+
+        if naming_scheme and "object" in naming_scheme:
+            fields_to_localize = re.finditer(r"object.(\w+)", naming_scheme)
+            for field_to_localize in fields_to_localize:
+                object_field_name = field_to_localize.group(0).replace("object.", "")
+                if not hasattr(self, object_field_name):
+                    continue
+                object_field = getattr(self, object_field_name)
+                if isinstance(object_field, datetime):
+                    strftime_pattern = re.compile(r".strftime\(.*?\)")
+                    strftime_match = strftime_pattern.search(
+                        naming_scheme, field_to_localize.end()
+                    )
+                    if strftime_match:
+                        strftime_var = strftime_match.group(0)
+                        naming_scheme = naming_scheme.replace(
+                            f"{field_to_localize.group(0)}{strftime_var}",
+                            self.localize(
+                                object_field,
+                                strftime_var.replace(".strftime(", "").replace(")", ""),
+                            ),
+                        )
         if naming_scheme:
             return safe_eval(naming_scheme, {"object": self, "time": time})
         elif self.client_order_ref:
