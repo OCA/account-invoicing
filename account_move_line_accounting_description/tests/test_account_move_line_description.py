eba028c4e684f87a0c977012a4629bc2ff2dc3d7
Ulrich Kilian <uk@science-and-more.de>
2025-12-12 17:44:56 +0100

    pre-commit checks

--- account_move_line_accounting_description/tests/test_account_move_line_description.py
+++ account_move_line_accounting_description/tests/test_account_move_line_description.py
@@ -3,7 +3,6 @@ from odoo.tests.common import TransactionCase
 
 
 class TestAccountLineDescription(TransactionCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -11,7 +10,9 @@ class TestAccountLineDescription(TransactionCase):
         cls.product_with_acc_desc = cls.env.ref("product.product_product_1")
         cls.product_without_acc_desc = cls.env.ref("product.product_product_2")
 
-        consumable_cat = cls.env["product.category"].search([("name", "=", "Consumable")])
+        consumable_cat = cls.env["product.category"].search(
+            [("name", "=", "Consumable")]
+        )
 
         cls.product_with_acc_desc.categ_id = consumable_cat
         cls.product_without_acc_desc.categ_id = consumable_cat
@@ -22,7 +23,6 @@ class TestAccountLineDescription(TransactionCase):
     # Test: product WITH accounting description
     # -------------------------------------------------------------------------
     def test_invoice_line_with_accounting_description(self):
-
         invoice_form = Form(
             self.env["account.move"].with_context(default_move_type="out_invoice")
         )
@@ -34,11 +34,13 @@ class TestAccountLineDescription(TransactionCase):
             line_form.price_unit = 2.99
 
         invoice = invoice_form.save()
-        line = invoice.invoice_line_ids.filtered(lambda l: l.product_id)
+        line = invoice.invoice_line_ids.filtered(lambda inv_line: inv_line.product_id)
 
         # Force English for reading both values
         line_name_en = line.with_context(lang="en_US").name
-        acc_desc_en = self.product_with_acc_desc.with_context(lang="en_US").accounting_description
+        acc_desc_en = self.product_with_acc_desc.with_context(
+            lang="en_US"
+        ).accounting_description
 
         self.assertEqual(line_name_en, acc_desc_en)
         self.assertEqual(line.product_id.name, line.external_name)
@@ -47,7 +49,6 @@ class TestAccountLineDescription(TransactionCase):
     # Test: product WITHOUT accounting description
     # -------------------------------------------------------------------------
     def test_invoice_line_without_accounting_description(self):
-
         # Build everything in English context
         env_en = self.env(context=dict(self.env.context, lang="en_US"))
 
@@ -55,7 +56,9 @@ class TestAccountLineDescription(TransactionCase):
         product_en = env_en.ref("product.product_product_2")
 
         # Ensure category in the English context
-        consumable_cat = env_en["product.category"].search([("name", "=", "Consumable")])
+        consumable_cat = env_en["product.category"].search(
+            [("name", "=", "Consumable")]
+        )
         product_en.categ_id = consumable_cat
 
         invoice_form = Form(
@@ -69,7 +72,7 @@ class TestAccountLineDescription(TransactionCase):
             line_form.price_unit = 2.99
 
         invoice = invoice_form.save()
-        line = invoice.invoice_line_ids.filtered(lambda l: l.product_id)
+        line = invoice.invoice_line_ids.filtered(lambda inv_line: inv_line.product_id)
 
         # Force English for comparison
         line_name_en = line.with_context(lang="en_US").name
