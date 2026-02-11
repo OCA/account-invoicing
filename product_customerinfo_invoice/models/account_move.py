2c10aa7c5f331d3bb37a2276c864a235ba023bb3
Fernando <fsanmartin@iternova.cl>
2026-02-11 10:39:35 -0300

    refactor: import api module in account_move.py

--- product_customerinfo_invoice/models/account_move.py
+++ product_customerinfo_invoice/models/account_move.py
@@ -1,7 +1,7 @@
 # Copyright 2024 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
