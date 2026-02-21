9e23347f6fe666039af38a1d4ae7bbc657787a18
CI Lab <ci-lab@example.com>
2026-02-21 23:29:46 +0100

    [18.0][FIX] account_billing: avoid fragile company creation in multicompany test

--- account_billing/tests/test_account_billing.py
+++ account_billing/tests/test_account_billing.py
@@ -231,7 +231,11 @@ class TestAccountBilling(TransactionCase):
         self.billing_model.browse(action["res_id"])
 
     def test_7_record_rule_company_restriction(self):
-        other_company = self.env["res.company"].create({"name": "Other Company"})
+        other_company = self.env["res.company"].search(
+            [("id", "!=", self.env.company.id)], limit=1
+        )
+        if not other_company:
+            self.skipTest("A secondary company is required for this test")
         billing_other = self.billing_model.with_company(other_company).create(
             {
                 "bill_type": "out_invoice",
