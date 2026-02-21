33a30fbd24989f833ec655cf350ca88dff0819c1
CI Lab <ci-lab@example.com>
2026-02-21 19:47:07 +0100

    [TEST] account_billing: create company instead of copy

--- account_billing/tests/test_account_billing.py
+++ account_billing/tests/test_account_billing.py
@@ -231,7 +231,7 @@ class TestAccountBilling(TransactionCase):
         self.billing_model.browse(action["res_id"])
 
     def test_7_record_rule_company_restriction(self):
-        other_company = self.env.company.copy({"name": "Other Company"})
+        other_company = self.env["res.company"].create({"name": "Other Company"})
         billing_other = self.billing_model.with_company(other_company).create(
             {
                 "bill_type": "out_invoice",
