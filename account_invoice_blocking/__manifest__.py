2a5f4c361870d8fa37a51ec2d24038d310876e2d
solomonprabu <solomon.workspace@gmail.com>
2026-05-08 14:19:31 +0530

    [MIG] account_invoice_blocking: Migration to 18.0

--- account_invoice_blocking/__manifest__.py
+++ account_invoice_blocking/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2016 Acsone SA/NV
+# Copyright 2026 CIT-services
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Blocking",
     "summary": "Set a blocking (No Follow-up) flag on invoices",
-    "version": "17.0.1.0.2",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting",
     "author": "Acsone SA/NV,Odoo Community Association (OCA)",
