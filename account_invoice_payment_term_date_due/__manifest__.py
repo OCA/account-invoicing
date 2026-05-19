017369f896486a7343181d6a75fc5a45d1258988
Don Kendall <dkendall@ledoweb.com>
2026-05-19 14:40:06 -0400

    [IMP] account_invoice_payment_term_date_due: apply 19.0 auto-transforms (odoo-mig skill)

--- account_invoice_payment_term_date_due/__manifest__.py
+++ account_invoice_payment_term_date_due/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Display invoices date due when using payment terms",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/account-invoicing",
