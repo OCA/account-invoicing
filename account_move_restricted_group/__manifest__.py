5a7fadc305ac403a532115298424c14ed3f5cc5a
Dante Pereyra // Solvos <dante.pereyra@solvos.es>
2026-05-27 11:55:54 +0200

    [ADD] account_move_restricted_group: Add new module to restrict access to account moves
    This module overrides access control with check_access().
    It ensures that:
    - Creation, Edition, and Deletion on account.move are strictly blocked
    for users who are not part of the account.group_account_invoice.
    - Users with Technical/Show Accounting Features - Readonly group or
    no Accounting role defined retain their original, restricted permissions,
    preventing other apps from escalating their privileges.

---
+++ account_move_restricted_group/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Move Restricted Group",
+    "summary": """
+        Restrict creation, edit and delete on tree and form view in account moves
+    """,
+    "author": "Solvos, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OCA/account-invoicing",
+    "depends": ["account"],
+    "data": [],
+    "installable": True,
+}
