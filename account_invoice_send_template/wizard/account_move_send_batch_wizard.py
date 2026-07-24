1caabd51010e6353edeff057c10fd4d58d26a34f
duc.tdm <duc.tdm@komit-consulting.com>
2026-07-24 16:32:55 +0700

    [MIG] account_invoice_send_template: Migration to 19.0

--- account_invoice_send_template/wizard/account_move_send_batch_wizard.py
+++ account_invoice_send_template/wizard/account_move_send_batch_wizard.py
@@ -8,17 +8,16 @@ class AccountMoveSendBatchWizard(models.TransientModel):
     _inherit = "account.move.send.batch.wizard"
 
     mail_template_id = fields.Many2one(
-        "mail.template",
-        store=True,
-        readonly=False,
-        domain=[("model_id", "=", "account.move")],
+        "mail.template", domain=[("model", "=", "account.move")]
     )
 
-    def action_send_and_print(self):
-        res = super().action_send_and_print()
+    def action_send_and_print(self, force_synchronous=False, allow_fallback_pdf=False):
+        res = super().action_send_and_print(
+            force_synchronous=force_synchronous, allow_fallback_pdf=allow_fallback_pdf
+        )
         if not self.mail_template_id:
             return res
-        sending_data = fields.first(self.move_ids).sending_data
+        sending_data = next(iter(self.move_ids), self.move_ids).sending_data
         sending_data.update({"mail_template_id": self.mail_template_id.id})
         self.move_ids.sending_data = sending_data
         return res
