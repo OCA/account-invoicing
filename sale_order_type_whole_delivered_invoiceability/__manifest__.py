b75e1cb26bff278e658b2b3cdb64bde346b139fb
Carlos Lopez <carlos.lopez@tecnativa.com>
2026-08-26 10:41:24 -0500

    [MIG] sale_order_type_whole_delivered_invoiceability: Migration to version 19.0

--- sale_order_type_whole_delivered_invoiceability/__manifest__.py
+++ sale_order_type_whole_delivered_invoiceability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Type Whole Delivered Invoiceability",
-    "version": "17.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
