a55563f06a1ad4749698d28e063ac561580d97d5
Florian da Costa <florian.dacosta@akretion.com>
2026-09-25 11:08:26 +0200

    [BACKPORT] account_invoice_line_mass_edit: backport to v16

--- account_invoice_line_mass_edit/__manifest__.py
+++ account_invoice_line_mass_edit/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Account Invoice Line Mass Edit",
-    "version": "18.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Mass edit the lines of an invoice",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-invoicing",
